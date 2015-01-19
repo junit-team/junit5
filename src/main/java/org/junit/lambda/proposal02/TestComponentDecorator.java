45da956b91957543903313435d5f909d128695be
Johannes Link <business@johanneslink.net>
2015-01-19 08:43:32 +0100

    Comment to explain the "Decorator" as used in proposal 2

--- src/main/java/org/junit/lambda/proposal02/TestComponentDecorator.java
+++ src/main/java/org/junit/lambda/proposal02/TestComponentDecorator.java
@@ -2,6 +2,12 @@ package org.junit.lambda.proposal02;
 
 import junit.framework.TestResult;
 
+/**
+ * As it's currently "implemented" TestComponentDecorator does not follow the GOF decorator pattern.
+ * Instead it's a combination of Decorator factory - using the build method - and an interceptor - when
+ * using the run method.
+ */
+
 public interface TestComponentDecorator {
 
     default TestComponent build(TestComponent testComponent) {
