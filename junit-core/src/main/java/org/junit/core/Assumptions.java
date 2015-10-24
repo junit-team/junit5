6673e124cd5888bf395be91513326737e8433e4b
Sam Brannen <sam@sambrannen.com>
2015-10-25 00:27:51 +0200

    Introduce basic support for Assumptions

---
+++ junit-core/src/main/java/org/junit/core/Assumptions.java
@@ -0,0 +1,39 @@
+
+package org.junit.core;
+
+import org.opentestalliance.TestAbortedException;
+
+/**
+ * @author Sam Brannen
+ * @since 5.0
+ */
+public final class Assumptions {
+
+	private Assumptions() {
+		/* no-op */
+	}
+
+	public static void assumeTrue(boolean condition) {
+		if (!condition) {
+			throw new TestAbortedException("Assumption failed: condition is not true");
+		}
+	}
+
+	public static void assumeFalse(boolean condition) {
+		if (condition) {
+			throw new TestAbortedException("Assumption failed: condition is not false");
+		}
+	}
+
+	public static void assumingThat(boolean condition, Executable executable) {
+		if (condition) {
+			try {
+				executable.execute();
+			}
+			catch (Throwable e) {
+				throw new RuntimeException("Wrapped exception thrown from Executable", e);
+			}
+		}
+	}
+
+}
