790df062f11e6538ce1077af1aed2b6c7c69c26b
Sam Brannen <sam@sambrannen.com>
2015-10-27 11:20:19 +0100

    Introduce @Test and @Ignore annotations

---
+++ junit-core/src/main/java/org/junit/core/annotation/Test.java
@@ -0,0 +1,21 @@
+
+package org.junit.core.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Sam Brannen
+ * @since 5.0
+ */
+@Target({ ElementType.TYPE, ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Test {
+
+	String name() default "";
+
+}
