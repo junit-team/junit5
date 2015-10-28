e581b0c65ad75be906934bdc92e36fdfba62391b
Sam Brannen <sam@sambrannen.com>
2015-10-28 15:09:19 +0100

    Polishing

--- junit-engine-api/src/main/java/org/junit/gen5/engine/TestDescriptor.java
+++ junit-engine-api/src/main/java/org/junit/gen5/engine/TestDescriptor.java
@@ -1,3 +1,4 @@
+
 package org.junit.gen5.engine;
 
 /**
@@ -5,25 +6,27 @@ package org.junit.gen5.engine;
  * @since 5.0
  */
 public interface TestDescriptor {
-  /**
-   * Get the unique identifier (UID) for the described test.
-   *
-   * <p>Uniqueness must be guaranteed across an entire test plan,
-   * regardless of how many engines are used behind the scenes.
-   *
-   * <p>The ID is simply the concatenation of the {@linkplain #getEngineId engine ID}
-   * and the {@linkplain #getTestId test ID} separated by a colon ({@code ":"})
-   * &mdash; for example, {@code myEngine:test-description-unique-within-my-engine}.
-   */
-  default String getUniqueId() {
-    return String.format("%s:%s", getEngineId(), getTestId());
-  };
 
-  String getEngineId();
+	/**
+	 * Get the unique identifier (UID) for the described test.
+	 *
+	 * <p>Uniqueness must be guaranteed across an entire test plan,
+	 * regardless of how many engines are used behind the scenes.
+	 *
+	 * <p>The ID is simply the concatenation of the {@linkplain #getEngineId engine ID}
+	 * and the {@linkplain #getTestId test ID} separated by a colon ({@code ":"})
+	 * &mdash; for example, {@code myEngine:test-description-unique-within-my-engine}.
+	 */
+	default String getUniqueId() {
+		return String.format("%s:%s", getEngineId(), getTestId());
+	};
 
-  String getTestId();
+	String getEngineId();
 
-  String getDisplayName();
+	String getTestId();
 
-  TestDescriptor getParent();
+	String getDisplayName();
+
+	TestDescriptor getParent();
+
 }
--- junit5-engine/src/main/java/org/junit/gen5/engine/junit5/JavaTestDescriptor.java
+++ junit5-engine/src/main/java/org/junit/gen5/engine/junit5/JavaTestDescriptor.java
@@ -1,12 +1,8 @@
 
 package org.junit.gen5.engine.junit5;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import org.junit.gen5.api.Test;
-import org.junit.gen5.commons.util.ObjectUtils;
-import org.junit.gen5.commons.util.Preconditions;
-import org.junit.gen5.engine.TestDescriptor;
+import static java.util.Collections.*;
+import static org.junit.gen5.commons.util.ObjectUtils.*;
 
 import java.lang.reflect.Method;
 import java.util.ArrayList;
@@ -14,9 +10,13 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static java.util.Collections.emptyList;
-import static java.util.Collections.unmodifiableList;
-import static org.junit.gen5.commons.util.ObjectUtils.nullSafeToString;
+import org.junit.gen5.api.Test;
+import org.junit.gen5.commons.util.ObjectUtils;
+import org.junit.gen5.commons.util.Preconditions;
+import org.junit.gen5.engine.TestDescriptor;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * {@link TestDescriptor} for tests based on Java classes and methods.
@@ -37,112 +37,120 @@ import static org.junit.gen5.commons.util.ObjectUtils.nullSafeToString;
 @Data
 @EqualsAndHashCode
 public class JavaTestDescriptor implements TestDescriptor {
-  // The following pattern only supports descriptors for test methods.
-  // TODO Support descriptors for test classes.
-  // TODO Decide if we want to support descriptors for packages.
-  private static final Pattern UID_PATTERN = Pattern.compile("^(.+):(.+)#(.+)\\((.*)\\)$");
 
-  public static JavaTestDescriptor from(final String uid) throws RuntimeException {
-    Preconditions.notNull(uid, "TestDescriptor UID must not be null");
-
-    Matcher matcher = UID_PATTERN.matcher(uid);
-    Preconditions.condition(matcher.matches(), () ->
-        String.format("Invalid format for %s UID: %s", JavaTestDescriptor.class.getSimpleName(), uid));
-
-    // TODO Validate contents of matched groups.
-    String engineId = matcher.group(1);
-    String className = matcher.group(2);
-    String methodName = matcher.group(3);
-    String methodParameters = matcher.group(4);
-
-    Class<?> clazz = loadClass(className);
-
-    System.out.println("DEBUG - method params: " + methodParameters);
-
-    List<Class<?>> paramTypeList = new ArrayList<>();
-    for (String type : methodParameters.split(",")) {
-      type = type.trim();
-      if (!type.isEmpty()) {
-        paramTypeList.add(loadClass(type));
-      }
-    }
-
-    Class<?>[] parameterTypes = paramTypeList.toArray(new Class<?>[paramTypeList.size()]);
-
-    try {
-      Method method = clazz.getDeclaredMethod(methodName, parameterTypes);
-      return new JavaTestDescriptor(engineId, method);
-    } catch (NoSuchMethodException e) {
-      throw new IllegalStateException("Failed to get method with name '" + methodName + "'.", e);
-    }
-  }
-
-  private final String uniqueId;
-  private final String engineId;
-  private final String testId;
-  private final String displayName;
-  private final TestDescriptor parent;
-  private final List<TestDescriptor> children;
-  private final Class<?> testClass;
-
-  private final Method testMethod;
-
-  public JavaTestDescriptor(String engineId, Method testMethod) {
-    this(engineId, testMethod.getDeclaringClass(), testMethod, null, null);
-  }
-
-  public JavaTestDescriptor(String engineId, Class<?> testClass, Method testMethod) {
-    this(engineId, testClass, testMethod, null, null);
-  }
+	// The following pattern only supports descriptors for test methods.
+	// TODO Support descriptors for test classes.
+	// TODO Decide if we want to support descriptors for packages.
+	private static final Pattern UID_PATTERN = Pattern.compile("^(.+):(.+)#(.+)\\((.*)\\)$");
 
 
-  public JavaTestDescriptor(String engineId, Class<?> testClass, Method testMethod,
-                            TestDescriptor parent, List<TestDescriptor> children) {
+	public static JavaTestDescriptor from(final String uid) throws RuntimeException {
+		Preconditions.notNull(uid, "TestDescriptor UID must not be null");
 
-    Preconditions.notEmpty(engineId, "engineId must not be null or empty");
-    if (testMethod == null) {
-      Preconditions.notNull(testClass, "testClass must not be null");
-    } else {
-      Preconditions.notNull(testMethod, "testMethod must not be null");
-      testClass = testMethod.getDeclaringClass();
-    }
+		Matcher matcher = UID_PATTERN.matcher(uid);
+		Preconditions.condition(matcher.matches(),
+			() -> String.format("Invalid format for %s UID: %s", JavaTestDescriptor.class.getSimpleName(), uid));
 
-    this.testClass = testClass;
-    this.testMethod = testMethod;
-    this.displayName = determineDisplayName(testClass, testMethod);
-    this.parent = parent;
-    this.children = (children != null ? unmodifiableList(children) : emptyList());
-    this.engineId = engineId;
-    this.testId = createTestId(testClass, testMethod);
-    this.uniqueId = this.engineId + ":" + this.testId;
-  }
+		// TODO Validate contents of matched groups.
+		String engineId = matcher.group(1);
+		String className = matcher.group(2);
+		String methodName = matcher.group(3);
+		String methodParameters = matcher.group(4);
 
-  private static Class<?> loadClass(String name) {
-    try {
-      // TODO Add support for primitive types and arrays.
-      return JavaTestDescriptor.class.getClassLoader().loadClass(name);
-    } catch (ClassNotFoundException e) {
-      throw new IllegalStateException("Failed to load class with name '" + name + "'.", e);
-    }
-  }
+		Class<?> clazz = loadClass(className);
 
-  private static String createTestId(Class<?> testClass, Method testMethod) {
-    return (testMethod != null ? String.format("%s#%s(%s)", testClass.getName(), testMethod.getName(),
-        nullSafeToString(testMethod.getParameterTypes())) : testClass.getName());
-  }
+		System.out.println("DEBUG - method params: " + methodParameters);
 
-  private static String determineDisplayName(Class<?> testClass, Method testMethod) {
-    if (testMethod != null) {
-      Test test = testMethod.getAnnotation(Test.class);
-      if (test != null) {
-        String customName = test.name();
-        if (!ObjectUtils.isEmpty(customName)) {
-          return customName;
-        }
-      }
-      return testMethod.getName();
-    } else {
-      return testClass.getSimpleName();
-    }
-  }
+		List<Class<?>> paramTypeList = new ArrayList<>();
+		for (String type : methodParameters.split(",")) {
+			type = type.trim();
+			if (!type.isEmpty()) {
+				paramTypeList.add(loadClass(type));
+			}
+		}
+
+		Class<?>[] parameterTypes = paramTypeList.toArray(new Class<?>[paramTypeList.size()]);
+
+		try {
+			Method method = clazz.getDeclaredMethod(methodName, parameterTypes);
+			return new JavaTestDescriptor(engineId, method);
+		}
+		catch (NoSuchMethodException e) {
+			throw new IllegalStateException("Failed to get method with name '" + methodName + "'.", e);
+		}
+	}
+
+
+	private final String uniqueId;
+	private final String engineId;
+	private final String testId;
+	private final String displayName;
+	private final TestDescriptor parent;
+	private final List<TestDescriptor> children;
+	private final Class<?> testClass;
+
+	private final Method testMethod;
+
+
+	public JavaTestDescriptor(String engineId, Method testMethod) {
+		this(engineId, testMethod.getDeclaringClass(), testMethod, null, null);
+	}
+
+	public JavaTestDescriptor(String engineId, Class<?> testClass, Method testMethod) {
+		this(engineId, testClass, testMethod, null, null);
+	}
+
+	public JavaTestDescriptor(String engineId, Class<?> testClass, Method testMethod, TestDescriptor parent,
+			List<TestDescriptor> children) {
+
+		Preconditions.notEmpty(engineId, "engineId must not be null or empty");
+		if (testMethod == null) {
+			Preconditions.notNull(testClass, "testClass must not be null");
+		}
+		else {
+			Preconditions.notNull(testMethod, "testMethod must not be null");
+			testClass = testMethod.getDeclaringClass();
+		}
+
+		this.testClass = testClass;
+		this.testMethod = testMethod;
+		this.displayName = determineDisplayName(testClass, testMethod);
+		this.parent = parent;
+		this.children = (children != null ? unmodifiableList(children) : emptyList());
+		this.engineId = engineId;
+		this.testId = createTestId(testClass, testMethod);
+		this.uniqueId = this.engineId + ":" + this.testId;
+	}
+
+	private static Class<?> loadClass(String name) {
+		try {
+			// TODO Add support for primitive types and arrays.
+			return JavaTestDescriptor.class.getClassLoader().loadClass(name);
+		}
+		catch (ClassNotFoundException e) {
+			throw new IllegalStateException("Failed to load class with name '" + name + "'.", e);
+		}
+	}
+
+	private static String createTestId(Class<?> testClass, Method testMethod) {
+		return (testMethod != null ? String.format("%s#%s(%s)", testClass.getName(), testMethod.getName(),
+			nullSafeToString(testMethod.getParameterTypes())) : testClass.getName());
+	}
+
+	private static String determineDisplayName(Class<?> testClass, Method testMethod) {
+		if (testMethod != null) {
+			Test test = testMethod.getAnnotation(Test.class);
+			if (test != null) {
+				String customName = test.name();
+				if (!ObjectUtils.isEmpty(customName)) {
+					return customName;
+				}
+			}
+			return testMethod.getName();
+		}
+		else {
+			return testClass.getSimpleName();
+		}
+	}
+
 }
