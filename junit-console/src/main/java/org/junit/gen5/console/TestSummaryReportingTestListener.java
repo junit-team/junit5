2cf2714baa150b4eff53c791c90894729b177f18
Sam Brannen <sam@sambrannen.com>
2015-10-28 19:54:31 +0100

    Polishing

--- junit-console/src/main/java/org/junit/gen5/console/TestSummaryReportingTestListener.java
+++ junit-console/src/main/java/org/junit/gen5/console/TestSummaryReportingTestListener.java
@@ -1,3 +1,4 @@
+
 package org.junit.gen5.console;
 
 import org.junit.gen5.engine.TestDescriptor;
@@ -12,96 +13,86 @@ import java.io.PrintStream;
  */
 public class TestSummaryReportingTestListener implements TestPlanExecutionListener, TestExecutionListener {
 
-  private final PrintStream out;
+	private final PrintStream out;
 
-  private int testsFound;
-  private int testsSkipped;
-  private int testsAborted;
-  private int testsSucceeded;
-  private int testsFailed;
+	private int testsFound;
+	private int testsSkipped;
+	private int testsAborted;
+	private int testsSucceeded;
+	private int testsFailed;
 
-  private long timeStarted;
-  private long timePaused;
-  private long timeFinished;
+	private long timeStarted;
+	private long timePaused;
+	private long timeFinished;
 
-  public TestSummaryReportingTestListener(PrintStream out) {
-    this.out = out;
-  }
 
-  @Override
-  public void testPlanExecutionStarted(int numberOfStaticTests) {
-    testsFound = numberOfStaticTests;
-    timeStarted = System.currentTimeMillis();
-  }
+	public TestSummaryReportingTestListener(PrintStream out) {
+		this.out = out;
+	}
 
-  @Override
-  public void testPlanExecutionPaused() {
-    timePaused = System.currentTimeMillis();
-  }
+	@Override
+	public void testPlanExecutionStarted(int numberOfStaticTests) {
+		testsFound = numberOfStaticTests;
+		timeStarted = System.currentTimeMillis();
+	}
 
-  @Override
-  public void testPlanExecutionRestarted() {
-    timeStarted += System.currentTimeMillis() - timePaused;
-    timePaused = 0;
-  }
+	@Override
+	public void testPlanExecutionPaused() {
+		timePaused = System.currentTimeMillis();
+	}
 
-  @Override
-  public void testPlanExecutionStopped() {
-    reportSummary("Test run stopped");
-  }
+	@Override
+	public void testPlanExecutionRestarted() {
+		timeStarted += System.currentTimeMillis() - timePaused;
+		timePaused = 0;
+	}
 
-  @Override
-  public void testPlanExecutionFinished() {
-    reportSummary("Test run finished");
-  }
+	@Override
+	public void testPlanExecutionStopped() {
+		reportSummary("Test run stopped");
+	}
 
-  private void reportSummary(String msg) {
-    timeFinished = System.currentTimeMillis();
+	@Override
+	public void testPlanExecutionFinished() {
+		reportSummary("Test run finished");
+	}
 
-    out.println(String.format(
-        "%s after %d ms\n" +
-            "[%10d tests found     ]\n" +
-            "[%10d tests skipped   ]\n" +
-            "[%10d tests aborted   ]\n" +
-            "[%10d tests failed    ]\n" +
-            "[%10d tests successful]\n",
-        msg,
-        timeFinished - timeStarted,
-        testsFound,
-        testsSkipped,
-        testsAborted,
-        testsFailed,
-        testsSucceeded
-    ));
-  }
+	private void reportSummary(String msg) {
+		timeFinished = System.currentTimeMillis();
 
-  @Override
-  public void dynamicTestFound(TestDescriptor testDescriptor) {
-    testsFound++;
-  }
+		out.println(String.format(
+			"%s after %d ms\n" + "[%10d tests found     ]\n" + "[%10d tests skipped   ]\n" + "[%10d tests aborted   ]\n"
+					+ "[%10d tests failed    ]\n" + "[%10d tests successful]\n",
+			msg, timeFinished - timeStarted, testsFound, testsSkipped, testsAborted, testsFailed, testsSucceeded));
+	}
 
-  @Override
-  public void testStarted(TestDescriptor testDescriptor) {
-  }
+	@Override
+	public void dynamicTestFound(TestDescriptor testDescriptor) {
+		testsFound++;
+	}
 
-  @Override
-  public void testSkipped(TestDescriptor testDescriptor, Throwable t) {
-    testsSkipped++;
-  }
+	@Override
+	public void testStarted(TestDescriptor testDescriptor) {
+	}
 
-  @Override
-  public void testAborted(TestDescriptor testDescriptor, Throwable t) {
-    testsAborted++;
-  }
+	@Override
+	public void testSkipped(TestDescriptor testDescriptor, Throwable t) {
+		testsSkipped++;
+	}
 
-  @Override
-  public void testFailed(TestDescriptor testDescriptor, Throwable t) {
-    testsFailed++;
-  }
+	@Override
+	public void testAborted(TestDescriptor testDescriptor, Throwable t) {
+		testsAborted++;
+	}
 
-  @Override
-  public void testSucceeded(TestDescriptor testDescriptor) {
-    testsSucceeded++;
-  }
+	@Override
+	public void testFailed(TestDescriptor testDescriptor, Throwable t) {
+		testsFailed++;
+	}
+
+	@Override
+	public void testSucceeded(TestDescriptor testDescriptor) {
+		testsSucceeded++;
+	}
 
 }
