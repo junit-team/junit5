cdfeb39f7688c4db0be0133eecc3d5c5f9757c5a
Sam Brannen <sam@sambrannen.com>
2015-10-28 21:57:54 +0100

    Introduce Spotless

---
+++ src/spotless/eclipse-public-license-1.0.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
