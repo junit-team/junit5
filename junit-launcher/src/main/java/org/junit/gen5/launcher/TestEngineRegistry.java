989a1fc59562f5719aad0f4c41ecbd0f23a0cb8f
Sam Brannen <sam@sambrannen.com>
2015-10-28 19:15:43 +0100

    Reduce visibility of TestEngineRegistry

--- junit-launcher/src/main/java/org/junit/gen5/launcher/TestEngineRegistry.java
+++ junit-launcher/src/main/java/org/junit/gen5/launcher/TestEngineRegistry.java
@@ -1,20 +1,24 @@
+
 package org.junit.gen5.launcher;
 
-import org.junit.gen5.engine.TestEngine;
-
 import java.util.ServiceLoader;
+
+import org.junit.gen5.engine.TestEngine;
 
 /**
  * @author Stefan Bechtold
  * @since 5.0
  */
-public class TestEngineRegistry {
-  private static Iterable<TestEngine> testEngines;
+class TestEngineRegistry {
 
-  public static Iterable<TestEngine> lookupAllTestEngines() {
-    if (testEngines == null) {
-      testEngines = ServiceLoader.load(TestEngine.class);
-    }
-    return testEngines;
-  }
+	private static Iterable<TestEngine> testEngines;
+
+
+	static Iterable<TestEngine> lookupAllTestEngines() {
+		if (testEngines == null) {
+			testEngines = ServiceLoader.load(TestEngine.class);
+		}
+		return testEngines;
+	}
+
 }
