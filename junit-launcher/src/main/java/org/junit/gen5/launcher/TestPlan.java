04a20efe12ada9b292a38175f86f195fbf222aed
Marc Philipp <mail@marcphilipp.de>
2015-10-29 10:20:27 +0100

    Remove state in TestPlan for now

--- junit-launcher/src/main/java/org/junit/gen5/launcher/TestPlan.java
+++ junit-launcher/src/main/java/org/junit/gen5/launcher/TestPlan.java
@@ -10,8 +10,8 @@
 
 package org.junit.gen5.launcher;
 
-import static java.util.Arrays.*;
-import static java.util.Collections.*;
+import static java.util.Arrays.asList;
+import static java.util.Collections.singleton;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -27,37 +27,14 @@ import org.junit.gen5.engine.TestEngine;
  */
 final class TestPlan {
 
-	private static enum State {
-		NEW, ACTIVE, PAUSED, STOPPED, COMPLETED;
-	}
-
-
 	/**
 	 * List of all TestDescriptors, including children.
 	 */
 	private final Collection<TestDescriptor> testDescriptors = new LinkedList<>();
 
-	private State state = State.NEW;
-
 
 	TestPlan() {
 		/* no-op */
-	}
-
-	public boolean isActive() {
-		return this.state == State.ACTIVE;
-	}
-
-	public boolean isPaused() {
-		return this.state == State.PAUSED;
-	}
-
-	public boolean isStopped() {
-		return this.state == State.STOPPED;
-	}
-
-	public boolean isCompleted() {
-		return this.state == State.COMPLETED;
 	}
 
 	public void addTest(TestDescriptor testDescriptor) {
@@ -78,26 +55,6 @@ final class TestPlan {
 
 	public List<TestDescriptor> getAllTestsForTestEngine(TestEngine testEngine) {
 		return this.testDescriptors.stream().filter(testEngine::supports).collect(Collectors.toList());
-	}
-
-	public void start() {
-		System.out.println("Starting test plan");
-		this.state = State.ACTIVE;
-	}
-
-	public void stop() {
-		System.out.println("Stopping test plan");
-		this.state = State.STOPPED;
-	}
-
-	public void pause() {
-		System.out.println("Pausing test plan");
-		this.state = State.PAUSED;
-	}
-
-	public void restart() {
-		System.out.println("Restarting test plan");
-		this.state = State.ACTIVE;
 	}
 
 }
