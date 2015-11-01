073bbb3f78bebd2c810d8ca3aefadea5136ca2d8
Johannes Link <business@johanneslink.net>
2015-11-01 20:00:38 +0100

    spotless applied

--- junit5-engine/src/main/java/org/junit/gen5/engine/junit5/descriptor/UniqueIdParts.java
+++ junit5-engine/src/main/java/org/junit/gen5/engine/junit5/descriptor/UniqueIdParts.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
 package org.junit.gen5.engine.junit5.descriptor;
 
 import java.util.ArrayList;
@@ -5,6 +15,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class UniqueIdParts {
+
 	private static final String SEPARATORS = ":$#";
 	private final List<String> parts = new ArrayList<>();
 
