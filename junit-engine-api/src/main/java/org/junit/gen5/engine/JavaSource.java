7b277f3be6157f9c31a03cd73200343bb1c50482
Marc Philipp <mail@marcphilipp.de>
2015-11-11 20:53:13 +0100

    Fix raw types warning

--- junit-engine-api/src/main/java/org/junit/gen5/engine/JavaSource.java
+++ junit-engine-api/src/main/java/org/junit/gen5/engine/JavaSource.java
@@ -15,7 +15,7 @@ import java.util.Optional;
 
 public class JavaSource implements TestSource {
 
-	private final Class javaClass;
+	private final Class<?> javaClass;
 
 	private final Method javaMethod;
 
