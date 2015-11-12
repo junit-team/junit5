e70c806c53ebef07a78fb71ee2a8efaa8e8e27cc
Matthias Merdes <Matthias.Merdes@heidelberg-mobil.com>
2015-11-12 08:38:55 +0100

    better name for exception instance

--- junit5-engine/src/main/java/org/junit/gen5/engine/junit5/execution/injection/MethodArgumentResolverEngine.java
+++ junit5-engine/src/main/java/org/junit/gen5/engine/junit5/execution/injection/MethodArgumentResolverEngine.java
@@ -33,8 +33,8 @@ public class MethodArgumentResolverEngine {
 		try {
 			return this.doPrepareArguments(methodTestDescriptor);
 		}
-		catch (Exception root) {
-			throw new ArgumentResolutionException("problem with argument resolution", root);
+		catch (Exception cause) {
+			throw new ArgumentResolutionException("problem with argument resolution", cause);
 		}
 
 	}
