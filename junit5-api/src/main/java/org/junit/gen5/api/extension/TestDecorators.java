3c05f522c5691b0308331fefe7274608a27b7315
Sam Brannen <sam@sambrannen.com>
2015-11-13 23:37:56 +0100

    Make @TestDecorators inherited within test class hierarchies

--- junit5-api/src/main/java/org/junit/gen5/api/extension/TestDecorators.java
+++ junit5-api/src/main/java/org/junit/gen5/api/extension/TestDecorators.java
@@ -12,6 +12,7 @@ package org.junit.gen5.api.extension;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -28,6 +29,7 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
+@Inherited
 public @interface TestDecorators {
 
 	Class<? extends TestDecorator>[]value();
