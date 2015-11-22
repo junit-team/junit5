e72d561e16ddba37f0719acdad919dab3b15d5e7
Johannes Link <business@johanneslink.net>
2015-11-22 19:53:25 +0100

    Engine detection in TestEngineRegistry is using the ReflectionUtils.getDefaultClassLoader()

--- junit-launcher/src/main/java/org/junit/gen5/launcher/TestEngineRegistry.java
+++ junit-launcher/src/main/java/org/junit/gen5/launcher/TestEngineRegistry.java
@@ -13,6 +13,7 @@ package org.junit.gen5.launcher;
 import java.util.ServiceLoader;
 import java.util.logging.Logger;
 
+import org.junit.gen5.commons.util.ReflectionUtils;
 import org.junit.gen5.engine.TestEngine;
 
 /**
@@ -26,8 +27,12 @@ class TestEngineRegistry {
 	private static Iterable<TestEngine> testEngines;
 
 	static Iterable<TestEngine> lookupAllTestEngines() {
+		return lookupAllTestEngines(ReflectionUtils.getDefaultClassLoader());
+	}
+
+	static Iterable<TestEngine> lookupAllTestEngines(ClassLoader classLoader) {
 		if (testEngines == null) {
-			testEngines = ServiceLoader.load(TestEngine.class);
+			testEngines = ServiceLoader.load(TestEngine.class, classLoader);
 			for (TestEngine testEngine : testEngines) {
 				LOG.info(() -> String.format("Discovered test engine with id: '%s'", testEngine.getId()));
 			}
