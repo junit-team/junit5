236d8879c6a669028b7e79e03611b9d9122183b8
Marc Philipp <mail@marcphilipp.de>
2015-11-25 17:31:12 +0100

    Include stack trace on skipped/aborted tests

--- surefire-junit5/src/main/java/org/junit/gen5/surefire/RunListenerAdapter.java
+++ surefire-junit5/src/main/java/org/junit/gen5/surefire/RunListenerAdapter.java
@@ -12,6 +12,7 @@ package org.junit.gen5.surefire;
 
 import static org.apache.maven.surefire.report.SimpleReportEntry.ignored;
 
+import java.lang.reflect.Method;
 import java.util.Optional;
 
 import org.apache.maven.surefire.report.PojoStackTraceWriter;
@@ -21,7 +22,6 @@ import org.apache.maven.surefire.report.StackTraceWriter;
 import org.junit.gen5.engine.JavaSource;
 import org.junit.gen5.engine.TestDescriptor;
 import org.junit.gen5.engine.TestExecutionListener;
-import org.junit.gen5.engine.TestSource;
 
 final class RunListenerAdapter implements TestExecutionListener {
 
@@ -33,46 +33,67 @@ final class RunListenerAdapter implements TestExecutionListener {
 
 	@Override
 	public void testStarted(TestDescriptor testDescriptor) {
-		runListener.testStarting(new SimpleReportEntry(getClassName(testDescriptor), testDescriptor.getDisplayName()));
+		runListener.testStarting(createReportEntry(testDescriptor));
 	}
 
 	@Override
 	public void testSkipped(TestDescriptor testDescriptor, Throwable t) {
-		StackTraceWriter stackTraceWriter = getStackTraceWriter(testDescriptor, t);
-		runListener.testSkipped(ignored(getClassName(testDescriptor), testDescriptor.getDisplayName(), t.getMessage()));
+		runListener.testSkipped(createReportEntry(testDescriptor, t));
 	}
 
 	@Override
 	public void testAborted(TestDescriptor testDescriptor, Throwable t) {
-		StackTraceWriter stackTraceWriter = getStackTraceWriter(testDescriptor, t);
-		runListener.testAssumptionFailure(
-			ignored(getClassName(testDescriptor), testDescriptor.getDisplayName(), t.getMessage()));
+		runListener.testAssumptionFailure(createReportEntry(testDescriptor, t));
 	}
 
 	@Override
 	public void testFailed(TestDescriptor testDescriptor, Throwable t) {
-		StackTraceWriter stackTraceWriter = getStackTraceWriter(testDescriptor, t);
-		runListener.testFailed(new SimpleReportEntry(getClassName(testDescriptor), testDescriptor.getDisplayName(),
-			stackTraceWriter, null));
+		runListener.testFailed(createReportEntry(testDescriptor, t));
 	}
 
 	@Override
 	public void testSucceeded(TestDescriptor testDescriptor) {
-		runListener.testSucceeded(new SimpleReportEntry(getClassName(testDescriptor), testDescriptor.getDisplayName()));
+		runListener.testSucceeded(createReportEntry(testDescriptor));
 	}
 
-	private StackTraceWriter getStackTraceWriter(TestDescriptor testDescriptor, Throwable t) {
-		return new PojoStackTraceWriter(getClassName(testDescriptor), testDescriptor.getDisplayName(), t);
+	private SimpleReportEntry createReportEntry(TestDescriptor testDescriptor) {
+		return new SimpleReportEntry(getClassNameOrUniqueId(testDescriptor), testDescriptor.getDisplayName());
 	}
 
-	private String getClassName(TestDescriptor testDescriptor) {
-		Optional<TestSource> testSource = testDescriptor.getSource();
-		if (testSource.isPresent() && testSource.get() instanceof JavaSource) {
-			JavaSource javaSource = (JavaSource) testSource.get();
-			if (javaSource.getJavaClass().isPresent()) {
-				return javaSource.getJavaClass().get().getName();
+	private SimpleReportEntry createReportEntry(TestDescriptor testDescriptor, Throwable throwable) {
+		Optional<JavaSource> javaSource = getJavaSource(testDescriptor);
+		if (javaSource.isPresent() && javaSource.get().getJavaClass().isPresent()) {
+			Class<?> sourceClass = javaSource.get().getJavaClass().get();
+			Optional<Method> sourceMethod = javaSource.get().getJavaMethod();
+			StackTraceWriter stackTraceWriter = getStackTraceWriter(sourceClass, sourceMethod, throwable);
+			return new SimpleReportEntry(getClassNameOrUniqueId(testDescriptor), testDescriptor.getDisplayName(),
+				stackTraceWriter, null);
+		}
+		return ignored(getClassNameOrUniqueId(testDescriptor), testDescriptor.getDisplayName(), throwable.getMessage());
+	}
+
+	private StackTraceWriter getStackTraceWriter(Class<?> sourceClass, Optional<Method> sourceMethod,
+			Throwable throwable) {
+		String className = sourceClass.getName();
+		String methodName = sourceMethod.map(Method::getName).orElse("");
+		return new PojoStackTraceWriter(className, methodName, throwable);
+	}
+
+	private String getClassNameOrUniqueId(TestDescriptor testDescriptor) {
+		return getClassName(testDescriptor).orElse(testDescriptor.getUniqueId());
+	}
+
+	private Optional<String> getClassName(TestDescriptor testDescriptor) {
+		Optional<JavaSource> javaSource = getJavaSource(testDescriptor);
+		if (javaSource.isPresent()) {
+			if (javaSource.get().getJavaClass().isPresent()) {
+				return javaSource.get().getJavaClass().map(Class::getName);
 			}
 		}
-		return testDescriptor.getUniqueId();
+		return Optional.empty();
+	}
+
+	private Optional<JavaSource> getJavaSource(TestDescriptor testDescriptor) {
+		return testDescriptor.getSource().filter(JavaSource.class::isInstance).map(JavaSource.class::cast);
 	}
 }
