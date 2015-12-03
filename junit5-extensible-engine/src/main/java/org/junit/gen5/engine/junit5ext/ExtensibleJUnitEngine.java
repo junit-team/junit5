65ae5c5d1451a8cebab3734cb0f66d07328cca6f
Stefan Bechtold <stefan.bechtold@me.com>
2015-12-03 13:25:46 +0100

    Created new TestEngine #29
    ------------------------------------------------------------------------
    On behalf of the community, the JUnit Lambda Team thanks
    synyx GmbH & Co. KG (http://www.synyx.de) for supporting the
    JUnit crowdfunding campaign!
    ------------------------------------------------------------------------

---
+++ junit5-extensible-engine/src/main/java/org/junit/gen5/engine/junit5ext/ExtensibleJUnitEngine.java
@@ -0,0 +1,18 @@
+package org.junit.gen5.engine.junit5ext;
+
+import org.junit.gen5.engine.EngineDescriptor;
+import org.junit.gen5.engine.ExecutionRequest;
+import org.junit.gen5.engine.TestEngine;
+import org.junit.gen5.engine.TestPlanSpecification;
+
+public class ExtensibleJUnitEngine implements TestEngine {
+  @Override
+  public void discoverTests(TestPlanSpecification specification, EngineDescriptor engineDescriptor) {
+
+  }
+
+  @Override
+  public void execute(ExecutionRequest request) {
+
+  }
+}
