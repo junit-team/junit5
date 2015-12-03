6373e52218166ac7fb5f48c711d75810f44ee463
Marc Philipp <mail@marcphilipp.de>
2015-12-03 20:23:16 +0100

    Remove pointless if

--- junit5-api/src/main/java/org/junit/gen5/api/Assertions.java
+++ junit5-api/src/main/java/org/junit/gen5/api/Assertions.java
@@ -28,9 +28,6 @@ public final class Assertions {
 	}
 
 	public static void fail(String message) {
-		if (message == null) {
-			throw new AssertionFailedError();
-		}
 		throw new AssertionFailedError(message);
 	}
 
