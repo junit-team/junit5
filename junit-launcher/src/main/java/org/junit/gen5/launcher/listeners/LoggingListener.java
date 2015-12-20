46de6ac0547ea8b752e14a15adb52dc791b10ffb
Sam Brannen <sam@sambrannen.com>
2015-12-20 16:10:27 +0100

    Document and overhaul LoggingListener
    ------------------------------------------------------------------------
    On behalf of the community, the JUnit Lambda Team thanks Gradle Inc.
    (https://gradle.org) for supporting the JUnit crowdfunding campaign!
    ------------------------------------------------------------------------

--- junit-launcher/src/main/java/org/junit/gen5/launcher/listeners/LoggingListener.java
+++ junit-launcher/src/main/java/org/junit/gen5/launcher/listeners/LoggingListener.java
@@ -20,47 +20,83 @@ import org.junit.gen5.launcher.TestExecutionListener;
 import org.junit.gen5.launcher.TestIdentifier;
 import org.junit.gen5.launcher.TestPlan;
 
+/**
+ * Simple {@link TestExecutionListener} for logging informational messages
+ * for all events via a {@link BiConsumer} that consumes {@code Throwable}
+ * and {@code Supplier<String>}.
+ *
+ * @since 5.0
+ * @see #forJavaUtilLogging()
+ * @see #forJavaUtilLogging(Level)
+ * @see LoggingListener#LoggingListener(BiConsumer)
+ */
 public class LoggingListener implements TestExecutionListener {
 
-	public static LoggingListener forJUL() {
+	/**
+	 * Create a {@code LoggingListener} which delegates to a
+	 * {@link java.util.logging.Logger} using a log level of
+	 * {@link Level#FINE FINE}.
+	 * @see #forJavaUtilLogging(Level)
+	 */
+	public static LoggingListener forJavaUtilLogging() {
+		return forJavaUtilLogging(Level.FINE);
+	}
+
+	/**
+	 * Create a {@code LoggingListener} which delegates to a
+	 * {@link java.util.logging.Logger} using the supplied
+	 * {@linkplain Level log level}.
+	 *
+	 * @param logLevel the log level to use
+	 * @see #forJavaUtilLogging()
+	 */
+	public static LoggingListener forJavaUtilLogging(Level logLevel) {
 		Logger logger = Logger.getLogger(LoggingListener.class.getName());
-		return new LoggingListener((thrown, messageSupplier) -> logger.log(Level.FINE, thrown, messageSupplier));
+		return new LoggingListener((t, messageSupplier) -> logger.log(logLevel, t, messageSupplier));
 	}
 
 	private final BiConsumer<Throwable, Supplier<String>> logger;
 
+	/**
+	 * Create a {@code LoggingListener} which delegates to the supplied
+	 * {@link BiConsumer} for consumption of logging messages.
+	 * @param logger a logger implemented as a {@code BiConsumer}
+	 *
+	 * @see #forJavaUtilLogging()
+	 * @see #forJavaUtilLogging(Level)
+	 */
 	public LoggingListener(BiConsumer<Throwable, Supplier<String>> logger) {
 		this.logger = logger;
 	}
 
 	@Override
 	public void testPlanExecutionStarted(TestPlan testPlan) {
-		log("testPlanExecutionStarted: %s", testPlan);
+		log("TestPlan Execution Started: %s", testPlan);
 	}
 
 	@Override
 	public void testPlanExecutionFinished(TestPlan testPlan) {
-		log("testPlanExecutionFinished: %s", testPlan);
+		log("TestPlan Execution Finished: %s", testPlan);
 	}
 
 	@Override
 	public void dynamicTestRegistered(TestIdentifier testIdentifier) {
-		log("dynamicTestRegistered: %s - %s", testIdentifier.getDisplayName(), testIdentifier.getUniqueId());
+		log("Dynamic Test Registered: %s - %s", testIdentifier.getDisplayName(), testIdentifier.getUniqueId());
 	}
 
 	@Override
 	public void executionStarted(TestIdentifier testIdentifier) {
-		log("testStarted: %s - %s", testIdentifier.getDisplayName(), testIdentifier.getUniqueId());
+		log("Execution Started: %s - %s", testIdentifier.getDisplayName(), testIdentifier.getUniqueId());
 	}
 
 	@Override
 	public void executionSkipped(TestIdentifier testIdentifier, String reason) {
-		log("testSkipped: %s - %s - %s", testIdentifier.getDisplayName(), testIdentifier.getUniqueId(), reason);
+		log("Execution Skipped: %s - %s - %s", testIdentifier.getDisplayName(), testIdentifier.getUniqueId(), reason);
 	}
 
 	@Override
 	public void executionFinished(TestIdentifier testIdentifier, TestExecutionResult testExecutionResult) {
-		logWithThrowable("testFinished: %s - %s - %s", testExecutionResult.getThrowable().orElse(null),
+		logWithThrowable("Execution Finished: %s - %s - %s", testExecutionResult.getThrowable().orElse(null),
 			testIdentifier.getDisplayName(), testIdentifier.getUniqueId(), testExecutionResult);
 	}
 
@@ -68,7 +104,8 @@ public class LoggingListener implements TestExecutionListener {
 		logWithThrowable(message, null, args);
 	}
 
-	private void logWithThrowable(String message, Throwable thrown, Object... args) {
-		logger.accept(thrown, () -> String.format(message, args));
+	private void logWithThrowable(String message, Throwable t, Object... args) {
+		logger.accept(t, () -> String.format(message, args));
 	}
+
 }
