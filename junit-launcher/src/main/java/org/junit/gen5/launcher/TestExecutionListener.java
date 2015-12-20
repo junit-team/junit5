dd8a3f15436923363aa1c485ee9070b22c5dfee1
Sam Brannen <sam@sambrannen.com>
2015-12-20 15:53:24 +0100

    Polish Javadoc for TestExecutionListener
    Issue: #34
    
    ------------------------------------------------------------------------
    On behalf of the community, the JUnit Lambda Team thanks Gradle Inc.
    (https://gradle.org) for supporting the JUnit crowdfunding campaign!
    ------------------------------------------------------------------------

--- junit-launcher/src/main/java/org/junit/gen5/launcher/TestExecutionListener.java
+++ junit-launcher/src/main/java/org/junit/gen5/launcher/TestExecutionListener.java
@@ -10,33 +10,29 @@
 
 package org.junit.gen5.launcher;
 
-import org.junit.gen5.engine.TestEngine;
 import org.junit.gen5.engine.TestExecutionResult;
 import org.junit.gen5.engine.TestExecutionResult.Status;
-import org.junit.gen5.launcher.listeners.LoggingListener;
-import org.junit.gen5.launcher.listeners.SummaryCreatingTestListener;
 
 /**
- * Register an instance of this class with {@link Launcher} to be notified of
+ * Register an instance of this class with a {@link Launcher} to be notified of
  * events that occur during test execution. All of the methods in this class
  * have empty default implementations; override one or more of them to receive
  * events.
  *
  * <p>JUnit provides two example implementations:
  * <ul>
- * <li>{@link LoggingListener}</li>
- * <li>{@link SummaryCreatingTestListener}</li>
+ * <li>{@link org.junit.gen5.launcher.listeners.LoggingListener}</li>
+ * <li>{@link org.junit.gen5.launcher.listeners.SummaryCreatingTestListener}</li>
  * </ul>
  *
- * <p>Contrary to JUnit 4, {@linkplain TestEngine engines} are supposed to
- * report events not only for {@linkplain TestIdentifier identifiers} that
- * represent executable leafs in the {@linkplain TestPlan test plan} but for
- * all intermediate containers, too. While both the JUnit 4 and 5 test engines
+ * <p>Contrary to JUnit 4, {@linkplain org.junit.gen5.engine.TestEngine test engines}
+ * are supposed to report events not only for {@linkplain TestIdentifier identifiers}
+ * that represent executable leaves in the {@linkplain TestPlan test plan} but also
+ * for all intermediate containers. While both the JUnit 4 and JUnit 5 engines
  * comply with this contract, there is no way to guarantee this for third-party
  * engines.
  *
  * @since 5.0
- *
  * @see Launcher
  * @see TestPlan
  * @see TestIdentifier
@@ -44,8 +40,8 @@ import org.junit.gen5.launcher.listeners.SummaryCreatingTestListener;
 public interface TestExecutionListener {
 
 	/**
-	 * Called when the execution of the {@link TestPlan} has started, before
-	 * any test has been executed.
+	 * Called when the execution of the {@link TestPlan} has started,
+	 * <em>before</em> any test has been executed.
 	 *
 	 * @param testPlan describes the tree of tests about to be executed
 	 */
@@ -53,8 +49,8 @@ public interface TestExecutionListener {
 	}
 
 	/**
-	 * Called when the execution of the {@link TestPlan} has finished, after
-	 * all tests have been executed.
+	 * Called when the execution of the {@link TestPlan} has finished,
+	 * <em>after</em> all tests have been executed.
 	 *
 	 * @param testPlan describes the tree of tests that have been executed
 	 */
@@ -62,10 +58,10 @@ public interface TestExecutionListener {
 	}
 
 	/**
-	 * Called when a new, dynamic {@link TestIdentifier} was registered.
+	 * Called when a new, dynamic {@link TestIdentifier} has been registered.
 	 *
-	 * <p>A dynamic test is a test that is not known a-priori and therefore not
-	 * contained in the original {@link TestPlan}.
+	 * <p>A <em>dynamic test</em> is a test that is not known a-priori and
+	 * therefore not contained in the original {@link TestPlan}.
 	 *
 	 * @param testIdentifier the identifier of the newly registered test
 	 * or container
@@ -75,13 +71,15 @@ public interface TestExecutionListener {
 
 	/**
 	 * Called when the execution of a leaf or subtree of the {@link TestPlan}
-	 * was skipped.
+	 * has been skipped.
 	 *
-	 * <p>The {@link TestIdentifier} may be a test or a container.
-	 * A skipped test or subtree of tests is never started or finished.
+	 * <p>The {@link TestIdentifier} may represent a test or a container.
+	 *
+	 * <p>A skipped test or subtree of tests must never be started or finished.
 	 *
 	 * @param testIdentifier the identifier of the skipped test or container
-	 * @param reason a human-readable message describing why it was skipped
+	 * @param reason a human-readable message describing why the execution
+	 * has been skipped
 	 */
 	default void executionSkipped(TestIdentifier testIdentifier, String reason) {
 	}
@@ -90,13 +88,13 @@ public interface TestExecutionListener {
 	 * Called when the execution of a leaf or subtree of the {@link TestPlan}
 	 * is about to be started.
 	 *
-	 * <p>The {@link TestIdentifier} may be a test or a
-	 * container. This method is only called when the test or container has
-	 * not been skipped.
+	 * <p>The {@link TestIdentifier} may represent a test or a container.
 	 *
-	 * <p>This method is called for container
-	 * {@linkplain TestIdentifier identifiers} before starting or skipping any
-	 * of its children.
+	 * <p>This method must only be called if the test or container has not
+	 * been skipped.
+	 *
+	 * <p>This method must be called for a container {@code TestIdentifier}
+	 * <em>before</em> starting or skipping any of its children.
 	 *
 	 * @param testIdentifier the identifier of the started test or container
 	 */
@@ -107,24 +105,23 @@ public interface TestExecutionListener {
 	 * Called when the execution of a leaf or subtree of the {@link TestPlan}
 	 * has finished, regardless of its result.
 	 *
-	 * <p>The {@link TestIdentifier} may
-	 * be a test or a container. This method is only called when the test or
-	 * container has not been skipped.
+	 * <p>The {@link TestIdentifier} may represent a test or a container.
 	 *
-	 * <p>This method is called for container
-	 * {@linkplain TestIdentifier identifiers} no sooner than all of its
-	 * children have been skipped or are finished.
+	 * <p>This method must only be called if the test or container has not
+	 * been skipped.
+	 *
+	 * <p>This method must be called for a container {@code TestIdentifier}
+	 * <em>after</em> all of its children have been skipped or have finished.
 	 *
 	 * <p>The {@link TestExecutionResult} describes the result of the execution
-	 * of the {@code testIdentifier}. It does not include or aggregate the
-	 * results of its children. E.g. a container with a single failing test will
-	 * report a result with status {@link Status#SUCCESSFUL SUCCESSFUL} even
-	 * though its child has reported a result with status
-	 * {@link Status#FAILED FAILED}.
+	 * of the {@code TestIdentifier}. The result does not include or aggregate
+	 * the results of its children. For example, a container with a failing
+	 * test will be reported as {@link Status#SUCCESSFUL SUCCESSFUL} even if
+	 * one or more of its children are reported as {@link Status#FAILED FAILED}.
 	 *
 	 * @param testIdentifier the identifier of the finished test or container
 	 * @param testExecutionResult the (unaggregated) result of the execution of
-	 * {@code testIdentifier}
+	 * {@code TestIdentifier}
 	 *
 	 * @see TestExecutionResult
 	 */
