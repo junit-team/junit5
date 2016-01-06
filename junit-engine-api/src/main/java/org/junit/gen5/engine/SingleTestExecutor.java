junit-team/junit5

commit 58d455c6da6ce482e079d27892c058dd759cc6a7

Throw Exception instead of Throwable in [Test]Executable
Issue: #64

------------------------------------------------------------------------
On behalf of the community, the JUnit Lambda Team thanks msg systems ag
(http://www.msg-systems.com) for supporting the JUnit crowdfunding
campaign!
------------------------------------------------------------------------

junit-engine-api/src/main/java/org/junit/gen5/engine/SingleTestExecutor.java
@@ -10,15 +10,20 @@
 
 package org.junit.gen5.engine;
 
-import static org.junit.gen5.engine.TestExecutionResult.*;
+import static org.junit.gen5.engine.TestExecutionResult.aborted;
+import static org.junit.gen5.engine.TestExecutionResult.failed;
+import static org.junit.gen5.engine.TestExecutionResult.successful;
 
 import org.opentest4j.TestAbortedException;
 
+/**
+ * @since 5.0
+ */
 class SingleTestExecutor {
 
 	interface TestExecutable {
 
-		void execute() throws Throwable;
+		void execute() throws Exception;
 
 	}
 
@@ -34,4 +39,5 @@ class SingleTestExecutor {
 			return failed(t);
 		}
 	}
+
 }

junit5-api/src/main/java/org/junit/gen5/api/Executable.java
@@ -12,11 +12,11 @@ package org.junit.gen5.api;
 
 /**
  * {@code Executable} is a functional interface that can be used to
- * implement any generic block of code that potentially throws a
- * {@link Throwable}.
+ * implement any generic block of code that potentially throws an
+ * {@link Exception}.
  *
  * <p>The {@code Executable} interface is similar to {@link java.lang.Runnable},
- * except that an {@code Executable} can throw any kind of exception.
+ * except that an {@code Executable} can throw an {@link Exception}.
  *
  * @since 5.0
  * @see Assertions#assertAll(Executable...)
@@ -27,6 +27,6 @@ package org.junit.gen5.api;
 @FunctionalInterface
 public interface Executable {
 
-	void execute() throws Throwable;
+	void execute() throws Exception;
 
 }

commit dc5b6d7d78167f417291c13c141ab2d68cf28e6d

Throw Throwable instead of Exception in Executable
This commit reverts the changes introduced in 58d455c and documents the
rationale for throwing Throwable instead of Exception in
Executable.execute().

Issue: #64, #89

------------------------------------------------------------------------
On behalf of the community, the JUnit Lambda Team thanks msg systems ag
(http://www.msg-systems.com) for supporting the JUnit crowdfunding
campaign!
------------------------------------------------------------------------

junit-engine-api/src/main/java/org/junit/gen5/engine/SingleTestExecutor.java
@@ -24,7 +24,7 @@ class SingleTestExecutor {
 
 	interface Executable {
 
-		void execute() throws Exception;
+		void execute() throws Throwable;
 
 	}
 

junit5-api/src/main/java/org/junit/gen5/api/Executable.java
@@ -12,11 +12,20 @@ package org.junit.gen5.api;
 
 /**
  * {@code Executable} is a functional interface that can be used to
- * implement any generic block of code that potentially throws an
- * {@link Exception}.
+ * implement any generic block of code that potentially throws a
+ * {@link Throwable}.
  *
  * <p>The {@code Executable} interface is similar to {@link java.lang.Runnable},
- * except that an {@code Executable} can throw an {@link Exception}.
+ * except that an {@code Executable} can throw any kind of exception.
+ *
+ * <h4>Rationale for throwing {@code Throwable} instead of {@code Exception}</h4>
+ *
+ * <p>Although Java applications typically throw exceptions that are instances
+ * of {@link java.lang.Exception}, {@link java.lang.RuntimeException},
+ * {@link java.lang.Error}, or {@link java.lang.AssertionError} (in testing
+ * scenarios), there may be use cases where an {@code Executable} needs to
+ * explicitly throw a {@code Throwable}. In order to support such specialized
+ * use cases, {@link #execute()} is declared to throw {@code Throwable}.
  *
  * @since 5.0
  * @see Assertions#assertAll(Executable...)
@@ -27,6 +36,6 @@ package org.junit.gen5.api;
 @FunctionalInterface
 public interface Executable {
 
-	void execute() throws Exception;
+	void execute() throws Throwable;
 
 }