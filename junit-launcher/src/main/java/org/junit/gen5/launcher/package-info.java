d98830bb1974731ab203523a92afc30832ed0bf5
Sam Brannen <sam@sambrannen.com>
2016-01-09 16:11:48 +0100

    Polishing
    ------------------------------------------------------------------------
    On behalf of the community, the JUnit Lambda Team thanks Klarna AB
    (http://www.klarna.com) for supporting the JUnit crowdfunding campaign!
    ------------------------------------------------------------------------

--- junit-launcher/src/main/java/org/junit/gen5/launcher/package-info.java
+++ junit-launcher/src/main/java/org/junit/gen5/launcher/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Public API for configuring and launching JUnit 5 test plans.
+ * Public API for configuring and launching test plans.
  *
  * <p>This API is typically used by IDEs and build tools.
  */
