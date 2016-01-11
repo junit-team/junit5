80e6a2438d09cbf90729e135fb31dc09f80be4cc
Johannes Link <business@johanneslink.net>
2016-01-11 09:52:47 +0100

    Checkstyle allow p tags on separate lines.
    ------------------------------------------------------------------------
    On behalf of the community, the JUnit Lambda Team thanks Klarna AB
    (http://www.klarna.com) for supporting the JUnit crowdfunding campaign!
    ------------------------------------------------------------------------

--- junit5-engine/src/main/java/org/junit/gen5/engine/junit5/descriptor/ClassTestDescriptor.java
+++ junit5-engine/src/main/java/org/junit/gen5/engine/junit5/descriptor/ClassTestDescriptor.java
@@ -49,8 +49,8 @@ import org.junit.gen5.engine.junit5.execution.TestInstanceProvider;
 
 /**
  * {@link TestDescriptor} for tests based on Java classes.
- *
- * <p>The pattern of the {@link #getUniqueId unique ID} takes the form of
+ * <p>
+ * The pattern of the {@link #getUniqueId unique ID} takes the form of
  * <code>{parent unique id}:{fully qualified class name}</code>.
  *
  * @since 5.0
