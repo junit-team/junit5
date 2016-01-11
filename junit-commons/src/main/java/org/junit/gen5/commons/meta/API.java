4773e3bd6015de452f4e5d1d649f3d0b65697abc
Marc Philipp <mail@marcphilipp.de>
2016-01-11 13:27:06 +0100

    Annotate API with @API(Internal)
    ------------------------------------------------------------------------
    On behalf of the community, the JUnit Lambda Team thanks Klarna AB
    (http://www.klarna.com) for supporting the JUnit crowdfunding campaign!
    ------------------------------------------------------------------------

--- junit-commons/src/main/java/org/junit/gen5/commons/meta/API.java
+++ junit-commons/src/main/java/org/junit/gen5/commons/meta/API.java
@@ -1,5 +1,16 @@
+/*
+ * Copyright 2015-2016 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 
 package org.junit.gen5.commons.meta;
+
+import static org.junit.gen5.commons.meta.API.Usage.Internal;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
@@ -11,6 +22,7 @@ import java.lang.annotation.Target;
 // TODO Check if SOURCE is sufficient
 @Retention(RetentionPolicy.CLASS)
 @Documented
+@API(Internal)
 public @interface API {
 
 	Usage value();
