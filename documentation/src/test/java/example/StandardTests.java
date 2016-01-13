83883931644e0187f1c2a8172026efa946b533eb
Marc Philipp <mail@marcphilipp.de>
2016-01-13 15:28:20 +0100

    Apply Spotless to documentation samples

--- documentation/src/test/java/example/StandardTests.java
+++ documentation/src/test/java/example/StandardTests.java
@@ -1,5 +1,14 @@
-package example;
+/*
+ * Copyright 2015-2016 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 
+package example;
 
 // tag::user_guide[]
 import org.junit.gen5.api.*;
@@ -7,19 +16,24 @@ import org.junit.gen5.api.*;
 class StandardTests {
 
 	@BeforeAll
-	static void initAll() {}
+	static void initAll() {
+	}
 
 	@BeforeEach
-	void init() {}
+	void init() {
+	}
 
 	@Test
-	void succeedingTest() {}
+	void succeedingTest() {
+	}
 
 	@AfterEach
-	void tearDown() {}
+	void tearDown() {
+	}
 
 	@AfterAll
-	static void tearDownAll() {}
+	static void tearDownAll() {
+	}
 
 }
 // end::user_guide[]
