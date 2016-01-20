6fef670e39632f3cac413b0b83b161dffde081a3
Johannes Link <business@johanneslink.net>
2016-01-20 15:11:59 +0100

    Replaced JUnit5Launcher with Launcher in some JavaDoc

--- junit-launcher/src/main/java/org/junit/gen5/launcher/listeners/package-info.java
+++ junit-launcher/src/main/java/org/junit/gen5/launcher/listeners/package-info.java
@@ -1,7 +1,7 @@
 /**
  * Common {@link org.junit.gen5.launcher.TestExecutionListener
  * TestExecutionListener} implementations and related support classes for
- * the JUnit {@link org.junit.gen5.launcher.main.JUnit5Launcher Launcher}.
+ * the JUnit {@link org.junit.gen5.launcher.Launcher Launcher}.
  */
 
 package org.junit.gen5.launcher.listeners;
