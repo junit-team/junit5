b4819111b70b9c5da364500276308d020f5b2c75
Stefan Bechtold <stefan.bechtold@me.com>
2016-01-21 16:52:24 +0100

    Extended JavaDoc for the TestResolver interface

--- junit5-engine/src/main/java/org/junit/gen5/engine/junit5/resolver/TestResolver.java
+++ junit5-engine/src/main/java/org/junit/gen5/engine/junit5/resolver/TestResolver.java
@@ -12,16 +12,30 @@ package org.junit.gen5.engine.junit5.resolver;
 
 import java.util.Optional;
 
+import org.junit.gen5.engine.DiscoveryFilter;
 import org.junit.gen5.engine.DiscoverySelector;
 import org.junit.gen5.engine.EngineDiscoveryRequest;
 import org.junit.gen5.engine.TestDescriptor;
 
 /**
- * A {@link TestResolver} is responsible for resolving different kind of test representatives. Each resolver can
- * contribute to the list of children of the given parent. It may only resolve children, that accomplish the
- * {@link EngineDiscoveryRequest}. The children are returned as list.
+ * A {@code TestResolver} is responsible for resolving different kind of test
+ * representatives. Each resolver can contribute to the list of children of
+ * the given parent.
+ *
+ * <p>A {@code TestResolver} must not modify existing tree nodes! It is only
+ * allowed to contribute new {@link TestDescriptor}. It is possible to attach
+ * new {@link TestDescriptor} by calling {@link TestDescriptor#addChild} on
+ * the parent.
+ *
+ * <p>{@code TestResolver} may only resolve children, that accomplish the
+ * {@link EngineDiscoveryRequest}. In this manner, each resolver may allow
+ * to add {@link DiscoveryFilter} to the {@link EngineDiscoveryRequest}.
+ * If it does, it must guarantee, that these filters are applied during test
+ * discovery.
  *
  * @since 5.0
+ * @see TestDescriptor
+ * @see DiscoveryFilter
  */
 public interface TestResolver {
 	void bindTestResolveryRegistry(TestResolverRegistry testResolverRegistry);
