15e458e0a307a2dbe68645d5e17d8d3f338ef77d
Sam Brannen <sam@sambrannen.com>
2016-01-21 18:18:35 +0100

    Document TagFilter

--- junit-launcher/src/main/java/org/junit/gen5/launcher/TagFilter.java
+++ junit-launcher/src/main/java/org/junit/gen5/launcher/TagFilter.java
@@ -17,30 +17,68 @@ import java.util.List;
 import org.junit.gen5.engine.FilterResult;
 import org.junit.gen5.engine.TestTag;
 
-public class TagFilter {
-	public static PostDiscoveryFilter includeTags(String... tagNames) {
-		return includeTags(asList(tagNames));
+/**
+ * Factory methods for creating {@link PostDiscoveryFilter PostDiscoveryFilters}
+ * based on <em>include</em> and <em>exclude</em> tags.
+ *
+ * @since 5.0
+ */
+public final class TagFilter {
+
+	private TagFilter() {
+		/* no-op */
 	}
 
-	public static PostDiscoveryFilter includeTags(List<String> includeTags) {
+	/**
+	 * Create an <em>include</em> filter based on the supplied {@code tags}.
+	 *
+	 * <p>Containers and tests will only be executed if they are tagged with
+	 * at least one of the supplied <em>include</em> tags.
+	 */
+	public static PostDiscoveryFilter includeTags(String... tags) {
+		return includeTags(asList(tags));
+	}
+
+	/**
+	 * Create an <em>include</em> filter based on the supplied {@code tags}.
+	 *
+	 * <p>Containers and tests will only be executed if they are tagged with
+	 * at least one of the supplied <em>include</em> tags.
+	 */
+	public static PostDiscoveryFilter includeTags(List<String> tags) {
 		// @formatter:off
         return descriptor -> FilterResult.includedIf(
 				descriptor.getTags().stream()
 					.map(TestTag::getName)
-					.anyMatch(includeTags::contains));
+					.map(String::trim)
+					.anyMatch(tags::contains));
         // @formatter:on
 	}
 
-	public static PostDiscoveryFilter excludeTags(String... tagNames) {
-		return excludeTags(asList(tagNames));
+	/**
+	 * Create an <em>exclude</em> filter based on the supplied {@code tags}.
+	 *
+	 * <p>Containers and tests will only be executed if they are <em>not</em>
+	 * tagged with any of the supplied <em>exclude</em> tags.
+	 */
+	public static PostDiscoveryFilter excludeTags(String... tags) {
+		return excludeTags(asList(tags));
 	}
 
-	public static PostDiscoveryFilter excludeTags(List<String> excludeTags) {
+	/**
+	 * Create an <em>exclude</em> filter based on the supplied {@code tags}.
+	 *
+	 * <p>Containers and tests will only be executed if they are <em>not</em>
+	 * tagged with any of the supplied <em>exclude</em> tags.
+	 */
+	public static PostDiscoveryFilter excludeTags(List<String> tags) {
 		// @formatter:off
         return descriptor -> FilterResult.includedIf(
 				descriptor.getTags().stream()
-                	.map(TestTag::getName)
-					.noneMatch(excludeTags::contains));
+					.map(TestTag::getName)
+					.map(String::trim)
+					.noneMatch(tags::contains));
         // @formatter:on
 	}
+
 }
