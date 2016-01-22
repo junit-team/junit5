3c759c88772af3f0a94c8de22525b0fb589d581f
Marc Philipp <mail@marcphilipp.de>
2016-01-22 21:44:30 +0100

    Test that TestIdentifiers are converted into Descriptions
    Issue: #101

--- junit-tests/src/test/java/org/junit/gen5/junit4/runner/JUnit5IntegratedTests.java
+++ junit-tests/src/test/java/org/junit/gen5/junit4/runner/JUnit5IntegratedTests.java
@@ -10,13 +10,19 @@
 
 package org.junit.gen5.junit4.runner;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
+import static org.junit.gen5.api.Assertions.assertEquals;
 import static org.junit.gen5.api.Assumptions.assumeFalse;
+import static org.junit.gen5.commons.util.CollectionUtils.getOnlyElement;
 import static org.junit.gen5.launcher.main.LauncherFactoryForTestingPurposesOnly.createLauncher;
+import static org.junit.runner.Description.createSuiteDescription;
 import static org.junit.runner.Description.createTestDescription;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.mock;
+
+import java.util.List;
 
 import org.junit.gen5.api.Test;
 import org.junit.gen5.engine.support.hierarchical.DummyTestEngine;
@@ -26,7 +32,27 @@ import org.junit.runner.notification.RunListener;
 import org.junit.runner.notification.RunNotifier;
 import org.mockito.InOrder;
 
-class JUnit5IntegratedExecutionTests {
+class JUnit5IntegratedTests {
+
+	@Test
+	void convertsTestIdentifiersIntoDescriptions() throws Exception {
+		DummyTestEngine engine = new DummyTestEngine("dummy");
+		engine.addTest("firstTest", () -> fail("expected to fail"));
+		engine.addTest("secondTest", () -> assumeFalse(true));
+
+		JUnit5 runner = new JUnit5(TestClass.class, createLauncher(engine));
+
+		Description runnerDescription = runner.getDescription();
+		assertEquals(createSuiteDescription(TestClass.class), runnerDescription);
+
+		Description engineDescription = getOnlyElement(runnerDescription.getChildren());
+		assertEquals(createSuiteDescription("dummy", new TestId("dummy")), engineDescription);
+
+		List<Description> testDescriptions = engineDescription.getChildren();
+		assertThat(testDescriptions).hasSize(2);
+		assertEquals(testDescription("dummy", "firstTest"), testDescriptions.get(0));
+		assertEquals(testDescription("dummy", "secondTest"), testDescriptions.get(1));
+	}
 
 	@Test
 	void notifiesRunListenerOfTestExecution() throws Exception {
@@ -41,7 +67,7 @@ class JUnit5IntegratedExecutionTests {
 
 		RunNotifier notifier = new RunNotifier();
 		notifier.addListener(runListener);
-		new JUnit5(Object.class, createLauncher(engine)).run(notifier);
+		new JUnit5(TestClass.class, createLauncher(engine)).run(notifier);
 
 		InOrder inOrder = inOrder(runListener);
 
@@ -63,6 +89,9 @@ class JUnit5IntegratedExecutionTests {
 
 	private static Description testDescription(String engineId, String testName) {
 		return createTestDescription(engineId, testName, new TestId(engineId + ":" + testName));
+	}
+
+	private static class TestClass {
 	}
 
 }
