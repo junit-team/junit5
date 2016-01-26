a9ad2088fa8fd42b1fb3557ac9707264a9b9b5f8
Marc Philipp <mail@marcphilipp.de>
2016-01-26 09:44:50 +0100

    Serialize Unique ID if it's not a String or a Number
    Issue: #40

--- junit4-engine/src/main/java/org/junit/gen5/engine/junit4/discovery/UniqueIdStringifier.java
+++ junit4-engine/src/main/java/org/junit/gen5/engine/junit4/discovery/UniqueIdStringifier.java
@@ -10,15 +10,45 @@
 
 package org.junit.gen5.engine.junit4.discovery;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.text.NumberFormat;
+import java.util.Base64;
+import java.util.Locale;
 import java.util.function.Function;
 
 class UniqueIdStringifier implements Function<Serializable, String> {
 
+	static final Charset CHARSET = StandardCharsets.UTF_8;
+
 	@Override
 	public String apply(Serializable uniqueId) {
-		// TODO #40 only use toString() for known classes; otherwise, serialize
-		return String.valueOf(uniqueId);
+		if (uniqueId instanceof CharSequence) {
+			return uniqueId.toString();
+		}
+		if (uniqueId instanceof Number) {
+			return NumberFormat.getInstance(Locale.US).format(uniqueId);
+		}
+		return encodeBase64(serialize(uniqueId));
+	}
+
+	private byte[] serialize(Serializable uniqueId) {
+		ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+		try (ObjectOutputStream out = new ObjectOutputStream(byteStream)) {
+			out.writeObject(uniqueId);
+		}
+		catch (IOException e) {
+			return uniqueId.toString().getBytes(CHARSET);
+		}
+		return byteStream.toByteArray();
+	}
+
+	private String encodeBase64(byte[] bytes) {
+		return new String(Base64.getEncoder().encode(bytes), CHARSET);
 	}
 
 }
