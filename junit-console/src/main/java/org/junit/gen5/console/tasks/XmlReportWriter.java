37cd4daf9f853ef156e1f99aa91301c562d5a5cf
Marc Philipp <mail@marcphilipp.de>
2016-01-27 14:57:35 +0100

    Extract XML writing into separate class
    Issue: #86

---
+++ junit-console/src/main/java/org/junit/gen5/console/tasks/XmlReportWriter.java
@@ -0,0 +1,169 @@
+/*
+ * Copyright 2015-2016 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.junit.gen5.console.tasks;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.stream.Collectors.toList;
+import static org.junit.gen5.commons.util.StringUtils.isNotBlank;
+import static org.junit.gen5.engine.TestExecutionResult.Status.FAILED;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.text.NumberFormat;
+import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
+
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+
+import org.junit.gen5.engine.TestExecutionResult;
+import org.junit.gen5.launcher.TestIdentifier;
+
+class XmlReportWriter {
+
+	private final XmlReportData reportData;
+
+	XmlReportWriter(XmlReportData reportData) {
+		this.reportData = reportData;
+	}
+
+	void writeXmlReport(TestIdentifier testIdentifier, File xmlFile) throws IOException, XMLStreamException {
+		// TODO #86 consider skipped/failed/aborted containers
+		// @formatter:off
+		List<TestIdentifier> tests = reportData.getTestPlan().getDescendants(testIdentifier)
+				.stream()
+				.filter(TestIdentifier::isTest)
+				.collect(toList());
+		// @formatter:on
+		if (!tests.isEmpty()) {
+			writeXmlReport(testIdentifier, tests, xmlFile);
+		}
+	}
+
+	private void writeXmlReport(TestIdentifier testIdentifier, List<TestIdentifier> tests, File xmlFile)
+			throws IOException, XMLStreamException {
+		try (Writer fileWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(xmlFile), UTF_8))) {
+			XMLOutputFactory factory = XMLOutputFactory.newInstance();
+			XMLStreamWriter xmlWriter = factory.createXMLStreamWriter(fileWriter);
+			xmlWriter.writeStartDocument();
+			writeTestsuite(testIdentifier, tests, xmlWriter);
+			xmlWriter.writeEndDocument();
+			xmlWriter.flush();
+			xmlWriter.close();
+		}
+	}
+
+	private void writeTestsuite(TestIdentifier testIdentifier, List<TestIdentifier> tests, XMLStreamWriter writer)
+			throws XMLStreamException {
+		writer.writeStartElement("testsuite");
+
+		long skipped = 0;
+		long failures = 0;
+		for (TestIdentifier test : tests) {
+			if (reportData.wasSkipped(test)) {
+				skipped++;
+			}
+			else if (reportData.wasFinished(test)) {
+				TestExecutionResult result = reportData.getResult(test);
+				if (result.getStatus() == FAILED) {
+					failures++;
+				}
+			}
+		}
+
+		writer.writeAttribute("name", testIdentifier.getDisplayName());
+		writer.writeAttribute("tests", String.valueOf(tests.size()));
+		// TODO #86 compute error count
+		writer.writeAttribute("skipped", String.valueOf(skipped));
+		writer.writeAttribute("failures", String.valueOf(failures));
+		writer.writeAttribute("errors", "0");
+
+		NumberFormat numberFormat = NumberFormat.getInstance(Locale.US);
+		writer.writeAttribute("time", numberFormat.format(reportData.getDurationInSeconds(testIdentifier)));
+
+		writer.writeComment("Unique ID: " + testIdentifier.getUniqueId().toString());
+
+		for (TestIdentifier test : tests) {
+			writeTestcase(test, numberFormat, writer);
+		}
+
+		writer.writeEndElement();
+	}
+
+	private void writeTestcase(TestIdentifier test, NumberFormat numberFormat, XMLStreamWriter writer)
+			throws XMLStreamException {
+		writer.writeStartElement("testcase");
+		writer.writeAttribute("name", test.getDisplayName());
+		Optional<TestIdentifier> parent = reportData.getTestPlan().getParent(test);
+		if (parent.isPresent()) {
+			writer.writeAttribute("classname", parent.get().getName());
+		}
+		writer.writeAttribute("time", numberFormat.format(reportData.getDurationInSeconds(test)));
+		// TODO #86 write error elements
+		writer.writeComment("Unique ID: " + test.getUniqueId().toString());
+		if (reportData.wasSkipped(test)) {
+			writeSkipped(reportData.getSkipReason(test), writer);
+		}
+		else if (reportData.wasFinished(test)) {
+			TestExecutionResult result = reportData.getResult(test);
+			if (result.getStatus() == FAILED) {
+				writeFailure(result.getThrowable(), writer);
+			}
+		}
+		writer.writeEndElement();
+	}
+
+	private void writeSkipped(String reason, XMLStreamWriter writer) throws XMLStreamException {
+		if (isNotBlank(reason)) {
+			writer.writeStartElement("skipped");
+			writer.writeCharacters(reason);
+			writer.writeEndElement();
+		}
+		else {
+			writer.writeEmptyElement("skipped");
+		}
+	}
+
+	private void writeFailure(Optional<Throwable> throwable, XMLStreamWriter writer) throws XMLStreamException {
+		writer.writeStartElement("failure");
+		if (throwable.isPresent()) {
+			writeFailureAttributesAndContent(throwable.get(), writer);
+		}
+		writer.writeEndElement();
+	}
+
+	private void writeFailureAttributesAndContent(Throwable throwable, XMLStreamWriter writer)
+			throws XMLStreamException {
+		if (throwable.getMessage() != null) {
+			writer.writeAttribute("message", throwable.getMessage());
+		}
+		writer.writeAttribute("type", throwable.getClass().getName());
+		writer.writeCharacters(readStackTrace(throwable));
+	}
+
+	// TODO #86 Move to ExceptionUtils
+	private String readStackTrace(Throwable throwable) {
+		StringWriter stringWriter = new StringWriter();
+		try (PrintWriter printWriter = new PrintWriter(stringWriter)) {
+			throwable.printStackTrace(printWriter);
+		}
+		return stringWriter.toString();
+	}
+
+}
--- junit-console/src/main/java/org/junit/gen5/console/tasks/XmlReportsWritingListener.java
+++ junit-console/src/main/java/org/junit/gen5/console/tasks/XmlReportsWritingListener.java
@@ -10,29 +10,13 @@
 
 package org.junit.gen5.console.tasks;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
-import static java.util.stream.Collectors.toList;
-import static org.junit.gen5.commons.util.StringUtils.isNotBlank;
-import static org.junit.gen5.engine.TestExecutionResult.Status.FAILED;
-
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.io.Writer;
 import java.nio.file.Files;
-import java.text.NumberFormat;
 import java.time.Clock;
-import java.util.List;
-import java.util.Locale;
-import java.util.Optional;
 
-import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamWriter;
 
 import org.junit.gen5.engine.TestExecutionResult;
 import org.junit.gen5.launcher.TestExecutionListener;
@@ -65,8 +49,8 @@ class XmlReportsWritingListener implements TestExecutionListener {
 			Files.createDirectories(reportsDir.toPath());
 		}
 		catch (IOException e) {
-			out.println("Could not create report directory: " + reportsDir);
-			e.printStackTrace(out);
+			String message = "Could not create report directory: " + reportsDir;
+			handleException(e, message);
 		}
 	}
 
@@ -85,10 +69,18 @@ class XmlReportsWritingListener implements TestExecutionListener {
 	public void executionFinished(TestIdentifier testIdentifier, TestExecutionResult result) {
 		reportData.markFinished(testIdentifier, result);
 		if (isARoot(testIdentifier)) {
-			// TODO #86 consider skipped/failed/aborted containers
-			// @formatter:off
-			File xmlFile = new File(reportsDir, "TEST-" + testIdentifier.getUniqueId() + ".xml");
-			writeXmlReport(testIdentifier, xmlFile);
+			writeXmlReportSafely(testIdentifier);
+		}
+	}
+
+	private void writeXmlReportSafely(TestIdentifier testIdentifier) {
+		File xmlFile = new File(reportsDir, "TEST-" + testIdentifier.getUniqueId() + ".xml");
+		try {
+			new XmlReportWriter(reportData).writeXmlReport(testIdentifier, xmlFile);
+		}
+		catch (XMLStreamException | IOException e) {
+			String message = "Could not write file: " + xmlFile;
+			handleException(e, message);
 		}
 	}
 
@@ -96,127 +88,9 @@ class XmlReportsWritingListener implements TestExecutionListener {
 		return !testIdentifier.getParentId().isPresent();
 	}
 
-	private void writeXmlReport(TestIdentifier testIdentifier, File xmlFile) {
-		List<TestIdentifier> tests = reportData.getTestPlan().getDescendants(testIdentifier).stream()
-				.filter(TestIdentifier::isTest)
-				.collect(toList());
-		// @formatter:on
-		if (!tests.isEmpty()) {
-			writeXmlReport(testIdentifier, tests, xmlFile);
-		}
-	}
-
-	private void writeXmlReport(TestIdentifier testIdentifier, List<TestIdentifier> tests, File xmlFile) {
-		try (Writer fileWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(xmlFile), UTF_8))) {
-			XMLOutputFactory factory = XMLOutputFactory.newInstance();
-			XMLStreamWriter xmlWriter = factory.createXMLStreamWriter(fileWriter);
-			xmlWriter.writeStartDocument();
-			writeTestsuite(testIdentifier, tests, xmlWriter);
-			xmlWriter.writeEndDocument();
-			xmlWriter.flush();
-			xmlWriter.close();
-		}
-		catch (XMLStreamException | IOException e) {
-			out.println("Could not write file: " + xmlFile);
-			e.printStackTrace(out);
-		}
-	}
-
-	private void writeTestsuite(TestIdentifier testIdentifier, List<TestIdentifier> tests, XMLStreamWriter writer)
-			throws XMLStreamException {
-		writer.writeStartElement("testsuite");
-
-		long skipped = 0;
-		long failures = 0;
-		for (TestIdentifier test : tests) {
-			if (reportData.wasSkipped(test)) {
-				skipped++;
-			}
-			else if (reportData.wasFinished(test)) {
-				TestExecutionResult result = reportData.getResult(test);
-				if (result.getStatus() == FAILED) {
-					failures++;
-				}
-			}
-		}
-
-		writer.writeAttribute("name", testIdentifier.getDisplayName());
-		writer.writeAttribute("tests", String.valueOf(tests.size()));
-		// TODO #86 compute error count
-		writer.writeAttribute("skipped", String.valueOf(skipped));
-		writer.writeAttribute("failures", String.valueOf(failures));
-		writer.writeAttribute("errors", "0");
-
-		NumberFormat numberFormat = NumberFormat.getInstance(Locale.US);
-		writer.writeAttribute("time", numberFormat.format(reportData.getDurationInSeconds(testIdentifier)));
-
-		writer.writeComment("Unique ID: " + testIdentifier.getUniqueId().toString());
-
-		for (TestIdentifier test : tests) {
-			writeTestcase(test, numberFormat, writer);
-		}
-
-		writer.writeEndElement();
-	}
-
-	private void writeTestcase(TestIdentifier test, NumberFormat numberFormat, XMLStreamWriter writer)
-			throws XMLStreamException {
-		writer.writeStartElement("testcase");
-		writer.writeAttribute("name", test.getDisplayName());
-		Optional<TestIdentifier> parent = reportData.getTestPlan().getParent(test);
-		if (parent.isPresent()) {
-			writer.writeAttribute("classname", parent.get().getName());
-		}
-		writer.writeAttribute("time", numberFormat.format(reportData.getDurationInSeconds(test)));
-		// TODO #86 write error elements
-		writer.writeComment("Unique ID: " + test.getUniqueId().toString());
-		if (reportData.wasSkipped(test)) {
-			writeSkipped(reportData.getSkipReason(test), writer);
-		}
-		else if (reportData.wasFinished(test)) {
-			TestExecutionResult result = reportData.getResult(test);
-			if (result.getStatus() == FAILED) {
-				writeFailure(result.getThrowable(), writer);
-			}
-		}
-		writer.writeEndElement();
-	}
-
-	private void writeSkipped(String reason, XMLStreamWriter writer) throws XMLStreamException {
-		if (isNotBlank(reason)) {
-			writer.writeStartElement("skipped");
-			writer.writeCharacters(reason);
-			writer.writeEndElement();
-		}
-		else {
-			writer.writeEmptyElement("skipped");
-		}
-	}
-
-	private void writeFailure(Optional<Throwable> throwable, XMLStreamWriter writer) throws XMLStreamException {
-		writer.writeStartElement("failure");
-		if (throwable.isPresent()) {
-			writeFailureAttributesAndContent(throwable.get(), writer);
-		}
-		writer.writeEndElement();
-	}
-
-	private void writeFailureAttributesAndContent(Throwable throwable, XMLStreamWriter writer)
-			throws XMLStreamException {
-		if (throwable.getMessage() != null) {
-			writer.writeAttribute("message", throwable.getMessage());
-		}
-		writer.writeAttribute("type", throwable.getClass().getName());
-		writer.writeCharacters(readStackTrace(throwable));
-	}
-
-	// TODO #86 Move to ExceptionUtils
-	private String readStackTrace(Throwable throwable) {
-		StringWriter stringWriter = new StringWriter();
-		try (PrintWriter printWriter = new PrintWriter(stringWriter)) {
-			throwable.printStackTrace(printWriter);
-		}
-		return stringWriter.toString();
+	private void handleException(Exception exception, String message) {
+		out.println(message);
+		exception.printStackTrace(out);
 	}
 
 }
