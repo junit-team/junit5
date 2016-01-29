956cf22e07364722c0c6d0facf78f07f0b825113
Marc Philipp <mail@marcphilipp.de>
2016-01-29 09:29:52 +0100

    Add tests for edge cases of XmlReportData
    Issue: #86

---
+++ junit-tests/src/test/java/org/junit/gen5/console/tasks/XmlReportDataTests.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2015-2016 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.junit.gen5.console.tasks;
+
+import static java.util.Collections.singleton;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.gen5.engine.TestExecutionResult.failed;
+import static org.junit.gen5.engine.TestExecutionResult.successful;
+
+import java.time.Clock;
+import java.util.Optional;
+
+import org.junit.gen5.api.Test;
+import org.junit.gen5.engine.TestDescriptorStub;
+import org.junit.gen5.engine.TestExecutionResult;
+import org.junit.gen5.engine.support.descriptor.EngineDescriptor;
+import org.junit.gen5.launcher.TestId;
+import org.junit.gen5.launcher.TestPlan;
+
+class XmlReportDataTests {
+
+	@Test
+	void resultOfTestIdentifierWithoutAnyReportedEventsIsEmpty() {
+		EngineDescriptor engineDescriptor = new EngineDescriptor("engine", "Engine");
+		engineDescriptor.addChild(new TestDescriptorStub("test"));
+		TestPlan testPlan = TestPlan.from(singleton(engineDescriptor));
+
+		XmlReportData reportData = new XmlReportData(testPlan, Clock.systemDefaultZone());
+		Optional<TestExecutionResult> result = reportData.getResult(testPlan.getTestIdentifier(new TestId("test")));
+
+		assertThat(result).isEmpty();
+	}
+
+	@Test
+	void resultOfTestIdentifierWithoutReportedEventsIsFailureOfAncestor() {
+		EngineDescriptor engineDescriptor = new EngineDescriptor("engine", "Engine");
+		engineDescriptor.addChild(new TestDescriptorStub("test"));
+		TestPlan testPlan = TestPlan.from(singleton(engineDescriptor));
+
+		XmlReportData reportData = new XmlReportData(testPlan, Clock.systemDefaultZone());
+		TestExecutionResult failureOfAncestor = failed(new RuntimeException("failed!"));
+		reportData.markFinished(testPlan.getTestIdentifier(new TestId("engine")), failureOfAncestor);
+
+		Optional<TestExecutionResult> result = reportData.getResult(testPlan.getTestIdentifier(new TestId("test")));
+
+		assertThat(result).contains(failureOfAncestor);
+	}
+
+	@Test
+	void resultOfTestIdentifierWithoutReportedEventsIsEmptyWhenAncestorWasSuccessful() {
+		EngineDescriptor engineDescriptor = new EngineDescriptor("engine", "Engine");
+		engineDescriptor.addChild(new TestDescriptorStub("test"));
+		TestPlan testPlan = TestPlan.from(singleton(engineDescriptor));
+
+		XmlReportData reportData = new XmlReportData(testPlan, Clock.systemDefaultZone());
+		reportData.markFinished(testPlan.getTestIdentifier(new TestId("engine")), successful());
+
+		Optional<TestExecutionResult> result = reportData.getResult(testPlan.getTestIdentifier(new TestId("test")));
+
+		assertThat(result).isEmpty();
+	}
+}
