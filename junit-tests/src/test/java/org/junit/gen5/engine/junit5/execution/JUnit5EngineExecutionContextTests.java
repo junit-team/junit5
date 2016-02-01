491f35e6cab788c83ed1f4f0316bc60ada8680b1
Johannes Link <business@johanneslink.net>
2016-02-01 09:22:27 +0100

    Added tests to JUnit5EngineExecutionContextTests

--- junit-tests/src/test/java/org/junit/gen5/engine/junit5/execution/JUnit5EngineExecutionContextTests.java
+++ junit-tests/src/test/java/org/junit/gen5/engine/junit5/execution/JUnit5EngineExecutionContextTests.java
@@ -15,8 +15,13 @@ import static org.junit.gen5.api.Assertions.assertSame;
 import org.junit.gen5.api.BeforeEach;
 import org.junit.gen5.api.Test;
 import org.junit.gen5.engine.EngineExecutionListener;
+import org.junit.gen5.engine.junit5.descriptor.ClassBasedContainerExtensionContext;
+import org.junit.gen5.engine.junit5.extension.ExtensionRegistry;
 import org.mockito.Mockito;
 
+/**
+ * Microtests for {@link JUnit5EngineExecutionContext}
+ */
 class JUnit5EngineExecutionContextTests {
 
 	private JUnit5EngineExecutionContext originalContext;
@@ -35,4 +40,43 @@ class JUnit5EngineExecutionContextTests {
 		assertSame(engineExecutionListener, newContext.getExecutionListener());
 	}
 
+	@Test
+	void extendWithAllAttributes() {
+		ClassBasedContainerExtensionContext extensionContext = new ClassBasedContainerExtensionContext(null, null,
+			null);
+		ExtensionRegistry extensionRegistry = new ExtensionRegistry();
+		TestInstanceProvider testInstanceProvider = Mockito.mock(TestInstanceProvider.class);
+		JUnit5EngineExecutionContext newContext = originalContext.extend() //
+		.withExtensionContext(extensionContext) //
+		.withExtensionRegistry(extensionRegistry) //
+		.withTestInstanceProvider(testInstanceProvider) //
+		.build();
+
+		assertSame(extensionContext, newContext.getExtensionContext());
+		assertSame(extensionRegistry, newContext.getExtensionRegistry());
+		assertSame(testInstanceProvider, newContext.getTestInstanceProvider());
+	}
+
+	@Test
+	void canOverrideAttributeWhenContextIsExtended() {
+		ClassBasedContainerExtensionContext extensionContext = new ClassBasedContainerExtensionContext(null, null,
+			null);
+		ExtensionRegistry extensionRegistry = new ExtensionRegistry();
+		TestInstanceProvider testInstanceProvider = Mockito.mock(TestInstanceProvider.class);
+		ClassBasedContainerExtensionContext newExtensionContext = new ClassBasedContainerExtensionContext(
+			extensionContext, null, null);
+
+		JUnit5EngineExecutionContext newContext = originalContext.extend() //
+		.withExtensionContext(extensionContext) //
+		.withExtensionRegistry(extensionRegistry) //
+		.withTestInstanceProvider(testInstanceProvider) //
+		.build() //
+		.extend() //
+		.withExtensionContext(newExtensionContext) //
+		.build();
+
+		assertSame(newExtensionContext, newContext.getExtensionContext());
+		assertSame(extensionRegistry, newContext.getExtensionRegistry());
+		assertSame(testInstanceProvider, newContext.getTestInstanceProvider());
+	}
 }
