d6e6287b789a59648cc13120b3fb5f5e1788d197
Johannes Link <business@johanneslink.net>
2016-03-09 20:08:26 +0100

    Fixed compile error after rebase with issue106-unique-id

--- junit5-engine/src/main/java/org/junit/gen5/engine/junit5/discoveryNEW/DiscoverySelectorResolver.java
+++ junit5-engine/src/main/java/org/junit/gen5/engine/junit5/discoveryNEW/DiscoverySelectorResolver.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2015-2016 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 
 package org.junit.gen5.engine.junit5.discoveryNEW;
 
