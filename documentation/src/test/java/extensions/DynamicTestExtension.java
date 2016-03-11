7e51ea615931c2e99132e4b03f0f42a4c148146b
Arne Burmeister <arne@burmeister-teltow.de>
2016-03-11 23:41:36 +0100

    #58 added documentation

--- documentation/src/test/java/extensions/DynamicTestExtension.java
+++ documentation/src/test/java/extensions/DynamicTestExtension.java
@@ -10,8 +10,6 @@
 
 package extensions;
 
-import static org.junit.gen5.commons.util.ReflectionUtils.invokeMethod;
-
 import java.util.stream.Stream;
 
 import org.junit.gen5.api.DynamicTest;
@@ -19,24 +17,27 @@ import org.junit.gen5.api.Executable;
 import org.junit.gen5.api.extension.DynamicTestCreator;
 import org.junit.gen5.api.extension.ExtensionContext;
 import org.junit.gen5.api.extension.MethodInvocationContext;
-import org.junit.gen5.commons.JUnitException;
+import org.junit.gen5.commons.util.ReflectionUtils;
 
+// @formatter:off
+// tag::user_guide[]
 public class DynamicTestExtension implements DynamicTestCreator {
 	@Override
-	public boolean supports(MethodInvocationContext methodInvocationContext, ExtensionContext extensionContext)
-			throws JUnitException {
-		Class<?>[] parameterTypes = methodInvocationContext.getMethod().getParameterTypes();
+	public boolean supports(MethodInvocationContext context, ExtensionContext extension) {
+		Class<?>[] parameterTypes = context.getMethod().getParameterTypes();
 		return parameterTypes.length == 1 && parameterTypes[0] == Boolean.TYPE;
 	}
 
 	@Override
-	public Stream<DynamicTest> replace(MethodInvocationContext methodInvocationContext,
-			ExtensionContext extensionContext) throws JUnitException {
-		return Stream.of(new DynamicTest("extensionTest", testExecutable(methodInvocationContext, true)),
-			new DynamicTest("extensionTest", testExecutable(methodInvocationContext, false)));
+	public Stream<DynamicTest> replace(MethodInvocationContext context, ExtensionContext extension) {
+		return Stream.of(
+				new DynamicTest("extensionTest", testExecutable(context, true)),
+				new DynamicTest("extensionTest", testExecutable(context, false)));
 	}
 
 	private Executable testExecutable(MethodInvocationContext context, boolean parameter) {
-		return () -> invokeMethod(context.getMethod(), context.getInstance(), parameter);
+		return () -> ReflectionUtils.invokeMethod(context.getMethod(), context.getInstance(), parameter);
 	}
 }
+// end::user_guide[]
+// @formatter:on
