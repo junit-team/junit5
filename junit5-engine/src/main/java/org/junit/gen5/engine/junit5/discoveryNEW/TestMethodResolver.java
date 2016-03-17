f83a1839e0aeece31b66bafac8e7e1f264315237
Johannes Link <business@johanneslink.net>
2016-03-17 11:42:55 +0100

    Can resolve individual unique ID of test method

--- junit5-engine/src/main/java/org/junit/gen5/engine/junit5/discoveryNEW/TestMethodResolver.java
+++ junit5-engine/src/main/java/org/junit/gen5/engine/junit5/discoveryNEW/TestMethodResolver.java
@@ -13,6 +13,7 @@ package org.junit.gen5.engine.junit5.discoveryNEW;
 import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.Method;
 import java.util.Collections;
+import java.util.Optional;
 import java.util.Set;
 
 import org.junit.gen5.engine.TestDescriptor;
@@ -20,8 +21,11 @@ import org.junit.gen5.engine.UniqueId;
 import org.junit.gen5.engine.junit5.descriptor.ClassTestDescriptor;
 import org.junit.gen5.engine.junit5.descriptor.MethodTestDescriptor;
 import org.junit.gen5.engine.junit5.discovery.IsTestMethod;
+import org.junit.gen5.engine.junit5.discovery.MethodFinder;
 
 public class TestMethodResolver implements ElementResolver {
+
+	public static final String SEGMENT_TYPE = "method";
 
 	@Override
 	public boolean canResolveElement(AnnotatedElement element, TestDescriptor parent) {
@@ -30,13 +34,17 @@ public class TestMethodResolver implements ElementResolver {
 			return false;
 		if (!(parent instanceof ClassTestDescriptor))
 			return false;
-		return new IsTestMethod().test((Method) element);
+		return isTestMethod((Method) element);
+	}
+
+	private boolean isTestMethod(Method candidate) {
+		return new IsTestMethod().test(candidate);
 	}
 
 	@Override
 	public UniqueId createUniqueId(AnnotatedElement element, TestDescriptor parent) {
 		Method testMethod = (Method) element;
-		return parent.getUniqueId().append("method", testMethod.getName() + "()");
+		return parent.getUniqueId().append(SEGMENT_TYPE, testMethod.getName() + "()");
 	}
 
 	@Override
@@ -46,12 +54,25 @@ public class TestMethodResolver implements ElementResolver {
 
 	@Override
 	public boolean canResolveUniqueId(UniqueId.Segment segment, TestDescriptor parent) {
-		return false;
+		//Do not collapse
+		if (!segment.getType().equals(SEGMENT_TYPE))
+			return false;
+		if (!(parent instanceof ClassTestDescriptor))
+			return false;
+		Optional<Method> optionalMethod = findMethod(segment, (ClassTestDescriptor) parent);
+		if (!optionalMethod.isPresent())
+			return false;
+		return isTestMethod(optionalMethod.get());
+	}
+
+	private Optional<Method> findMethod(UniqueId.Segment segment, ClassTestDescriptor parent) {
+		return new MethodFinder().findMethod(segment.getValue(), parent.getTestClass());
 	}
 
 	@Override
 	public TestDescriptor resolve(UniqueId.Segment segment, TestDescriptor parent, UniqueId uniqueId) {
-		return null;
+		Optional<Method> optionalMethod = findMethod(segment, (ClassTestDescriptor) parent);
+		return resolveMethod(optionalMethod.get(), (ClassTestDescriptor) parent, uniqueId);
 	}
 
 	private TestDescriptor resolveMethod(Method testMethod, ClassTestDescriptor parentClassDescriptor,
