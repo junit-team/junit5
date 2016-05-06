e7952f6e91ef1f0f4d2ba597ed83442b272efba4
Matthias Merdes <Matthias.Merdes@heidelberg-mobil.com>
2016-05-06 23:56:26 +0200

    #58 spotless formatting and unused imports cleanup

--- junit-tests/src/test/java/org/junit/gen5/engine/junit5/discovery/DiscoverySelectorResolverTests.java
+++ junit-tests/src/test/java/org/junit/gen5/engine/junit5/discovery/DiscoverySelectorResolverTests.java
@@ -12,7 +12,6 @@ package org.junit.gen5.engine.junit5.discovery;
 
 import static org.junit.gen5.api.Assertions.*;
 import static org.junit.gen5.engine.junit5.discovery.JUnit5UniqueIdBuilder.*;
-import static org.junit.gen5.api.Assertions.*;
 import static org.junit.gen5.launcher.main.TestDiscoveryRequestBuilder.request;
 
 import java.io.File;
