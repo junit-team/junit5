0161bf20d055ac2bd2e15a20996379cbc2811c52
Stefan Bechtold <stefan.bechtold@me.com>
2016-05-13 12:51:11 +0200

    Polish: Split tests into nested hierarchies for better readability  #233

--- junit-tests/src/test/java/org/junit/gen5/launcher/main/TestDiscoveryRequestBuilderTests.java
+++ junit-tests/src/test/java/org/junit/gen5/launcher/main/TestDiscoveryRequestBuilderTests.java
@@ -27,6 +27,7 @@ import java.lang.reflect.Method;
 import java.util.List;
 
 import org.assertj.core.util.Files;
+import org.junit.gen5.api.Nested;
 import org.junit.gen5.api.Test;
 import org.junit.gen5.commons.util.PreconditionViolationException;
 import org.junit.gen5.engine.DiscoveryFilter;
@@ -46,185 +47,191 @@ import org.junit.gen5.launcher.TestDiscoveryRequest;
  */
 public class TestDiscoveryRequestBuilderTests {
 
-	@Test
-	public void packagesAreStoredInDiscoveryRequest() throws Exception {
-		// @formatter:off
-		TestDiscoveryRequest discoveryRequest = request()
-				.select(
-						forPackageName("org.junit.gen5.engine")
-				).build();
-		// @formatter:on
-
-		List<String> packageSelectors = discoveryRequest.getSelectorsByType(PackageSelector.class).stream().map(
-			PackageSelector::getPackageName).collect(toList());
-		assertThat(packageSelectors).contains("org.junit.gen5.engine");
-	}
-
-	@Test
-	public void classesAreStoredInDiscoveryRequest() throws Exception {
-		// @formatter:off
-		TestDiscoveryRequest discoveryRequest = request()
-				.select(
-						forClassName(TestDiscoveryRequestBuilderTests.class.getName()),
-						forClass(SampleTestClass.class)
-				)
-			.build();
-		// @formatter:on
-
-		List<Class<?>> classes = discoveryRequest.getSelectorsByType(ClassSelector.class).stream().map(
-			ClassSelector::getTestClass).collect(toList());
-		assertThat(classes).contains(SampleTestClass.class, TestDiscoveryRequestBuilderTests.class);
-	}
-
-	@Test
-	public void methodsByNameAreStoredInDiscoveryRequest() throws Exception {
-		Class<?> testClass = SampleTestClass.class;
-		Method testMethod = testClass.getMethod("test");
-
-		// @formatter:off
-		TestDiscoveryRequest discoveryRequest = request()
-				.select(forMethod(SampleTestClass.class.getName(), "test"))
-				.build();
-		// @formatter:on
-
-		List<MethodSelector> methodSelectors = discoveryRequest.getSelectorsByType(MethodSelector.class);
-		assertThat(methodSelectors).hasSize(1);
-
-		MethodSelector methodSelector = methodSelectors.get(0);
-		assertThat(methodSelector.getTestClass()).isEqualTo(testClass);
-		assertThat(methodSelector.getTestMethod()).isEqualTo(testMethod);
-	}
-
-	@Test
-	public void methodsByClassAreStoredInDiscoveryRequest() throws Exception {
-		Class<?> testClass = SampleTestClass.class;
-		Method testMethod = testClass.getMethod("test");
-
-		// @formatter:off
-		DiscoveryRequest discoveryRequest = (DiscoveryRequest) request()
-				.select(
-						MethodSelector.forMethod(SampleTestClass.class, "test")
-				).build();
-		// @formatter:on
-
-		List<MethodSelector> methodSelectors = discoveryRequest.getSelectorsByType(MethodSelector.class);
-		assertThat(methodSelectors).hasSize(1);
-
-		MethodSelector methodSelector = methodSelectors.get(0);
-		assertThat(methodSelector.getTestClass()).isEqualTo(testClass);
-		assertThat(methodSelector.getTestMethod()).isEqualTo(testMethod);
-	}
-
-	@Test
-	public void unavailableFoldersAreNotStoredInDiscoveryRequest() throws Exception {
-		// @formatter:off
-		TestDiscoveryRequest discoveryRequest = request()
-				.select(
-						ClasspathSelector.forPath("/some/local/path")
-				).build();
-		// @formatter:on
-
-		List<String> folders = discoveryRequest.getSelectorsByType(ClasspathSelector.class).stream().map(
-			ClasspathSelector::getClasspathRoot).map(File::getAbsolutePath).collect(toList());
-
-		assertThat(folders).isEmpty();
-	}
-
-	@Test
-	public void availableFoldersAreStoredInDiscoveryRequest() throws Exception {
-		File temporaryFolder = Files.newTemporaryFolder();
-		try {
+	@Nested
+	class DiscoverySelectionTests {
+		@Test
+		public void packagesAreStoredInDiscoveryRequest() throws Exception {
 			// @formatter:off
 			TestDiscoveryRequest discoveryRequest = request()
 					.select(
-							ClasspathSelector.forPath(temporaryFolder.getAbsolutePath())
+							forPackageName("org.junit.gen5.engine")
+					).build();
+			// @formatter:on
+
+			List<String> packageSelectors = discoveryRequest.getSelectorsByType(PackageSelector.class).stream().map(
+				PackageSelector::getPackageName).collect(toList());
+			assertThat(packageSelectors).contains("org.junit.gen5.engine");
+		}
+
+		@Test
+		public void classesAreStoredInDiscoveryRequest() throws Exception {
+			// @formatter:off
+			TestDiscoveryRequest discoveryRequest = request()
+					.select(
+							forClassName(TestDiscoveryRequestBuilderTests.class.getName()),
+							forClass(SampleTestClass.class)
+					)
+				.build();
+			// @formatter:on
+
+			List<Class<?>> classes = discoveryRequest.getSelectorsByType(ClassSelector.class).stream().map(
+				ClassSelector::getTestClass).collect(toList());
+			assertThat(classes).contains(SampleTestClass.class, TestDiscoveryRequestBuilderTests.class);
+		}
+
+		@Test
+		public void methodsByNameAreStoredInDiscoveryRequest() throws Exception {
+			Class<?> testClass = SampleTestClass.class;
+			Method testMethod = testClass.getMethod("test");
+
+			// @formatter:off
+			TestDiscoveryRequest discoveryRequest = request()
+					.select(forMethod(SampleTestClass.class.getName(), "test"))
+					.build();
+			// @formatter:on
+
+			List<MethodSelector> methodSelectors = discoveryRequest.getSelectorsByType(MethodSelector.class);
+			assertThat(methodSelectors).hasSize(1);
+
+			MethodSelector methodSelector = methodSelectors.get(0);
+			assertThat(methodSelector.getTestClass()).isEqualTo(testClass);
+			assertThat(methodSelector.getTestMethod()).isEqualTo(testMethod);
+		}
+
+		@Test
+		public void methodsByClassAreStoredInDiscoveryRequest() throws Exception {
+			Class<?> testClass = SampleTestClass.class;
+			Method testMethod = testClass.getMethod("test");
+
+			// @formatter:off
+			DiscoveryRequest discoveryRequest = (DiscoveryRequest) request()
+					.select(
+							MethodSelector.forMethod(SampleTestClass.class, "test")
+					).build();
+			// @formatter:on
+
+			List<MethodSelector> methodSelectors = discoveryRequest.getSelectorsByType(MethodSelector.class);
+			assertThat(methodSelectors).hasSize(1);
+
+			MethodSelector methodSelector = methodSelectors.get(0);
+			assertThat(methodSelector.getTestClass()).isEqualTo(testClass);
+			assertThat(methodSelector.getTestMethod()).isEqualTo(testMethod);
+		}
+
+		@Test
+		public void unavailableFoldersAreNotStoredInDiscoveryRequest() throws Exception {
+			// @formatter:off
+			TestDiscoveryRequest discoveryRequest = request()
+					.select(
+							ClasspathSelector.forPath("/some/local/path")
 					).build();
 			// @formatter:on
 
 			List<String> folders = discoveryRequest.getSelectorsByType(ClasspathSelector.class).stream().map(
 				ClasspathSelector::getClasspathRoot).map(File::getAbsolutePath).collect(toList());
 
-			assertThat(folders).contains(temporaryFolder.getAbsolutePath());
+			assertThat(folders).isEmpty();
 		}
-		finally {
-			temporaryFolder.delete();
+
+		@Test
+		public void availableFoldersAreStoredInDiscoveryRequest() throws Exception {
+			File temporaryFolder = Files.newTemporaryFolder();
+			try {
+				// @formatter:off
+				TestDiscoveryRequest discoveryRequest = request()
+						.select(
+								ClasspathSelector.forPath(temporaryFolder.getAbsolutePath())
+						).build();
+				// @formatter:on
+
+				List<String> folders = discoveryRequest.getSelectorsByType(ClasspathSelector.class).stream().map(
+					ClasspathSelector::getClasspathRoot).map(File::getAbsolutePath).collect(toList());
+
+				assertThat(folders).contains(temporaryFolder.getAbsolutePath());
+			}
+			finally {
+				temporaryFolder.delete();
+			}
+		}
+
+		@Test
+		public void uniqueIdsAreStoredInDiscoveryRequest() throws Exception {
+			// @formatter:off
+			TestDiscoveryRequest discoveryRequest = request()
+					.select(
+							forUniqueId("engine:bla:foo:bar:id1"),
+							forUniqueId("engine:bla:foo:bar:id2")
+					).build();
+			// @formatter:on
+
+			List<String> uniqueIds = discoveryRequest.getSelectorsByType(UniqueIdSelector.class).stream().map(
+				UniqueIdSelector::getUniqueId).collect(toList());
+
+			assertThat(uniqueIds).contains("engine:bla:foo:bar:id1", "engine:bla:foo:bar:id2");
 		}
 	}
 
-	@Test
-	public void uniqueIdsAreStoredInDiscoveryRequest() throws Exception {
-		// @formatter:off
-		TestDiscoveryRequest discoveryRequest = request()
-				.select(
-						forUniqueId("engine:bla:foo:bar:id1"),
-						forUniqueId("engine:bla:foo:bar:id2")
-				).build();
-		// @formatter:on
+	@Nested
+	class DiscoveryFilterTests {
+		@Test
+		public void engineFiltersAreStoredInDiscoveryRequest() throws Exception {
+			// @formatter:off
+			TestDiscoveryRequest discoveryRequest = request()
+					.filter(
+							EngineIdFilter.byEngineId("engine1"),
+							EngineIdFilter.byEngineId("engine2")
+					).build();
+			// @formatter:on
 
-		List<String> uniqueIds = discoveryRequest.getSelectorsByType(UniqueIdSelector.class).stream().map(
-			UniqueIdSelector::getUniqueId).collect(toList());
+			List<String> engineIds = discoveryRequest.getEngineIdFilters().stream().map(
+				EngineIdFilter::getEngineId).collect(toList());
+			assertThat(engineIds).hasSize(2);
+			assertThat(engineIds).contains("engine1", "engine2");
+		}
 
-		assertThat(uniqueIds).contains("engine:bla:foo:bar:id1", "engine:bla:foo:bar:id2");
-	}
+		@Test
+		@SuppressWarnings("rawtypes")
+		public void discoveryFiltersAreStoredInDiscoveryRequest() throws Exception {
 
-	@Test
-	public void engineFiltersAreStoredInDiscoveryRequest() throws Exception {
-		// @formatter:off
-		TestDiscoveryRequest discoveryRequest = request()
-				.filter(
-						EngineIdFilter.byEngineId("engine1"),
-						EngineIdFilter.byEngineId("engine2")
-				).build();
-		// @formatter:on
+			// @formatter:off
+			TestDiscoveryRequest discoveryRequest = request()
+					.filter(
+							new DiscoveryFilterStub("filter1"),
+							new DiscoveryFilterStub("filter2")
+					).build();
+			// @formatter:on
 
-		List<String> engineIds = discoveryRequest.getEngineIdFilters().stream().map(
-			EngineIdFilter::getEngineId).collect(toList());
-		assertThat(engineIds).hasSize(2);
-		assertThat(engineIds).contains("engine1", "engine2");
-	}
+			List<String> filterStrings = discoveryRequest.getDiscoveryFiltersByType(DiscoveryFilter.class).stream().map(
+				DiscoveryFilter::toString).collect(toList());
+			assertThat(filterStrings).hasSize(2);
+			assertThat(filterStrings).contains("filter1", "filter2");
+		}
 
-	@Test
-	@SuppressWarnings("rawtypes")
-	public void discoveryFiltersAreStoredInDiscoveryRequest() throws Exception {
+		@Test
+		public void postDiscoveryFiltersAreStoredInDiscoveryRequest() throws Exception {
 
-		// @formatter:off
-		TestDiscoveryRequest discoveryRequest = request()
-				.filter(
-						new DiscoveryFilterStub("filter1"),
-						new DiscoveryFilterStub("filter2")
-				).build();
-		// @formatter:on
+			// @formatter:off
+			TestDiscoveryRequest discoveryRequest = request()
+					.filter(
+							new PostDiscoveryFilterStub("postFilter1"),
+							new PostDiscoveryFilterStub("postFilter2")
+					).build();
+			// @formatter:on
 
-		List<String> filterStrings = discoveryRequest.getDiscoveryFiltersByType(DiscoveryFilter.class).stream().map(
-			DiscoveryFilter::toString).collect(toList());
-		assertThat(filterStrings).hasSize(2);
-		assertThat(filterStrings).contains("filter1", "filter2");
-	}
+			List<String> filterStrings = discoveryRequest.getPostDiscoveryFilters().stream().map(
+				PostDiscoveryFilter::toString).collect(toList());
+			assertThat(filterStrings).hasSize(2);
+			assertThat(filterStrings).contains("postFilter1", "postFilter2");
+		}
 
-	@Test
-	public void postDiscoveryFiltersAreStoredInDiscoveryRequest() throws Exception {
+		@Test
+		public void exceptionForIllegalFilterClass() throws Exception {
+			PreconditionViolationException exception = expectThrows(PreconditionViolationException.class,
+				() -> request().filter(o -> excluded("reason")));
 
-		// @formatter:off
-		TestDiscoveryRequest discoveryRequest = request()
-				.filter(
-						new PostDiscoveryFilterStub("postFilter1"),
-						new PostDiscoveryFilterStub("postFilter2")
-				).build();
-		// @formatter:on
-
-		List<String> filterStrings = discoveryRequest.getPostDiscoveryFilters().stream().map(
-			PostDiscoveryFilter::toString).collect(toList());
-		assertThat(filterStrings).hasSize(2);
-		assertThat(filterStrings).contains("postFilter1", "postFilter2");
-	}
-
-	@Test
-	public void exceptionForIllegalFilterClass() throws Exception {
-		PreconditionViolationException exception = expectThrows(PreconditionViolationException.class,
-			() -> request().filter(o -> excluded("reason")));
-
-		assertEquals("Filter must implement EngineIdFilter, PostDiscoveryFilter or DiscoveryFilter.",
-			exception.getMessage());
+			assertEquals("Filter must implement EngineIdFilter, PostDiscoveryFilter or DiscoveryFilter.",
+				exception.getMessage());
+		}
 	}
 
 	private static class SampleTestClass {
