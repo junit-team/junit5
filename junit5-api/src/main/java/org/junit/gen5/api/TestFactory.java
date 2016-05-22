f1ff92c111309653dcf3f83aa8c58e4f181c4edf
Matthias Merdes <Matthias.Merdes@heidelberg-mobil.com>
2016-05-22 15:05:12 +0200

    #58 added JavaDoc to core dynamic test classes

--- junit5-api/src/main/java/org/junit/gen5/api/TestFactory.java
+++ junit5-api/src/main/java/org/junit/gen5/api/TestFactory.java
@@ -20,6 +20,24 @@ import java.lang.annotation.Target;
 
 import org.junit.gen5.commons.meta.API;
 
+/**
+ * {@code @TestFactory} is used to signal that the annotated method is a <em>test factory</em> method.
+ *
+ * <p>In contrast to {@code @Test} it is not ifself a test case but creates test cases.
+ * Such a {@code @TestFactory} method must return a Stream or Collection of {@code DynamicTest} instances.
+ * These {@code DynamicTest}s will then be executed lazily
+ * enabling dynamic and even non-deterministic generation of test cases.
+ *
+ * <p>{@code @TestFactory} methods must not be {@code private} or {@code static}.
+ *
+ * <p>{@code @TestFactory} methods may optionally declare parameters to be
+ * resolved by {@link org.junit.gen5.api.extension.MethodParameterResolver
+ * MethodParameterResolvers}.
+ *
+ * @since 5.0
+ * @see Test
+ * @see DynamicTest
+ */
 @Target({ ElementType.ANNOTATION_TYPE, ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
--- junit5-engine/src/main/java/org/junit/gen5/engine/junit5/discovery/TestFactoryMethodResolver.java
+++ junit5-engine/src/main/java/org/junit/gen5/engine/junit5/discovery/TestFactoryMethodResolver.java
@@ -12,12 +12,24 @@ package org.junit.gen5.engine.junit5.discovery;
 
 import java.lang.reflect.Method;
 
+import org.junit.gen5.api.TestFactory;
 import org.junit.gen5.commons.util.StringUtils;
 import org.junit.gen5.engine.TestDescriptor;
 import org.junit.gen5.engine.UniqueId;
 import org.junit.gen5.engine.junit5.descriptor.ClassTestDescriptor;
 import org.junit.gen5.engine.junit5.descriptor.TestFactoryTestDescriptor;
 
+/**
+ * {@code TestFactoryMethodResolver} is a special {@code ElementResolver}
+ * which is able to resolve test factory methods denoted by {@code @TestFactory}.
+ *
+ * <p>It will create {@code TestFactoryTestDescriptor} instances.
+ *
+ * @since 5.0
+ * @see ElementResolver
+ * @see TestFactory
+ * @see TestFactoryTestDescriptor
+ */
 public class TestFactoryMethodResolver extends TestMethodResolver {
 
 	public static final String SEGMENT_TYPE = "test-factory";
