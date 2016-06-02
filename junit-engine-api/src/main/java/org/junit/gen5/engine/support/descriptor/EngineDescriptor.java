2da1d352c506d9e9e52f68ae2b8d8448261b65f3
Sam Brannen <sam@sambrannen.com>
2016-06-02 22:59:54 +0200

    Document EngineDescriptor
    Issue: #234

--- junit-engine-api/src/main/java/org/junit/gen5/engine/support/descriptor/EngineDescriptor.java
+++ junit-engine-api/src/main/java/org/junit/gen5/engine/support/descriptor/EngineDescriptor.java
@@ -14,9 +14,14 @@ import static org.junit.gen5.commons.meta.API.Usage.Experimental;
 
 import org.junit.gen5.commons.meta.API;
 import org.junit.gen5.commons.util.Preconditions;
+import org.junit.gen5.engine.TestDescriptor;
+import org.junit.gen5.engine.TestEngine;
 import org.junit.gen5.engine.UniqueId;
 
 /**
+ * An {@code EngineDescriptor} is a {@link TestDescriptor} for a specific
+ * {@link TestEngine}.
+ *
  * @since 5.0
  */
 @API(Experimental)
@@ -24,6 +29,17 @@ public class EngineDescriptor extends AbstractTestDescriptor {
 
 	private final String displayName;
 
+	/**
+	 * Create a new {@code EngineDescriptor} with the supplied {@link UniqueId}
+	 * and display name.
+	 *
+	 * @param uniqueId the {@code UniqueId} for the described {@code TestEngine};
+	 * never {@code null}
+	 * @param displayName the display name for the  described {@code TestEngine};
+	 * never {@code null} or empty
+	 * @see TestEngine#getId()
+	 * @see TestDescriptor#getDisplayName()
+	 */
 	public EngineDescriptor(UniqueId uniqueId, String displayName) {
 		super(uniqueId);
 		this.displayName = Preconditions.notBlank(displayName, "display name must not be null or empty");
@@ -34,11 +50,23 @@ public class EngineDescriptor extends AbstractTestDescriptor {
 		return this.displayName;
 	}
 
+	/**
+	 * Always returns {@code false}: a {@link TestEngine} is never a test.
+	 *
+	 * @see org.junit.gen5.engine.TestDescriptor#isTest()
+	 * @see #isContainer()
+	 */
 	@Override
 	public final boolean isTest() {
 		return false;
 	}
 
+	/**
+	 * Always returns {@code true}: a {@link TestEngine} is always a container.
+	 *
+	 * @see org.junit.gen5.engine.TestDescriptor#isContainer()
+	 * @see #isTest()
+	 */
 	@Override
 	public final boolean isContainer() {
 		return true;
