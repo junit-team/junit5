97ec8e2f3eae7577b3d90470ecc4237e053caf08
Sam Brannen <sam@sambrannen.com>
2016-06-12 12:21:54 +0200

    Introduce nested extensions for engines and tags in the Gradle plugin
    Closes: #307

---
+++ junit-gradle/src/main/groovy/org/junit/gen5/gradle/EnginesExtension.groovy
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2015-2016 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.junit.gen5.gradle
+
+/**
+ * TestEngine configuration options for the JUnit Platform Gradle plugin.
+ *
+ * @since 5.0
+ */
+class EnginesExtension {
+
+	/**
+	 * A list of <em>engine IDs</em> to be included when building the test plan.
+	 */
+	List<String> include = []
+
+	/**
+	 * A list of <em>engine IDs</em> to be excluded when building the test plan.
+	 */
+	List<String> exclude = []
+
+	/**
+	 * Add one or more <em>engine IDs</em> to be included when building the test plan.
+	 */
+	void include(String... engineIds) {
+		this.include.addAll engineIds
+	}
+
+	/**
+	 * Add one or more <em>engine IDs</em> to be excluded when building the test plan.
+	 */
+	void exclude(String... engineIds) {
+		this.exclude.addAll engineIds
+	}
+
+}
