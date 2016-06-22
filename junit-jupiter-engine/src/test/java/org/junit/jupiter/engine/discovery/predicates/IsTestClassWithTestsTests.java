0be836f8e21b4d97972d36090d4b3e6f58590650
Matthias Merdes <Matthias.Merdes@heidelberg-mobil.com>
2016-06-22 16:22:34 +0200

    #299 test for the IsTestClassWithTests predicate

---
+++ junit-jupiter-engine/src/test/java/org/junit/jupiter/engine/discovery/predicates/IsTestClassWithTestsTests.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2015-2016 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.junit.jupiter.engine.discovery.predicates;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.function.Predicate;
+
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
+
+public class IsTestClassWithTestsTests {
+
+	private final Predicate<Class<?>> isTestClassWithTests = new IsTestClassWithTests();
+
+	@Test
+	void standardTestClassEvaluatesToTrue() {
+		assertTrue(isTestClassWithTests.test(ClassWithTestCases.class));
+	}
+
+	@Test
+	void classWithTestFactoryEvaluatesToTrue() {
+		assertTrue(isTestClassWithTests.test(ClassWithTestFactory.class));
+	}
+
+	@Test
+	void classWithNestedTestCasesEvaluatesToTrue() {
+		assertTrue(isTestClassWithTests.test(ClassWithNestedTestCases.class));
+	}
+
+}
+
+class ClassWithTestFactory {
+
+	@TestFactory
+	Collection<DynamicTest> factory() {
+		return new ArrayList<>();
+	}
+
+}
+
+class ClassWithTestCases {
+
+	@Test
+	void first() {
+	}
+
+	@Test
+	void second() {
+	}
+
+}
+
+class ClassWithNestedTestCases {
+
+	@Nested
+	class InnerClass {
+
+		@Test
+		void first() {
+		}
+
+		@Test
+		void second() {
+		}
+
+	}
+}
