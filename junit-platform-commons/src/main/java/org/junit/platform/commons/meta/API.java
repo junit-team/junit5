12b8c03f3d2f6411256ffc9487f12315dec37c86
Mihails Volkovs <mihails.volkovs@gmail.com>
2016-07-03 21:20:06 +0300

    Making annotation available via Java Reflection

--- junit-platform-commons/src/main/java/org/junit/platform/commons/meta/API.java
+++ junit-platform-commons/src/main/java/org/junit/platform/commons/meta/API.java
@@ -29,7 +29,7 @@ import java.lang.annotation.Target;
  * @since 1.0
  */
 @Target({ ElementType.TYPE, ElementType.METHOD, ElementType.CONSTRUCTOR })
-@Retention(RetentionPolicy.CLASS)
+@Retention(RetentionPolicy.RUNTIME)
 @Documented
 @API(Internal)
 public @interface API {
