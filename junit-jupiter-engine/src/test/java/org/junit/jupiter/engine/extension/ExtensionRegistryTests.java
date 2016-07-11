junit-team/junit5

commit 6c146c5a76da048a935c0615a1033fcca7846736

Delete RegisteredExtension and simplify ExtensionRegistry

junit-jupiter-engine/src/main/java/org/junit/jupiter/engine/extension/ExtensionRegistry.java
@@ -17,11 +17,11 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 import java.util.logging.Logger;
 import java.util.stream.Stream;
 
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.Extension;
 import org.junit.platform.commons.meta.API;
 import org.junit.platform.commons.util.Preconditions;
@@ -43,20 +43,8 @@ public class ExtensionRegistry {
 
 	private static final Logger LOG = Logger.getLogger(ExtensionRegistry.class.getName());
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	private static final List<RegisteredExtension> DEFAULT_EXTENSIONS = Collections.unmodifiableList(
-		Arrays.asList(new RegisteredExtension(new DisabledCondition(), ExtensionRegistry.class),
-			new RegisteredExtension(new TestInfoParameterResolver(), ExtensionRegistry.class),
-			new RegisteredExtension(new TestReporterParameterResolver(), ExtensionRegistry.class)));
-
-	/**
-	 * Factory for creating a new empty root registry.
-	 *
-	 * @return a new {@code ExtensionRegistry}
-	 */
-	public static ExtensionRegistry createEmptyRegistry() {
-		return new ExtensionRegistry(Optional.empty());
-	}
+	private static final List<Extension> DEFAULT_EXTENSIONS = Collections.unmodifiableList(
+		Arrays.asList(new DisabledCondition(), new TestInfoParameterResolver(), new TestReporterParameterResolver()));
 
 	/**
 	 * Factory for creating and populating a new root registry with the default
@@ -65,7 +53,7 @@ public class ExtensionRegistry {
 	 * @return a new {@code ExtensionRegistry}
 	 */
 	public static ExtensionRegistry createRegistryWithDefaultExtensions() {
-		ExtensionRegistry extensionRegistry = new ExtensionRegistry(Optional.empty());
+		ExtensionRegistry extensionRegistry = new ExtensionRegistry(null);
 		DEFAULT_EXTENSIONS.forEach(extensionRegistry::registerDefaultExtension);
 		return extensionRegistry;
 	}
@@ -84,49 +72,19 @@ public class ExtensionRegistry {
 
 		Preconditions.notNull(parentRegistry, "parentRegistry must not be null");
 
-		ExtensionRegistry registry = new ExtensionRegistry(Optional.of(parentRegistry));
+		ExtensionRegistry registry = new ExtensionRegistry(parentRegistry);
 		extensionTypes.forEach(registry::registerExtension);
 		return registry;
 	}
 
-	private final Optional<ExtensionRegistry> parent;
+	private final ExtensionRegistry parent;
 
 	private final Set<Class<? extends Extension>> registeredExtensionTypes = new LinkedHashSet<>();
 
-	private final List<RegisteredExtension<?>> registeredExtensions = new ArrayList<>();
+	private final List<Extension> registeredExtensions = new ArrayList<>();
 
-	ExtensionRegistry(Optional<ExtensionRegistry> parent) {
+	private ExtensionRegistry(ExtensionRegistry parent) {
 		this.parent = parent;
-	}
-
-	/**
-	 * @return all extension types registered in this registry or one of its ancestors
-	 */
-	Set<Class<? extends Extension>> getRegisteredExtensionTypes() {
-		Set<Class<? extends Extension>> allRegisteredExtensionTypes = new LinkedHashSet<>();
-		this.parent.ifPresent(
-			parentRegistry -> allRegisteredExtensionTypes.addAll(parentRegistry.getRegisteredExtensionTypes()));
-		allRegisteredExtensionTypes.addAll(this.registeredExtensionTypes);
-		return Collections.unmodifiableSet(allRegisteredExtensionTypes);
-	}
-
-	/**
-	 * @return all {@code RegisteredExtensions} in this registry or one of its ancestors
-	 */
-	@SuppressWarnings("unchecked")
-	<E extends Extension> List<RegisteredExtension<E>> getRegisteredExtensions(Class<E> extensionType) {
-
-		List<RegisteredExtension<E>> extensions = new ArrayList<>();
-		this.parent.ifPresent(
-			parentRegistry -> extensions.addAll(parentRegistry.getRegisteredExtensions(extensionType)));
-
-		// @formatter:off
-		this.registeredExtensions.stream()
-				.filter(registeredExtension -> extensionType.isAssignableFrom(registeredExtension.getExtension().getClass()))
-				.forEach(extension -> extensions.add((RegisteredExtension<E>) extension));
-		// @formatter:on
-
-		return extensions;
 	}
 
 	/**
@@ -138,13 +96,14 @@ public class ExtensionRegistry {
 	 * @see #stream(Class)
 	 */
 	public <E extends Extension> List<E> getExtensions(Class<E> extensionType) {
-
 		List<E> extensions = new ArrayList<>();
-		this.parent.ifPresent(parent -> extensions.addAll(parent.getExtensions(extensionType)));
+
+		if (this.parent != null) {
+			extensions.addAll(this.parent.getExtensions(extensionType));
+		}
 
 		// @formatter:off
 		this.registeredExtensions.stream()
-				.map(RegisteredExtension::getExtension)
 				.filter(extension -> extensionType.isAssignableFrom(extension.getClass()))
 				.map(extensionType::cast)
 				.forEach(extensions::add);
@@ -158,6 +117,7 @@ public class ExtensionRegistry {
 	 * the specified type, in reverse order.
 	 *
 	 * @param extensionType the type of {@link Extension} to get
+	 * @see #getExtensions(Class)
 	 * @see #stream(Class)
 	 */
 	public <E extends Extension> List<E> getReversedExtensions(Class<E> extensionType) {
@@ -188,29 +148,40 @@ public class ExtensionRegistry {
 	 * @param extensionType the type of extension to register
 	 */
 	void registerExtension(Class<? extends Extension> extensionType) {
-
-		boolean extensionAlreadyRegistered = getRegisteredExtensionTypes().stream().anyMatch(
-			registeredType -> registeredType.equals(extensionType));
-
+		boolean extensionAlreadyRegistered = this.registeredExtensionTypes.contains(extensionType);
 		if (!extensionAlreadyRegistered) {
 			registerExtension(ReflectionUtils.newInstance(extensionType));
 			this.registeredExtensionTypes.add(extensionType);
 		}
 	}
 
-	@SuppressWarnings("rawtypes")
-	private void registerDefaultExtension(RegisteredExtension registeredExtension) {
-		this.registeredExtensions.add(registeredExtension);
-		this.registeredExtensionTypes.add(registeredExtension.getExtension().getClass());
+	private void registerDefaultExtension(Extension extension) {
+		this.registeredExtensions.add(extension);
+		this.registeredExtensionTypes.add(extension.getClass());
 	}
 
 	private void registerExtension(Extension extension) {
 		registerExtension(extension, extension);
 	}
 
+	/**
+	 * Register the supplied {@link Extension} in this registry, without checking
+	 * if an extension of that type already exists in this registry.
+	 *
+	 * <h4>Semantics for Source</h4>
+	 * <p>If an extension is registered declaratively via {@link ExtendWith @ExtendWith},
+	 * the {@code source} and the {@code extension} should be the same object. However,
+	 * if an extension is registered programmatically &mdash; for example, as a lambda
+	 * expression or method reference &mdash; the {@code source} object should be the
+	 * underlying {@link java.lang.reflect.Method} that implements the extension
+	 * API, or similar.
+	 *
+	 * @param extension the extension to register
+	 * @param source the source of the extension
+	 */
 	public void registerExtension(Extension extension, Object source) {
 		LOG.finer(() -> String.format("Registering extension [%s] from source [%s].", extension, source));
-		this.registeredExtensions.add(new RegisteredExtension<>(extension, source));
+		this.registeredExtensions.add(extension);
 	}
 
 }

junit-jupiter-engine/src/test/java/org/junit/jupiter/engine/extension/ExtensionRegistryTests.java
@@ -10,12 +10,15 @@
 
 package org.junit.jupiter.engine.extension;
 
+import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.engine.extension.ExtensionRegistry.createRegistryFrom;
+import static org.junit.jupiter.engine.extension.ExtensionRegistry.createRegistryWithDefaultExtensions;
 
-import java.util.Optional;
-import java.util.Set;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.junit.jupiter.api.Test;
@@ -29,10 +32,11 @@ import org.junit.jupiter.api.extension.TestExecutionCondition;
  */
 public class ExtensionRegistryTests {
 
+	private final ExtensionRegistry registry = createRegistryWithDefaultExtensions();
+
 	@Test
 	void newRegistryWithoutParentHasDefaultExtensions() {
-		ExtensionRegistry registry = ExtensionRegistry.createRegistryWithDefaultExtensions();
-		Set<Class<? extends Extension>> extensions = registry.getRegisteredExtensionTypes();
+		List<Extension> extensions = registry.getExtensions(Extension.class);
 
 		assertEquals(3, extensions.size());
 		assertExtensionRegistered(registry, DisabledCondition.class);
@@ -46,7 +50,6 @@ public class ExtensionRegistryTests {
 
 	@Test
 	void registerExtensionByImplementingClass() {
-		ExtensionRegistry registry = new ExtensionRegistry(Optional.empty());
 		registry.registerExtension(MyExtension.class);
 
 		assertExtensionRegistered(registry, MyExtension.class);
@@ -55,7 +58,7 @@ public class ExtensionRegistryTests {
 		registry.registerExtension(MyExtension.class);
 		registry.registerExtension(MyExtension.class);
 
-		assertEquals(1, registry.getRegisteredExtensionTypes().size());
+		assertEquals(1, registry.getExtensions(MyExtension.class).size());
 		assertExtensionRegistered(registry, MyExtension.class);
 		assertEquals(1, countExtensions(registry, MyExtensionApi.class));
 
@@ -66,7 +69,6 @@ public class ExtensionRegistryTests {
 
 	@Test
 	void registerExtensionThatImplementsMultipleExtensionApis() {
-		ExtensionRegistry registry = new ExtensionRegistry(Optional.empty());
 		registry.registerExtension(MultipleExtension.class);
 
 		assertExtensionRegistered(registry, MultipleExtension.class);
@@ -77,15 +79,15 @@ public class ExtensionRegistryTests {
 
 	@Test
 	void extensionsAreInheritedFromParent() {
-		ExtensionRegistry parent = new ExtensionRegistry(Optional.empty());
+		ExtensionRegistry parent = registry;
 		parent.registerExtension(MyExtension.class);
 
-		ExtensionRegistry child = ExtensionRegistry.createRegistryFrom(parent, singletonList(YourExtension.class));
+		ExtensionRegistry child = createRegistryFrom(parent, singletonList(YourExtension.class));
 		assertExtensionRegistered(child, MyExtension.class);
 		assertExtensionRegistered(child, YourExtension.class);
 		assertEquals(2, countExtensions(child, MyExtensionApi.class));
 
-		ExtensionRegistry grandChild = new ExtensionRegistry(Optional.of(child));
+		ExtensionRegistry grandChild = createRegistryFrom(child, emptyList());
 		assertExtensionRegistered(grandChild, MyExtension.class);
 		assertExtensionRegistered(grandChild, YourExtension.class);
 		assertEquals(2, countExtensions(grandChild, MyExtensionApi.class));
@@ -93,7 +95,6 @@ public class ExtensionRegistryTests {
 
 	@Test
 	void canStreamOverRegisteredExtension() {
-		ExtensionRegistry registry = new ExtensionRegistry(Optional.empty());
 		registry.registerExtension(MyExtension.class);
 
 		AtomicBoolean hasRun = new AtomicBoolean(false);
@@ -106,48 +107,12 @@ public class ExtensionRegistryTests {
 		assertTrue(hasRun.get());
 	}
 
-	@Test
-	void registerExtensionFromLambdaExpression() {
-		ExtensionRegistry registry = new ExtensionRegistry(Optional.empty());
-		registry.registerExtension((MyExtensionApi) test -> {
-		}, this);
-
-		assertBehaviorForExtensionRegisteredFromLambdaExpressionOrMethodReference(registry);
-	}
-
-	@Test
-	void registerExtensionFromMethodReference() {
-		ExtensionRegistry registry = new ExtensionRegistry(Optional.empty());
-		registry.registerExtension((MyExtensionApi) this::consumeString, this);
-		assertBehaviorForExtensionRegisteredFromLambdaExpressionOrMethodReference(registry);
-	}
-
-	/**
-	 * "Implements" MyExtensionApi.
-	 */
-	private void consumeString(String test) {
-		/* no-op */
-	}
-
-	private void assertBehaviorForExtensionRegisteredFromLambdaExpressionOrMethodReference(ExtensionRegistry registry) {
-		AtomicBoolean hasRun = new AtomicBoolean(false);
-
-		registry.getRegisteredExtensions(MyExtensionApi.class).forEach(registeredExtension -> {
-			Class<? extends MyExtensionApi> lambdaType = registeredExtension.getExtension().getClass();
-			assertTrue(lambdaType.getName().contains("$Lambda$"));
-			assertEquals(getClass().getName(), registeredExtension.getSource().getClass().getName());
-			hasRun.set(true);
-		});
-
-		assertTrue(hasRun.get());
-	}
-
 	private long countExtensions(ExtensionRegistry registry, Class<? extends Extension> extensionType) {
 		return registry.stream(extensionType).count();
 	}
 
 	private void assertExtensionRegistered(ExtensionRegistry registry, Class<? extends Extension> extensionType) {
-		assertTrue(registry.getRegisteredExtensionTypes().contains(extensionType),
+		assertFalse(registry.getExtensions(extensionType).isEmpty(),
 			() -> extensionType.getSimpleName() + " should be present");
 	}
 

junit-jupiter-engine/src/main/java/org/junit/jupiter/engine/extension/RegisteredExtension.java
@@ -1,82 +1,0 @@
-/*
- * Copyright 2015-2016 the original author or authors.
- *
- * All rights reserved. This program and the accompanying materials are
- * made available under the terms of the Eclipse Public License v1.0 which
- * accompanies this distribution and is available at
- *
- * http://www.eclipse.org/legal/epl-v10.html
- */
-
-package org.junit.jupiter.engine.extension;
-
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.extension.Extension;
-import org.junit.platform.commons.util.Preconditions;
-import org.junit.platform.commons.util.ToStringBuilder;
-
-/**
- * Represents an {@link Extension} registered in an {@link ExtensionRegistry}.
- *
- * @param <E> the type of registered {@link Extension}
- * @since 5.0
- */
-class RegisteredExtension<E extends Extension> {
-
-	private final E extension;
-
-	private final Object source;
-
-	/**
-	 * Construct a new {@code RegisteredExtension} from the supplied
-	 * extension and source.
-	 *
-	 * <p>See {@link #getSource()} for an explanation of the semantics for
-	 * the {@code source}.
-	 *
-	 * @param extension the physical {@code Extension} which is registered;
-	 * never {@code null}
-	 * @param source the <em>source</em> of the extension; used solely for
-	 * error reporting and logging; never {@code null}
-	 */
-	RegisteredExtension(E extension, Object source) {
-		this.extension = Preconditions.notNull(extension, "extension must not be null");
-		this.source = Preconditions.notNull(source, "source must not be null");
-	}
-
-	/**
-	 * Get the physical implementation of the registered {@link Extension}.
-	 */
-	E getExtension() {
-		return this.extension;
-	}
-
-	/**
-	 * Get the <em>source</em> of the registered {@link #getExtension Extension}.
-	 *
-	 * <p>The source is used solely for error reporting and logging.
-	 *
-	 * <h4>Semantics for Source</h4>
-	 * <p>If an extension is registered declaratively via
-	 * {@link ExtendWith @ExtendWith}, this method
-	 * and {@link #getExtension()} will return the same object. However, if an
-	 * extension is registered programmatically &mdash; for example, as a lambda
-	 * expression or method reference &mdash; the {@code source} object may be the
-	 * underlying {@link java.lang.reflect.Method} that implements the extension
-	 * API, or similar.
-	 */
-	Object getSource() {
-		return this.source;
-	}
-
-	@Override
-	public String toString() {
-		// @formatter:off
-		return new ToStringBuilder(this)
-			.append("extension", this.extension)
-			.append("source", this.source)
-			.toString();
-		// @formatter:on
-	}
-
-}

junit-jupiter-engine/src/test/java/org/junit/jupiter/engine/execution/ExecutableInvokerTests.java
@@ -46,7 +46,7 @@ class ExecutableInvokerTests {
 	private Method method;
 
 	private final ExtensionContext extensionContext = mock(ExtensionContext.class);
-	private ExtensionRegistry extensionRegistry = ExtensionRegistry.createEmptyRegistry();
+	private ExtensionRegistry extensionRegistry = ExtensionRegistry.createRegistryWithDefaultExtensions();
 
 	@Test
 	void constructorInjection() throws Exception {

commit a57efb21ba0e8ad775e7f852c1095b3edd5f2990

Ensure extensions cannot be registered as duplicates
Commit 6c146c5a76 introduced a regression that allowed an extension
implementation to be registered multiple times if registered in both
a parent and child extension context (e.g. at the class-level and
method-level).

This commit fixes this by reinstating logic for querying the parent
extension context as well when adding an extension to a child context.

This commit also improves the documentation on extension inheritance in
the User Guide.

Fixes: #390

junit-jupiter-engine/src/main/java/org/junit/jupiter/engine/extension/ExtensionRegistry.java
@@ -139,17 +139,25 @@ public class ExtensionRegistry {
 	}
 
 	/**
+	 * Determine if the supplied type is already registered in this registry or in a
+	 * parent registry.
+	 */
+	private boolean isAlreadyRegistered(Class<? extends Extension> extensionType) {
+		return (this.registeredExtensionTypes.contains(extensionType)
+				|| (this.parent != null && this.parent.isAlreadyRegistered(extensionType)));
+	}
+
+	/**
 	 * Instantiate an extension of the given type using its default constructor
 	 * and register it in this registry.
 	 *
 	 * <p>A new {@link Extension} will not be registered if an extension of the
-	 * given type already exists in this registry.
+	 * given type already exists in this registry or a parent registry.
 	 *
 	 * @param extensionType the type of extension to register
 	 */
 	void registerExtension(Class<? extends Extension> extensionType) {
-		boolean extensionAlreadyRegistered = this.registeredExtensionTypes.contains(extensionType);
-		if (!extensionAlreadyRegistered) {
+		if (!isAlreadyRegistered(extensionType)) {
 			registerExtension(ReflectionUtils.newInstance(extensionType));
 			this.registeredExtensionTypes.add(extensionType);
 		}

junit-jupiter-engine/src/test/java/org/junit/jupiter/engine/extension/ExtensionRegistryTests.java
@@ -10,6 +10,7 @@
 
 package org.junit.jupiter.engine.extension;
 
+import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -88,6 +89,23 @@ public class ExtensionRegistryTests {
 		assertEquals(2, countExtensions(child, MyExtensionApi.class));
 
 		ExtensionRegistry grandChild = createRegistryFrom(child, emptyList());
+		assertExtensionRegistered(grandChild, MyExtension.class);
+		assertExtensionRegistered(grandChild, YourExtension.class);
+		assertEquals(2, countExtensions(grandChild, MyExtensionApi.class));
+	}
+
+	@Test
+	void registeringSameExtensionImplementationInParentAndChildDoesNotResultInDuplicate() {
+		ExtensionRegistry parent = registry;
+		parent.registerExtension(MyExtension.class);
+		assertEquals(1, countExtensions(parent, MyExtensionApi.class));
+
+		ExtensionRegistry child = createRegistryFrom(parent, asList(MyExtension.class, YourExtension.class));
+		assertExtensionRegistered(child, MyExtension.class);
+		assertExtensionRegistered(child, YourExtension.class);
+		assertEquals(2, countExtensions(child, MyExtensionApi.class));
+
+		ExtensionRegistry grandChild = createRegistryFrom(child, asList(MyExtension.class, YourExtension.class));
 		assertExtensionRegistered(grandChild, MyExtension.class);
 		assertExtensionRegistered(grandChild, YourExtension.class);
 		assertEquals(2, countExtensions(grandChild, MyExtensionApi.class));