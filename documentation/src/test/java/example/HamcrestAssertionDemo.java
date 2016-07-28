7c6aaa1230218987c3dcbec641b3fdc50cc0836c
Sam Brannen <sam@sambrannen.com>
2016-07-28 15:45:25 +0200

    Improve documentation for 3rd party assertion library support

---
+++ documentation/src/test/java/example/HamcrestAssertionDemo.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2015-2016 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package example;
+
+//tag::user_guide[]
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class HamcrestAssertionDemo {
+
+	@Test
+	void assertWithHamcrestMatcher() {
+		assertThat(2 + 1, is(equalTo(3)));
+	}
+
+}
+//end::user_guide[]
