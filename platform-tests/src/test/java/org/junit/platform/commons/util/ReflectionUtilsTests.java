fe9ef9b830cf0fa6e4da759647d37ee148c7e708
Sam Brannen <sam@sambrannen.com>
2016-08-07 15:18:27 -0400

    Improve code coverage for ReflectionUtils.loadMethod()

--- platform-tests/src/test/java/org/junit/platform/commons/util/ReflectionUtilsTests.java
+++ platform-tests/src/test/java/org/junit/platform/commons/util/ReflectionUtilsTests.java
@@ -287,6 +287,7 @@ public class ReflectionUtilsTests {
 		assertThat(ReflectionUtils.loadMethod(PrivateClass.class.getName() + "#privateMethod")).isPresent();
 		assertThat(ReflectionUtils.loadMethod("  " + PrivateClass.class.getName() + "#privateMethod  ")).isPresent();
 
+		assertThat(ReflectionUtils.loadMethod("org.example.NonexistentClass#nonexistentMethod")).isEmpty();
 		assertThat(ReflectionUtils.loadMethod(PublicClass.class.getName() + "#nonexistentMethod")).isEmpty();
 
 		// missing java.lang.String parameter type
