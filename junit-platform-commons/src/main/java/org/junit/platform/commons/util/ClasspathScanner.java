1ace4082e4983e784cccff079923ce26713459c2
Sam Brannen <sam@sambrannen.com>
2016-08-17 21:08:28 +0200

    Delete debug code

--- junit-platform-commons/src/main/java/org/junit/platform/commons/util/ClasspathScanner.java
+++ junit-platform-commons/src/main/java/org/junit/platform/commons/util/ClasspathScanner.java
@@ -131,7 +131,6 @@ class ClasspathScanner {
 		}
 
 		for (File file : files) {
-			System.err.println("file: " + file);
 			if (isClassFile(file)) {
 				processClassFileSafely(packageName, file, classFilter, classes);
 			}
