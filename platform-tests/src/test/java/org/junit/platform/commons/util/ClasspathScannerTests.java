bc9cb234579e507039ad1e3ac7a2507972ef9c7b
Sam Brannen <sam@sambrannen.com>
2016-08-29 18:11:46 +0200

    Clean up warning in ClasspathScannerTests

--- platform-tests/src/test/java/org/junit/platform/commons/util/ClasspathScannerTests.java
+++ platform-tests/src/test/java/org/junit/platform/commons/util/ClasspathScannerTests.java
@@ -123,17 +123,18 @@ class ClasspathScannerTests {
 
 	@Test
 	void findAllClassesInPackageWithinJarFile() throws Exception {
-
 		URL jarfile = getClass().getResource("/jartest.jar");
-		URLClassLoader classLoader = new URLClassLoader(new URL[] { jarfile });
-		ClasspathScanner classpathScanner = new ClasspathScanner(() -> classLoader, ReflectionUtils::loadClass);
 
-		List<Class<?>> classes = classpathScanner.scanForClassesInPackage("org.junit.platform.jartest.included",
-			clazz -> true);
-		assertThat(classes).hasSize(2);
-		List<String> classNames = classes.stream().map(Class::getSimpleName).collect(Collectors.toList());
-		assertTrue(classNames.contains("Included"));
-		assertTrue(classNames.contains("RecursivelyIncluded"));
+		try (URLClassLoader classLoader = new URLClassLoader(new URL[] { jarfile })) {
+			ClasspathScanner classpathScanner = new ClasspathScanner(() -> classLoader, ReflectionUtils::loadClass);
+
+			List<Class<?>> classes = classpathScanner.scanForClassesInPackage("org.junit.platform.jartest.included",
+				clazz -> true);
+			assertThat(classes).hasSize(2);
+			List<String> classNames = classes.stream().map(Class::getSimpleName).collect(Collectors.toList());
+			assertTrue(classNames.contains("Included"));
+			assertTrue(classNames.contains("RecursivelyIncluded"));
+		}
 	}
 
 	@Test
