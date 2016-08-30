2486378463d9159354870c32d0d328dfc53ab771
Matthias Merdes <Matthias.Merdes@heidelberg-mobil.com>
2016-08-30 17:43:35 +0200

    Support rules of type ExternalResource
    new integration test with 2 rules
    and after invocation verification
    
    #433

---
+++ junit-jupiter-engine/src/test/java/org/junit/jupiter/engine/vintage/rulesupport/MultipleExternalResourceLegacySupportTests.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2015-2016 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.junit.jupiter.engine.vintage.rulesupport;
+
+import org.junit.Rule;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.rules.ExternalResource;
+
+@ExtendWith(ExternalResourceLegacySupport.class)
+public class MultipleExternalResourceLegacySupportTests {
+
+	private static boolean beforeOfRule1WasExecuted = false;
+	private static boolean beforeOfRule2WasExecuted = false;
+
+	private static boolean afterOfRule1WasExecuted = false;
+	private static boolean afterOfRule2WasExecuted = false;
+
+	@Rule
+	public ExternalResource resource1 = new ExternalResource() {
+		@Override
+		protected void before() throws Throwable {
+			beforeOfRule1WasExecuted = true;
+		}
+
+		@Override
+		protected void after() {
+			afterOfRule1WasExecuted = true;
+		}
+	};
+
+	@Rule
+	public ExternalResource resource2 = new ExternalResource() {
+		@Override
+		protected void before() throws Throwable {
+			beforeOfRule2WasExecuted = true;
+		}
+
+		@Override
+		protected void after() {
+			afterOfRule2WasExecuted = true;
+		}
+	};
+
+	@Test
+	void beforeMethodsOfbothRulesWereExecuted() {
+		assert beforeOfRule1WasExecuted;
+		//		assert beforeOfRule2WasExecuted;
+	}
+
+	@AfterAll
+	static void afterMethodsOfBothRulesWereExecuted() {
+		assert afterOfRule1WasExecuted;
+		//        assert afterOfRule2WasExecuted;
+	}
+
+}
