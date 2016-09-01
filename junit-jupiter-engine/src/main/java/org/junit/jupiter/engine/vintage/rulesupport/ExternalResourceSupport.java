c8761c9cb2bfc122f12b874e76bedde3e3c6d2f8
Matthias Merdes <Matthias.Merdes@heidelberg-mobil.com>
2016-09-01 14:19:05 +0200

    Compose support of method and field support
    The ExternalResourceSupport is now composed of the two separately implemented
    support classes for method and field support, respectively.
    Includes a test to verify the relative order of both cases.
    
    #433

--- junit-jupiter-engine/src/main/java/org/junit/jupiter/engine/vintage/rulesupport/ExternalResourceSupport.java
+++ junit-jupiter-engine/src/main/java/org/junit/jupiter/engine/vintage/rulesupport/ExternalResourceSupport.java
@@ -10,85 +10,25 @@
 
 package org.junit.jupiter.engine.vintage.rulesupport;
 
-import static org.junit.platform.commons.util.ReflectionUtils.MethodSortOrder.HierarchyDown;
-
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.List;
-import java.util.function.Consumer;
-import java.util.stream.Stream;
-
-import org.junit.Rule;
 import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.TestExtensionContext;
-import org.junit.platform.commons.util.AnnotationUtils;
-import org.junit.rules.ExternalResource;
 
 public class ExternalResourceSupport implements BeforeEachCallback, AfterEachCallback {
 
-	private final Class<Rule> annotationType = Rule.class;
-	private final Class<ExternalResource> ruleType = ExternalResource.class;
+	private AbstractExternalResourceSupport fieldSupport = new ExternalResourceFieldSupport();
+	private AbstractExternalResourceSupport methodSupport = new ExternalResourceMethodSupport();
 
 	@Override
 	public void beforeEach(TestExtensionContext context) throws Exception {
-		this.invokeAppropriateMethodOnRuleAnnotatedFields(context, GenericBeforeAndAfterAdvice::before);
-		this.invokeAppropriateMethodOnRuleAnnotatedMethods(context, GenericBeforeAndAfterAdvice::before);
+		this.fieldSupport.beforeEach(context);
+		this.methodSupport.beforeEach(context);
 	}
 
 	@Override
 	public void afterEach(TestExtensionContext context) throws Exception {
-		this.invokeAppropriateMethodOnRuleAnnotatedFields(context, GenericBeforeAndAfterAdvice::after);
-		this.invokeAppropriateMethodOnRuleAnnotatedMethods(context, GenericBeforeAndAfterAdvice::after);
-	}
-
-	private void invokeAppropriateMethodOnRuleAnnotatedFields(TestExtensionContext context,
-			Consumer<GenericBeforeAndAfterAdvice> methodCaller) {
-		// @formatter:off
-        Stream<AbstractTestRuleAdapter> ruleAdapters = this.findRuleAnnotatedFields(context)
-                .map(field -> new RuleAnnotatedField(context, field))
-                .map(annotatedField -> new ExternalResourceAdapter(annotatedField.getTestRuleInstance()));
-		// @formatter:on
-
-		ruleAdapters.forEach(methodCaller::accept);
-	}
-
-	private void invokeAppropriateMethodOnRuleAnnotatedMethods(TestExtensionContext context,
-			Consumer<GenericBeforeAndAfterAdvice> methodCaller) {
-		// @formatter:off
-        Stream<AbstractTestRuleAdapter> ruleAdapters = this.findRuleAnnotatedMethods(context)
-                .map(method -> new RuleAnnotatedMethod(context, method))
-                .map(annotatedMethod -> new ExternalResourceAdapter(annotatedMethod.getTestRuleInstance()));
-		// @formatter:on
-
-		ruleAdapters.forEach(methodCaller::accept);
-	}
-
-	private Stream<Field> findRuleAnnotatedFields(TestExtensionContext context) {
-		Object testInstance = context.getTestInstance();
-		return findAnnotatedFields(testInstance, this.ruleType, this.annotationType);
-	}
-
-	private Stream<Method> findRuleAnnotatedMethods(TestExtensionContext context) {
-		Object testInstance = context.getTestInstance();
-		List<Method> annotatedMethods = AnnotationUtils.findAnnotatedMethods(testInstance.getClass(),
-			this.annotationType, HierarchyDown);
-
-		return annotatedMethods.stream();
-	}
-
-	// TODO: decide whether this should be promoted to AnnotationUtils
-	private static Stream<Field> findAnnotatedFields(Object instance, Class<?> fieldType,
-			Class<? extends Annotation> annotationType) {
-		Field[] declaredFields = instance.getClass().getDeclaredFields();
-
-		// @formatter:off
-        return Arrays.stream(declaredFields)
-                .filter(field -> fieldType.isAssignableFrom(field.getType()))
-                .filter(field -> field.isAnnotationPresent(annotationType));
-		// @formatter:on
+		this.fieldSupport.afterEach(context);
+		this.methodSupport.afterEach(context);
 	}
 
 }
