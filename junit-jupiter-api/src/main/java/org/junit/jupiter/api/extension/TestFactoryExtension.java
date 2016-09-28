2fdc9a73ce0906e3a4c6631b1c6a7eff6e8a2d18
nipa <nipa@codefx.org>
2016-09-28 14:15:26 +0200

    Create extension point 'TestFactoryExtension'

---
+++ junit-jupiter-api/src/main/java/org/junit/jupiter/api/extension/TestFactoryExtension.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2015-2016 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.junit.jupiter.api.extension;
+
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.platform.commons.meta.API;
+
+import java.util.stream.Stream;
+
+import static org.junit.platform.commons.meta.API.Usage.Experimental;
+
+/**
+ * TODO
+ *
+ * @since 5.0
+ */
+@API(Experimental)
+public interface TestFactoryExtension extends Extension {
+
+	// TODO
+
+}
