1cb3dad84749ad4443c2d00a0f60485796d930c7
Matthias Merdes <Matthias.Merdes@heidelberg-mobil.com>
2017-01-16 11:43:51 +0100

    Introduce ClassNameFilter that excludes class names
    #314

---
+++ junit-platform-engine/src/main/java/org/junit/platform/engine/discovery/AbstractClassNameFilter.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2015-2017 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.junit.platform.engine.discovery;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+
+import org.junit.platform.commons.util.Preconditions;
+
+/**
+ * Abstract {@link ClassNameFilter} that servers as a superclass
+ * for filters including or excluding fully qualified class names
+ * based on pattern-matching.
+ *
+ * @since 1.0
+ */
+abstract class AbstractClassNameFilter implements ClassNameFilter {
+
+	protected final List<Pattern> patterns;
+	protected final String patternDescription;
+
+	AbstractClassNameFilter(String... patterns) {
+		Preconditions.notEmpty(patterns, "patterns must not be null or empty");
+		Preconditions.containsNoNullElements(patterns, "patterns must not contain null elements");
+		this.patterns = Arrays.stream(patterns).map(Pattern::compile).collect(toList());
+		this.patternDescription = Arrays.stream(patterns).collect(joining("' OR '", "'", "'"));
+	}
+
+	@Override
+	public abstract Predicate<String> toPredicate();
+
+	protected Optional<Pattern> findMatchingPattern(String className) {
+		return this.patterns.stream().filter(pattern -> pattern.matcher(className).matches()).findAny();
+	}
+
+}
--- platform-tests/src/test/java/org/junit/platform/engine/discovery/ClassNameFilterTests.java
+++ platform-tests/src/test/java/org/junit/platform/engine/discovery/ClassNameFilterTests.java
@@ -82,4 +82,64 @@ class ClassNameFilterTests {
 					+ secondRegex + "'");
 	}
 
+	@Test
+	void excludeClassNamePatternsChecksPreconditions() {
+		assertThatThrownBy(() -> ClassNameFilter.excludeClassNamePatterns((String[]) null)) //
+				.isInstanceOf(PreconditionViolationException.class) //
+				.hasMessage("patterns must not be null or empty");
+		assertThatThrownBy(() -> ClassNameFilter.excludeClassNamePatterns(new String[0])) //
+				.isInstanceOf(PreconditionViolationException.class) //
+				.hasMessage("patterns must not be null or empty");
+		assertThatThrownBy(() -> ClassNameFilter.excludeClassNamePatterns(new String[] { null })) //
+				.isInstanceOf(PreconditionViolationException.class) //
+				.hasMessage("patterns must not contain null elements");
+	}
+
+	@Test
+	void excludeClassNamePatternsWithSinglePattern() {
+		String regex = "^java\\.lang\\..*";
+
+		ClassNameFilter filter = ClassNameFilter.excludeClassNamePatterns(regex);
+
+		assertThat(filter).hasToString("Excludes class names that match regular expression '" + regex + "'");
+
+		assertTrue(filter.apply("java.lang.String").excluded());
+		assertFalse(filter.toPredicate().test("java.lang.String"));
+
+		assertThat(filter.apply("java.lang.String").getReason()).contains(
+			"Class name [java.lang.String] matches excluded pattern: '" + regex + "'");
+
+		assertTrue(filter.apply("java.time.Instant").included());
+		assertTrue(filter.toPredicate().test("java.time.Instant"));
+		assertThat(filter.apply("java.time.Instant").getReason()).contains(
+			"Class name [java.time.Instant] does not match any excluded pattern: '" + regex + "'");
+	}
+
+	@Test
+	void excludeClassNamePatternsWithMultiplePatterns() {
+		String firstRegex = "^java\\.lang\\..*";
+		String secondRegex = "^java\\.util\\..*";
+
+		ClassNameFilter filter = ClassNameFilter.excludeClassNamePatterns(firstRegex, secondRegex);
+
+		assertThat(filter).hasToString(
+			"Excludes class names that match regular expression '" + firstRegex + "' OR '" + secondRegex + "'");
+
+		assertTrue(filter.apply("java.lang.String").excluded());
+		assertFalse(filter.toPredicate().test("java.lang.String"));
+		assertThat(filter.apply("java.lang.String").getReason()).contains(
+			"Class name [java.lang.String] matches excluded pattern: '" + firstRegex + "'");
+
+		assertTrue(filter.apply("java.util.Collection").excluded());
+		assertFalse(filter.toPredicate().test("java.util.Collection"));
+		assertThat(filter.apply("java.util.Collection").getReason()).contains(
+			"Class name [java.util.Collection] matches excluded pattern: '" + secondRegex + "'");
+
+		assertFalse(filter.apply("java.time.Instant").excluded());
+		assertTrue(filter.toPredicate().test("java.time.Instant"));
+		assertThat(filter.apply("java.time.Instant").getReason()).contains(
+			"Class name [java.time.Instant] does not match any excluded pattern: '" + firstRegex + "' OR '"
+					+ secondRegex + "'");
+	}
+
 }
