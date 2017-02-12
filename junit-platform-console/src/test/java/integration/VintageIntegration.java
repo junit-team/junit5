fbb4c2f6a7d6229fe7dc34f88bb77f5e4a895311
Christian Stein <sormuras@gmail.com>
2017-02-13 00:26:57 +0100

    Add integration test
    Fixes #648 by running some sample tests (which includes failing and passing tests)
    reads the output, and ensures that we get the expected result. This integration test
    relies on external test plan execution using the standalone jar.
    
    When the something similar to the incorrect usage of a Stream's filter() method
    that removed all tests from being run happens, this integration test will fail.

---
+++ junit-platform-console/src/test/java/integration/VintageIntegration.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2015-2017 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package integration;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.junit.runners.MethodSorters.NAME_ASCENDING;
+
+import org.junit.FixMethodOrder;
+import org.junit.Ignore;
+import org.junit.Test;
+
+@FixMethodOrder(NAME_ASCENDING)
+public class VintageIntegration {
+
+	@Test
+	@Ignore("integr4tion test")
+	public void ignored() {
+		fail("this test should be ignored");
+	}
+
+	@Test
+	public void succ3ssful() {
+		assertEquals(3, 1 + 2);
+	}
+
+	@Test
+	public void f4il() {
+		fail("f4iled");
+	}
+}
