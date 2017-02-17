964135f5ee6a9de3e2ef8a4c6d4005884e1a4f0e
Marc Philipp <mail@marcphilipp.de>
2017-02-17 21:29:32 +0100

    Executes ParameterizedTest for simple ArgumentsSources

--- junit-jupiter-params/src/main/java/org/junit/jupiter/params/ArgumentsSources.java
+++ junit-jupiter-params/src/main/java/org/junit/jupiter/params/ArgumentsSources.java
@@ -10,15 +10,20 @@
 
 package org.junit.jupiter.params;
 
+import static org.junit.platform.commons.meta.API.Usage.Experimental;
+
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import org.junit.platform.commons.meta.API;
+
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
+@API(Experimental)
 public @interface ArgumentsSources {
 
 	/**
