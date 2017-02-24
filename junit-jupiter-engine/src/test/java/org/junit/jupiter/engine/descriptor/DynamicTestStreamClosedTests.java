3c3c42f6fe38e1052eb3301cdfde37df14463345
Liam Clark <L.A.Clark@student.tudelft.nl>
2017-02-24 12:26:29 +0100

    Remove redundant unchecked from DynamicTestStreamClosedTest

--- junit-jupiter-engine/src/test/java/org/junit/jupiter/engine/descriptor/DynamicTestStreamClosedTests.java
+++ junit-jupiter-engine/src/test/java/org/junit/jupiter/engine/descriptor/DynamicTestStreamClosedTests.java
@@ -35,7 +35,6 @@ public class DynamicTestStreamClosedTests {
 	private TestExtensionContext testExtensionContext;
 	private boolean isClosed;
 
-	@SuppressWarnings("unchecked")
 	@BeforeEach
 	public void before() {
 		testExtensionContext = mock(TestExtensionContext.class);
