8b2822ba9bbe9b08d14d0f080575187c291f3ed0
Christian Stein <sormuras@gmail.com>
2017-03-04 15:23:11 +0100

    Print multi-line messages indented

--- platform-tests/src/test/java/org/junit/platform/console/tasks/TreePrinterTests.java
+++ platform-tests/src/test/java/org/junit/platform/console/tasks/TreePrinterTests.java
@@ -64,7 +64,7 @@ class TreePrinterTests {
 		assertIterableEquals(
 			Arrays.asList( //
 				"╷", //
-				"├─ engine zero ↷", //
+				"├─ engine zero ↷ none", //
 				"├─ engine one ✔", //
 				"├─ engine two ✘", //
 				"└─ engine three ■"), //
