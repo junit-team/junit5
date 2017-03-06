532138a42b71c3b356d2e7a86d96d022f76ea2ba
Christian Stein <sormuras@gmail.com>
2017-03-06 14:00:57 +0100

    Use correct charset in ConsoleLauncher

--- platform-tests/src/test/java/org/junit/platform/console/ConsoleLauncherWrapper.java
+++ platform-tests/src/test/java/org/junit/platform/console/ConsoleLauncherWrapper.java
@@ -52,7 +52,7 @@ public class ConsoleLauncherWrapper {
 		try {
 			PrintStream streamOut = new PrintStream(out, false, charset.name());
 			PrintStream streamErr = new PrintStream(err, false, charset.name());
-			this.consoleLauncher = new ConsoleLauncher(parser, streamOut, streamErr);
+			this.consoleLauncher = new ConsoleLauncher(parser, streamOut, streamErr, charset);
 		}
 		catch (UnsupportedEncodingException exception) {
 			throw new AssertionError("Charset instance created but unsupported?!", exception);
