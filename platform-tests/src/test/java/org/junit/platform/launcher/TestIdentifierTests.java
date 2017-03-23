junit-team/junit5

commit 32804d0a7e22b6e37e08bc2f1e7f517dec1005e0

Redesign isTest() and isContainer() in TestDescriptor
Prior to this commit, there were ambiguities between the isTest() and
isContainer() methods in TestDescriptor. Due to the fact that each was
typically backed by a boolean value, there were four possible
combinations of those values which didn't fully represent the goal of
the feature.

This commit addresses these ambiguities by introducing a new
TestDescriptor.Type enumeration with the following constants: ENGINE,
CONTAINER, TEST, CONTAINER_AND_TEST. Any given descriptor can now only
be assigned one of the enumerated constants, and the logic for
TestDescriptor.isTest() and TestDescriptor.isContainer() is now based
solely on the descriptor type.

Furthermore, this commit introduces TestDescriptor.prune() and
TestDescriptor.pruneTree() which allow TestEngine authors to customize
what happens when TestPlan pruning is triggered by the JUnit Platform.

Issue: #508
PR: #720

junit-platform-launcher/src/main/java/org/junit/platform/launcher/TestIdentifier.java
@@ -59,7 +59,7 @@ public final class TestIdentifier implements Serializable {
 		Optional<TestSource> source = testDescriptor.getSource();
 		Set<TestTag> tags = testDescriptor.getTags();
 		boolean test = testDescriptor.isTest();
-		boolean container = !test || !testDescriptor.getChildren().isEmpty();
+		boolean container = testDescriptor.isContainer();
 		Optional<String> parentId = testDescriptor.getParent().map(
 			parentDescriptor -> parentDescriptor.getUniqueId().toString());
 		String legacyReportingName = testDescriptor.getLegacyReportingName();

junit-jupiter-engine/src/main/java/org/junit/jupiter/engine/descriptor/ClassTestDescriptor.java
@@ -102,13 +102,8 @@ public class ClassTestDescriptor extends JupiterTestDescriptor {
 	}
 
 	@Override
-	public final boolean isTest() {
-		return false;
-	}
-
-	@Override
-	public final boolean isContainer() {
-		return true;
+	public Type getType() {
+		return Type.CONTAINER;
 	}
 
 	@Override

junit-jupiter-engine/src/main/java/org/junit/jupiter/engine/descriptor/DynamicTestTestDescriptor.java
@@ -32,13 +32,8 @@ class DynamicTestTestDescriptor extends JupiterTestDescriptor {
 	}
 
 	@Override
-	public boolean isTest() {
-		return true;
-	}
-
-	@Override
-	public boolean isContainer() {
-		return false;
+	public Type getType() {
+		return Type.TEST;
 	}
 
 	@Override

junit-jupiter-engine/src/main/java/org/junit/jupiter/engine/descriptor/MethodTestDescriptor.java
@@ -68,13 +68,8 @@ public class MethodTestDescriptor extends MethodBasedTestDescriptor {
 	}
 
 	@Override
-	public boolean isTest() {
-		return true;
-	}
-
-	@Override
-	public boolean isContainer() {
-		return false;
+	public Type getType() {
+		return Type.TEST;
 	}
 
 	// --- Node ----------------------------------------------------------------

junit-jupiter-engine/src/main/java/org/junit/jupiter/engine/descriptor/TestFactoryTestDescriptor.java
@@ -47,13 +47,8 @@ public class TestFactoryTestDescriptor extends MethodTestDescriptor {
 	// --- TestDescriptor ------------------------------------------------------
 
 	@Override
-	public boolean isTest() {
-		return false;
-	}
-
-	@Override
-	public boolean isContainer() {
-		return true;
+	public Type getType() {
+		return Type.CONTAINER;
 	}
 
 	@Override

junit-jupiter-engine/src/main/java/org/junit/jupiter/engine/descriptor/TestTemplateTestDescriptor.java
@@ -42,13 +42,8 @@ public class TestTemplateTestDescriptor extends MethodBasedTestDescriptor {
 	}
 
 	@Override
-	public boolean isTest() {
-		return false;
-	}
-
-	@Override
-	public boolean isContainer() {
-		return true;
+	public Type getType() {
+		return Type.CONTAINER;
 	}
 
 	@Override

junit-jupiter-engine/src/main/java/org/junit/jupiter/engine/discovery/DiscoverySelectorResolver.java
@@ -64,7 +64,7 @@ public class DiscoverySelectorResolver {
 		request.getSelectorsByType(UniqueIdSelector.class).forEach(selector -> {
 			javaElementsResolver.resolveUniqueId(selector.getUniqueId());
 		});
-		pruneTree(engineDescriptor);
+		engineDescriptor.pruneTree();
 	}
 
 	private JavaElementsResolver createJavaElementsResolver(TestDescriptor engineDescriptor) {
@@ -75,15 +75,6 @@ public class DiscoverySelectorResolver {
 		resolvers.add(new TestFactoryMethodResolver());
 		resolvers.add(new TestTemplateMethodResolver());
 		return new JavaElementsResolver(engineDescriptor, resolvers);
-	}
-
-	private void pruneTree(TestDescriptor root) {
-		TestDescriptor.Visitor removeChildrenWithoutTests = (descriptor) -> {
-			if (!descriptor.isRoot() && !descriptor.hasTests()) {
-				descriptor.removeFromHierarchy();
-			}
-		};
-		root.accept(removeChildrenWithoutTests);
 	}
 
 }

junit-jupiter-engine/src/test/java/org/junit/jupiter/engine/descriptor/TestTemplateTestDescriptorTests.java
@@ -42,14 +42,10 @@ class TestTemplateTestDescriptorTests {
 
 	private AbstractTestDescriptor containerTestDescriptorWithTags(UniqueId uniqueId, Set<TestTag> tags) {
 		return new AbstractTestDescriptor(uniqueId, "testDescriptor with tags") {
-			@Override
-			public boolean isContainer() {
-				return true;
-			}
 
 			@Override
-			public boolean isTest() {
-				return false;
+			public Type getType() {
+				return Type.CONTAINER;
 			}
 
 			@Override

commit 197691cf22bafa44e1b55171c53af1fcf40a9cb9

TestIdentifier now uses TestDescriptor.Type
Addresses the "4 boolean states" issue of #407 by using the same
enumeration `TestDescriptor.Type` introduced by #720.

junit-platform-launcher/src/main/java/org/junit/platform/launcher/TestIdentifier.java
@@ -45,8 +45,7 @@ public final class TestIdentifier implements Serializable {
 	private final String legacyReportingName;
 	private final TestSource source;
 	private final Set<TestTag> tags;
-	private final boolean test;
-	private final boolean container;
+	private final TestDescriptor.Type type;
 
 	/**
 	 * Factory for creating a new {@link TestIdentifier} from a {@link TestDescriptor}.
@@ -56,25 +55,24 @@ public final class TestIdentifier implements Serializable {
 		Preconditions.notNull(testDescriptor, "TestDescriptor must not be null");
 		String uniqueId = testDescriptor.getUniqueId().toString();
 		String displayName = testDescriptor.getDisplayName();
-		Optional<TestSource> source = testDescriptor.getSource();
+		TestSource source = testDescriptor.getSource().orElse(null);
 		Set<TestTag> tags = testDescriptor.getTags();
-		boolean test = testDescriptor.isTest();
-		boolean container = testDescriptor.isContainer();
-		Optional<String> parentId = testDescriptor.getParent().map(
-			parentDescriptor -> parentDescriptor.getUniqueId().toString());
+		TestDescriptor.Type type = testDescriptor.getType();
+		String parentId = testDescriptor.getParent().map(
+			parentDescriptor -> parentDescriptor.getUniqueId().toString()).orElse(null);
 		String legacyReportingName = testDescriptor.getLegacyReportingName();
-		return new TestIdentifier(uniqueId, displayName, source, tags, test, container, parentId, legacyReportingName);
+		return new TestIdentifier(uniqueId, displayName, source, tags, type, parentId, legacyReportingName);
 	}
 
-	TestIdentifier(String uniqueId, String displayName, Optional<TestSource> source, Set<TestTag> tags, boolean test,
-			boolean container, Optional<String> parentId, String legacyReportingName) {
+	TestIdentifier(String uniqueId, String displayName, TestSource source, Set<TestTag> tags, TestDescriptor.Type type,
+			String parentId, String legacyReportingName) {
+		Preconditions.notNull(type, "TestDescriptor.Type must not be null");
 		this.uniqueId = uniqueId;
-		this.parentId = parentId.orElse(null);
+		this.parentId = parentId;
 		this.displayName = displayName;
-		this.source = source.orElse(null);
+		this.source = source;
 		this.tags = unmodifiableSet(new LinkedHashSet<>(tags));
-		this.test = test;
-		this.container = container;
+		this.type = type;
 		this.legacyReportingName = legacyReportingName;
 	}
 
@@ -137,17 +135,34 @@ public final class TestIdentifier implements Serializable {
 	}
 
 	/**
+	 * Get the underlying descriptor type.
+	 *
+	 * @return the underlying descriptor type; never {@code null}
+	 */
+	public TestDescriptor.Type getType() {
+		return type;
+	}
+
+	/**
 	 * Determine if this identifier represents a test.
+	 *
+	 * @return {@code true} if the underlying descriptor type represents a test,
+	 * {@code false} otherwise
+	 * @see TestDescriptor.Type#isTest()
 	 */
 	public boolean isTest() {
-		return this.test;
+		return getType().isTest();
 	}
 
 	/**
 	 * Determine if this identifier represents a container.
+	 *
+	 * @return {@code true} if the underlying descriptor type represents a container,
+	 * {@code false} otherwise
+	 * @see TestDescriptor.Type#isContainer()
 	 */
 	public boolean isContainer() {
-		return this.container;
+		return getType().isContainer();
 	}
 
 	/**
@@ -194,8 +209,7 @@ public final class TestIdentifier implements Serializable {
 				.append("legacyReportingName", this.legacyReportingName)
 				.append("source", this.source)
 				.append("tags", this.tags)
-				.append("test", this.test)
-				.append("container", this.container)
+				.append("type", this.type)
 				.toString();
 		// @formatter:on
 	}

junit-platform-surefire-provider/src/test/java/org/junit/platform/surefire/provider/RunListenerAdapterTests.java
@@ -222,12 +222,13 @@ class RunListenerAdapterTests {
 		when(parent.getUniqueId()).thenReturn(newId());
 		when(parent.getDisplayName()).thenReturn(parentDisplay);
 		when(parent.getLegacyReportingName()).thenReturn(parentDisplay);
+		when(parent.getType()).thenReturn(TestDescriptor.Type.CONTAINER);
 		TestIdentifier parentId = TestIdentifier.from(parent);
 
 		// The (child) test case that is to be executed as part of a test plan.
 		TestDescriptor child = mock(TestDescriptor.class);
 		when(child.getUniqueId()).thenReturn(newId());
-		when(child.isTest()).thenReturn(true);
+		when(child.getType()).thenReturn(TestDescriptor.Type.TEST);
 
 		// Ensure the child source is null yet that there is a parent -- the special case to be tested.
 		when(child.getSource()).thenReturn(Optional.empty());

platform-tests/src/test/java/org/junit/platform/launcher/TestIdentifierTests.java
@@ -17,8 +17,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.platform.commons.util.SerializationUtils.serializeAndDeserialize;
 
-import java.util.Optional;
-
 import org.junit.jupiter.api.Test;
 import org.junit.platform.engine.TestDescriptor;
 import org.junit.platform.engine.TestTag;
@@ -41,16 +39,32 @@ class TestIdentifierTests {
 	}
 
 	@Test
+	void inheritsTypeFromDescriptor() {
+		TestDescriptor descriptor = new TestDescriptorStub(UniqueId.root("aType", "uniqueId"), "displayName");
+		TestIdentifier identifier = TestIdentifier.from(descriptor);
+		assertEquals(TestDescriptor.Type.TEST, identifier.getType());
+		assertTrue(identifier.isTest());
+		assertFalse(identifier.isContainer());
+
+		descriptor.addChild(new TestDescriptorStub(UniqueId.root("aChild", "uniqueId"), "displayName"));
+		identifier = TestIdentifier.from(descriptor);
+		assertEquals(TestDescriptor.Type.CONTAINER, identifier.getType());
+		assertFalse(identifier.isTest());
+		assertTrue(identifier.isContainer());
+	}
+
+	@Test
 	void serialization() throws Exception {
 		TestIdentifier identifier = serializeAndDeserialize(//
-			new TestIdentifier("uniqueId", "displayName", Optional.of(new ClassSource(TestIdentifierTests.class)),
-				singleton(TestTag.create("aTag")), true, false, Optional.of("parentId"), "reportingName"));
+			new TestIdentifier("uniqueId", "displayName", new ClassSource(TestIdentifierTests.class),
+				singleton(TestTag.create("aTag")), TestDescriptor.Type.TEST, "parentId", "reportingName"));
 
 		assertEquals("uniqueId", identifier.getUniqueId());
 		assertEquals("displayName", identifier.getDisplayName());
 		assertEquals("reportingName", identifier.getLegacyReportingName());
 		assertThat(identifier.getSource()).contains(new ClassSource(TestIdentifierTests.class));
 		assertEquals(singleton(TestTag.create("aTag")), identifier.getTags());
+		assertEquals(TestDescriptor.Type.TEST, identifier.getType());
 		assertTrue(identifier.isTest());
 		assertFalse(identifier.isContainer());
 		assertThat(identifier.getParentId()).contains("parentId");