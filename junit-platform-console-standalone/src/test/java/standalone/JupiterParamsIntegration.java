33d260362716beaf7575c561b2c8348973cb4576
Marc Philipp <mail@marcphilipp.de>
2017-03-31 21:30:24 +0200

    Include junit-jupiter-params in standalone JAR
    Fixes #759.

---
+++ junit-platform-console-standalone/src/test/java/standalone/JupiterParamsIntegration.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2015-2017 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package standalone;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class JupiterParamsIntegration {
+
+	@ParameterizedTest(name = "[{index}] argument={0}")
+	@ValueSource(strings = "test")
+	void parameterizedTest(String argument) {
+		assertEquals("test", argument);
+	}
+}
