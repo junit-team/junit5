643947e73c66156b0f01fbb1b7a3a0d803d48e1a
Liam Clark <L.A.Clark@student.tudelft.nl>
2017-04-09 00:28:04 +0200

    Consider static member classes as potential JUnit4 tests
    The IsPotentialJUnit4TestClass now checks if a memberclass is declared static.
    If so it lets it through the filter.
    
    fixes #781

---
+++ junit-vintage-engine/src/test/java/org/junit/vintage/engine/discovery/IsPotentialJUnit4TestClassTests.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2015-2017 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.junit.vintage.engine.discovery;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class IsPotentialJUnit4TestClassTests {
+
+	private IsPotentialJUnit4TestClass isPotentialJUnit4TestClass = new IsPotentialJUnit4TestClass();
+
+	@Test
+	void staticMemberClass() {
+		assertTrue(isPotentialJUnit4TestClass.test(Foo.class));
+	}
+
+	public static class Foo {
+
+	}
+
+	@Test
+	void nonPublicClass() {
+		assertFalse(isPotentialJUnit4TestClass.test(Bar.class));
+	}
+
+	static class Bar {
+
+	}
+
+	@Test
+	void abstractClass() {
+		assertFalse(isPotentialJUnit4TestClass.test(Baz.class));
+	}
+
+	public static abstract class Baz {
+
+	}
+
+	@Test
+	void anonymousClass() {
+		Foo foo = new Foo() {
+
+		};
+
+		assertFalse(isPotentialJUnit4TestClass.test(foo.getClass()));
+	}
+
+	public class FooBaz {
+
+	}
+
+	@Test
+	void publicInnerClass() {
+		assertFalse(isPotentialJUnit4TestClass.test(FooBaz.class));
+	}
+}
