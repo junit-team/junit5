bae2768d7f34bbc2310834393bd212ea1dd343b8
Marc Philipp <mail@marcphilipp.de>
2017-04-09 20:13:37 +0200

    Test that a context’s ParameterResolver can resolve constructor args

--- junit-jupiter-engine/src/test/java/org/junit/jupiter/engine/TestTemplateInvocationTests.java
+++ junit-jupiter-engine/src/test/java/org/junit/jupiter/engine/TestTemplateInvocationTests.java
@@ -227,6 +227,25 @@ class TestTemplateInvocationTests extends AbstractJupiterTestEngineTests {
 	}
 
 	@Test
+	void contextParameterResolverCanResolveConstructorArguments() {
+		LauncherDiscoveryRequest request = request().selectors(
+			selectMethod(MyTestTemplateTestCaseWithConstructor.class, "template", "java.lang.String")).build();
+
+		ExecutionEventRecorder eventRecorder = executeTests(request);
+
+		assertRecordedExecutionEventsContainsExactly(eventRecorder.getExecutionEvents(), //
+			wrappedInContainerEvents(MyTestTemplateTestCase.class, //
+				event(container("template"), started()), //
+				event(dynamicTestRegistered("test-template-invocation:#1"), displayName("[1] foo")), //
+				event(test("test-template-invocation:#1"), started()), //
+				event(test("test-template-invocation:#1"), finishedSuccessfully()), //
+				event(dynamicTestRegistered("test-template-invocation:#2"), displayName("[2] bar")), //
+				event(test("test-template-invocation:#2"), started()), //
+				event(test("test-template-invocation:#2"), finishedSuccessfully()), //
+				event(container("template"), finishedSuccessfully())));
+	}
+
+	@Test
 	void templateWithDynamicTestInstancePostProcessorIsInvoked() {
 		LauncherDiscoveryRequest request = request().selectors(
 			selectMethod(MyTestTemplateTestCase.class, "templateWithDynamicTestInstancePostProcessor")).build();
@@ -412,6 +431,21 @@ class TestTemplateInvocationTests extends AbstractJupiterTestEngineTests {
 		@ExtendWith(InvocationContextProviderWithCloseableStream.class)
 		@TestTemplate
 		void templateWithCloseableStream() {
+		}
+	}
+
+	@ExtendWith(StringParameterResolvingInvocationContextProvider.class)
+	static class MyTestTemplateTestCaseWithConstructor {
+
+		private final String constructorParameter;
+
+		MyTestTemplateTestCaseWithConstructor(String constructorParameter) {
+			this.constructorParameter = constructorParameter;
+		}
+
+		@TestTemplate
+		void template(String parameter) {
+			assertEquals(constructorParameter, parameter);
 		}
 	}
 
