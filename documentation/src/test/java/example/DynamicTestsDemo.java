3e87fccb9acbf2d90d20c686b9d1a1b75220698b
Christian Stein <sormuras@gmail.com>
2017-05-30 09:35:42 +0200

    Document dynamic container feature
    Closes #831

--- documentation/src/test/java/example/DynamicTestsDemo.java
+++ documentation/src/test/java/example/DynamicTestsDemo.java
@@ -45,24 +45,6 @@ class DynamicTestsDemo {
 	}
 
 	@TestFactory
-	Stream<DynamicNode> dynamicTestsWithContainers() {
-		// end::user_guide[]
-		// @formatter:off
-		// tag::user_guide[]
-		return Stream.of("A", "B", "C")
-			.map(input -> dynamicContainer("Container " + input, Stream.of(
-				dynamicTest("not null", () -> assertNotNull(input)),
-				dynamicContainer("properties", Stream.of(
-					dynamicTest("length > 0", () -> assertTrue(input.length() > 0)),
-					dynamicTest("not empty", () -> assertFalse(input.isEmpty()))
-				))
-			)));
-		// end::user_guide[]
-		// @formatter:on
-		// tag::user_guide[]
-	}
-
-	@TestFactory
 	Collection<DynamicTest> dynamicTestsFromCollection() {
 		// end::user_guide[]
 		// @formatter:off
@@ -165,6 +147,26 @@ class DynamicTestsDemo {
 
 		// Returns a stream of dynamic tests.
 		return DynamicTest.stream(inputGenerator, displayNameGenerator, testExecutor);
+	}
+
+	@TestFactory
+	Stream<DynamicNode> dynamicTestsWithContainers() {
+		// end::user_guide[]
+		// @formatter:off
+		// tag::user_guide[]
+		return Stream.of("A", "B", "C")
+			.map(input -> dynamicContainer("Container " + input,
+				Stream.of(
+				dynamicTest("not null", () -> assertNotNull(input)),
+				dynamicContainer("properties",
+					Stream.of(
+					dynamicTest("length > 0", () -> assertTrue(input.length() > 0)),
+					dynamicTest("not empty", () -> assertFalse(input.isEmpty()))
+				))
+			)));
+		// end::user_guide[]
+		// @formatter:on
+		// tag::user_guide[]
 	}
 
 }
