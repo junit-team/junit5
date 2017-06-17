24defc7225f8f9679da04dace88305073b601530
Marc Philipp <mail@marcphilipp.de>
2017-06-17 13:01:47 +0200

    Document org.junit.jupiter.params.ParameterizedTest#name
    Issue: #830

--- junit-jupiter-params/src/main/java/org/junit/jupiter/params/ParameterizedTest.java
+++ junit-jupiter-params/src/main/java/org/junit/jupiter/params/ParameterizedTest.java
@@ -66,10 +66,21 @@ import org.junit.platform.commons.meta.API;
 public @interface ParameterizedTest {
 
 	/**
-	 * @return a custom display name; never blank or consisting solely of
-	 * whitespace
+	 * The name pattern to be used for invocations of the parameterized test;
+	 * never blank or consisting solely of whitespace.
+	 *
+	 * <p>You may use the following placeholders:
+	 * <ul>
+	 * <li>{@code {index}}: the current invocation index (1-based)</li>
+	 * <li>{@code {arguments}}: the complete, comma-separated arguments list</li>
+	 * <li>{@code {0}}, {@code {1}}, etc.: an individual argument</li>
+	 * </ul>
+	 *
+	 * <p>For the latter, you may use {@link java.text.MessageFormat} patterns
+	 * to customize formatting of values.
+	 *
+	 * @see java.text.MessageFormat
 	 */
-	// TODO [#830] Document name().
 	String name() default "[{index}] {arguments}";
 
 }
