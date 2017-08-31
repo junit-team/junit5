b6c03c6a9f368186e15c2b3071b4459e8bc12f33
Sam Brannen <sam@sambrannen.com>
2017-08-31 17:31:28 +0200

    Extract ApiReport class from ApiReportGenerator

--- platform-tests/src/test/java/org/junit/ApiReportGenerator.java
+++ platform-tests/src/test/java/org/junit/ApiReportGenerator.java
@@ -10,9 +10,6 @@
 
 package org.junit;
 
-import java.io.PrintStream;
-import java.lang.reflect.Modifier;
-import java.nio.CharBuffer;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.EnumMap;
@@ -33,28 +30,22 @@ class ApiReportGenerator {
 
 	private static final Logger logger = Logger.getLogger(ApiReportGenerator.class.getName());
 
-	private static final String MARKDOWN_FORMAT = "%-52s | %-42s | %-12s | %-27s%n";
-
 	private static final String EOL = System.lineSeparator();
 
 	public static void main(String... args) {
 		ApiReportGenerator reportGenerator = new ApiReportGenerator();
 
 		// scan all types below "org.junit" package
-		reportGenerator.scanPackages("org.junit");
+		ApiReport apiReport = reportGenerator.generateReport("org.junit");
 
-		reportGenerator.printMarkdownReport(System.out);
+		apiReport.printMarkdownReport(System.out);
 	}
 
-	private List<Class<?>> types;
-
-	private final Map<Usage, List<Class<?>>> usageMap = new EnumMap<>(Usage.class);
-
-	public void scanPackages(String... packages) {
+	ApiReport generateReport(String... packages) {
+		// Scan packages
 		ScanResult scanResult = new FastClasspathScanner(packages).scan();
 
 		// Collect names
-
 		List<String> names = new ArrayList<>();
 		names.addAll(scanResult.getNamesOfClassesWithAnnotation(API.class));
 		names.addAll(scanResult.getNamesOfAnnotationsWithMetaAnnotation(API.class));
@@ -69,74 +60,26 @@ class ApiReportGenerator {
 		});
 
 		// Collect types
-
-		this.types = scanResult.classNamesToClassRefs(names);
+		List<Class<?>> types = scanResult.classNamesToClassRefs(names);
 		// only retain directly annotated types
-		this.types.removeIf(c -> !c.isAnnotationPresent(API.class));
-		this.types.sort(Comparator.comparing(type -> type.getName()));
+		types.removeIf(c -> !c.isAnnotationPresent(API.class));
+		types.sort(Comparator.comparing(type -> type.getName()));
 
 		logger.fine(() -> {
-			StringBuilder builder = new StringBuilder("Listing of all " + this.types.size() + " annotated types:");
+			StringBuilder builder = new StringBuilder("Listing of all " + types.size() + " annotated types:");
 			builder.append(EOL);
-			this.types.forEach(e -> builder.append(e).append(EOL));
+			types.forEach(e -> builder.append(e).append(EOL));
 			return builder.toString();
 		});
 
-		// ---------------------------------------------------------------------
-
-		// Build usage map
+		// Build map
+		Map<Usage, List<Class<?>>> declarationsMap = new EnumMap<>(Usage.class);
 		for (Usage usage : Usage.values()) {
-			this.usageMap.put(usage, new ArrayList<>());
+			declarationsMap.put(usage, new ArrayList<>());
 		}
-		this.types.forEach(type -> this.usageMap.get(type.getAnnotation(API.class).value()).add(type));
-	}
+		types.forEach(type -> declarationsMap.get(type.getAnnotation(API.class).value()).add(type));
 
-	public void printMarkdownReport(PrintStream out) {
-		out.println("# `@API` Declarations");
-		out.println();
-		out.println("Discovered " + this.types.size() + " types with `@API` declarations.");
-		this.usageMap.forEach((k, v) -> this.printMarkdown(k, v, out));
-	}
-
-	private void printMarkdown(Usage usage, List<Class<?>> types, PrintStream out) {
-		out.printf("%n## `@API(%s)`%n", usage);
-		out.printf("%nDiscovered %d `@API(%s)` declarations.%n", types.size(), usage);
-		if (types.size() > 0) {
-			out.printf("%n");
-			out.printf(MARKDOWN_FORMAT, "PACKAGE NAME", "CLASS NAME", "TYPE", "MODIFIERS");
-			out.printf(MARKDOWN_FORMAT, dashes(52), dashes(42), dashes(12), dashes(27));
-			types.forEach(type -> this.printMarkdown(type, out));
-		}
-	}
-
-	private void printMarkdown(Class<?> type, PrintStream out) {
-		out.printf(MARKDOWN_FORMAT, //
-			code(type.getPackage().getName()), //
-			code(type.getSimpleName()), //
-			code(getKind(type)), //
-			code(Modifier.toString(type.getModifiers())) //
-		);
-	}
-
-	private static String getKind(Class<?> type) {
-		if (type.isAnnotation()) {
-			return "annotation";
-		}
-		if (type.isEnum()) {
-			return "enum";
-		}
-		if (type.isInterface()) {
-			return "interface";
-		}
-		return "class";
-	}
-
-	private static String code(String element) {
-		return "`" + element + "`";
-	}
-
-	private static String dashes(int length) {
-		return CharBuffer.allocate(length).toString().replace('\0', '-');
+		return new ApiReport(types, declarationsMap);
 	}
 
 }
