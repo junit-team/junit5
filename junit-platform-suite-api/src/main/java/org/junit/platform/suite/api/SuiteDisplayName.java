46c3e741213b7ed20e05cbbef462fc44e23bb0ad
Sam Brannen <sam@sambrannen.com>
2017-08-31 16:07:12 +0200

    Introduce @SuiteDisplayName in the platform suite API
    Issue: #1036

---
+++ junit-platform-suite-api/src/main/java/org/junit/platform/suite/api/SuiteDisplayName.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2015-2017 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v20.html
+ */
+
+package org.junit.platform.suite.api;
+
+import static org.junit.platform.commons.meta.API.Usage.Maintained;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.junit.platform.commons.meta.API;
+
+/**
+ * {@code @SuiteDisplayName} is used to declare a {@linkplain #value custom
+ * display name} for the annotated test class that is executed as a test suite
+ * on the JUnit Platform.
+ *
+ * <p>Display names are typically used for test reporting in IDEs and build
+ * tools and may contain spaces, special characters, and even emoji.
+ *
+ * <h4>JUnit 4 Suite Support</h4>
+ * <p>Test suites can be run on the JUnit Platform in a JUnit 4 environment via
+ * {@code @RunWith(JUnitPlatform.class)}.
+ *
+ * @since 1.1
+ * @see org.junit.platform.runner.JUnitPlatform
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Documented
+@API(Maintained)
+public @interface SuiteDisplayName {
+
+	/**
+	 * Custom display name for the annotated class.
+	 *
+	 * @return a custom display name; never blank or consisting solely of
+	 * whitespace
+	 */
+	String value();
+
+}
