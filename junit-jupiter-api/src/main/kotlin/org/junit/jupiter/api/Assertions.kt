b3ab661c7b3877198bf4607399e82a87bad8e246
Jonathan Leitschuh <Jonathan.Leitschuh@plexxi.com>
2017-09-14 18:45:12 -0400

    Cleanup Kolin Assertion PR code post merge to master

--- junit-jupiter-api/src/main/kotlin/org/junit/jupiter/api/Assertions.kt
+++ junit-jupiter-api/src/main/kotlin/org/junit/jupiter/api/Assertions.kt
@@ -2,16 +2,17 @@
  * Copyright 2015-2017 the original author or authors.
  *
  * All rights reserved. This program and the accompanying materials are
- * made available under the terms of the Eclipse Public License v1.0 which
+ * made available under the terms of the Eclipse Public License v2.0 which
  * accompanies this distribution and is available at
  *
- * http://www.eclipse.org/legal/epl-v10.html
+ * http://www.eclipse.org/legal/epl-v20.html
  */
+@file:API(status = EXPERIMENTAL, since = "5.0.1")
 package org.junit.jupiter.api
 
+import org.apiguardian.api.API
+import org.apiguardian.api.API.Status.EXPERIMENTAL
 import org.junit.jupiter.api.function.Executable
-import org.junit.platform.commons.meta.API
-import org.junit.platform.commons.meta.API.Usage.Experimental
 import java.util.function.Supplier
 import java.util.stream.Stream
 
@@ -23,33 +24,25 @@ internal fun ExecutableStream.convert() = map { Executable(it) }
 
 /**
  * @see Assertions.assertAll
- * @since 5.0
  */
-@API(Experimental)
 fun assertAll(executables: ExecutableStream) =
     Assertions.assertAll(executables.convert())
 
 /**
  * @see Assertions.assertAll
- * @since 5.0
  */
-@API(Experimental)
 fun assertAll(heading: String?, executables: ExecutableStream) =
     Assertions.assertAll(heading, executables.convert())
 
 /**
  * @see Assertions.assertAll
- * @since 5.0
  */
-@API(Experimental)
 fun assertAll(vararg executables: () -> Unit) =
     assertAll(executables.toList().stream())
 
 /**
  * @see Assertions.assertAll
- * @since 5.0
  */
-@API(Experimental)
 fun assertAll(heading: String?, vararg executables: () -> Unit) =
     assertAll(heading, executables.toList().stream())
 
@@ -62,9 +55,7 @@ fun assertAll(heading: String?, vararg executables: () -> Unit) =
  * assertEquals("Talk to a duck", exception.message)
  * ```
  * @see Assertions.assertThrows
- * @since 5.0
  */
-@API(Experimental)
 inline fun <reified T : Throwable> assertThrows(noinline executable: () -> Unit): T =
     Assertions.assertThrows(T::class.java, Executable(executable))
 
@@ -77,9 +68,7 @@ inline fun <reified T : Throwable> assertThrows(noinline executable: () -> Unit)
  * assertEquals("Talk to a duck", exception.message)
  * ```
  * @see Assertions.assertThrows
- * @since 5.0
  */
-@API(Experimental)
 inline fun <reified T : Throwable> assertThrows(message: String, noinline executable: () -> Unit): T =
     assertThrows({ message }, executable)
 
@@ -92,9 +81,7 @@ inline fun <reified T : Throwable> assertThrows(message: String, noinline execut
  * assertEquals("Talk to a duck", exception.message)
  * ```
  * @see Assertions.assertThrows
- * @since 5.0
  */
-@API(Experimental)
 inline fun <reified T : Throwable> assertThrows(noinline message: () -> String, noinline executable: () -> Unit): T =
     Assertions.assertThrows(T::class.java, Executable(executable), Supplier {
         /*
