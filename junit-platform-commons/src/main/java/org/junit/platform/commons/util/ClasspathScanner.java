f922f8e2cf1a5574325c7777d7532704d8ccb774
Sam Brannen <sam@sambrannen.com>
2017-09-30 15:08:28 +0200

    Log class loading errors in ClasspathScanner at DEBUG level
    Issue: #1083

--- junit-platform-commons/src/main/java/org/junit/platform/commons/util/ClasspathScanner.java
+++ junit-platform-commons/src/main/java/org/junit/platform/commons/util/ClasspathScanner.java
@@ -188,7 +188,7 @@ class ClasspathScanner {
 
 	private void logMalformedClassName(Path classFile, String fullyQualifiedClassName, InternalError ex) {
 		try {
-			logger.warn(ex, () -> format("The java.lang.Class loaded from path [%s] has a malformed class name [%s].",
+			logger.debug(ex, () -> format("The java.lang.Class loaded from path [%s] has a malformed class name [%s].",
 				classFile.toAbsolutePath(), fullyQualifiedClassName));
 		}
 		catch (Throwable t) {
@@ -198,7 +198,7 @@ class ClasspathScanner {
 	}
 
 	private void logGenericFileProcessingException(Path classFile, Throwable throwable) {
-		logger.warn(throwable, () -> format("Failed to load java.lang.Class for path [%s] during classpath scanning.",
+		logger.debug(throwable, () -> format("Failed to load java.lang.Class for path [%s] during classpath scanning.",
 			classFile.toAbsolutePath()));
 	}
 
--- platform-tests/src/test/java/org/junit/platform/commons/util/ClasspathScannerTests.java
+++ platform-tests/src/test/java/org/junit/platform/commons/util/ClasspathScannerTests.java
@@ -74,7 +74,7 @@ class ClasspathScannerTests {
 		};
 
 		assertClassesScannedWhenExceptionIsThrown(malformedClassNameSimulationFilter);
-		assertWarningMessageLogged(listener, "Failed to load java.lang.Class for path .+ during classpath scanning.");
+		assertDebugMessageLogged(listener, "Failed to load java.lang.Class for path .+ during classpath scanning.");
 	}
 
 	@Test
@@ -89,7 +89,7 @@ class ClasspathScannerTests {
 		};
 
 		assertClassesScannedWhenExceptionIsThrown(malformedClassNameSimulationFilter);
-		assertWarningMessageLogged(listener, "The java.lang.Class loaded from path .+ has a malformed class name .+");
+		assertDebugMessageLogged(listener, "The java.lang.Class loaded from path .+ has a malformed class name .+");
 	}
 
 	@Test
@@ -102,7 +102,7 @@ class ClasspathScannerTests {
 		};
 
 		assertClassesScannedWhenExceptionIsThrown(otherInternalErrorSimulationFilter);
-		assertWarningMessageLogged(listener, "Failed to load java.lang.Class for path .+ during classpath scanning.");
+		assertDebugMessageLogged(listener, "Failed to load java.lang.Class for path .+ during classpath scanning.");
 	}
 
 	@Test
@@ -115,7 +115,7 @@ class ClasspathScannerTests {
 		};
 
 		assertClassesScannedWhenExceptionIsThrown(runtimeExceptionSimulationFilter);
-		assertWarningMessageLogged(listener, "Failed to load java.lang.Class for path .+ during classpath scanning.");
+		assertDebugMessageLogged(listener, "Failed to load java.lang.Class for path .+ during classpath scanning.");
 	}
 
 	private void assertClassesScannedWhenExceptionIsThrown(Predicate<Class<?>> filter) throws Exception {
@@ -125,9 +125,9 @@ class ClasspathScannerTests {
 		assertThat(classes.size()).isGreaterThanOrEqualTo(150);
 	}
 
-	private void assertWarningMessageLogged(LogRecordListener listener, String regex) {
+	private void assertDebugMessageLogged(LogRecordListener listener, String regex) {
 		// @formatter:off
-		assertThat(listener.getLogRecords(ClasspathScanner.class, Level.WARNING)
+		assertThat(listener.getLogRecords(ClasspathScanner.class, Level.FINE)
 				.map(LogRecord::getMessage)
 				.filter(m -> m.matches(regex))
 				.count()
@@ -356,7 +356,7 @@ class ClasspathScannerTests {
 
 		@Override
 		public Enumeration<URL> getResources(String name) throws IOException {
-			throw new IOException();
+			throw new IOException("Demo I/O error");
 		}
 	}
 
