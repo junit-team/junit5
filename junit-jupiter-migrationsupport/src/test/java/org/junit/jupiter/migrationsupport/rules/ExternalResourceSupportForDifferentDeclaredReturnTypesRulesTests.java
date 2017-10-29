60a82fcb9307bf528925ebf55b4870784b34f35d
Marc Philipp <mail@marcphilipp.de>
2017-10-29 18:43:43 +0100

    Only instantiate TestRules once, only take into account actual type
    Issues: #1128

--- junit-jupiter-migrationsupport/src/test/java/org/junit/jupiter/migrationsupport/rules/ExternalResourceSupportForDifferentDeclaredReturnTypesRulesTests.java
+++ junit-jupiter-migrationsupport/src/test/java/org/junit/jupiter/migrationsupport/rules/ExternalResourceSupportForDifferentDeclaredReturnTypesRulesTests.java
@@ -10,78 +10,69 @@
 
 package org.junit.jupiter.migrationsupport.rules;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.migrationsupport.rules.FailAfterAllHelper.fail;
 
 import org.junit.Rule;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.rules.ExternalResource;
-import org.junit.rules.Verifier;
+import org.junit.rules.TestRule;
 
-@EnableRuleMigrationSupport
-public class EnableRuleMigrationSupportWithBothRuleTypesTests {
+@ExtendWith(ExternalResourceSupport.class)
+class ExternalResourceSupportForDifferentDeclaredReturnTypesRulesTests {
+
+	private static boolean beforeOfRule1WasExecuted = false;
+	private static boolean beforeOfRule2WasExecuted = false;
 
 	private static boolean afterOfRule1WasExecuted = false;
-
-	private static boolean beforeOfRule2WasExecuted = false;
 	private static boolean afterOfRule2WasExecuted = false;
 
-	private static int numberOfRule1InstancesCreated = 0;
-	private static int numberOfRule2InstancesCreated = 0;
+	@Rule
+	public MyExternalResource1 getResource1() {
+		return new MyExternalResource1();
+	}
 
 	@Rule
-	public Verifier verifier1 = new Verifier() {
-		{
-			numberOfRule1InstancesCreated++;
-		}
-
-		@Override
-		protected void verify() throws Throwable {
-			afterOfRule1WasExecuted = true;
-		}
-	};
-
-	@Rule
-	public ExternalResource getResource2() {
+	public TestRule getResource2() {
 		return new ExternalResource() {
-			{
-				numberOfRule2InstancesCreated++;
-			}
-
-			private Object instance;
-
 			@Override
 			protected void before() throws Throwable {
-				instance = this;
 				beforeOfRule2WasExecuted = true;
 			}
 
 			@Override
 			protected void after() {
-				assertNotNull(instance);
-				assertSame(instance, this);
 				afterOfRule2WasExecuted = true;
 			}
 		};
 	}
 
 	@Test
-	void beforeMethodOfBothRule2WasExecuted() {
+	void beforeMethodsOfBothRulesWereExecuted() {
+		assertTrue(beforeOfRule1WasExecuted);
 		assertTrue(beforeOfRule2WasExecuted);
 	}
 
 	@AfterAll
 	static void afterMethodsOfBothRulesWereExecuted() {
-		assertEquals(1, numberOfRule1InstancesCreated);
-		assertEquals(1, numberOfRule2InstancesCreated);
 		if (!afterOfRule1WasExecuted)
 			fail();
 		if (!afterOfRule2WasExecuted)
 			fail();
+	}
+
+	private static class MyExternalResource1 extends ExternalResource {
+		@Override
+		protected void before() throws Throwable {
+			beforeOfRule1WasExecuted = true;
+		}
+
+		@Override
+		protected void after() {
+			afterOfRule1WasExecuted = true;
+		}
 	}
 
 }
