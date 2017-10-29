4f5d424ebf6f67b49eb0ce5cc0f9bf877b246b5e
Marc Philipp <mail@marcphilipp.de>
2017-10-29 18:43:43 +0100

    Only instantiate TestRules once, only take into account actual type
    Issues: #1128

--- junit-jupiter-migrationsupport/src/main/java/org/junit/jupiter/migrationsupport/rules/TestRuleSupport.java
+++ junit-jupiter-migrationsupport/src/main/java/org/junit/jupiter/migrationsupport/rules/TestRuleSupport.java
@@ -10,24 +10,27 @@
 
 package org.junit.jupiter.migrationsupport.rules;
 
+import static java.util.stream.Collectors.toList;
 import static org.junit.platform.commons.util.AnnotationUtils.findPublicAnnotatedFields;
 import static org.junit.platform.commons.util.AnnotationUtils.isAnnotated;
 import static org.junit.platform.commons.util.ReflectionUtils.findMethods;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.Member;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 import org.junit.Rule;
 import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ExtensionContext.Namespace;
 import org.junit.jupiter.api.extension.TestExecutionExceptionHandler;
 import org.junit.jupiter.migrationsupport.rules.adapter.AbstractTestRuleAdapter;
 import org.junit.jupiter.migrationsupport.rules.adapter.GenericBeforeAndAfterAdvice;
@@ -41,7 +44,7 @@ import org.junit.rules.TestRule;
  */
 class TestRuleSupport implements BeforeEachCallback, TestExecutionExceptionHandler, AfterEachCallback {
 
-	private static final Consumer<List<Member>> NO_OP = members -> {
+	private static final Consumer<List<TestRuleAnnotatedMember>> NO_OP = members -> {
 	};
 
 	private final Class<? extends TestRule> ruleType;
@@ -54,26 +57,25 @@ class TestRuleSupport implements BeforeEachCallback, TestExecutionExceptionHandl
 		this.ruleType = ruleType;
 	}
 
-	private List<Member> findRuleAnnotatedMembers(Object testInstance) {
-		List<Member> members = new ArrayList<>();
-		members.addAll(findAnnotatedFields(testInstance));
-		members.addAll(findAnnotatedMethods(testInstance));
-		return members;
+	private List<TestRuleAnnotatedMember> findRuleAnnotatedMembers(Object testInstance) {
+		// @formatter:off
+		return Stream.concat(
+					findAnnotatedFields(testInstance).stream(),
+					findAnnotatedMethods(testInstance).stream())
+				.map(member -> TestRuleAnnotatedMemberFactory.from(testInstance, member))
+				.collect(toList());
+		// @formatter:on
 	}
 
 	private List<Method> findAnnotatedMethods(Object testInstance) {
 		Predicate<Method> isRuleMethod = method -> isAnnotated(method, Rule.class);
-		Predicate<Method> hasCorrectReturnType = method -> method.getReturnType().isAssignableFrom(getRuleType());
+		Predicate<Method> hasCorrectReturnType = method -> TestRule.class.isAssignableFrom(method.getReturnType());
 
 		return findMethods(testInstance.getClass(), isRuleMethod.and(hasCorrectReturnType));
 	}
 
 	private List<Field> findAnnotatedFields(Object testInstance) {
-		return findPublicAnnotatedFields(testInstance.getClass(), getRuleType(), Rule.class);
-	}
-
-	private Class<? extends TestRule> getRuleType() {
-		return this.ruleType;
+		return findPublicAnnotatedFields(testInstance.getClass(), TestRule.class, Rule.class);
 	}
 
 	@Override
@@ -83,7 +85,7 @@ class TestRuleSupport implements BeforeEachCallback, TestExecutionExceptionHandl
 
 	@Override
 	public void handleTestExecutionException(ExtensionContext context, Throwable throwable) throws Throwable {
-		int numRuleAnnotatedMembers = invokeAppropriateMethodOnRuleAnnotatedMembers(context, Collections::reverse,
+		long numRuleAnnotatedMembers = invokeAppropriateMethodOnRuleAnnotatedMembers(context, Collections::reverse,
 			advice -> {
 				try {
 					advice.handleTestExecutionException(throwable);
@@ -110,21 +112,28 @@ class TestRuleSupport implements BeforeEachCallback, TestExecutionExceptionHandl
 	/**
 	 * @return the number of appropriate rule-annotated members that were discovered
 	 */
-	private int invokeAppropriateMethodOnRuleAnnotatedMembers(ExtensionContext context, Consumer<List<Member>> ordering,
-			Consumer<GenericBeforeAndAfterAdvice> methodCaller) {
-
-		Object testInstance = context.getRequiredTestInstance();
-		List<Member> members = findRuleAnnotatedMembers(testInstance);
-		ordering.accept(members);
-
+	private long invokeAppropriateMethodOnRuleAnnotatedMembers(ExtensionContext context,
+			Consumer<List<TestRuleAnnotatedMember>> ordering, Consumer<GenericBeforeAndAfterAdvice> methodCaller) {
 		// @formatter:off
-		members.stream()
-				.map(member -> TestRuleAnnotatedMemberFactory.from(testInstance, member))
+		return Stream.of(getRuleAnnotatedMembers(context))
+				.map(ArrayList::new)
+				.peek(ordering::accept)
+				.flatMap(Collection::stream)
+				.filter(annotatedMember -> this.ruleType.isInstance(annotatedMember.getTestRule()))
 				.map(this.adapterGenerator)
-				.forEach(methodCaller::accept);
+				.peek(methodCaller::accept)
+				.count();
 		// @formatter:on
+	}
 
-		return members.size();
+	@SuppressWarnings("unchecked")
+	private List<TestRuleAnnotatedMember> getRuleAnnotatedMembers(ExtensionContext context) {
+		Object testInstance = context.getRequiredTestInstance();
+		Namespace namespace = Namespace.create(TestRuleSupport.class, context.getRequiredTestClass());
+		// @formatter:off
+		return context.getStore(namespace)
+				.getOrComputeIfAbsent("rule-annotated-members", key -> findRuleAnnotatedMembers(testInstance), List.class);
+		// @formatter:on
 	}
 
 }
