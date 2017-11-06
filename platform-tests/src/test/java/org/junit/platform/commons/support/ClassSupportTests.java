2e61253c4b2c748614c026cbdbf408ddd37eae75
Johannes Link <business@johanneslink.net>
2017-11-06 09:42:31 +0100

    Added org.junit.platform.commons.support.ClassSupport

---
+++ platform-tests/src/test/java/org/junit/platform/commons/support/ClassSupportTests.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2015-2017 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v20.html
+ */
+
+package org.junit.platform.commons.support;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import java.util.function.Function;
+
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.ClassUtils;
+
+/**
+ * @since 1.0.2
+ */
+class ClassSupportTests {
+
+	@Test
+	void nullSafeToStringDelegates() throws Throwable {
+		assertEquals(ClassUtils.nullSafeToString(String.class, List.class),
+			ClassSupport.nullSafeToString(String.class, List.class));
+
+		Function<Class<?>, String> classToStringMapper = aClass -> aClass.getSimpleName() + "-Test";
+		assertEquals(ClassUtils.nullSafeToString(classToStringMapper, String.class, List.class),
+				ClassSupport.nullSafeToString(classToStringMapper, String.class, List.class));
+	}
+}
