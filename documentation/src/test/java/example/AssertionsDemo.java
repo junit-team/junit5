27c9c0db6bd7b304505143b8ad1f780e146ccf8e
Jonathan Leitschuh <Jonathan.Leitschuh@plexxi.com>
2017-11-11 17:03:51 +0100

    Add Kotlin specific assertions
    Closes #924.

--- documentation/src/test/java/example/AssertionsDemo.java
+++ documentation/src/test/java/example/AssertionsDemo.java
@@ -143,23 +143,3 @@ class AssertionsDemo {
 }
 // end::user_guide[]
 // @formatter:on
-
-class Person {
-
-	private final String firstName;
-	private final String lastName;
-
-	Person(String firstName, String lastName) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-	}
-
-	String getFirstName() {
-		return firstName;
-	}
-
-	String getLastName() {
-		return lastName;
-	}
-
-}
---
+++ junit-jupiter-engine/src/test/kotlin/org/junit/jupiter/api/AssertionsAssertAllKotlinTests.kt
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2015-2017 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v20.html
+ */
+package org.junit.jupiter.api
+
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.AssertionTestUtils.assertMessageStartsWith
+import org.junit.jupiter.api.AssertionTestUtils.expectAssertionFailedError
+import org.opentest4j.AssertionFailedError
+import org.opentest4j.MultipleFailuresError
+import java.util.stream.Stream
+import kotlin.reflect.KClass
+
+/**
+ * Unit tests for JUnit Jupiter [org.junit.jupiter.api] top-level assertion functions.
+ */
+class AssertionsAssertAllKotlinTests {
+
+    // Bonus: no null check tests as these get handled by the compiler!
+
+    @Test
+    fun `assertAll with functions that do not throw exceptions`() {
+        assertAll(
+            { assertTrue(true) },
+            { assertFalse(false) },
+            { assertTrue(true) }
+        )
+    }
+
+    @Test
+    fun `assertAll with functions that throw AssertionErrors`() {
+        val multipleFailuresError = assertThrows<MultipleFailuresError> {
+            assertAll(
+                { assertFalse(true) },
+                { assertFalse(true) }
+            )
+        }
+        assertExpectedExceptionTypes(multipleFailuresError, AssertionFailedError::class, AssertionFailedError::class)
+    }
+
+    @Test
+    fun `assertAll with streamOf functions that throw AssertionErrors`() {
+        val multipleFailuresError = assertThrows<MultipleFailuresError>("Should have thrown multiple errors") {
+            assertAll(Stream.of({ assertFalse(true) }, { assertFalse(true) }))
+        }
+        assertExpectedExceptionTypes(multipleFailuresError, AssertionFailedError::class, AssertionFailedError::class)
+    }
+
+    @Test
+    fun `assertThrows that does not have exception thrown does not throw KotlinNullPointer`() {
+        val assertionMessage = "This will not throw an exception"
+        val error = assertThrows<AssertionFailedError>("assertThrows did not throw the correct exception") {
+            assertThrows<IllegalStateException>(assertionMessage) { }
+            // This should never execute
+            expectAssertionFailedError()
+        }
+        assertMessageStartsWith(error, assertionMessage)
+    }
+
+    companion object {
+        fun assertExpectedExceptionTypes(
+            multipleFailuresError: MultipleFailuresError,
+            vararg exceptionTypes: KClass<out Throwable>) =
+            AssertAllAssertionsTests.assertExpectedExceptionTypes(
+                multipleFailuresError, *exceptionTypes.map { it.java }.toTypedArray())
+    }
+}
