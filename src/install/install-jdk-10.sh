7954ab6a68286407a13e3154ba94c215069b1056
Christian Stein <sormuras@gmail.com>
2017-11-13 21:43:14 +0100

    Add JDK 10 Early-Access to Travis CI build matrix
    jdk-10-ea+30 from http://jdk.java.net/10/

---
+++ src/install/install-jdk-10.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+JDK_FEATURE=10
+JDK_BUILD=30
+JDK_ARCHIVE=jdk-${JDK_FEATURE}-ea+${JDK_BUILD}_linux-x64_bin.tar.gz
+
+cd ~
+wget http://download.java.net/java/jdk${JDK_FEATURE}/archive/${JDK_BUILD}/binaries/${JDK_ARCHIVE}
+tar -xzf ${JDK_ARCHIVE}
+export JAVA_HOME=~/jdk-${JDK_FEATURE}
+export PATH=${JAVA_HOME}/bin:$PATH
+cd -
+java --version
