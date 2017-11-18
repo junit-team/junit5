e697a717764c8bbc6f85f08c3aa756c873750097
Christian Stein <sormuras@gmail.com>
2017-11-18 19:19:06 +0100

    Extract "most recent build" number automatically

--- src/install/install-jdk-10.sh
+++ src/install/install-jdk-10.sh
@@ -2,7 +2,10 @@
 set -e
 
 JDK_FEATURE=10
-JDK_BUILD=32
+TMP=$(curl -L jdk.java.net/${JDK_FEATURE})              # extract most recent "JDK_BUILD" number from web page source...
+TMP="${TMP#*Most recent build: jdk-${JDK_FEATURE}-ea+}" # remove everything before the number
+TMP="${TMP%%<*}"                                        # remove everything after the number
+JDK_BUILD="$(echo -e "${TMP}" | tr -d '[:space:]')"     # remove all whitespace
 JDK_ARCHIVE=jdk-${JDK_FEATURE}-ea+${JDK_BUILD}_linux-x64_bin.tar.gz
 
 cd ~
