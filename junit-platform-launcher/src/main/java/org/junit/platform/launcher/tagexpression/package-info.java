86da5700bf98f73f54aa1511e810b581b3c2455e
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2018-01-01 19:42:24 +0100

    Do not reference package private classes in the package description

--- junit-platform-launcher/src/main/java/org/junit/platform/launcher/tagexpression/package-info.java
+++ junit-platform-launcher/src/main/java/org/junit/platform/launcher/tagexpression/package-info.java
@@ -1,6 +1,5 @@
 /**
- * The tag expression language {@link org.junit.platform.launcher.tagexpression.Parser}
- * and related support classes for.
+ * The tag expression language parser and related support classes for.
  */
 
 package org.junit.platform.launcher.tagexpression;
