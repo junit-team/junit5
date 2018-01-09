f7c49036d727fe5dcd3c54c703e43fabad8f2386
Marc Philipp <mail@marcphilipp.de>
2018-01-09 21:29:11 +0100

    Document and polish LegacyReportingUtils
    Issue: #1182

--- junit-platform-launcher/src/main/java/org/junit/platform/launcher/listeners/LegacyReportingUtils.java
+++ junit-platform-launcher/src/main/java/org/junit/platform/launcher/listeners/LegacyReportingUtils.java
@@ -10,17 +10,48 @@
 
 package org.junit.platform.launcher.listeners;
 
-import static org.apiguardian.api.API.Status.EXPERIMENTAL;
+import static org.apiguardian.api.API.Status.MAINTAINED;
 
 import org.apiguardian.api.API;
+import org.junit.platform.commons.util.Preconditions;
 import org.junit.platform.engine.support.descriptor.ClassSource;
 import org.junit.platform.launcher.TestIdentifier;
 import org.junit.platform.launcher.TestPlan;
 
-@API(status = EXPERIMENTAL, since = "1.1")
+/**
+ * Utility methods for dealing with legacy reporting infrastructure, such as
+ * reporting systems built on the Ant-based XML reporting format for JUnit 4.
+ *
+ * @since 1.1
+ */
+@API(status = MAINTAINED, since = "1.1")
 public class LegacyReportingUtils {
 
-	public static String getLegacyReportingClassName(TestPlan testPlan, TestIdentifier testIdentifier) {
+	///CLOVER:OFF
+	private LegacyReportingUtils() {
+		/* no-op */
+	}
+	///CLOVER:ON
+
+	/**
+	 * Get the class name for the supplied {@link TestIdentifier} using the
+	 * supplied {@link TestPlan}.
+	 *
+	 * <p>This implementation attempts to find the closest test identifier with
+	 * a {@link ClassSource} by traversing the hierarchy upwards towards the
+	 * root starting with the supplied test identifier. In case no such source
+	 * is found, it falls back to using the parent's
+	 * {@linkplain TestIdentifier#getLegacyReportingName legacy reporting name}.
+	 *
+	 * @param testPlan the test plan that contains the {@code TestIdentifier};
+	 *                 never {@code null}
+	 * @param testIdentifier the identifier to determine the class name for;
+	 *                 never {@code null}
+	 * @see TestIdentifier#getLegacyReportingName
+	 */
+	public static String getClassName(TestPlan testPlan, TestIdentifier testIdentifier) {
+		Preconditions.notNull(testPlan, "testPlan must not be null");
+		Preconditions.notNull(testIdentifier, "testIdentifier must not be null");
 		for (TestIdentifier current = testIdentifier; current != null; current = getParent(testPlan, current)) {
 			ClassSource source = getClassSource(current);
 			if (source != null) {
