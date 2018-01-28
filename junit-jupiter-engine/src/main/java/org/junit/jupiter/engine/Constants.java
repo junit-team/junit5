09f2ca2d4087fdfd01c1fb1bc5034f2cee126184
Christian Stein <sormuras@gmail.com>
2018-01-29 00:20:55 +0100

    Introduce @DisabledIf annotation
    - Merge logic of @EnabledIf and @DisabledIf execution
    condition into a single implementation.

--- junit-jupiter-engine/src/main/java/org/junit/jupiter/engine/Constants.java
+++ junit-jupiter-engine/src/main/java/org/junit/jupiter/engine/Constants.java
@@ -10,9 +10,12 @@
 
 package org.junit.jupiter.engine;
 
+import static org.apiguardian.api.API.Status.EXPERIMENTAL;
 import static org.apiguardian.api.API.Status.STABLE;
 
 import org.apiguardian.api.API;
+import org.junit.jupiter.api.DisabledIf;
+import org.junit.jupiter.api.EnabledIf;
 
 /**
  * Collection of constants related to the {@link JupiterTestEngine}.
@@ -87,6 +90,90 @@ public final class Constants {
 
 	private Constants() {
 		/* no-op */
+	}
+
+	/**
+	 * Script-related constants used by {@link DisabledIf} and {@link EnabledIf}.
+	 *
+	 * <p>Also declares names used for script {@link javax.script.Bindings bindings}.
+	 */
+	@API(status = EXPERIMENTAL, since = "5.1")
+	public interface Script {
+
+		String DEFAULT_ENGINE_NAME = "Nashorn";
+
+		/**
+		 * Names used for script {@link javax.script.Bindings bindings}.
+		 */
+		interface Bind {
+
+			/**
+			 * Set of all tags assigned to the current extension context.
+			 *
+			 * <p>Value type: {@code Set<String>}
+			 *
+			 * @see org.junit.jupiter.api.extension.ExtensionContext#getTags()
+			 */
+			String JUNIT_TAGS = "junitTags";
+
+			/**
+			 * Unique ID associated with the current extension context.
+			 *
+			 * <p>Value type: {@code String}
+			 *
+			 * @see org.junit.jupiter.api.extension.ExtensionContext#getUniqueId()
+			 */
+			String JUNIT_UNIQUE_ID = "junitUniqueId";
+
+			/**
+			 * Display name of the test or container.
+			 *
+			 * <p>Value type: {@code String}
+			 *
+			 * @see org.junit.jupiter.api.extension.ExtensionContext#getDisplayName()
+			 */
+			String JUNIT_DISPLAY_NAME = "junitDisplayName";
+
+			/**
+			 * Accessor for JUnit Platform configuration parameters.
+			 *
+			 * <p>Usage: {@code junitConfigurationParameter.get(key) -> String}
+			 *
+			 * @see org.junit.jupiter.api.extension.ExtensionContext#getConfigurationParameter(String)
+			 */
+			String JUNIT_CONFIGURATION_PARAMETER = "junitConfigurationParameter";
+
+			/**
+			 * Accessor for JVM system properties.
+			 *
+			 * <p>Usage: {@code systemProperty.get(key) -> String}
+			 *
+			 * @see System#getProperty(String)
+			 */
+			String SYSTEM_PROPERTY = "systemProperty";
+
+			/**
+			 * Accessor for operating system environment variables.
+			 *
+			 * <p>Usage: {@code systemEnvironment.get(key) -> String}
+			 *
+			 * @see System#getenv(String)
+			 */
+			String SYSTEM_ENVIRONMENT = "systemEnvironment";
+
+		}
+
+		/**
+		 * Placeholders usable in reason format string.
+		 */
+		interface Reason {
+
+			String DEFAULT_PATTERN = "Script `{source}` evaluated to: {result}";
+
+			String ANNOTATION_PLACEHOLDER = "{annotation}";
+			String RESULT_PLACEHOLDER = "{result}";
+			String SOURCE_PLACEHOLDER = "{source}";
+		}
 	}
 
 }
