65c8640e052ca9d8236d739fc405f8dc3d6f73cf
Sam Brannen <sam@sambrannen.com>
2018-02-03 17:22:30 +0100

    Document predefined @Disabled*/@Enabled* in User Guide
    Issue: #219

--- documentation/src/test/java/example/ConditionalTestExecutionDemo.java
+++ documentation/src/test/java/example/ConditionalTestExecutionDemo.java
@@ -12,15 +12,113 @@ package example;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.condition.JRE.JAVA_10;
+import static org.junit.jupiter.api.condition.JRE.JAVA_8;
+import static org.junit.jupiter.api.condition.JRE.JAVA_9;
+import static org.junit.jupiter.api.condition.OS.LINUX;
+import static org.junit.jupiter.api.condition.OS.MAC;
+import static org.junit.jupiter.api.condition.OS.WINDOWS;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.time.LocalDate;
 
 import org.junit.jupiter.api.DisabledIf;
 import org.junit.jupiter.api.EnabledIf;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
+import org.junit.jupiter.api.condition.DisabledOnJre;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
+import org.junit.jupiter.api.condition.EnabledOnJre;
+import org.junit.jupiter.api.condition.EnabledOnOs;
 
 class ConditionalTestExecutionDemo {
+
+	// tag::user_guide_os[]
+	@Test
+	@EnabledOnOs(MAC)
+	void onlyOnMacOs() {
+		// ...
+	}
+
+	@TestOnMac
+	void testOnMac() {
+		// ...
+	}
+
+	@Test
+	@EnabledOnOs({ LINUX, MAC })
+	void onLinuxOrMac() {
+		// ...
+	}
+
+	@Test
+	@DisabledOnOs(WINDOWS)
+	void notOnWindows() {
+		// ...
+	}
+
+	@Target(ElementType.METHOD)
+	@Retention(RetentionPolicy.RUNTIME)
+	@Test
+	@EnabledOnOs(MAC)
+	@interface TestOnMac {
+	}
+	// end::user_guide_os[]
+
+	// tag::user_guide_jre[]
+	@Test
+	@EnabledOnJre(JAVA_8)
+	void onlyOnJava8() {
+		// ...
+	}
+
+	@Test
+	@EnabledOnJre({ JAVA_9, JAVA_10 })
+	void onJava9Or10() {
+		// ...
+	}
+
+	@Test
+	@DisabledOnJre(JAVA_9)
+	void notOnJava9() {
+		// ...
+	}
+	// end::user_guide_jre[]
+
+	// tag::user_guide_system_property[]
+	@Test
+	@EnabledIfSystemProperty(named = "os.arch", matches = ".*64.*")
+	void onlyOn64BitArchitectures() {
+		// ...
+	}
+
+	@Test
+	@DisabledIfSystemProperty(named = "ci-server", matches = "true")
+	void notOnCiServer() {
+		// ...
+	}
+	// end::user_guide_system_property[]
+
+	// tag::user_guide_environment_variable[]
+	@Test
+	@EnabledIfEnvironmentVariable(named = "ENV", matches = "staging-server")
+	void onlyOnStagingServer() {
+		// ...
+	}
+
+	@Test
+	@DisabledIfEnvironmentVariable(named = "ENV", matches = ".*development.*")
+	void notOnDeveloperWorkstation() {
+		// ...
+	}
+	// end::user_guide_environment_variable[]
 
 	// tag::user_guide_scripts[]
 	@Test // Static JavaScript expression.
