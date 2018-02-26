8bb185077816ec00a6152b5fe4a3b9a7e81296dc
MiniDigger <mini.digger@live.de>
2018-02-26 13:54:21 +0100

    Declare proper JDK version in example usage
    copy paste typos are the best typos

--- src/install/install-jdk.sh
+++ src/install/install-jdk.sh
@@ -14,8 +14,8 @@
 #   install-jdk.sh -F 9            | install most recent OpenJDK 9
 #   install-jdk.sh -F 10           | install most recent OpenJDK 10
 #   install-jdk.sh -F 10 -L BCL    | install most recent OracleJDK 10
-#   install-jdk.sh -F 11           | install most recent OpenJDK 10
-#   install-jdk.sh -F 11 -L BCL    | install most recent OracleJDK 10
+#   install-jdk.sh -F 11           | install most recent OpenJDK 11
+#   install-jdk.sh -F 11 -L BCL    | install most recent OracleJDK 11
 #
 # Options
 #
