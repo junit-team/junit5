b65487d1ac53097b67d176a435a7dd75fc79ffc8
Christian Stein <sormuras@gmail.com>
2018-03-06 09:48:02 +0100

    Add test cases for failing reflective operations in dynamic tests
    Addresses #1322

--- junit-jupiter-engine/src/test/java/org/junit/jupiter/api/DynamicTestTests.java
+++ junit-jupiter-engine/src/test/java/org/junit/jupiter/api/DynamicTestTests.java
@@ -12,12 +12,17 @@ package org.junit.jupiter.api;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import org.opentest4j.AssertionFailedError;
 
 class DynamicTestTests {
 
@@ -50,6 +55,47 @@ class DynamicTestTests {
 			throw new Throwable("Baz!");
 		}
 		this.assertedValues.add(str);
+	}
+
+	@Test
+	void reflectiveOperationThrowingAssertionFailedError() {
+		Throwable t48 = assertThrows(AssertionFailedError.class,
+			() -> dynamicTest("1 == 48", this::assertOneEquals48).getExecutable().execute());
+		assertThat(t48).hasMessage("expected: <1> but was: <48>");
+
+		Throwable t49 = assertThrows(AssertionFailedError.class,
+			() -> dynamicTest("1 == 49", this::assertOnEquals49).getExecutable().execute());
+		assertThat(t49).hasMessage("expected: <1> but was: <49>");
+	}
+
+	@Test
+	@Disabled("https://github.com/junit-team/junit5/issues/1322")
+	void reflectiveOperationThrowingUnexpectedException() {
+		// Fails with: Unexpected exception type thrown
+		//   ==> expected: <org.opentest4j.AssertionFailedError>
+		//        but was: <java.lang.reflect.InvocationTargetException>
+		Throwable t50 = assertThrows(AssertionFailedError.class,
+			() -> dynamicTest("1 == 50", this::assertOneEquals50).getExecutable().execute());
+		assertThat(t50).hasMessage("expected: <1> but was: <50>");
+	}
+
+	private void assertOneEquals48() {
+		Assertions.assertEquals(1, 48);
+	}
+
+	private void assertOnEquals49() throws Throwable {
+		Method method = Assertions.class.getMethod("assertEquals", int.class, int.class);
+		try {
+			method.invoke(null, 1, 49);
+		}
+		catch (InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	private void assertOneEquals50() throws Throwable {
+		Method method = Assertions.class.getMethod("assertEquals", int.class, int.class);
+		method.invoke(null, 1, 50);
 	}
 
 }
