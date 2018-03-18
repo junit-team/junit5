6562151534ca8fcde315285ab0746acd9119379d
Nelson R. Llewellyn <4811061+nrllewellyn@users.noreply.github.com>
2018-03-18 11:48:54 -0400

    Adds JavaDoc details

--- junit-jupiter-theories/src/main/java/org/junit/jupiter/theories/annotations/DataPoints.java
+++ junit-jupiter-theories/src/main/java/org/junit/jupiter/theories/annotations/DataPoints.java
@@ -6,11 +6,19 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Annotation that indicates that an element should be treated as a collection of data points for use with theories. May be used on:
+ * Annotation that indicates that an element should be treated as a group of data points for use with theories. May be used on:
  * <ul>
- * <li>Static fields containing collections</li>
- * <li>Static methods returning collections: Methods may be called multiple times, so they should always return the same values</li>
- * <li>Non-static fields containing collections: This requires that the class be annotated with {@code @TestInstance(TestInstance.Lifecycle.PER_CLASS)}</li>
+ * <li>Static fields containing groups of data points</li>
+ * <li>Static methods returning groups of data points: Methods may be called multiple times, so they should always return the same values</li>
+ * <li>Non-static fields containing groups of data points: This requires that the class be annotated with
+ * {@code @TestInstance(TestInstance.Lifecycle.PER_CLASS)}</li>
+ * <li>Non-static methods: See the limitations for static methods and non-static fields above.</li>
+ * </ul>
+ * <p>
+ * Supported data point group types:
+ * <ul>
+ * <li>{@link java.util.Collection}</li>
+ * <li>Arrays</li>
  * </ul>
  */
 @Target({ElementType.METHOD, ElementType.FIELD})
