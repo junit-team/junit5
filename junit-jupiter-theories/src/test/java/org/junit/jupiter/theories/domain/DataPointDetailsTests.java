2150455b6a5fcdeda6ad1eac336ca328548a750b
Nelson R. Llewellyn <4811061+nrllewellyn@users.noreply.github.com>
2018-03-18 11:41:55 -0400

    Adds tests for domain objects

---
+++ junit-jupiter-theories/src/test/java/org/junit/jupiter/theories/domain/DataPointDetailsTests.java
@@ -0,0 +1,33 @@
+package org.junit.jupiter.theories.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Tests for {@link DataPointDetails}.
+ */
+class DataPointDetailsTests {
+    @Test
+    public void testConstructorAndGetters() {
+        //Setup
+        Object expectedValue = "Hello world";
+        List<String> expectedQualifiers = Arrays.asList("foo", "bar", "baz");
+        String expectedSourceName = "testSource";
+
+        //Test
+        DataPointDetails objectUnderTest = new DataPointDetails(expectedValue, expectedQualifiers, expectedSourceName);
+
+        Object actualValue = objectUnderTest.getValue();
+        List<String> actualQualifiers = objectUnderTest.getQualifiers();
+        String actualSourceName = objectUnderTest.getSourceName();
+
+        //Verify
+        assertEquals(expectedValue, actualValue);
+        assertEquals(expectedQualifiers, actualQualifiers);
+        assertEquals(expectedSourceName, actualSourceName);
+    }
+}
---
+++ junit-jupiter-theories/src/test/java/org/junit/jupiter/theories/domain/TheoryParameterDetailsTests.java
@@ -0,0 +1,46 @@
+package org.junit.jupiter.theories.domain;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.theories.annotations.suppliers.IntValues;
+
+import java.lang.annotation.Annotation;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Tests for {@linke TheoryParameterDetails}.
+ */
+class TheoryParameterDetailsTests {
+    @Test
+    public void testConstructorAndGetters() {
+        //Setup
+        int expectedIndex = 42;
+        Class<?> expectedType = int.class;
+        Class<?> expectedNonPrimitiveType = Integer.class;
+        String expectedName = "testParameter";
+        List<String> expectedQualifiers = Arrays.asList("foo", "bar", "baz");
+        Optional<? extends Annotation> expectedParameterSupplierAnnotation = Optional.empty();
+
+        //Test
+        TheoryParameterDetails objectUnderTest = new TheoryParameterDetails(expectedIndex, expectedType, expectedName, expectedQualifiers,
+                expectedParameterSupplierAnnotation);
+      
+        int actualIndex = objectUnderTest.getIndex();
+        Class<?> actualType = objectUnderTest.getType();
+        Class<?> actualNonPrimitiveType = objectUnderTest.getNonPrimitiveType();
+        String actualName = objectUnderTest.getName();
+        List<String> actualQualifiers = objectUnderTest.getQualifiers();
+        Optional<? extends Annotation> actualParameterSupplierAnnotation = objectUnderTest.getParameterSupplierAnnotation();
+
+        //Verify
+        assertEquals(expectedIndex, actualIndex);
+        assertEquals(expectedType, actualType);
+        assertEquals(expectedNonPrimitiveType, actualNonPrimitiveType);
+        assertEquals(expectedName, actualName);
+        assertEquals(expectedQualifiers, actualQualifiers);
+        assertEquals(expectedParameterSupplierAnnotation, actualParameterSupplierAnnotation);
+    }
+}
