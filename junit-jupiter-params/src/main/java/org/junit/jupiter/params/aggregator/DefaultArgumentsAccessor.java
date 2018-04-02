0d3fc005a654ffeb8ffb26721a3584f27e696571
geo_m <georgemoraitis2@gmail.com>
2018-04-02 19:51:22 +0300

    Remove unneeded import

--- junit-jupiter-params/src/main/java/org/junit/jupiter/params/aggregator/DefaultArgumentsAccessor.java
+++ junit-jupiter-params/src/main/java/org/junit/jupiter/params/aggregator/DefaultArgumentsAccessor.java
@@ -11,7 +11,6 @@
 package org.junit.jupiter.params.aggregator;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 
 import org.junit.jupiter.params.converter.DefaultArgumentConverter;
