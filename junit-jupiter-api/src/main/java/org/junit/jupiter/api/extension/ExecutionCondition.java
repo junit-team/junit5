5ccd1a8fbfade1af3c7bd8afd197aace5ead87f6
Sam Brannen <sam@sambrannen.com>
2018-04-05 15:45:55 +0200

    Crosslink between @Disabled* and ExecutionCondition

--- junit-jupiter-api/src/main/java/org/junit/jupiter/api/extension/ExecutionCondition.java
+++ junit-jupiter-api/src/main/java/org/junit/jupiter/api/extension/ExecutionCondition.java
@@ -27,6 +27,16 @@ import org.apiguardian.api.API;
  *
  * @since 5.0
  * @see org.junit.jupiter.api.Disabled
+ * @see org.junit.jupiter.api.condition.EnabledIf
+ * @see org.junit.jupiter.api.condition.DisabledIf
+ * @see org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable
+ * @see org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable
+ * @see org.junit.jupiter.api.condition.EnabledIfSystemProperty
+ * @see org.junit.jupiter.api.condition.DisabledIfSystemProperty
+ * @see org.junit.jupiter.api.condition.EnabledOnJre
+ * @see org.junit.jupiter.api.condition.DisabledOnJre
+ * @see org.junit.jupiter.api.condition.EnabledOnOs
+ * @see org.junit.jupiter.api.condition.DisabledOnOs
  */
 @FunctionalInterface
 @API(status = STABLE, since = "5.0")
