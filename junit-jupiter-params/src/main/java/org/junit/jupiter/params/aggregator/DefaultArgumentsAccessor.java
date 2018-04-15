1aa05b7261fbeb3c2bd8c1eef77e04cf58d25e69
Sam Brannen <sam@sambrannen.com>
2018-04-15 16:55:50 +0200

    Introduce unit tests for ArgumentsAccessor
    Issue: #1370

--- junit-jupiter-params/src/main/java/org/junit/jupiter/params/aggregator/DefaultArgumentsAccessor.java
+++ junit-jupiter-params/src/main/java/org/junit/jupiter/params/aggregator/DefaultArgumentsAccessor.java
@@ -19,6 +19,7 @@ import java.util.List;
 
 import org.apiguardian.api.API;
 import org.junit.jupiter.params.converter.DefaultArgumentConverter;
+import org.junit.platform.commons.util.ClassUtils;
 import org.junit.platform.commons.util.Preconditions;
 
 /**
@@ -36,7 +37,7 @@ public class DefaultArgumentsAccessor implements ArgumentsAccessor {
 
 	private final Object[] arguments;
 
-	public DefaultArgumentsAccessor(Object[] arguments) {
+	public DefaultArgumentsAccessor(Object... arguments) {
 		Preconditions.notNull(arguments, "Arguments array must not be null");
 		this.arguments = arguments;
 	}
@@ -56,8 +57,10 @@ public class DefaultArgumentsAccessor implements ArgumentsAccessor {
 			Object convertedValue = DefaultArgumentConverter.INSTANCE.convert(value, requiredType);
 			return requiredType.cast(convertedValue);
 		}
-		catch (ClassCastException ex) {
-			String message = format("Argument [%s] at index [%d] could not be converted or cast to [%s]", value, index,
+		catch (Exception ex) {
+			String message = format(
+				"Argument at index [%d] with value [%s] and type [%s] could not be converted or cast to type [%s].",
+				index, value, ClassUtils.nullSafeToString(value == null ? null : value.getClass()),
 				requiredType.getName());
 			throw new ArgumentsAccessorException(message, ex);
 		}
