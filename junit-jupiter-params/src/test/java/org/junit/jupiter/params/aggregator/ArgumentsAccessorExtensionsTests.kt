75b74e8824814cd4d4154e35e3731ce4020fd911
Wilder Pereira <wilder_roberto@hotmail.com>
2018-05-01 23:07:30 -0300

    Implement test for ArgumentsAccessorExtensions
    Issue: #1382

---
+++ junit-jupiter-params/src/test/java/org/junit/jupiter/params/aggregator/ArgumentsAccessorExtensionsTests.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2015-2018 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v20.html
+ */
+package org.junit.jupiter.params.aggregator
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Test
+
+/**
+ * Unit tests for [ArgumentsAccessorExtensions].
+ */
+class ArgumentsAccessorExtensionsTests {
+
+    @Test
+    fun getWithCast() {
+        assertEquals(1, DefaultArgumentsAccessor(1).get<Int>(0))
+        assertEquals('A', DefaultArgumentsAccessor('A').get<Char>(0))
+    }
+
+    @Test
+    fun getWithCastToIncompatibleType() {
+        val exception = assertThrows<ArgumentAccessException>(ArgumentAccessException::class.java
+        ) { DefaultArgumentsAccessor(Integer.valueOf(1)).get<Char>(0) }
+        assertThat(exception.message).isEqualTo(
+                "Argument at index [0] with value [1] and type [java.lang.Integer] could not be converted or cast to type [java.lang.Character].")
+    }
+
+}
