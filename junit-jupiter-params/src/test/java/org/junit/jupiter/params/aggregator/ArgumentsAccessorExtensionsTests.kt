3cd1d3d45832177b1b736466a00051b69108547f
Wilder Pereira <wilder_roberto@hotmail.com>
2018-05-02 22:35:45 -0300

    Use the assertThrows extension function from Assertions.kt.
    Issue: #1382

--- junit-jupiter-params/src/test/java/org/junit/jupiter/params/aggregator/ArgumentsAccessorExtensionsTests.kt
+++ junit-jupiter-params/src/test/java/org/junit/jupiter/params/aggregator/ArgumentsAccessorExtensionsTests.kt
@@ -11,8 +11,8 @@ package org.junit.jupiter.params.aggregator
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 /**
  * Unit tests for [ArgumentsAccessorExtensions].
@@ -27,8 +27,10 @@ class ArgumentsAccessorExtensionsTests {
 
     @Test
     fun getWithCastToIncompatibleType() {
-        val exception = assertThrows<ArgumentAccessException>(ArgumentAccessException::class.java
-        ) { DefaultArgumentsAccessor(Integer.valueOf(1)).get<Char>(0) }
+        val exception = assertThrows<ArgumentAccessException> {
+            DefaultArgumentsAccessor(Integer.valueOf(1)).get<Char>(0)
+        }
+
         assertThat(exception.message).isEqualTo(
                 "Argument at index [0] with value [1] and type [java.lang.Integer] could not be converted or cast to type [java.lang.Character].")
     }
