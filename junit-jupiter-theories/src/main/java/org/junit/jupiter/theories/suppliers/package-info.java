cbd6ea5bd39ab31312ac24b1ee4a3c7dc9e3553f
Nelson R. Llewellyn <4811061+nrllewellyn@users.noreply.github.com>
2018-05-11 00:39:02 -0400

    Add package-info files

---
+++ junit-jupiter-theories/src/main/java/org/junit/jupiter/theories/suppliers/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Argument supplier API
+ * ({@link org.junit.jupiter.theories.suppliers.TheoryArgumentSupplier})
+ * and reference implementations for common primitive and JVM types.
+ */
+
+package org.junit.jupiter.theories.suppliers;
