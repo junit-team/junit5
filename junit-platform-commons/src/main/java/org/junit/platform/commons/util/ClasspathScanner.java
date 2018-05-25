0d76218a9d3867824c43d498a82cbb935cd1f5da
Johannes Ernst <jernst@indiecomputing.com>
2018-05-25 04:17:33 +0000

    ZipPath.relativize() returns paths like ../foo when attempting to look for test classes in packages contained in JAR files. So determineFullyQualifiedClassName constructs "fully qualified class names" that look like com.example.bar...foo.Someclass instead of com.example.foo.Someclass. Reimplemented the method without using relativize, and simplified.

--- junit-platform-commons/src/main/java/org/junit/platform/commons/util/ClasspathScanner.java
+++ junit-platform-commons/src/main/java/org/junit/platform/commons/util/ClasspathScanner.java
@@ -12,7 +12,6 @@ package org.junit.platform.commons.util;
 
 import static java.lang.String.format;
 import static java.util.Collections.emptyList;
-import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 import static org.junit.platform.commons.util.BlacklistedExceptions.rethrowIfBlacklisted;
 import static org.junit.platform.commons.util.ClassFileVisitor.CLASS_FILE_SUFFIX;
@@ -30,7 +29,6 @@ import java.util.Optional;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
-import java.util.stream.Stream;
 
 import org.junit.platform.commons.logging.Logger;
 import org.junit.platform.commons.logging.LoggerFactory;
@@ -147,31 +145,22 @@ class ClasspathScanner {
 	}
 
 	private String determineFullyQualifiedClassName(Path baseDir, String basePackageName, Path classFile) {
-		// @formatter:off
-		return Stream.of(
-					basePackageName,
-					determineSubpackageName(baseDir, classFile),
-					determineSimpleClassName(classFile)
-				)
-				.filter(value -> !value.isEmpty()) // Handle default package appropriately.
-				.collect(joining(PACKAGE_SEPARATOR_STRING));
-		// @formatter:on
-	}
-
-	private String determineSimpleClassName(Path classFile) {
-		String fileName = classFile.getFileName().toString();
-		return fileName.substring(0, fileName.length() - CLASS_FILE_SUFFIX.length());
-	}
-
-	private String determineSubpackageName(Path baseDir, Path classFile) {
-		Path relativePath = baseDir.relativize(classFile.getParent());
+		String baseDirString = baseDir.toString();
 		String pathSeparator = baseDir.getFileSystem().getSeparator();
-		String subpackageName = relativePath.toString().replace(pathSeparator, PACKAGE_SEPARATOR_STRING);
-		if (subpackageName.endsWith(pathSeparator)) {
-			// Workaround for JDK bug: https://bugs.openjdk.java.net/browse/JDK-8153248
-			subpackageName = subpackageName.substring(0, subpackageName.length() - pathSeparator.length());
+		String basePackageString = basePackageName.replace(PACKAGE_SEPARATOR_STRING, pathSeparator);
+		if (baseDirString.endsWith(basePackageString)) {
+			baseDirString = baseDirString.substring(0, baseDirString.length() - basePackageString.length());
 		}
-		return subpackageName;
+		String name = classFile.toString();
+		if (name.startsWith(baseDirString)) {
+			name = name.substring(baseDirString.length());
+		}
+		if (name.startsWith(pathSeparator)) {
+			name = name.substring(1);
+		}
+		name = name.substring(0, name.length() - CLASS_FILE_SUFFIX.length());
+		name = name.replace(pathSeparator, PACKAGE_SEPARATOR_STRING);
+		return name;
 	}
 
 	private void handleInternalError(Path classFile, String fullyQualifiedClassName, InternalError ex) {
