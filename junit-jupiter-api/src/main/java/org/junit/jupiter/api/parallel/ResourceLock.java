159bb54726b1be3364cc41b19246d1cafa25a691
Sam Brannen <sam@sambrannen.com>
2018-06-25 19:11:40 +0300

    Polish JavaDoc for @ResourceLock

--- junit-jupiter-api/src/main/java/org/junit/jupiter/api/parallel/ResourceLock.java
+++ junit-jupiter-api/src/main/java/org/junit/jupiter/api/parallel/ResourceLock.java
@@ -22,20 +22,19 @@ import org.apiguardian.api.API;
 
 /**
  * {@code @ResourceLock} is used to declare that the annotated test class or test
- * method requires access to a resource identified by a key.
+ * method requires access to a shared resource identified by a key.
  *
- * <p>The resource key is specified using {@link #value()}. In addition,
- * {@link #mode()} allows to specify whether the annotated test class or test
- * method requires {@link ResourceAccessMode#READ_WRITE} or only
- * {@link ResourceAccessMode#READ} access to the resource. In the former case,
+ * <p>The resource key is specified via {@link #value}. In addition,
+ * {@link #mode} allows you to specify whether the annotated test class or test
+ * method requires {@link ResourceAccessMode#READ_WRITE READ_WRITE} or only
+ * {@link ResourceAccessMode#READ READ} access to the resource. In the former case,
  * execution of the annotated element will occur while no other test class or
- * test method that uses this resource is being executed. In the latter case,
+ * test method that uses the shared resource is being executed. In the latter case,
  * the annotated element may be executed concurrently with other test classes or
- * methods that also require {@link ResourceAccessMode#READ} access but not at
- * the same time as any other test that requires
- * {@link ResourceAccessMode#READ_WRITE} access.
+ * methods that also require {@code READ} access but not at the same time as any
+ * other test that requires {@code READ_WRITE} access.
  *
- * <p>This annotation can be repeated to declare the use of multiple resources.
+ * <p>This annotation can be repeated to declare the use of multiple shared resources.
  *
  * @see Resources
  * @see ResourceAccessMode
@@ -57,6 +56,8 @@ public @interface ResourceLock {
 
 	/**
 	 * The resource access mode.
+	 *
+	 * <p>Defaults to {@link ResourceAccessMode#READ_WRITE READ_WRITE}.
 	 *
 	 * @see ResourceAccessMode
 	 */
