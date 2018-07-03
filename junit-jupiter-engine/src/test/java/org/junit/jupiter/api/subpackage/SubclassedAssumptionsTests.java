dc893654e6071c4c71631db46c870cc7030e0ac5
Sam Brannen <sam@sambrannen.com>
2018-07-03 14:47:55 +0200

    Allow Assertions and Assumptions classes to be extended
    Prior to this commit, it was not possible to extend the Assertions and
    Assumptions classes since they both were final and had private
    constructors.
    
    This commit removes this restriction in order to support unusual
    circumstances in which it may be "forbidden" to use static imports.
    
    Note, however, that a note has been added to the class-level JavaDoc for
    these classes in order to voice the opinion of the JUnit Team that it is
    a best practice to use static imports for methods defined in the
    Assertions and Assumptions classes.
    
    Resolves: #1484

---
+++ junit-jupiter-engine/src/test/java/org/junit/jupiter/api/subpackage/SubclassedAssumptionsTests.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2015-2018 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v20.html
+ */
+
+package org.junit.jupiter.api.subpackage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.Test;
+import org.opentest4j.TestAbortedException;
+
+/**
+ * Tests which verify that {@link Assumptions} can be subclassed.
+ *
+ * @since 5.3
+ */
+class SubclassedAssumptionsTests extends Assumptions {
+
+	@Test
+	void assumeTrueWithBooleanTrue() {
+		String foo = null;
+		try {
+			assumeTrue(true);
+			foo = "foo";
+		}
+		finally {
+			assertEquals("foo", foo);
+		}
+	}
+
+	@Test
+	void assumeFalseWithBooleanTrue() {
+		TestAbortedException exception = assertThrows(TestAbortedException.class, () -> assumeFalse(true));
+		assertEquals("Assumption failed: assumption is not false", exception.getMessage());
+	}
+
+}
