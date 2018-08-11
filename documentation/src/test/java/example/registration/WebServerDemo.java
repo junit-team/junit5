7b3d11f462e99614e50b352efcc7e5549e840b15
Stefano Mazzucco <stefano-m@users.noreply.github.com>
2018-08-11 18:02:06 +0000

    Document registering extensions via static fields in Kotlin
    JUnit Jupiter extensions can be registered programmatically using
    `@RegisterExtension`. When one wants to register extensions that
    implement class-level extension APIs, they have to be registered as
    static fields.
    
    In the Kotlin programming language this is normally achieved using the
    `@JvmStatic` annotation.  However, that will not work because a private
    field will be generated, and private fields are filtered out by JUnit
    Jupiter. The correct approach is to use `@JvmField` instead.
    
    This commit documents the correct approach and provides a Kotlin example
    analogous to the existing Java one.
    
    Resolves #1500.

--- documentation/src/test/java/example/registration/WebServerDemo.java
+++ documentation/src/test/java/example/registration/WebServerDemo.java
@@ -13,8 +13,6 @@ package example.registration;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.BeforeAllCallback;
-import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
 // tag::user_guide[]
@@ -40,49 +38,4 @@ class WebServerDemo {
 	}
 
 }
-// end::user_guide[]
-
-class WebServerExtension implements BeforeAllCallback {
-
-	static Builder builder() {
-		return new Builder();
-	}
-
-	String getServerUrl() {
-		return "http://example.org:8181";
-	}
-
-	@Override
-	public void beforeAll(ExtensionContext context) {
-		/* no-op for demo */
-	}
-
-	static class Builder {
-
-		public Builder enableSecurity(boolean b) {
-			return this;
-		}
-
-		public WebServerExtension build() {
-			return new WebServerExtension();
-		}
-
-	}
-
-}
-
-class WebClient {
-
-	WebResponse get(String string) {
-		return new WebResponse();
-	}
-
-}
-
-class WebResponse {
-
-	int getResponseStatus() {
-		return 200;
-	}
-
-}
+//end::user_guide[]
