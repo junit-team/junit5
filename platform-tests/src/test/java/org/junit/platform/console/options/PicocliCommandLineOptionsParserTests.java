6da5a3c389b62b15546df85ea8164b64178e0536
Remko Popma <remkop@yahoo.com>
2018-08-12 09:05:44 +0900

    #1498 spotless fix

--- platform-tests/src/test/java/org/junit/platform/console/options/PicocliCommandLineOptionsParserTests.java
+++ platform-tests/src/test/java/org/junit/platform/console/options/PicocliCommandLineOptionsParserTests.java
@@ -530,7 +530,8 @@ class PicocliCommandLineOptionsParserTests {
 	void parseInvalidConfigurationParametersWithDuplicateKey(ArgsType type) {
 		Exception e = assertThrows(JUnitException.class, () -> type.parseArgLine("--config foo=bar --config foo=baz"));
 
-		assertThat(e.getMessage()).isEqualTo("Error parsing command-line arguments: Duplicate key 'foo' for values 'bar' and 'baz'.");
+		assertThat(e.getMessage()).isEqualTo(
+			"Error parsing command-line arguments: Duplicate key 'foo' for values 'bar' and 'baz'.");
 		assertThat(e.getCause().getMessage()).isEqualTo("Duplicate key 'foo' for values 'bar' and 'baz'.");
 	}
 
