678470503450ff478eb566a8582bfede5fddd514
Marc Philipp <mail@marcphilipp.de>
2018-08-14 21:04:50 +0200

    Make ArgumentsAccessor Kotlin extension function accessible
    Prior to this commit, the `META-INF/junit-jupiter-params.kotlin_module`
    file was filtered out by the `shadowJar` task. This file contains
    metadata required by the Kotlin compiler to lookup extension functions.
    Now, the file ends up in the JAR file.
    
    In addition, `platform-tooling-support-tests` now contains an
    integration test that verifies that extension functions are accessible
    to downstream Kotlin projects.
    
    Fixes #1546.

---
+++ platform-tooling-support-tests/src/test/java/platform/tooling/support/tests/GradleKotlinExtensionsTests.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2015-2018 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v20.html
+ */
+
+package platform.tooling.support.tests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.nio.file.Paths;
+
+import de.sormuras.bartholdy.tool.GradleWrapper;
+
+import org.junit.jupiter.api.Test;
+import platform.tooling.support.Request;
+
+/**
+ * @since 1.3
+ */
+class GradleKotlinExtensionsTests {
+
+	@Test
+	void gradle_wrapper() {
+		var result = Request.builder() //
+				.setTool(new GradleWrapper(Paths.get(".."))) //
+				.setProject("gradle-kotlin-extensions") //
+				.addArguments("build", "--no-daemon", "--debug", "--stacktrace") //
+				.build() //
+				.run();
+
+		assertEquals(0, result.getExitCode());
+		assertTrue(result.getOutputLines("out").stream().anyMatch(line -> line.contains("BUILD SUCCESSFUL")));
+	}
+}
