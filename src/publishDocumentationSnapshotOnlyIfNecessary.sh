5e4307445e54d2e59a61f3586dd5eb8bd2b0c9c8
Christian Stein <sormuras@gmail.com>
2018-09-29 19:52:23 +0200

    Sort checksum file and clean up echos

--- src/publishDocumentationSnapshotOnlyIfNecessary.sh
+++ src/publishDocumentationSnapshotOnlyIfNecessary.sh
@@ -13,23 +13,29 @@ mkdir --parents "${checksum_directory}"
 md5sum documentation/documentation.gradle > "${current}"
 md5sum $(find documentation/src/ -type f) >> "${current}"
 md5sum $(find . -wholename '**/src/main/java/*.java') >> "${current}"
-stat "${current}"
+sort --output "${current}" "${current}"
+echo
 md5sum "${current}"
 
 #
 # compare current with published sums
 #
 curl --silent --output "${published}" "${github_pages_url}"
+md5sum "${published}"
 if cmp --silent "${current}" "${published}" ; then
   #
   # no changes detected: we're done
   #
+  echo
   echo "Already published documentation with same source checksum."
+  echo
 else
   #
   # update checksum file and trigger new documentation build and upload
   #
+  echo
   echo "Creating and publishing documentation..."
+  echo
   cp --force "${current}" "${published}"
   ./gradlew --scan gitPublishPush
 fi
