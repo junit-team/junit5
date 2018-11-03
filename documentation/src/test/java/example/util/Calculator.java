a867ccfd2d107fc0dc8d9add0483c5d1cedae4e7
saikat <psychsane@gmail.com>
2018-11-03 16:23:17 +0530

    Add Calculator class to make the assertions realistic
    Sample class copied from junit5-demo project:
    https://github.com/sbrannen/junit5-demo/blob/master/src/main/java/demo/Calculator.java
    
    Issue: #1651

---
+++ documentation/src/test/java/example/util/Calculator.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2015-2018 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v20.html
+ */
+
+package example.util;
+
+public class Calculator {
+
+	public static int add(int a, int b) {
+		return a + b;
+	}
+
+	public static int subtract(int a, int b) {
+		return a - b;
+	}
+
+	public static int multiply(int a, int b) {
+		return a * b;
+	}
+
+	public static int divide(int a, int b) {
+		return a / b;
+	}
+
+}
