436342980610f009077bd5d01221f88b75e767c6
Christian Stein <sormuras@gmail.com>
2018-11-28 11:35:43 +0100

    Introduce runtime dependency from Jupiter API to its Engine
    Prior to this commit users needed to declare a runtime dependency for
    the Jupiter Engine despite a compile time dependency to the Jupiter API
    was already in place:
    ```
      testCompile('org.junit.jupiter:junit-jupiter-api:<version>')
      testRuntime('org.junit.jupiter:junit-jupiter-engine:<version>')
    ```
    This commit introduces the runtime dependency to `junit-jupiter-engine`
    per default. This allows users to omit the explicit declaration of the
    runtime dependency:
    ```
      testCompile('org.junit.jupiter:junit-jupiter-api:<version>')
    ```
    
    This also works for Maven-based dependency resolution build tools.
    
    Addresses issue #1669

--- junit-jupiter-api/junit-jupiter-api.gradle.kts
+++ junit-jupiter-api/junit-jupiter-api.gradle.kts
@@ -4,6 +4,7 @@ dependencies {
 	api("org.opentest4j:opentest4j:${Versions.ota4j}")
 	api(project(":junit-platform-commons"))
 	compileOnly("org.jetbrains.kotlin:kotlin-stdlib")
+	runtimeOnly(project(":junit-jupiter-engine"))
 }
 
 tasks.jar {
