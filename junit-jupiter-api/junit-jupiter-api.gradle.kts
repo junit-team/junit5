e11febe4445e8b6528f61e9faab2d9741f8f5356
Christian Stein <sormuras@gmail.com>
2018-11-30 15:41:56 +0100

    Remove runtime dependency from Jupiter API to its Engine
    Reverts #1669

--- junit-jupiter-api/junit-jupiter-api.gradle.kts
+++ junit-jupiter-api/junit-jupiter-api.gradle.kts
@@ -1,13 +1,9 @@
-import org.gradle.plugins.ide.eclipse.model.Classpath
-import org.gradle.plugins.ide.eclipse.model.ProjectDependency
-
 description = "JUnit Jupiter API"
 
 dependencies {
 	api("org.opentest4j:opentest4j:${Versions.ota4j}")
 	api(project(":junit-platform-commons"))
 	compileOnly("org.jetbrains.kotlin:kotlin-stdlib")
-	runtimeOnly(project(":junit-jupiter-engine"))
 }
 
 tasks.jar {
@@ -17,12 +13,3 @@ tasks.jar {
 		)
 	}
 }
-
-// Remove runtimeOnly dependency on junit-jupiter-engine and junit-platform-engine.
-// See https://github.com/junit-team/junit5/issues/1669
-eclipse.classpath.file.whenMerged(Action<Classpath> {
-	entries.removeAll {
-		it is ProjectDependency &&
-				(it.path.contains("junit-jupiter-engine") || it.path.contains("junit-platform-engine"))
-	}
-})
