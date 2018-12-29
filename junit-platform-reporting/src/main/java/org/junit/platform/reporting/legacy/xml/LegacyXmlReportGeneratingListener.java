285251bdba08a63e40614e5dd98e9c4cb0574602
Alexey Parfenov <alepar@gmail.com>
2018-12-28 16:56:08 -0800

    last missing import

--- junit-platform-reporting/src/main/java/org/junit/platform/reporting/legacy/xml/LegacyXmlReportGeneratingListener.java
+++ junit-platform-reporting/src/main/java/org/junit/platform/reporting/legacy/xml/LegacyXmlReportGeneratingListener.java
@@ -19,6 +19,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Clock;
+import java.util.Optional;
 
 import javax.xml.stream.XMLStreamException;
 
