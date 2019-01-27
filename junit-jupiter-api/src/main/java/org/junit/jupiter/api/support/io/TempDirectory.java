73dfc230a840d446c8d85870993b5f657c1a3892
Sam Brannen <sam@sambrannen.com>
2019-01-27 18:40:32 +0100

    Remove custom file system support in TempDirectory extension
    In order to reduce the scope of the TempDirectory extension in JUnit
    Jupiter 5.4, this commit removes the support for custom file systems
    and temporary directory names by removing the ParentDirProvider and
    TempDirContext APIs and corresponding factory methods.
    
    Issue: #1752

--- junit-jupiter-api/src/main/java/org/junit/jupiter/api/support/io/TempDirectory.java
+++ junit-jupiter-api/src/main/java/org/junit/jupiter/api/support/io/TempDirectory.java
@@ -19,13 +19,11 @@ import static org.junit.platform.commons.util.ReflectionUtils.makeAccessible;
 
 import java.io.File;
 import java.io.IOException;
-import java.lang.annotation.Annotation;
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Parameter;
@@ -34,11 +32,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.List;
-import java.util.Optional;
 import java.util.SortedMap;
 import java.util.TreeMap;
-import java.util.concurrent.Callable;
 import java.util.function.Predicate;
 
 import org.apiguardian.api.API;
@@ -52,7 +47,6 @@ import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.api.extension.ParameterResolutionException;
 import org.junit.jupiter.api.extension.ParameterResolver;
 import org.junit.platform.commons.util.ExceptionUtils;
-import org.junit.platform.commons.util.Preconditions;
 import org.junit.platform.commons.util.ReflectionUtils;
 
 /**
@@ -85,20 +79,8 @@ import org.junit.platform.commons.util.ReflectionUtils;
  * directory fails, this extension will throw an {@link IOException} that will
  * cause the test or test class to fail.
  *
- * <p>By default, this extension will use the default
- * {@link java.nio.file.FileSystem FileSystem} to create temporary directories
- * in the default location. However, you may instantiate this extension using
- * the {@link TempDirectory#createInCustomDirectory(ParentDirProvider)} or
- * {@link TempDirectory#createInCustomDirectory(Callable)} factory method and
- * register it via {@link org.junit.jupiter.api.extension.RegisterExtension @RegisterExtension}
- * to pass a custom provider to configure the parent directory for all temporary
- * directories created by this extension. This allows the use of this extension
- * with any third-party {@code FileSystem} implementation &mdash; for example,
- * <a href="https://github.com/google/jimfs">Jimfs</a>.
- *
  * @since 5.4
  * @see TempDir
- * @see ParentDirProvider
  * @see Files#createTempDirectory
  */
 @API(status = EXPERIMENTAL, since = "5.4")
@@ -121,236 +103,9 @@ public final class TempDirectory implements BeforeAllCallback, BeforeEachCallbac
 	public @interface TempDir {
 	}
 
-	/**
-	 * {@code TempDirContext} encapsulates the <em>context</em> in which
-	 * {@link TempDir @TempDir} is declared.
-	 *
-	 * @see ParentDirProvider
-	 */
-	public interface TempDirContext {
-
-		/**
-		 * Get the {@link AnnotatedElement} associated with this context.
-		 *
-		 * <p>The annotated element will be the corresponding {@link Field} or
-		 * {@link Parameter} on which {@link TempDir @TempDir} is declared.
-		 *
-		 * <p>Favor this method over more specific methods whenever the
-		 * {@code AnnotatedElement} API suits the task at hand &mdash; for example,
-		 * when looking up annotations regardless of concrete element type.
-		 *
-		 * @return the {@code AnnotatedElement}; never {@code null}
-		 * @see #getField()
-		 * @see #getParameterContext()
-		 */
-		AnnotatedElement getElement();
-
-		/**
-		 * Get the {@link Field} associated with this context, if available.
-		 *
-		 * <p>If this method returns an empty {@code Optional},
-		 * {@link #getParameterContext()} will return a non-empty {@code Optional}.
-		 *
-		 * @return an {@code Optional} containing the field; never {@code null} but
-		 * potentially empty
-		 * @see #getElement()
-		 * @see #getParameterContext()
-		 */
-		Optional<Field> getField();
-
-		/**
-		 * Get the {@link ParameterContext} associated with this context, if
-		 * available.
-		 *
-		 * <p>If this method returns an empty {@code Optional},
-		 * {@link #getField()} will return a non-empty {@code Optional}.
-		 *
-		 * @return an {@code Optional} containing the {@code ParameterContext};
-		 * never {@code null} but potentially empty
-		 * @see #getElement()
-		 * @see #getField()
-		 */
-		Optional<ParameterContext> getParameterContext();
-
-		/**
-		 * Determine if an annotation of {@code annotationType} is either
-		 * <em>present</em> or <em>meta-present</em> on the {@link Field} or
-		 * {@link Parameter} associated with this context.
-		 *
-		 * <h3>WARNING</h3>
-		 * <p>Favor the use of this method over directly invoking
-		 * {@link Parameter#isAnnotationPresent(Class)} due to a bug in {@code javac}
-		 * on JDK versions prior to JDK 9.
-		 *
-		 * @param annotationType the annotation type to search for; never {@code null}
-		 * @return {@code true} if the annotation is present or meta-present
-		 * @see #findAnnotation(Class)
-		 * @see #findRepeatableAnnotations(Class)
-		 */
-		boolean isAnnotated(Class<? extends Annotation> annotationType);
-
-		/**
-		 * Find the first annotation of {@code annotationType} that is either
-		 * <em>present</em> or <em>meta-present</em> on the {@link Field} or
-		 * {@link Parameter} associated with this context.
-		 *
-		 * <h3>WARNING</h3>
-		 * <p>Favor the use of this method over directly invoking annotation lookup
-		 * methods in the {@link Parameter} API due to a bug in {@code javac} on JDK
-		 * versions prior to JDK 9.
-		 *
-		 * @param <A> the annotation type
-		 * @param annotationType the annotation type to search for; never {@code null}
-		 * @return an {@code Optional} containing the annotation; never {@code null} but
-		 * potentially empty
-		 * @see #isAnnotated(Class)
-		 * @see #findRepeatableAnnotations(Class)
-		 */
-		<A extends Annotation> Optional<A> findAnnotation(Class<A> annotationType);
-
-		/**
-		 * Find all <em>repeatable</em> {@linkplain Annotation annotations} of
-		 * {@code annotationType} that are either <em>present</em> or
-		 * <em>meta-present</em> on the {@link Field} or {@link Parameter}
-		 * associated with this context.
-		 *
-		 * <h3>WARNING</h3>
-		 * <p>Favor the use of this method over directly invoking annotation lookup
-		 * methods in the {@link Parameter} API due to a bug in {@code javac} on JDK
-		 * versions prior to JDK 9.
-		 *
-		 * @param <A> the annotation type
-		 * @param annotationType the repeatable annotation type to search for; never
-		 * {@code null}
-		 * @return the list of all such annotations found; neither {@code null} nor
-		 * mutable, but potentially empty
-		 * @see #isAnnotated(Class)
-		 * @see #findAnnotation(Class)
-		 * @see java.lang.annotation.Repeatable
-		 */
-		<A extends Annotation> List<A> findRepeatableAnnotations(Class<A> annotationType);
-
-	}
-
-	/**
-	 * {@code ParentDirProvider} can be used to configure a custom parent
-	 * directory for all temporary directories created by the
-	 * {@link TempDirectory} extension this is used with.
-	 *
-	 * @see org.junit.jupiter.api.extension.RegisterExtension
-	 * @see TempDirectory#createInCustomDirectory(ParentDirProvider)
-	 */
-	@FunctionalInterface
-	public interface ParentDirProvider {
-
-		/**
-		 * Get the parent directory for all temporary directories created by the
-		 * {@link TempDirectory} extension this is used with.
-		 *
-		 * @param tempDirContext the context for the field or parameter for which a
-		 * temporary directory should be created; never {@code null}
-		 * @param extensionContext the current extension context; never {@code null}
-		 * @return the parent directory for all temporary directories; never
-		 * {@code null}
-		 */
-		Path get(TempDirContext tempDirContext, ExtensionContext extensionContext) throws Exception;
-	}
-
-	/**
-	 * {@code TempDirProvider} is used internally to define how the temporary
-	 * directory is created.
-	 *
-	 * <p>The temporary directory is by default created on the regular
-	 * file system, but the user can also provide a custom file system
-	 * by using the {@link ParentDirProvider}. An instance of
-	 * {@code TempDirProvider} executes these (and possibly other) strategies.
-	 *
-	 * @see TempDirectory.ParentDirProvider
-	 */
-	@FunctionalInterface
-	private interface TempDirProvider {
-
-		CloseablePath get(TempDirContext tempDirContext, ExtensionContext extensionContext, String dirPrefix);
-	}
-
 	private static final Namespace NAMESPACE = Namespace.create(TempDirectory.class);
 	private static final String KEY = "temp.dir";
 	private static final String TEMP_DIR_PREFIX = "junit";
-
-	private final TempDirProvider tempDirProvider;
-
-	private TempDirectory(TempDirProvider tempDirProvider) {
-		this.tempDirProvider = Preconditions.notNull(tempDirProvider, "TempDirProvider must not be null");
-	}
-
-	/**
-	 * Create a new {@code TempDirectory} extension that uses the default
-	 * {@link java.nio.file.FileSystem FileSystem} and creates temporary
-	 * directories in the default location.
-	 *
-	 * <p>This constructor is used by the JUnit Jupiter Engine when the
-	 * extension is registered via
-	 * {@link org.junit.jupiter.api.extension.ExtendWith @ExtendWith}.
-	 */
-	public TempDirectory() {
-		this((__, ___, dirPrefix) -> createDefaultTempDir(dirPrefix));
-	}
-
-	/**
-	 * Create a {@code TempDirectory} extension that uses the default
-	 * {@link java.nio.file.FileSystem FileSystem} and creates temporary
-	 * directories in the default location.
-	 *
-	 * <p>You may use this factory method when registering this extension via
-	 * {@link org.junit.jupiter.api.extension.RegisterExtension @RegisterExtension},
-	 * although you might prefer the simpler registration via
-	 * {@link org.junit.jupiter.api.extension.ExtendWith @ExtendWith}.
-	 *
-	 * @return a {@code TempDirectory} extension; never {@code null}
-	 */
-	public static TempDirectory createInDefaultDirectory() {
-		return new TempDirectory();
-	}
-
-	/**
-	 * Create a {@code TempDirectory} extension that uses the supplied
-	 * {@link ParentDirProvider} to configure the parent directory for the
-	 * temporary directories created by this extension.
-	 *
-	 * <p>You may use this factory method when registering this extension via
-	 * {@link org.junit.jupiter.api.extension.RegisterExtension @RegisterExtension}.
-	 *
-	 * @param parentDirProvider a {@code ParentDirProvider} used to configure the
-	 * parent directory for the temporary directories created by this extension;
-	 * never {@code null}
-	 * @return a {@code TempDirectory} extension; never {@code null}
-	 */
-	public static TempDirectory createInCustomDirectory(ParentDirProvider parentDirProvider) {
-		Preconditions.notNull(parentDirProvider, "ParentDirProvider must not be null");
-
-		// @formatter:off
-		return new TempDirectory((tempDirContext, extensionContext, dirPrefix) ->
-				createCustomTempDir(parentDirProvider, tempDirContext, extensionContext, dirPrefix));
-		// @formatter:on
-	}
-
-	/**
-	 * Returns a {@code TempDirectory} extension that uses the supplied
-	 * {@link Callable} to configure the parent directory for the temporary
-	 * directories created by this extension.
-	 *
-	 * <p>You may use this factory method when registering this extension via
-	 * {@link org.junit.jupiter.api.extension.RegisterExtension @RegisterExtension}.
-	 *
-	 * @param parentDirProvider a {@code Callable} that returns a {@code Path}
-	 * used to configure the parent directory for the temporary directories
-	 * created by this extension; never {@code null}
-	 * @return a {@code TempDirectory} extension; never {@code null}
-	 */
-	public static TempDirectory createInCustomDirectory(Callable<Path> parentDirProvider) {
-		Preconditions.notNull(parentDirProvider, "parentDirProvider must not be null");
-		return createInCustomDirectory((tempDirContext, extensionContext) -> parentDirProvider.call());
-	}
 
 	/**
 	 * Perform field injection for non-private, {@code static} fields (i.e.,
@@ -376,8 +131,7 @@ public final class TempDirectory implements BeforeAllCallback, BeforeEachCallbac
 		findAnnotatedFields(context.getRequiredTestClass(), TempDir.class, predicate).forEach(field -> {
 			assertValidFieldCandidate(field);
 			try {
-				makeAccessible(field).set(testInstance,
-					getPathOrFile(field.getType(), DefaultTempDirContext.from(field), context));
+				makeAccessible(field).set(testInstance, getPathOrFile(field.getType(), context));
 			}
 			catch (Throwable t) {
 				ExceptionUtils.throwAsUncheckedException(t);
@@ -414,7 +168,7 @@ public final class TempDirectory implements BeforeAllCallback, BeforeEachCallbac
 	public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) {
 		Class<?> parameterType = parameterContext.getParameter().getType();
 		assertSupportedType("parameter", parameterType);
-		return getPathOrFile(parameterType, DefaultTempDirContext.from(parameterContext), extensionContext);
+		return getPathOrFile(parameterType, extensionContext);
 	}
 
 	private void assertSupportedType(String target, Class<?> type) {
@@ -424,52 +178,20 @@ public final class TempDirectory implements BeforeAllCallback, BeforeEachCallbac
 		}
 	}
 
-	private Object getPathOrFile(Class<?> type, TempDirContext tempDirContext, ExtensionContext extensionContext) {
+	private Object getPathOrFile(Class<?> type, ExtensionContext extensionContext) {
 		Path path = extensionContext.getStore(NAMESPACE) //
-				.getOrComputeIfAbsent(KEY,
-					key -> tempDirProvider.get(tempDirContext, extensionContext, TEMP_DIR_PREFIX), //
-					CloseablePath.class) //
+				.getOrComputeIfAbsent(KEY, key -> createTempDir(), CloseablePath.class) //
 				.get();
 
-		if (type == Path.class) {
-			return path;
-		}
-		try {
-			return path.toFile();
-		}
-		catch (UnsupportedOperationException ex) { // not default filesystem
-			String message = String.format(
-				"The configured FileSystem does not support conversion to a %s; declare a %s instead.",
-				File.class.getName(), Path.class.getName());
-			throw new ExtensionConfigurationException(message, ex);
-		}
+		return (type == Path.class) ? path : path.toFile();
 	}
 
-	private static CloseablePath createDefaultTempDir(String dirPrefix) {
+	private static CloseablePath createTempDir() {
 		try {
-			return new CloseablePath(Files.createTempDirectory(dirPrefix));
+			return new CloseablePath(Files.createTempDirectory(TEMP_DIR_PREFIX));
 		}
 		catch (Exception ex) {
 			throw new ExtensionConfigurationException("Failed to create default temp directory", ex);
-		}
-	}
-
-	private static CloseablePath createCustomTempDir(ParentDirProvider parentDirProvider, TempDirContext tempDirContext,
-			ExtensionContext extensionContext, String dirPrefix) {
-
-		Path parentDir;
-		try {
-			parentDir = parentDirProvider.get(tempDirContext, extensionContext);
-			Preconditions.notNull(parentDir, "ParentDirProvider returned null for the parent directory");
-		}
-		catch (Exception ex) {
-			throw new ExtensionConfigurationException("Failed to get parent directory from provider", ex);
-		}
-		try {
-			return new CloseablePath(Files.createTempDirectory(parentDir, dirPrefix));
-		}
-		catch (Exception ex) {
-			throw new ExtensionConfigurationException("Failed to create custom temp directory", ex);
 		}
 	}
 
--- junit-jupiter-engine/src/test/java/org/junit/jupiter/api/support/io/TempDirectoryTests.java
+++ junit-jupiter-engine/src/test/java/org/junit/jupiter/api/support/io/TempDirectoryTests.java
@@ -22,23 +22,14 @@ import static org.junit.platform.testkit.engine.EventConditions.finishedWithFail
 import static org.junit.platform.testkit.engine.TestExecutionResultConditions.cause;
 import static org.junit.platform.testkit.engine.TestExecutionResultConditions.instanceOf;
 import static org.junit.platform.testkit.engine.TestExecutionResultConditions.message;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.FileSystem;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.spi.FileSystemProvider;
-import java.util.Arrays;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.function.Supplier;
-
-import com.google.common.jimfs.Jimfs;
 
 import org.assertj.core.api.Condition;
 import org.junit.jupiter.api.AfterAll;
@@ -54,13 +45,8 @@ import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.extension.Extension;
 import org.junit.jupiter.api.extension.ExtensionConfigurationException;
-import org.junit.jupiter.api.extension.ExtensionContext.Namespace;
-import org.junit.jupiter.api.extension.ExtensionContext.Store;
-import org.junit.jupiter.api.extension.ExtensionContext.Store.CloseableResource;
 import org.junit.jupiter.api.extension.ParameterResolutionException;
-import org.junit.jupiter.api.extension.RegisterExtension;
 import org.junit.jupiter.api.support.io.TempDirectory.TempDir;
 import org.junit.jupiter.engine.AbstractJupiterTestEngineTests;
 import org.junit.platform.testkit.engine.EngineExecutionResults;
@@ -220,23 +206,6 @@ class TempDirectoryTests extends AbstractJupiterTestEngineTests {
 	}
 
 	@Nested
-	@DisplayName("resolves temp dir with custom parent dir")
-	class WithCustomParentDir {
-
-		@Test
-		@DisplayName("from Callable<Path>")
-		void resolvesTempDirWithCustomParentDirFromCallable() {
-			assertSeparateTempDirsForParameterInjection(ParentDirFromCallableTestCase.class);
-		}
-
-		@Test
-		@DisplayName("from ParentDirProvider")
-		void resolvesTempDirWithCustomParentDirFromProvider() {
-			assertSeparateTempDirsForParameterInjection(ParentDirFromProviderTestCase.class);
-		}
-	}
-
-	@Nested
 	@DisplayName("reports failure")
 	@TestMethodOrder(OrderAnnotation.class)
 	class Failures {
@@ -273,80 +242,6 @@ class TempDirectoryTests extends AbstractJupiterTestEngineTests {
 				message(m -> m.matches("Failed to resolve parameter \\[java.lang.String .+\\] in method \\[.+\\]: .+")),
 					cause(instanceOf(ExtensionConfigurationException.class)),
 					cause(message("Can only resolve @TempDir parameter of type java.nio.file.Path or java.io.File but was: java.lang.String")));
-			// @formatter:on
-		}
-
-		@Test
-		@DisplayName("when @TempDir is used on instance field of type File with a custom FileSystem")
-		@Order(3)
-		void onlySupportsInstanceFieldsOfTypeFileForDefaultFileSystem() {
-			var results = executeTestsForClass(InvalidFileFieldInjection.class);
-
-			// @formatter:off
-			TempDirectoryTests.assertSingleFailedTest(results,
-				instanceOf(ExtensionConfigurationException.class),
-				message("The configured FileSystem does not support conversion to a java.io.File; declare a java.nio.file.Path instead."),
-					cause(instanceOf(UnsupportedOperationException.class))
-			);
-			// @formatter:on
-		}
-
-		@Test
-		@DisplayName("when @TempDir is used on parameter of type File with a custom FileSystem")
-		@Order(4)
-		void onlySupportsParametersOfTypeFileForDefaultFileSystem() {
-			var results = executeTestsForClass(InvalidFileParameterInjection.class);
-
-			// @formatter:off
-			TempDirectoryTests.assertSingleFailedTest(results,
-				instanceOf(ParameterResolutionException.class),
-				message(m -> m.matches("Failed to resolve parameter \\[java.io.File .+\\] in method \\[.+\\]: .+")),
-					cause(instanceOf(ExtensionConfigurationException.class)),
-					cause(message("The configured FileSystem does not support conversion to a java.io.File; declare a java.nio.file.Path instead.")),
-						cause(cause(instanceOf(UnsupportedOperationException.class))));
-			// @formatter:on
-		}
-
-		@Test
-		@DisplayName("when attempt to create temp dir fails")
-		@Order(5)
-		void failedCreationAttemptMakesTestFail() {
-			var results = executeTestsForClass(FailedCreationAttemptTestCase.class);
-
-			// @formatter:off
-			TempDirectoryTests.assertSingleFailedTest(results,
-				instanceOf(ParameterResolutionException.class),
-				message(m -> m.matches("Failed to resolve parameter \\[java.nio.file.Path .+\\] in method \\[.+\\]: .+")),
-					cause(instanceOf(ExtensionConfigurationException.class)),
-					cause(message("Failed to create custom temp directory")),
-						cause(cause(instanceOf(RuntimeException.class))),
-						cause(cause(message("Simulated creation failure"))));
-			// @formatter:on
-		}
-
-		@Test
-		@DisplayName("when attempt to delete temp dir fails")
-		@Order(6)
-		void failedDeletionAttemptMakesTestFail() {
-			var results = executeTestsForClass(FailedDeletionAttemptTestCase.class);
-
-			assertSingleFailedTest(results, IOException.class, "Failed to delete temp directory");
-		}
-
-		@Test
-		@DisplayName("when attempt to get parent dir from ParentDirProvider fails")
-		@Order(7)
-		void erroneousParentDirProviderMakesTestFail() {
-			var results = executeTestsForClass(ErroneousParentDirProviderTestCase.class);
-
-			// @formatter:off
-			TempDirectoryTests.assertSingleFailedTest(results,
-				instanceOf(ParameterResolutionException.class),
-				message(m -> m.matches("Failed to resolve parameter \\[java.nio.file.Path .+?\\] in method \\[.+?\\]: .+")),
-					cause(instanceOf(ExtensionConfigurationException.class)),
-					cause(message("Failed to get parent directory from provider")),
-						cause(cause(instanceOf(IOException.class))),
-						cause(cause(message("something went horribly wrong"))));
 			// @formatter:on
 		}
 
@@ -780,185 +675,6 @@ class TempDirectoryTests extends AbstractJupiterTestEngineTests {
 			assertTrue(Files.exists(path));
 		}
 
-	}
-
-	@DisplayName("User can't rely on java.io.File parameter injection type for custom filesystems")
-	static class InvalidFileParameterInjection {
-		private FileSystem fileSystem;
-
-		@BeforeEach
-		void createFileSystem() {
-			fileSystem = Jimfs.newFileSystem();
-		}
-
-		@AfterEach
-		void closeFileSystem() throws Exception {
-			fileSystem.close();
-		}
-
-		@RegisterExtension
-		@SuppressWarnings("unused")
-		Extension tempDirectory = TempDirectory.createInCustomDirectory(
-			() -> Files.createDirectories(fileSystem.getPath("tmp")));
-
-		@Test
-		@DisplayName("File can't be injected using Jimfs FileSystem")
-		void failingInjection(@TempDir File tempDir) {
-			fail("this should never be called");
-		}
-	}
-
-	@DisplayName("User can't rely on java.io.File field injection type for custom filesystems")
-	static class InvalidFileFieldInjection {
-
-		@TempDir
-		File tempDir;
-
-		private static FileSystem fileSystem;
-
-		@BeforeAll
-		static void createFileSystem() {
-			fileSystem = Jimfs.newFileSystem();
-		}
-
-		@AfterAll
-		static void closeFileSystem() throws Exception {
-			fileSystem.close();
-		}
-
-		@RegisterExtension
-		@SuppressWarnings("unused")
-		static Extension tempDirectory = TempDirectory.createInCustomDirectory(
-			() -> Files.createDirectories(fileSystem.getPath("tmp")));
-
-		@Test
-		void test() {
-			fail("this should never be called");
-		}
-	}
-
-	static class ParentDirFromCallableTestCase extends BaseSeparateTempDirsParameterInjectionTestCase {
-
-		private static FileSystem fileSystem;
-
-		@BeforeAll
-		static void createFileSystem() {
-			fileSystem = Jimfs.newFileSystem();
-		}
-
-		@AfterAll
-		static void closeFileSystem() throws Exception {
-			assertThat(tempDirs.getFirst()).doesNotExist();
-			assertThat(tempDirs.getLast()).doesNotExist();
-			fileSystem.close();
-		}
-
-		@RegisterExtension
-		@SuppressWarnings("unused")
-		Extension tempDirectory = TempDirectory.createInCustomDirectory(
-			() -> Files.createDirectories(fileSystem.getPath("tmp")));
-
-	}
-
-	static class ParentDirFromProviderTestCase extends BaseSeparateTempDirsParameterInjectionTestCase {
-
-		@RegisterExtension
-		@SuppressWarnings("unused")
-		Extension tempDirectory = TempDirectory.createInCustomDirectory((tempDirContext, extensionContext) -> {
-			Store store = extensionContext.getRoot().getStore(Namespace.GLOBAL);
-			FileSystem fileSystem = store.getOrComputeIfAbsent("jimfs.fileSystem", key -> new JimfsFileSystemResource(),
-				JimfsFileSystemResource.class).get();
-			return Files.createDirectories(fileSystem.getPath("tmp"));
-		});
-
-		static class JimfsFileSystemResource implements CloseableResource {
-			private final FileSystem fileSystem;
-
-			JimfsFileSystemResource() {
-				this.fileSystem = Jimfs.newFileSystem();
-			}
-
-			FileSystem get() {
-				return fileSystem;
-			}
-
-			@Override
-			public void close() throws IOException {
-				assertThat(tempDirs.getFirst()).doesNotExist();
-				assertThat(tempDirs.getLast()).doesNotExist();
-				fileSystem.close();
-			}
-		}
-	}
-
-	static class FailedCreationAttemptTestCase {
-
-		private FileSystem fileSystem = mock(FileSystem.class);
-
-		@BeforeEach
-		void prepareFileSystem() {
-			when(fileSystem.getPath(any())).thenAnswer(invocation -> {
-				Path path = mock(Path.class, Arrays.toString(invocation.getArguments()));
-				when(path.getFileSystem()).thenThrow(new RuntimeException("Simulated creation failure"));
-				return path;
-			});
-		}
-
-		@RegisterExtension
-		@SuppressWarnings("unused")
-		Extension tempDirectory = TempDirectory.createInCustomDirectory(() -> fileSystem.getPath("tmp"));
-
-		@Test
-		void test(@SuppressWarnings("unused") @TempDir Path tempDir) {
-			fail("this should never be called");
-		}
-	}
-
-	static class FailedDeletionAttemptTestCase {
-
-		private FileSystem fileSystem = mock(FileSystem.class);
-
-		@BeforeEach
-		@SuppressWarnings("unchecked")
-		void prepareFileSystem() throws Exception {
-			FileSystemProvider provider = mock(FileSystemProvider.class);
-			when(provider.readAttributes(any(), any(Class.class), any())) //
-					.thenAnswer(invocation -> mock(invocation.getArgument(1)));
-			doThrow(new IOException("Simulated deletion failure")).when(provider).delete(any());
-			when(fileSystem.provider()).thenReturn(provider);
-			when(fileSystem.getPath(any())).thenAnswer(invocation -> {
-				Path path = mock(Path.class, Arrays.toString(invocation.getArguments()));
-				when(path.getFileSystem()).thenReturn(fileSystem);
-				when(path.toAbsolutePath()).thenReturn(path);
-				when(path.resolve(any(Path.class))).thenAnswer(invocation1 -> invocation1.getArgument(0));
-				when(path.toFile()).thenThrow(UnsupportedOperationException.class);
-				when(path.relativize(any(Path.class))).thenAnswer(invocation1 -> invocation1.getArgument(0));
-				return path;
-			});
-		}
-
-		@RegisterExtension
-		@SuppressWarnings("unused")
-		Extension tempDirectory = TempDirectory.createInCustomDirectory(() -> fileSystem.getPath("tmp"));
-
-		@Test
-		void test(@TempDir Path tempDir) {
-			assertNotNull(tempDir);
-		}
-	}
-
-	static class ErroneousParentDirProviderTestCase {
-
-		@RegisterExtension
-		@SuppressWarnings("unused")
-		Extension tempDirectory = TempDirectory.createInCustomDirectory(() -> {
-			throw new IOException("something went horribly wrong");
-		});
-
-		@Test
-		void test(@SuppressWarnings("unused") @TempDir Path tempDir) {
-			fail("this should never be called");
-		}
 	}
 
 	private static void writeFile(Path tempDir, TestInfo testInfo) throws IOException {
