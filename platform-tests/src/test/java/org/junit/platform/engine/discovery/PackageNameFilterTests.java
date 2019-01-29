b1aec2ec4c1495b93912db25afdfbb2be8e33598
JUnit Team <team@junit.org>
2019-01-29 16:54:22 +0100

    Update copyright headers

--- platform-tests/src/test/java/org/junit/platform/engine/discovery/PackageNameFilterTests.java
+++ platform-tests/src/test/java/org/junit/platform/engine/discovery/PackageNameFilterTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2018 the original author or authors.
+ * Copyright 2015-2019 the original author or authors.
  *
  * All rights reserved. This program and the accompanying materials are
  * made available under the terms of the Eclipse Public License v2.0 which
