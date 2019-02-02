495e7caf4abb0e7d8e6611a0d9fbf067b6f7c2db
Marc Philipp <mail@marcphilipp.de>
2019-02-02 11:31:48 +0100

    Upgrade nexus-publish plugin to 0.2.0

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -29,7 +29,7 @@ object Versions {
     val buildScanPlugin = "2.0.2"
     val gitPublishPlugin = "1.0.1"
     val jmhPlugin = "0.4.7"
-    val nexusPublishPlugin = "0.1.1"
+    val nexusPublishPlugin = "0.2.0"
     val shadowPlugin = "4.0.1"
     val spotlessPlugin = "3.16.0"
     val versioningPlugin = "2.7.1"
