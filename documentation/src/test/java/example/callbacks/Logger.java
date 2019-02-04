3744db0e69142c6cd52647c89ad734a4b2dcad41
Sam Brannen <sam@sambrannen.com>
2019-02-04 21:30:38 +0100

    Polish logging indentation for lifecycle callback demos
    Issue: #1620

--- documentation/src/test/java/example/callbacks/Logger.java
+++ documentation/src/test/java/example/callbacks/Logger.java
@@ -19,34 +19,35 @@ class Logger {
 	static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(Logger.class.getName());
 
 	static void beforeAllMethod(String text) {
-		log(() -> "  @BeforeAll " + text);
+		log(() -> "@BeforeAll " + text);
 	}
 
 	static void beforeEachCallback(Extension extension) {
-		log(() -> extension.getClass().getSimpleName() + ".beforeEach()");
+		log(() -> "  " + extension.getClass().getSimpleName() + ".beforeEach()");
 	}
 
 	static void beforeEachMethod(String text) {
-		log(() -> "  @BeforeEach " + text);
+		log(() -> "    @BeforeEach " + text);
 	}
 
 	static void testMethod(String text) {
-		log(() -> "    @Test " + text);
+		log(() -> "      @Test " + text);
 	}
 
 	static void afterEachMethod(String text) {
-		log(() -> "  @AfterEach " + text);
+		log(() -> "    @AfterEach " + text);
 	}
 
 	static void afterEachCallback(Extension extension) {
-		log(() -> extension.getClass().getSimpleName() + ".afterEach()");
+		log(() -> "  " + extension.getClass().getSimpleName() + ".afterEach()");
 	}
 
 	static void afterAllMethod(String text) {
-		log(() -> "  @AfterAll " + text);
+		log(() -> "@AfterAll " + text);
 	}
 
 	private static void log(Supplier<String> supplier) {
+		// System.err.println(supplier.get());
 		logger.info(supplier);
 	}
 
