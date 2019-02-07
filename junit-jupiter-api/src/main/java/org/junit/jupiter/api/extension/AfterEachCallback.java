4544de38863e2abd2c52d4b430e3915184d11939
Marc Philipp <mail@marcphilipp.de>
2019-02-07 20:54:02 +0100

    Document wrapping for lifecycle callback extensions
    Based on the excellent work of @sbrannen in the user guide.
    
    Resolves #1620.

--- junit-jupiter-api/src/main/java/org/junit/jupiter/api/extension/AfterEachCallback.java
+++ junit-jupiter-api/src/main/java/org/junit/jupiter/api/extension/AfterEachCallback.java
@@ -31,6 +31,24 @@ import org.apiguardian.api.API;
  * <p>Consult the documentation in {@link Extension} for details on
  * constructor requirements.
  *
+ * <h3>Wrapping Behavior</h3>
+ *
+ * <p>JUnit Jupiter guarantees <em>wrapping behavior</em> for multiple
+ * registered extensions that implement lifecycle callbacks such as
+ * {@link BeforeAllCallback}, {@link AfterAllCallback},
+ * {@link BeforeEachCallback}, {@link AfterEachCallback},
+ * {@link BeforeTestExecutionCallback}, and {@link AfterTestExecutionCallback}.
+ *
+ * <p>That means that, given two extensions {@code Extension1} and
+ * {@code Extension2} with {@code Extension1} registered before
+ * {@code Extension2}, any "before" callbacks implemented by {@code Extension1}
+ * are guaranteed to execute before any "before" callbacks implemented by
+ * {@code Extension2}. Similarly, given the two same two extensions registered
+ * in the same order, any "after" callbacks implemented by {@code Extension1}
+ * are guaranteed to execute after any "after" callbacks implemented by
+ * {@code Extension2}. {@code Extension1} is therefore said to <em>wrap</em>
+ * {@code Extension2}.
+ *
  * @since 5.0
  * @see org.junit.jupiter.api.AfterEach
  * @see BeforeEachCallback
