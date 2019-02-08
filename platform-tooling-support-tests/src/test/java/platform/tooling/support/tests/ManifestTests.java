b625fb6c85bca440f9ef5e2264be6221d6f8a1ad
Christian Stein <sormuras@gmail.com>
2019-02-08 14:05:47 +0100

    Fix Specification-Version entry in JAR manifest files
    Since the conversion from Groovy to Kotlin the regular expression used
    to determine the Specification-Version value produced something like:
    
      Specification-Version: MatchGroup(value=5.4.0, range=0..4)
    
    This commit fixes it by accessing the value of the match group. Also
    an integration test is added that verifies this and other manifest
    entries.
    
    Closes #1766

---
+++ platform-tooling-support-tests/src/test/java/platform/tooling/support/tests/ManifestTests.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2015-2019 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v20.html
+ */
+
+package platform.tooling.support.tests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.File;
+import java.lang.module.ModuleFinder;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.jar.Attributes;
+import java.util.jar.JarFile;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import platform.tooling.support.Helper;
+
+/**
+ * @since 1.5
+ */
+class ManifestTests {
+
+	@ParameterizedTest
+	@MethodSource("platform.tooling.support.Helper#loadModuleDirectoryNames")
+	void packageNamesStartWithNameOfTheModule(String module) throws Exception {
+		var version = Helper.version(module);
+		var modulePath = modulePath(module);
+		var uri = ModuleFinder.of(modulePath).findAll().iterator().next().location().orElseThrow();
+		try (var jar = new JarFile(new File(uri))) {
+			var attributes = jar.getManifest().getMainAttributes();
+			assertValue(attributes, "Built-By", "JUnit Team");
+			assertValue(attributes, "Specification-Title", module);
+			assertValue(attributes, "Specification-Version", specificationVersion(version));
+			assertValue(attributes, "Specification-Vendor", "junit.org");
+			assertValue(attributes, "Implementation-Title", module);
+			assertValue(attributes, "Implementation-Version", version);
+			assertValue(attributes, "Implementation-Vendor", "junit.org");
+			assertValue(attributes, "Automatic-Module-Name", "org." + module.replace('-', '.'));
+			switch (module) {
+				case "junit-platform-commons":
+					assertValue(attributes, "Multi-Release", "true");
+					break;
+				case "junit-platform-console":
+					assertValue(attributes, "Main-Class", "org.junit.platform.console.ConsoleLauncher");
+					break;
+			}
+		}
+	}
+
+	private static Path modulePath(String module) {
+		var version = Helper.version(module);
+		var archive = module + '-' + version + ".jar";
+		return Paths.get("..", module, "build", "libs", archive);
+	}
+
+	private static String specificationVersion(String version) {
+		var dash = version.indexOf('-');
+		if (dash < 0) {
+			return version;
+		}
+		return version.substring(0, dash);
+	}
+
+	private static void assertValue(Attributes attributes, String name, String expected) {
+		var actual = attributes.getValue(name);
+		assertEquals(expected, actual,
+			String.format("Manifest attribute %s expected to be %s, but is: %s", name, expected, actual));
+	}
+}
