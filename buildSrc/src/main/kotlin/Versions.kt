0f08962a328f7b6605f73a2d1626f9637fc79bf6
Marc Philipp <mail@marcphilipp.de>
2019-03-09 17:53:58 +0100

    Upgrade dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,24 +4,24 @@ object Versions {
 
     // Languages
     val jvmTarget = JavaVersion.VERSION_1_8
-    val kotlin = "1.3.10"
+    val kotlin = "1.3.21"
 
     // Dependencies
     val apiGuardian = "1.0.0"
     val junit4 = "4.12"
     val ota4j = "1.1.1"
-    val picocli = "3.8.2"
-    val univocity = "2.7.6"
+    val picocli = "3.9.5"
+    val univocity = "2.8.1"
 
     // Test Dependencies
     val archunit = "0.9.3"
-    val assertJ = "3.11.1"
+    val assertJ = "3.12.1"
     val bartholdy = "0.2.3"
-    val classgraph = "4.6.1"
+    val classgraph = "4.8.13"
     val commonsIo = "2.6"
     val javaCompilerScriptEngine = "0.1.2"
-    val log4j = "2.11.1"
-    val mockito = "2.24.5"
+    val log4j = "2.11.2"
+    val mockito = "2.25.0"
     val slf4j = "1.7.25"
 
     // Plugins
