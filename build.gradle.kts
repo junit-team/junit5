71cbdf1073492fb578bb0343d7dd74abae6f43ee
Marc Philipp <mail@marcphilipp.de>
2019-03-21 16:06:15 +0100

    Upgradle to 5.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -145,6 +145,13 @@ allprojects {
 		}
 	}
 
+	// Workaround for https://youtrack.jetbrains.com/issue/KT-29823, should be fixed in Kotlin Plugin 1.3.30
+	configurations.getByName("apiElements") {
+		attributes {
+			attribute(Usage.USAGE_ATTRIBUTE, project.objects.named(Usage::class.java, Usage.JAVA_API_JARS))
+		}
+	}
+
 	checkstyle {
 		toolVersion = Versions.checkstyle
 		configDir = rootProject.file("src/checkstyle")
