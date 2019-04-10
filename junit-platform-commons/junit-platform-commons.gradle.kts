732439fe399b722deec92de91497e9a1d384a02a
Christian Stein <sormuras@gmail.com>
2019-04-10 12:44:14 +0200

    Move ModuleUtils to junit-platform-commons
    Create multi-release JAR by updating the already generated modular JAR.

--- junit-platform-commons/junit-platform-commons.gradle.kts
+++ junit-platform-commons/junit-platform-commons.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.spi.ToolProvider
+
 plugins {
 	`java-library-conventions`
 }
@@ -6,4 +8,13 @@ description = "JUnit Platform Commons"
 
 dependencies {
 	api("org.apiguardian:apiguardian-api:${Versions.apiGuardian}")
+}
+
+tasks.jar {
+	doLast {
+		ToolProvider.findFirst("jar").get().run(System.out, System.err, "--update",
+				"--file", archiveFile.get().asFile.absolutePath,
+				"--release", "9",
+				"-C", "$buildDir/classes/java/module/org.junit.platform.commons", ".")
+	}
 }
