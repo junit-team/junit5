d64b822947e257d0afa01004e3ecf4f7bcc724e5
Marc Philipp <mail@marcphilipp.de>
2019-05-25 15:38:30 +0200

    Document Timeout annotation

--- junit-jupiter-api/src/main/java/org/junit/jupiter/api/Timeout.java
+++ junit-jupiter-api/src/main/java/org/junit/jupiter/api/Timeout.java
@@ -23,8 +23,50 @@ import java.util.concurrent.TimeUnit;
 import org.apiguardian.api.API;
 
 /**
- * {@code @Timeout} is used to define a timeout for a test method or all test
- * methods within one class and it's {@link Nested @Nested} classes.
+ * {@code @Timeout} is used to define a timeout for a method or all testable
+ * methods within one class and its {@link Nested @Nested} classes.
+ *
+ * <p>This annotation may also be used on lifecycle methods annotated with
+ * {@link BeforeAll @BeforeAll}, {@link BeforeEach @BeforeEach},
+ * {@link AfterEach @AfterEach}, or {@link AfterAll @AfterAll}.
+ *
+ * <p>Applying this annotation to a test class has the same effect as applying
+ * it to all testable methods, i.e. all methods annotated or meta-annotated with
+ * {@link Test @Test}, {@link TestFactory @TestFactory}, or
+ * {@link TestTemplate @TestTemplate}, but not to its lifecycle methods.
+ *
+ * <h3>Default Timeouts</h3>
+ *
+ * <p>If this annotation is not present, no timeout will be used unless a
+ * default timeout is defined via one of the following configuration parameters:
+ *
+ * <dl>
+ *     <dt>{@code junit.jupiter.execution.timeout.default}</dt>
+ *     <dd>Default timeout for all testable and lifecycle methods</dd>
+ *     <dt>{@code junit.jupiter.execution.timeout.testable.method.default}</dt>
+ *     <dd>Default timeout for all testable methods</dd>
+ *     <dt>{@code junit.jupiter.execution.timeout.test.method.default}</dt>
+ *     <dd>Default timeout for {@link Test @Test} methods</dd>
+ *     <dt>{@code junit.jupiter.execution.timeout.testtemplate.method.default}</dt>
+ *     <dd>Default timeout for {@link TestTemplate @TestTemplate} methods</dd>
+ *     <dt>{@code junit.jupiter.execution.timeout.testfactory.method.default}</dt>
+ *     <dd>Default timeout for {@link TestFactory @TestFactory} methods</dd>
+ *     <dt>{@code junit.jupiter.execution.timeout.lifecycle.method.default}</dt>
+ *     <dd>Default timeout for all lifecycle methods</dd>
+ *     <dt>{@code junit.jupiter.execution.timeout.beforeall.method.default}</dt>
+ *     <dd>Default timeout for {@link BeforeAll @BeforeAll} methods</dd>
+ *     <dt>{@code junit.jupiter.execution.timeout.beforeeach.method.default}</dt>
+ *     <dd>Default timeout for {@link BeforeEach @BeforeEach} methods</dd>
+ *     <dt>{@code junit.jupiter.execution.timeout.aftereach.method.default}</dt>
+ *     <dd>Default timeout for {@link AfterEach @AfterEach} methods</dd>
+ *     <dt>{@code junit.jupiter.execution.timeout.afterall.method.default}</dt>
+ *     <dd>Default timeout for {@link AfterAll @AfterAll} methods</dd>
+ * </dl>
+ *
+ * <p>More specific configuration parameters override less specific ones. For
+ * example, {@code junit.jupiter.execution.timeout.test.method.default}
+ * overrides {@code junit.jupiter.execution.timeout.testable.method.default}
+ * which overrides {@code junit.jupiter.execution.timeout.default}.
  *
  * @since 5.5
  */
@@ -34,7 +76,20 @@ import org.apiguardian.api.API;
 @Inherited
 @API(status = EXPERIMENTAL, since = "5.5")
 public @interface Timeout {
+
+	/**
+	 * The duration of this timeout.
+	 *
+	 * @return timeout duration; must be a positive number
+	 */
 	long value();
 
+	/**
+	 * The time unit of this timeout.
+	 *
+	 * @return time unit
+	 * @see TimeUnit
+	 */
 	TimeUnit unit() default TimeUnit.SECONDS;
+
 }
