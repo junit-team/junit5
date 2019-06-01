4428eaea3cdf615401cd3d9a4eb9b5b858224bac
bish <hardcore_army@web.de>
2019-06-01 21:38:59 +0200

    Defines the annoation Requirement
    The annotation "Requirement" shall be used to mark a test was written to ensure the correct implementation of the annotated requirement.
    
    Issue: #1900

---
+++ junit-jupiter-api/src/main/java/org/junit/jupiter/api/Requirement.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2015-2019 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+
+package org.junit.jupiter.api;
+
+import static org.apiguardian.api.API.Status.EXPERIMENTAL;
+
+import java.lang.annotation.*;
+
+import org.apiguardian.api.API;
+
+/**
+ * {@code @Requirement} marks a test to show that was written to ensure the correct implementation of the annotated requirement.
+ */
+@Target({ ElementType.ANNOTATION_TYPE, ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@API(status = EXPERIMENTAL)
+public @interface Requirement {
+
+	String id();
+}
