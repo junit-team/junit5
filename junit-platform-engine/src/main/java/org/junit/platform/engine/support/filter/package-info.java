fd44f877badd2e2c6d0132e437ce9e8782c98aa6
Marc Philipp <mail@marcphilipp.de>
2019-06-06 22:30:05 +0200

    Resurrect ClasspathScanningSupport and deprecate it
    Because jqwik and mainrunner are using it.
    
    @sormuras @jlink Please update your engines! 😉

---
+++ junit-platform-engine/src/main/java/org/junit/platform/engine/support/filter/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * {@link org.junit.platform.engine.Filter}-related support classes intended to be
+ * used by test engine implementations.
+ */
+
+package org.junit.platform.engine.support.filter;
