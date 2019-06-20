5ab70ffce7cc4fe21b2674a0ea80cd74a89168d9
Marc Philipp <mail@marcphilipp.de>
2019-06-20 17:49:26 +0200

    Fix typo

--- junit-platform-console-standalone/junit-platform-console-standalone.gradle.kts
+++ junit-platform-console-standalone/junit-platform-console-standalone.gradle.kts
@@ -53,7 +53,7 @@ tasks {
 					// Version-aware binaries are already included - set Multi-Release flag here.
 					// See https://openjdk.java.net/jeps/238 for details
 					// Note: the "jar --update ... --release X" command does not work with the
-					// shadowed JAR as it contains nested classes that do comply multi-release jars.
+					// shadowed JAR as it contains nested classes that do not comply with multi-release jars.
 					"Multi-Release" to true
 			))
 		}
