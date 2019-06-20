b6192bb1bd53eacd27c525729f11da0a9501c72e
Sam Brannen <sbrannen@pivotal.io>
2019-06-20 15:38:11 +0300

    Document that @TempDir fields must not be private

--- junit-jupiter-api/src/main/java/org/junit/jupiter/api/io/TempDir.java
+++ junit-jupiter-api/src/main/java/org/junit/jupiter/api/io/TempDir.java
@@ -26,12 +26,12 @@ import org.junit.jupiter.api.extension.ExtensionConfigurationException;
 import org.junit.jupiter.api.extension.ParameterResolutionException;
 
 /**
- * {@code @TempDir} can be used to annotate a field in a test class or a
- * parameter in a lifecycle method or test method of type {@link Path} or
+ * {@code @TempDir} can be used to annotate a non-private field in a test class
+ * or a parameter in a lifecycle method or test method of type {@link Path} or
  * {@link File} that should be resolved into a temporary directory.
  *
  * <p>Please note that {@code @TempDir} is not supported on constructor
- * parameters. Please use field injection instead, by annotating an
+ * parameters. Please use field injection instead, by annotating a non-private
  * instance field with {@code @TempDir}.
  *
  * <h3>Temporary Directory Creation</h3>
