0f94d9ca258b1314fb3efe9a92b05d23ed3812c1
Christian Stein <sormuras@gmail.com>
2019-07-01 11:34:35 +0200

    Remove script-based condition sections from documentation
    Issue: #1902

--- documentation/src/test/java/example/ConditionalTestExecutionDemo.java
+++ documentation/src/test/java/example/ConditionalTestExecutionDemo.java
@@ -10,8 +10,6 @@
 
 package example;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.condition.JRE.JAVA_10;
 import static org.junit.jupiter.api.condition.JRE.JAVA_8;
 import static org.junit.jupiter.api.condition.JRE.JAVA_9;
@@ -23,16 +21,12 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.time.LocalDate;
 
-import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.DisabledIf;
 import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
 import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 import org.junit.jupiter.api.condition.DisabledOnJre;
 import org.junit.jupiter.api.condition.DisabledOnOs;
-import org.junit.jupiter.api.condition.EnabledIf;
 import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 import org.junit.jupiter.api.condition.EnabledOnJre;
@@ -119,54 +113,5 @@ class ConditionalTestExecutionDemo {
 		// ...
 	}
 	// end::user_guide_environment_variable[]
-
-	// tag::user_guide_scripts[]
-	@Test // Static JavaScript expression.
-	@EnabledIf("2 * 3 == 6")
-	void willBeExecuted() {
-		// ...
-	}
-
-	@RepeatedTest(10) // Dynamic JavaScript expression.
-	@DisabledIf("Math.random() < 0.314159")
-	void mightNotBeExecuted() {
-		// ...
-	}
-
-	@Test // Regular expression testing bound system property.
-	@DisabledIf("/32/.test(systemProperty.get('os.arch'))")
-	void disabledOn32BitArchitectures() {
-		assertFalse(System.getProperty("os.arch").contains("32"));
-	}
-
-	@Test
-	@EnabledIf("'CI' == systemEnvironment.get('ENV')")
-	void onlyOnCiServer() {
-		assertTrue("CI".equals(System.getenv("ENV")));
-	}
-
-	@Test // Multi-line script, custom engine name and custom reason.
-	// end::user_guide_scripts[]
-	// @formatter:off
-	// tag::user_guide_scripts[]
-	@EnabledIf(value = {
-					"load('nashorn:mozilla_compat.js')",
-					"importPackage(java.time)",
-					"",
-					"var today = LocalDate.now()",
-					"var tomorrow = today.plusDays(1)",
-					"tomorrow.isAfter(today)"
-				},
-				engine = "nashorn",
-				reason = "Self-fulfilling: {result}")
-	// end::user_guide_scripts[]
-	// @formatter:on
-	// tag::user_guide_scripts[]
-	void theDayAfterTomorrow() {
-		LocalDate today = LocalDate.now();
-		LocalDate tomorrow = today.plusDays(1);
-		assertTrue(tomorrow.isAfter(today));
-	}
-	// end::user_guide_scripts[]
 
 }
