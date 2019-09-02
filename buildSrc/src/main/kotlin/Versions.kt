148a8af71c4010ed4e1aa3d9dc804f367e39edf7
Marc Philipp <mail@marcphilipp.de>
2019-09-02 19:43:28 +0200

    Upgrade dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -9,18 +9,18 @@ object Versions {
     val junit4 = "4.12"
     val ota4j = "1.2.0"
     val picocli = "3.9.6"
-    val univocity = "2.8.2"
+    val univocity = "2.8.3"
 
     // Test Dependencies
     val archunit = "0.11.0"
-    val assertJ = "3.13.1"
+    val assertJ = "3.13.2"
     val bartholdy = "0.2.3"
-    val classgraph = "4.8.41"
+    val classgraph = "4.8.47"
     val commonsIo = "2.6"
     val groovy = "3.0.0-beta-3"
-    val log4j = "2.11.2"
+    val log4j = "2.12.1"
     val mockito = "3.0.0"
-    val slf4j = "1.7.26"
+    val slf4j = "1.7.28"
 
     // Asciidoctor
     val asciidoctorDiagram = "1.5.9"
