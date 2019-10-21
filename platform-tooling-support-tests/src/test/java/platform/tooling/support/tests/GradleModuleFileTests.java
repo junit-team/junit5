93b6fccf2031c483c39d9d0019bdf747988069eb
Marc Philipp <mail@marcphilipp.de>
2019-10-21 20:41:11 +0200

    Re-add checks for BOM in published POMs and GMM
    This reverts commit 424316b26480b1cc16a655cf1383e6d85f9071d8.

--- platform-tooling-support-tests/src/test/java/platform/tooling/support/tests/GradleModuleFileTests.java
+++ platform-tooling-support-tests/src/test/java/platform/tooling/support/tests/GradleModuleFileTests.java
@@ -49,6 +49,17 @@ class GradleModuleFileTests {
 			"      },", //
 			"      \"dependencies\": [", //
 			"        {", //
+			"          \"group\": \"org.junit\",", //
+			"          \"module\": \"junit-bom\",", //
+			"          \"version\": {", //
+			">> VERSION >>", //
+			"          },", //
+			"          \"attributes\": {", //
+			"            \"org.gradle.category\": \"platform\"", //
+			"          },", //
+			"          \"endorseStrictVersions\": true", //
+			"        },", //
+			"        {", //
 			"          \"group\": \"org.junit.jupiter\",", //
 			"          \"module\": \"junit-jupiter-api\",", //
 			"          \"version\": {", //
@@ -79,6 +90,17 @@ class GradleModuleFileTests {
 			"        \"org.gradle.usage\": \"java-runtime\"", //
 			"      },", //
 			"      \"dependencies\": [", //
+			"        {", //
+			"          \"group\": \"org.junit\",", //
+			"          \"module\": \"junit-bom\",", //
+			"          \"version\": {", //
+			">> VERSION >>", //
+			"          },", //
+			"          \"attributes\": {", //
+			"            \"org.gradle.category\": \"platform\"", //
+			"          },", //
+			"          \"endorseStrictVersions\": true", //
+			"        },", //
 			"        {", //
 			"          \"group\": \"org.junit.jupiter\",", //
 			"          \"module\": \"junit-jupiter-api\",", //
