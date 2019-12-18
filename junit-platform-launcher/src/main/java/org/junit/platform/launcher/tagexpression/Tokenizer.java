a6a552df67d0e2d20d3286e32db0ddd53cc60aec
Marc Philipp <mail@marcphilipp.de>
2019-12-18 10:24:55 +0100

    Use Scanner to implement Tokenizer

--- junit-platform-launcher/src/main/java/org/junit/platform/launcher/tagexpression/Tokenizer.java
+++ junit-platform-launcher/src/main/java/org/junit/platform/launcher/tagexpression/Tokenizer.java
@@ -10,47 +10,32 @@
 
 package org.junit.platform.launcher.tagexpression;
 
-import static java.util.Arrays.stream;
 import static java.util.Collections.emptyList;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
 /**
  * @since 1.1
  */
 class Tokenizer {
 
+	private static final Pattern PATTERN = Pattern.compile("\\s*(?:[()!|&]|(?:[^\\s()!|&]+))");
+
 	List<Token> tokenize(String infixTagExpression) {
 		if (infixTagExpression == null) {
 			return emptyList();
 		}
-
-		return deriveTokensFrom(infixTagExpression, trimmedTokenStringsFrom(infixTagExpression));
-	}
-
-	private List<String> trimmedTokenStringsFrom(String infixTagExpression) {
-		String[] parts = infixTagExpression.replaceAll("([()!|&])", " $1 ").split("\\s");
-		return stream(parts).filter(part -> !part.isEmpty()).collect(Collectors.toList());
-	}
-
-	private List<Token> deriveTokensFrom(String infixTagExpression, List<String> trimmedTokens) {
-		int startIndex = 0;
-		List<Token> tokens = new ArrayList<>(trimmedTokens.size());
-		for (String trimmedToken : trimmedTokens) {
-			Token token = extractTokenStartingAt(infixTagExpression, startIndex, trimmedToken);
-			startIndex = token.endIndexExclusive();
-			tokens.add(token);
+		List<Token> parts = new ArrayList<>();
+		try (Scanner scanner = new Scanner(infixTagExpression)) {
+			String match;
+			while ((match = scanner.findWithinHorizon(PATTERN, 0)) != null) {
+				parts.add(new Token(scanner.match().start(), match));
+			}
 		}
-		return tokens;
-	}
-
-	private Token extractTokenStartingAt(String infixTagExpression, int startIndex, String trimmedToken) {
-		int foundAt = infixTagExpression.indexOf(trimmedToken, startIndex);
-		int endIndex = foundAt + trimmedToken.length();
-		String rawToken = infixTagExpression.substring(startIndex, endIndex);
-		return new Token(startIndex, rawToken);
+		return parts;
 	}
 
 }
