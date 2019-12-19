e3b2dc19ff6ac7e2c126f316b4c9dbe67866196e
Christian Stein <sormuras@gmail.com>
2019-12-19 18:31:00 +0100

    Revise module declarations of "org.junit.vintage" group
    Issue #2063

--- junit-vintage-engine/src/module/org.junit.vintage.engine/module-info.java
+++ junit-vintage-engine/src/module/org.junit.vintage.engine/module-info.java
@@ -8,10 +8,16 @@
  * https://www.eclipse.org/legal/epl-v20.html
  */
 
+/**
+ * JUnit Vintage provides a {@linkplain TestEngine} for running JUnit 3 and 4 based tests on the platform.
+ *
+ * @moduleGraph
+ * @since 5.5.0
+ */
 module org.junit.vintage.engine {
-	requires transitive junit; // 4
-	requires transitive org.apiguardian.api;
-	requires transitive org.junit.platform.engine;
+	requires junit; // 4
+	requires org.apiguardian.api;
+	requires org.junit.platform.engine;
 
 	provides org.junit.platform.engine.TestEngine
 			with org.junit.vintage.engine.VintageTestEngine;
