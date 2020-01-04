4f95223a3ac7a9b8af9a3d1b40bbb509b1921a28
Marc Philipp <mail@marcphilipp.de>
2020-01-04 16:08:18 +0100

    Upgrade dependencies
    Except for Log4J which prints annoying warnings:
    https://issues.apache.org/jira/browse/LOG4J2-2739

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -8,19 +8,19 @@ object Versions {
     val apiGuardian = "1.1.0"
     val junit4 = "4.12"
     val ota4j = "1.2.0"
-    val picocli = "4.0.4"
-    val univocity = "2.8.3"
+    val picocli = "4.1.4"
+    val univocity = "2.8.4"
 
     // Test Dependencies
     val archunit = "0.12.0"
     val assertJ = "3.14.0"
     val bartholdy = "0.2.3"
-    val classgraph = "4.8.48"
+    val classgraph = "4.8.59"
     val commonsIo = "2.6"
-    val groovy = "3.0.0-rc-1"
+    val groovy = "3.0.0-rc-2"
     val log4j = "2.12.1"
-    val mockito = "3.1.0"
-    val slf4j = "1.7.28"
+    val mockito = "3.2.4"
+    val slf4j = "1.7.30"
 
     // Asciidoctor
     val asciidoctorDiagram = "1.5.9"
@@ -31,7 +31,7 @@ object Versions {
     // Tools
     val checkstyle = "8.25"
     val jacoco = "0.8.5"
-    val jmh = "1.21"
+    val jmh = "1.22"
     val ktlint = "0.35.0"
     val surefire = "2.22.2"
     var bnd = "4.3.1"
