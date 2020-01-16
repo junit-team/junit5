1795228a511decafec80eede60b589551b3fcbeb
Christian Stein <sormuras@gmail.com>
2020-01-16 17:41:44 +0100

    Add module description

--- junit-platform-runner/src/module/org.junit.platform.runner/module-info.java
+++ junit-platform-runner/src/module/org.junit.platform.runner/module-info.java
@@ -1,4 +1,3 @@
-
 /*
  * Copyright 2015-2020 the original author or authors.
  *
@@ -9,6 +8,10 @@
  * https://www.eclipse.org/legal/epl-v20.html
  */
 
+/**
+ * {@code Runner} and annotations for configuring and executing tests on the
+ * JUnit Platform in a JUnit 4 environment.
+ */
 module org.junit.platform.runner {
 	requires transitive junit; // 4
 	requires transitive org.apiguardian.api;
