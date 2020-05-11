7e2b05daea3c26f72418310018d8ac68e37baf8e
Christian Stein <sormuras@gmail.com>
2020-05-11 09:37:29 +0200

    Always register JFR-based event-firing listener
    Could be guarded by a launcher configuration flag to opt-out via:
    `LauncherConfig.builder().enableJavaFlightRecordingListener(false);`

--- junit-platform-launcher/src/module/org.junit.platform.launcher/module-info.java
+++ junit-platform-launcher/src/module/org.junit.platform.launcher/module-info.java
@@ -32,7 +32,4 @@ module org.junit.platform.launcher {
 	uses org.junit.platform.engine.TestEngine;
 	uses org.junit.platform.launcher.PostDiscoveryFilter;
 	uses org.junit.platform.launcher.TestExecutionListener;
-
-	provides org.junit.platform.launcher.TestExecutionListener
-			with org.junit.platform.launcher.listeners.FlightRecordingListener;
 }
