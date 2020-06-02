f92bd280ba11f8353a21b37c0a655fdc420acb40
Marc Philipp <mail@marcphilipp.de>
2020-06-02 20:20:50 +0200

    Upgrade to bnd 5.1.0

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,6 +10,6 @@ repositories {
 dependencies {
 	implementation(kotlin("gradle-plugin"))
 	implementation("de.marcphilipp.gradle:nexus-publish-plugin:0.4.0")
-	implementation("biz.aQute.bnd:biz.aQute.bnd.gradle:5.0.0")
+	implementation("biz.aQute.bnd:biz.aQute.bnd.gradle:5.1.0")
 	implementation("com.github.jengelman.gradle.plugins:shadow:5.2.0")
 }
