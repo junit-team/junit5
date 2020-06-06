18c27aadae2aaf76626da22f74f61ac8e990c83a
Marc Philipp <mail@marcphilipp.de>
2020-06-06 14:20:44 +0200

    Always call pre-destroy callbacks if instance was created
    Prior to this commit, pre-destroy callbacks were not called when an
    instance was created but post-processing it failed. Now, they are always
    called if the instance was created, regardless if any subsequent step
    failed.
    
    Resolves #2312.

--- junit-jupiter-engine/src/main/java/org/junit/jupiter/engine/descriptor/ClassBasedTestDescriptor.java
+++ junit-jupiter-engine/src/main/java/org/junit/jupiter/engine/descriptor/ClassBasedTestDescriptor.java
@@ -180,8 +180,11 @@ public abstract class ClassBasedTestDescriptor extends JupiterTestDescriptor {
 			// Eagerly load test instance for BeforeAllCallbacks, if necessary,
 			// and store the instance in the ExtensionContext.
 			ClassExtensionContext extensionContext = (ClassExtensionContext) context.getExtensionContext();
-			throwableCollector.execute(() -> extensionContext.setTestInstances(
-				context.getTestInstancesProvider().getTestInstances(context.getExtensionRegistry())));
+			throwableCollector.execute(() -> {
+				TestInstances testInstances = context.getTestInstancesProvider().getTestInstances(
+					context.getExtensionRegistry(), throwableCollector);
+				extensionContext.setTestInstances(testInstances);
+			});
 		}
 
 		if (throwableCollector.isEmpty()) {
@@ -213,7 +216,7 @@ public abstract class ClassBasedTestDescriptor extends JupiterTestDescriptor {
 			invokeAfterAllCallbacks(context);
 		}
 
-		if (isPerClassLifecycle(context)) {
+		if (isPerClassLifecycle(context) && context.getExtensionContext().getTestInstance().isPresent()) {
 			invokeTestInstancePreDestroyCallbacks(context);
 		}
 
@@ -252,24 +255,30 @@ public abstract class ClassBasedTestDescriptor extends JupiterTestDescriptor {
 	private TestInstancesProvider testInstancesProvider(JupiterEngineExecutionContext parentExecutionContext,
 			ClassExtensionContext extensionContext) {
 
-		return (registry, registrar) -> extensionContext.getTestInstances().orElseGet(
-			() -> instantiateAndPostProcessTestInstance(parentExecutionContext, extensionContext, registry, registrar));
+		return (registry, registrar, throwableCollector) -> extensionContext.getTestInstances().orElseGet(
+			() -> instantiateAndPostProcessTestInstance(parentExecutionContext, extensionContext, registry, registrar,
+				throwableCollector));
 	}
 
 	private TestInstances instantiateAndPostProcessTestInstance(JupiterEngineExecutionContext parentExecutionContext,
-			ExtensionContext extensionContext, ExtensionRegistry registry, ExtensionRegistrar registrar) {
+			ExtensionContext extensionContext, ExtensionRegistry registry, ExtensionRegistrar registrar,
+			ThrowableCollector throwableCollector) {
 
-		TestInstances instances = instantiateTestClass(parentExecutionContext, registry, registrar, extensionContext);
-		invokeTestInstancePostProcessors(instances.getInnermostInstance(), registry, extensionContext);
-		// In addition, we register extensions from instance fields here since the
-		// best time to do that is immediately following test class instantiation
-		// and post processing.
-		registerExtensionsFromFields(registrar, this.testClass, instances.getInnermostInstance());
+		TestInstances instances = instantiateTestClass(parentExecutionContext, registry, registrar, extensionContext,
+			throwableCollector);
+		throwableCollector.execute(() -> {
+			invokeTestInstancePostProcessors(instances.getInnermostInstance(), registry, extensionContext);
+			// In addition, we register extensions from instance fields here since the
+			// best time to do that is immediately following test class instantiation
+			// and post processing.
+			registerExtensionsFromFields(registrar, this.testClass, instances.getInnermostInstance());
+		});
 		return instances;
 	}
 
 	protected abstract TestInstances instantiateTestClass(JupiterEngineExecutionContext parentExecutionContext,
-			ExtensionRegistry registry, ExtensionRegistrar registrar, ExtensionContext extensionContext);
+			ExtensionRegistry registry, ExtensionRegistrar registrar, ExtensionContext extensionContext,
+			ThrowableCollector throwableCollector);
 
 	protected TestInstances instantiateTestClass(Optional<TestInstances> outerInstances, ExtensionRegistry registry,
 			ExtensionContext extensionContext) {
