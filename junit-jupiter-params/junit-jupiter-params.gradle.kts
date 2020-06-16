f7314e800a5975d01e4bc2d79d530107a0bda8bc
Marc Philipp <mail@marcphilipp.de>
2020-06-16 20:58:04 +0200

    Try using extra-java-module-info plugin

--- junit-jupiter-params/junit-jupiter-params.gradle.kts
+++ junit-jupiter-params/junit-jupiter-params.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
 	`kotlin-library-conventions`
 	`shadow-conventions`
+	id("de.jjohannes.extra-java-module-info") version "0.1"
 }
 
 apply(from = "$rootDir/gradle/testing.gradle.kts")
@@ -24,6 +25,13 @@ dependencies {
 
 	compileOnly("org.jetbrains.kotlin:kotlin-stdlib")
 	testImplementation("org.jetbrains.kotlin:kotlin-stdlib")
+}
+
+extraJavaModuleInfo {
+	automaticModule("univocity-parsers-${versions["univocity-parsers"]}.jar", "univocity-parsers")
+	automaticModule("kotlin-stdlib-1.3.72.jar", "kotlin-stdlib")
+	automaticModule("kotlin-stdlib-common-1.3.72.jar", "kotlin-stdlib-common")
+	automaticModule("annotations-13.0.jar", "jetbrains-annotations")
 }
 
 tasks {
