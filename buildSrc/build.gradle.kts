182c6ee2fbc78e0fc514f4b9a7f80e2a6ddd22d2
Sam Brannen <sbrannen@vmware.com>
2020-06-16 09:02:10 +0200

    Upgrade to Shadow plugin 6.0.0

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,5 +11,5 @@ dependencies {
 	implementation(kotlin("gradle-plugin"))
 	implementation("de.marcphilipp.gradle:nexus-publish-plugin:0.4.0")
 	implementation("biz.aQute.bnd:biz.aQute.bnd.gradle:5.1.0")
-	implementation("com.github.jengelman.gradle.plugins:shadow:5.2.0")
+	implementation("com.github.jengelman.gradle.plugins:shadow:6.0.0")
 }
