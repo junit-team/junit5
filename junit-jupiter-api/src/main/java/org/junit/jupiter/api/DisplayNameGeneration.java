e69411de14309fcbe325ce1fe08b28ee92b40401
Marc Philipp <mail@marcphilipp.de>
2020-08-01 14:14:05 +0200

    Promote DisplayNameGeneration and its dependencies
    Issue: #2362

--- junit-jupiter-api/src/main/java/org/junit/jupiter/api/DisplayNameGeneration.java
+++ junit-jupiter-api/src/main/java/org/junit/jupiter/api/DisplayNameGeneration.java
@@ -10,7 +10,7 @@
 
 package org.junit.jupiter.api;
 
-import static org.apiguardian.api.API.Status.EXPERIMENTAL;
+import static org.apiguardian.api.API.Status.STABLE;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
@@ -33,7 +33,7 @@ import org.apiguardian.api.API;
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Inherited
-@API(status = EXPERIMENTAL, since = "5.4")
+@API(status = STABLE, since = "5.7")
 public @interface DisplayNameGeneration {
 
 	/**
