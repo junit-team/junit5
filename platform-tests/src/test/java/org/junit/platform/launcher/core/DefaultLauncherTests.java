a5387a37e6c483d50ea72e8c162f87b881aefd77
Marc Philipp <mail@marcphilipp.de>
2020-08-11 20:52:56 +0200

    Prevent multiple executions of a TestPlan
    The `Launcher` now fails with a descriptive error message when a
    previously executed `TestPlan` is attempted to be executed again. The
    Javadoc of `Launcher` now clearly states that a TestPlan must not be
    executed more than once.
    
    Resolves #2379.

--- platform-tests/src/test/java/org/junit/platform/launcher/core/DefaultLauncherTests.java
+++ platform-tests/src/test/java/org/junit/platform/launcher/core/DefaultLauncherTests.java
@@ -698,7 +698,7 @@ class DefaultLauncherTests {
 	}
 
 	@Test
-	void launcherCanExecuteTestPlan() {
+	void launcherCanExecuteTestPlanExactlyOnce() {
 		TestEngine engine = mock(TestEngine.class);
 		when(engine.getId()).thenReturn("some-engine");
 		when(engine.discover(any(), any())).thenAnswer(invocation -> {
@@ -712,6 +712,9 @@ class DefaultLauncherTests {
 
 		launcher.execute(testPlan);
 		verify(engine, times(1)).execute(any());
+
+		var e = assertThrows(PreconditionViolationException.class, () -> launcher.execute(testPlan));
+		assertEquals(e.getMessage(), "TestPlan must only be executed once");
 	}
 
 	@Test
