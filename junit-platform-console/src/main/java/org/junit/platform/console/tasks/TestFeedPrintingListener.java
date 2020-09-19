2acee4faa5007d288b19804bc0665597631586f0
halitanildonmez <halitanil.donmez@gmail.com>
2020-09-19 16:40:28 +0200

    Issue #2381 adding new methods, organizing

--- junit-platform-console/src/main/java/org/junit/platform/console/tasks/TestFeedPrintingListener.java
+++ junit-platform-console/src/main/java/org/junit/platform/console/tasks/TestFeedPrintingListener.java
@@ -1,10 +1,8 @@
 package org.junit.platform.console.tasks;
 
 import org.junit.platform.engine.TestExecutionResult;
-import org.junit.platform.engine.reporting.ReportEntry;
 import org.junit.platform.launcher.TestExecutionListener;
 import org.junit.platform.launcher.TestIdentifier;
-import org.junit.platform.launcher.TestPlan;
 
 import java.io.PrintWriter;
 
@@ -22,35 +20,38 @@ public class TestFeedPrintingListener implements TestExecutionListener {
 
     @Override
     public void executionSkipped(TestIdentifier testIdentifier, String reason) {
-        String msg = generateMessageFromUniqueId(testIdentifier.getUniqueId());
-        if (testIdentifier.getParentId().isPresent())
-            msg += " > " + testIdentifier.getParentId().get();
-        msg += " > " + testIdentifier.getDisplayName();
-        println(Color.DYNAMIC, "%s \n %s", msg, reason);
+        if (testIdentifier.isContainer())
+            return;
+        String msg = printTestIdenfifier(testIdentifier);
+        println(Color.DYNAMIC, "%s SKIPPED\n %s", msg, reason);
     }
 
     @Override
     public void executionStarted(TestIdentifier testIdentifier) {
-        String msg = generateMessageFromUniqueId(testIdentifier.getUniqueId());
-        if (testIdentifier.getParentId().isPresent())
-            msg += " > " + testIdentifier.getParentId().get();
-        msg += " > " + testIdentifier.getDisplayName();
-        println(Color.DYNAMIC, "%s STARTED", msg);
+        if (testIdentifier.isContainer())
+            return;
+        String msg = printTestIdenfifier(testIdentifier);
+        println(Color.DYNAMIC, "%s > STARTED", msg);
     }
 
     @Override
     public void executionFinished(TestIdentifier testIdentifier, TestExecutionResult testExecutionResult) {
+        if (testIdentifier.isContainer())
+            return;
         TestExecutionResult.Status status = testExecutionResult.getStatus();
-        String msg = generateMessageFromUniqueId(testIdentifier.getUniqueId());
-        if (testIdentifier.getParentId().isPresent())
-            msg += " > " + testIdentifier.getParentId().get();
-        msg += " > " + testIdentifier.getDisplayName();
+        String msg = printTestIdenfifier(testIdentifier);
         if (testExecutionResult.getThrowable().isPresent()) {
-            println(Color.DYNAMIC, "%s > %s\n %s", msg, status.toString(),
+            println(Color.DYNAMIC, "%s > %s \n %s", msg, status.toString(),
                     testExecutionResult.getThrowable().get().getMessage());
         } else {
-            println(Color.DYNAMIC, "%s > %s", msg, status.toString());
+            println(Color.DYNAMIC, "%s > %s ", msg, status.toString());
         }
+    }
+
+    private String printTestIdenfifier (TestIdentifier testIdentifier) {
+        String msg = generateMessageFromUniqueId(testIdentifier.getUniqueId());
+        msg += " > " + testIdentifier.getDisplayName();
+        return msg;
     }
 
     private void println(Color color, String format, Object... args) {
@@ -69,28 +70,31 @@ public class TestFeedPrintingListener implements TestExecutionListener {
 
     private String generateMessageFromUniqueId(String uniqueId) {
         String[] messages = uniqueId.split("/");
-        String engine = parseUniqueId(messages[0]);
+        String engine = parseEngine(messages[0]);
         String output = engine + "";
-        if (messages.length < 2)
-            return output;
-        for (int i = 1; i < messages.length - 1; i++) {
-            output += " > " + parseUniqueId(messages[i]);
+        for (int i = 1; i < messages.length; i++) {
+            String message = messages[i];
+            if (message.indexOf("class:") != -1) {
+                output += " > " + parseMessage(message, "class:");
+            } else if (message.indexOf("method:") != -1) {
+                output += " > " + parseMessage(message, "method:");
+            }
         }
         return output;
     }
 
-    private String parseUniqueId(String uniqueId) {
+    private String parseEngine(String uniqueId) {
         if (uniqueId.contains("junit-jupiter")) {
             return "JUnit Jupiter";
         } else if (uniqueId.contains("junit-vintage")) {
             return "JUnit Vintage";
         }
-        return parseClassName(uniqueId);
+        return uniqueId;
     }
 
-    private String parseClassName(String className) {
+    private String parseMessage(String className, String prefix) {
         String parsedClassName = className.replaceAll("\\[", "")
-                .replaceAll("]", "").replaceAll("class:", "");
+                .replaceAll("]", "").replaceAll(prefix, "");
         return parsedClassName;
     }
 }
