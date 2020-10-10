c74bba43710decdabd51f9b6d10bd5d3e8393d71
Marc Philipp <mail@marcphilipp.de>
2020-10-10 11:18:51 +0200

    Upgrade to Shadow 6.1.0

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,6 +11,6 @@ dependencies {
 	implementation(kotlin("gradle-plugin"))
 	implementation("de.marcphilipp.gradle:nexus-publish-plugin:0.4.0")
 	implementation("biz.aQute.bnd:biz.aQute.bnd.gradle:5.1.2")
-	implementation("com.github.jengelman.gradle.plugins:shadow:6.0.0")
+	implementation("com.github.jengelman.gradle.plugins:shadow:6.1.0")
 	implementation("org.gradle:test-retry-gradle-plugin:1.1.8")
 }
