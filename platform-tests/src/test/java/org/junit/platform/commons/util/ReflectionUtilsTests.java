c01da2de56cfc4d6745ea43c74f00a4ce11f6ac8
Marc Philipp <mail@marcphilipp.de>
2020-10-11 11:16:50 +0200

    Remove unnecessary empty array arguments

--- platform-tests/src/test/java/org/junit/platform/commons/util/ReflectionUtilsTests.java
+++ platform-tests/src/test/java/org/junit/platform/commons/util/ReflectionUtilsTests.java
@@ -192,7 +192,6 @@ class ReflectionUtilsTests {
 		// @formatter:off
 		assertThat(ReflectionUtils.newInstance(C.class, "one", "two")).isNotNull();
 		assertThat(ReflectionUtils.newInstance(C.class)).isNotNull();
-		assertThat(ReflectionUtils.newInstance(C.class, new Object[0])).isNotNull();
 
 		assertThrows(PreconditionViolationException.class, () -> ReflectionUtils.newInstance(C.class, "one", null));
 		assertThrows(PreconditionViolationException.class, () -> ReflectionUtils.newInstance(C.class, null, "two"));
