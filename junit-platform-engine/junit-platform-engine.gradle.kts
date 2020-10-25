3a488297672d04e3ca80c03b39c47c6851648d7d
Marc Philipp <mail@marcphilipp.de>
2020-10-25 18:24:59 +0100

    Update to work with 16.ea.6.lm-open

--- junit-platform-engine/junit-platform-engine.gradle.kts
+++ junit-platform-engine/junit-platform-engine.gradle.kts
@@ -18,19 +18,21 @@ dependencies {
 }
 
 multiReleaseSources {
-	releases.add(15)
+	releases.add(16)
 }
 
+val compileMainRelease16Java by tasks.existing(JavaCompile::class)
+
 tasks.jar {
-	val release15ClassesDir = sourceSets["mainRelease15"].java.classesDirectory
-	inputs.dir(release15ClassesDir).withPathSensitivity(PathSensitivity.RELATIVE)
+	val release16ClassesDir = sourceSets["mainRelease16"].java.classesDirectory
+	inputs.dir(release16ClassesDir).withPathSensitivity(PathSensitivity.RELATIVE)
 	doLast {
 		exec {
-			val javaHome: String by project
+			val javaHome = compileMainRelease16Java.get().javaCompiler.get().metadata.installationPath.asFile.absolutePath
 			commandLine("$javaHome/bin/jar", "--update",
 					"--file", archiveFile.get().asFile.absolutePath,
-					"--release", "15",
-					"-C", release15ClassesDir.get().asFile.absolutePath, ".")
+					"--release", "16",
+					"-C", release16ClassesDir.get().asFile.absolutePath, ".")
 		}
 	}
 }
