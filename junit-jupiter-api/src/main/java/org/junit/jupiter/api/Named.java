9528e6856826ab4eba68e0ae02dfd26ce6384f6c
Juliette de Rancourt <derancourt.juliette@gmail.com>
2021-01-11 13:30:20 +0100

    Add API annotation on Named

--- junit-jupiter-api/src/main/java/org/junit/jupiter/api/Named.java
+++ junit-jupiter-api/src/main/java/org/junit/jupiter/api/Named.java
@@ -10,6 +10,11 @@
 
 package org.junit.jupiter.api;
 
+import static org.apiguardian.api.API.Status.EXPERIMENTAL;
+
+import org.apiguardian.api.API;
+
+@API(status = EXPERIMENTAL, since = "5.8")
 public interface Named<T> {
 
 	static <T> Named<T> of(String name, T payload) {
