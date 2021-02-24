c6b363f36373732977848dbb90998c28a35ce85d
Marc Philipp <mail@marcphilipp.de>
2021-02-24 21:26:02 +0100

    Upgrade bnd to 5.3.0

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 
 dependencies {
 	implementation(kotlin("gradle-plugin"))
-	implementation("biz.aQute.bnd:biz.aQute.bnd.gradle:5.2.0")
+	implementation("biz.aQute.bnd:biz.aQute.bnd.gradle:5.3.0")
 	implementation("com.github.jengelman.gradle.plugins:shadow:6.1.0")
 	implementation("org.gradle:test-retry-gradle-plugin:1.2.0")
 }
