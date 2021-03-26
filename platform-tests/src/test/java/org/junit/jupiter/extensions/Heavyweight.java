1090f7c2388d4b6baa59d2855a829038d8fd9a5b
Marc Philipp <mail@marcphilipp.de>
2021-03-26 13:20:05 +0100

    Merge Heavyweight test classes

--- platform-tests/src/test/java/org/junit/jupiter/extensions/Heavyweight.java
+++ platform-tests/src/test/java/org/junit/jupiter/extensions/Heavyweight.java
@@ -57,9 +57,9 @@ public class Heavyweight implements ParameterResolver, BeforeEachCallback {
 	 * {@link ResourceValue#close()} method implementation is needed to comply
 	 * with both interfaces.
 	 */
-	private static class ResourceValue implements Resource, CloseableResource, AutoCloseable {
+	static class ResourceValue implements Resource, CloseableResource, AutoCloseable {
 
-		private static final AtomicInteger creations = new AtomicInteger();
+		static final AtomicInteger creations = new AtomicInteger();
 		private final AtomicInteger usages = new AtomicInteger();
 
 		@SuppressWarnings("unused") // used via reflection
@@ -72,7 +72,6 @@ public class Heavyweight implements ParameterResolver, BeforeEachCallback {
 		public void close() {
 			// Close resources here.
 			assertEquals(9, usages.get(), "Usage counter mismatch!");
-			usages.set(0);
 		}
 
 		@Override
