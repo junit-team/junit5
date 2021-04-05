be423a858d8a69e3f76a53c85048258139a7b0d0
Marc Philipp <mail@marcphilipp.de>
2021-04-05 14:08:15 +0200

    Use Type enum

--- junit-platform-reporting/src/main/java/org/junit/platform/reporting/open/xml/LegacyReportingName.java
+++ junit-platform-reporting/src/main/java/org/junit/platform/reporting/open/xml/LegacyReportingName.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright 2015-2021 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+
 package org.junit.platform.reporting.open.xml;
 
 import org.opentest4j.reporting.events.api.ChildElement;
@@ -5,12 +15,12 @@ import org.opentest4j.reporting.events.api.Context;
 import org.opentest4j.reporting.events.api.QualifiedName;
 import org.opentest4j.reporting.events.core.Metadata;
 
-public class LegacyReportingName extends ChildElement<Metadata, LegacyReportingName> {
+class LegacyReportingName extends ChildElement<Metadata, LegacyReportingName> {
 
-    public static final QualifiedName ELEMENT = QualifiedName.of(JUnitFactory.NAMESPACE, "legacyReportingName");
+	static final QualifiedName ELEMENT = QualifiedName.of(JUnitFactory.NAMESPACE, "legacyReportingName");
 
-    LegacyReportingName(Context context, String value) {
-        super(context, ELEMENT);
-        withContent(value);
-    }
+	LegacyReportingName(Context context, String value) {
+		super(context, ELEMENT);
+		withContent(value);
+	}
 }
--- junit-platform-reporting/src/main/java/org/junit/platform/reporting/open/xml/Type.java
+++ junit-platform-reporting/src/main/java/org/junit/platform/reporting/open/xml/Type.java
@@ -1,16 +1,27 @@
+/*
+ * Copyright 2015-2021 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+
 package org.junit.platform.reporting.open.xml;
 
+import org.junit.platform.engine.TestDescriptor;
 import org.opentest4j.reporting.events.api.ChildElement;
 import org.opentest4j.reporting.events.api.Context;
 import org.opentest4j.reporting.events.api.QualifiedName;
 import org.opentest4j.reporting.events.core.Metadata;
 
-public class Type extends ChildElement<Metadata, Type> {
+class Type extends ChildElement<Metadata, Type> {
 
-    public static final QualifiedName ELEMENT = QualifiedName.of(JUnitFactory.NAMESPACE, "type");
+	public static final QualifiedName ELEMENT = QualifiedName.of(JUnitFactory.NAMESPACE, "type");
 
-    Type(Context context, String value) {
-        super(context, ELEMENT);
-        withContent(value);
-    }
+	Type(Context context, TestDescriptor.Type type) {
+		super(context, ELEMENT);
+		withContent(type.toString());
+	}
 }
