8793d5adbbf5d93a9e772aaa4ece2314a8c0a672
Marc Philipp <mail@marcphilipp.de>
2021-04-05 14:08:15 +0200

    Use Type enum

--- junit-platform-reporting/src/main/java/org/junit/platform/reporting/open/xml/JUnitFactory.java
+++ junit-platform-reporting/src/main/java/org/junit/platform/reporting/open/xml/JUnitFactory.java
@@ -1,20 +1,21 @@
 package org.junit.platform.reporting.open.xml;
 
+import org.junit.platform.engine.TestDescriptor;
 import org.opentest4j.reporting.events.api.Factory;
 import org.opentest4j.reporting.events.api.Namespace;
 
-public class JUnitFactory {
+class JUnitFactory {
 
     public static Namespace NAMESPACE = Namespace.of("https://schemas.opentest4j.org/reporting/junit/1.0");
 
     private JUnitFactory() {
     }
 
-    public static Factory<LegacyReportingName> legacyReportingName(String legacyReportingName) {
+    static Factory<LegacyReportingName> legacyReportingName(String legacyReportingName) {
         return context -> new LegacyReportingName(context, legacyReportingName);
     }
 
-    public static Factory<Type> type(String value) {
-        return context -> new Type(context, value);
+    static Factory<Type> type(TestDescriptor.Type type) {
+        return context -> new Type(context, type);
     }
 }
