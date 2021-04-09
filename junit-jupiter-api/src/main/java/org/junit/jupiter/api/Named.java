db3067ba7cdc579af5711369c8b25cc578c798f5
Oliver Weiler <oliver.weiler@meinestadt.de>
2021-04-09 14:17:33 +0200

    Reformat code
    Issue: #2587

--- junit-jupiter-api/src/main/java/org/junit/jupiter/api/Named.java
+++ junit-jupiter-api/src/main/java/org/junit/jupiter/api/Named.java
@@ -14,8 +14,6 @@ import static org.apiguardian.api.API.Status.STABLE;
 
 import org.apiguardian.api.API;
 
-import java.lang.Object;
-
 /**
  * {@code Named} is used to wrap an object and give it a name.
  *
@@ -24,15 +22,15 @@ import java.lang.Object;
 @API(status = STABLE, since = "5.8")
 public interface Named<T> {
 
-    /**
-     * Factory method for creating an instance of {@code Named} based on a {@code name} and a {@code payload}
-     *
-     * @param name the name to be used for the wrapped object
-     * @param payload the object to be wrapped
-     * @param <T> the type of the payload
-     * @return an instance of {@code Named}; never {@code null}
-     * @see #named(String, java.lang.Object)
-     */
+	/**
+	 * Factory method for creating an instance of {@code Named} based on a {@code name} and a {@code payload}
+	 *
+	 * @param name the name to be used for the wrapped object
+	 * @param payload the object to be wrapped
+	 * @param <T> the type of the payload
+	 * @return an instance of {@code Named}; never {@code null}
+	 * @see #named(String, java.lang.Object)
+	 */
 	static <T> Named<T> of(String name, T payload) {
 		return new Named<T>() {
 			@Override
@@ -52,18 +50,18 @@ public interface Named<T> {
 		};
 	}
 
-    /**
-     * Factory method for creating an instance of {@code Named} based on a {@code name} and a {@code payload}
-     *
-     * <p>This method is an <em>alias</em> for {@link Named#of} and is
-     * intended to be used when statically imported &mdash; for example, via:
-     * {@code import static org.junit.jupiter.api.Named.named;}
-     *
-     * @param name the name to be used for the wrapped object
-     * @param payload the object to be wrapped
-     * @param <T> the type of the payload
-     * @return an instance of {@code Named}; never {@code null}
-q     */
+	/**
+	 * Factory method for creating an instance of {@code Named} based on a {@code name} and a {@code payload}
+	 *
+	 * <p>This method is an <em>alias</em> for {@link Named#of} and is
+	 * intended to be used when statically imported &mdash; for example, via:
+	 * {@code import static org.junit.jupiter.api.Named.named;}
+	 *
+	 * @param name the name to be used for the wrapped object
+	 * @param payload the object to be wrapped
+	 * @param <T> the type of the payload
+	 * @return an instance of {@code Named}; never {@code null}
+	 */
 	static <T> Named<T> named(String name, T payload) {
 		return of(name, payload);
 	}
