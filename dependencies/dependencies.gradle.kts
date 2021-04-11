6e3954dcb388b24dcacd407b4ea35a9cdf69d51a
Marc Philipp <mail@marcphilipp.de>
2021-04-11 13:48:42 +0200

    Move SLF4J to version catalog

--- dependencies/dependencies.gradle.kts
+++ dependencies/dependencies.gradle.kts
@@ -6,8 +6,6 @@ dependencies {
 	constraints {
 		api("org.openjdk.jmh:jmh-core:${versions["jmh"]}")
 		api("org.openjdk.jmh:jmh-generator-annprocess:${versions["jmh"]}")
-		api("org.slf4j:slf4j-jdk14:${versions["slf4j"]}")
-		api("org.mockito:mockito-junit-jupiter:${versions["mockito"]}")
 		api("biz.aQute.bnd:biz.aQute.bndlib:${versions["bnd"]}")
 		api("org.spockframework:spock-core:${versions["spock"]}")
 		api("com.github.gunnarmorling:jfrunit:${versions["jfrunit"]}")
