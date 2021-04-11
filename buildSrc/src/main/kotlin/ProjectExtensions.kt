81b40ca24b238aded2d8109224dd008012919077
Marc Philipp <mail@marcphilipp.de>
2021-04-11 18:14:43 +0200

    Move tool versions to version catalog

--- buildSrc/src/main/kotlin/ProjectExtensions.kt
+++ buildSrc/src/main/kotlin/ProjectExtensions.kt
@@ -4,9 +4,3 @@ import org.gradle.kotlin.dsl.provideDelegate
 
 val Project.javaModuleName: String
     get() = "org." + this.name.replace('-', '.')
-
-val Project.versions: Versions
-    get() {
-        var versions: Versions? by rootProject.extra
-        return versions ?: Versions(rootProject).also { versions = it }
-    }
