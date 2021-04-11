7f2c1f963e75df24043dad6bf965536b4388e8a0
Marc Philipp <mail@marcphilipp.de>
2021-04-11 14:07:11 +0200

    Move JOOX to version catalog

--- dependencies/dependencies.gradle.kts
+++ dependencies/dependencies.gradle.kts
@@ -7,6 +7,5 @@ dependencies {
 		api("org.openjdk.jmh:jmh-core:${versions["jmh"]}")
 		api("org.openjdk.jmh:jmh-generator-annprocess:${versions["jmh"]}")
 		api("biz.aQute.bnd:biz.aQute.bndlib:${versions["bnd"]}")
-		api("org.jooq:joox:${versions["joox"]}")
 	}
 }
