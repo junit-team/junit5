588661f440bd2076ea5b0441299081df88c6aabc
Marc Philipp <mail@marcphilipp.de>
2021-06-19 15:25:25 +0200

    Fix comment
    Co-authored-by: Sam Brannen <sbrannen@pivotal.io>

--- junit-jupiter-engine/src/test/java/org/junit/jupiter/api/AssertThrowsExactlyAssertionsTests.java
+++ junit-jupiter-engine/src/test/java/org/junit/jupiter/api/AssertThrowsExactlyAssertionsTests.java
@@ -247,7 +247,7 @@ class AssertThrowsExactlyAssertionsTests {
 			assertMessageStartsWith(ex, "Unexpected exception type thrown ==> ");
 			assertMessageContains(ex, "expected: <java.lang.IllegalStateException>");
 			// As of the time of this writing, the class name of the above anonymous inner
-			// class is org.junit.jupiter.api.AssertionsAssertThrowsTests$2; however, hard
+			// class is org.junit.jupiter.api.AssertThrowsExactlyAssertionsTests$2; however, hard
 			// coding "$2" is fragile. So we just check for the presence of the "$"
 			// appended to this class's name.
 			assertMessageContains(ex, "but was: <" + getClass().getName() + "$");
