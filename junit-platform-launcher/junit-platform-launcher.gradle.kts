6a58d79c06ba4c3dae9c9596a37fb44ae72fd367
Raymond Augé <raymond.auge@liferay.com>
2021-06-28 14:43:02 -0400

    follow up of #2544 to enable a fully resolveable OSGi solution
    What this means is that merely by consuming the Jupiter client APIs results in requiring a fully fleshed out runtime including launcher as follows
    - junit-jupiter-api & junit-jupiter-params Requires a org.junit.platform.engine=junit-jupiter
    - junit-jupiter-engine Provides org.junit.platform.engine=junit-jupiter
    - junit-vintage-engine Provides org.junit.platform.engine=junit-vintage
    - junit-jupiter-engine & junit-vintage-engine Requires org.junit.platform.launcher=junit-platform-launcher
    - junit-platform-launcher Provides org.junit.platform.launcher=junit-platform-launcher
    
    The solution is solvable by consuming junit-jupiter-api & junit-jupiter-params in your tests OR in the case of vintage
    having @Requirement(namespace = "org.junit.platform.engine", name = "junit-vintage")
    
    Signed-off-by: Raymond Augé <raymond.auge@liferay.com>

--- junit-platform-launcher/junit-platform-launcher.gradle.kts
+++ junit-platform-launcher/junit-platform-launcher.gradle.kts
@@ -10,4 +10,19 @@ dependencies {
 	api(projects.junitPlatformEngine)
 
 	compileOnlyApi(libs.apiguardian)
+
+	osgiVerification(projects.junitJupiterEngine)
+}
+
+tasks {
+	jar {
+		bundle {
+			bnd("""
+				Provide-Capability:\
+					org.junit.platform.launcher;\
+						org.junit.platform.launcher='junit-platform-launcher';\
+						version:Version="${'$'}{version_cleanup;${project.version}}"
+			""")
+		}
+	}
 }
