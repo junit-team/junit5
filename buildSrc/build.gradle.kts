bb83427958ce60538884988a5571dd9dd1e30139
Marc Philipp <mail@marcphilipp.de>
2021-07-03 12:44:06 +0200

    Revert "Downgrade Test Distribution plugin to work around repo downtime"
    This reverts commit 41111fa2e98c3b75d3d3342301f04b10887624f2.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
 repositories {
 	mavenCentral()
 	gradlePluginPortal()
+	maven(url = "https://repo.gradle.org/gradle/enterprise-libs-release-candidates-local/")
 }
 
 dependencies {
@@ -14,7 +15,7 @@ dependencies {
 	implementation("biz.aQute.bnd:biz.aQute.bnd.gradle:5.3.0")
 	implementation("gradle.plugin.com.github.jengelman.gradle.plugins:shadow:7.0.0")
 	implementation("org.gradle:test-retry-gradle-plugin:1.2.1")
-	compileOnly("com.gradle.enterprise:test-distribution-gradle-plugin:2.0.3")
+	compileOnly("com.gradle.enterprise:test-distribution-gradle-plugin:2.1.1-rc-2")
 }
 
 tasks.withType<KotlinCompile>().configureEach {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,10 +3,11 @@ import com.gradle.enterprise.gradleplugin.internal.extension.BuildScanExtensionW
 pluginManagement {
 	repositories {
 		gradlePluginPortal()
+		maven(url = "https://repo.gradle.org/gradle/enterprise-libs-release-candidates-local/")
 	}
 	plugins {
 		id("com.gradle.enterprise") version "3.6.2"
-		id("com.gradle.enterprise.test-distribution") version "2.0.3"
+		id("com.gradle.enterprise.test-distribution") version "2.1.1-rc-2"
 		id("com.gradle.common-custom-user-data-gradle-plugin") version "1.3"
 		id("net.nemerosa.versioning") version "2.14.0"
 		id("com.github.ben-manes.versions") version "0.39.0"
