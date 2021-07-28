b1b3b96d677493d9f9600779713a9ed63ad408ac
Dennis Bijlsma <d.bijlsma@sig.eu>
2021-07-28 13:55:44 +0200

    Add a method that isn't very good

--- Example.java
+++ Example.java
@@ -74,4 +74,38 @@ public class Example {
             default : throw new AssertionError(e);
         } 
     } 
+    
+    public int complexMethod4(int a) {
+        switch (a) {
+            case 1 : return 1;
+            case 2 : return 2;
+            case 3 : return 3;
+            case 4 : return 4;
+            case 5 : return 5;
+            case 6 : return 6;
+            case 7 : return 7;
+            case 8 : return 8;
+            case 9 : return 9;
+            case 10 : return 10;
+            case 11 : return 11;
+            default : throw new AssertionError(e);
+        } 
+    }
+    
+    public int complexMethod5(int a) {
+        switch (a) {
+            case 1 : return 1;
+            case 2 : return 2;
+            case 3 : return 3;
+            case 4 : return 4;
+            case 5 : return 5;
+            case 6 : return 6;
+            case 7 : return 7;
+            case 8 : return 8;
+            case 9 : return 9;
+            case 10 : return 10;
+            case 11 : return 11;
+            default : throw new AssertionError(e);
+        } 
+    }
 }
