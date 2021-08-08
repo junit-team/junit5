06fef29c9e796c947bcd8ed4c3f3cb3a0598f168
Marc Philipp <mail@marcphilipp.de>
2021-08-08 14:38:00 +0200

    Resolve Gradle deprecation warning

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -121,7 +121,7 @@ include("junit-bom")
 
 includeBuild("../open-test-reporting") {
 	dependencySubstitution {
-		substitute(module("org.opentest4j.reporting:lib")).with(project(":"))
+		substitute(module("org.opentest4j.reporting:lib")).using(project(":"))
 	}
 }
 
