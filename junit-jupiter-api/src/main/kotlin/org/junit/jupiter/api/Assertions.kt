3f605f18c413322a2015e316b456c4e0e4906394
William Boman <william@redwill.se>
2021-08-17 10:31:30 +0200

    Inline lambdas passed to assertDoesNotThrows to support suspending functions (#2684)
    This commit adds support for suspending functions when `assertDoesNotThrow` is called from Kotlin.
    
    Closes #2674.

--- junit-jupiter-api/src/main/kotlin/org/junit/jupiter/api/Assertions.kt
+++ junit-jupiter-api/src/main/kotlin/org/junit/jupiter/api/Assertions.kt
@@ -158,8 +158,8 @@ inline fun <reified T : Throwable> assertThrows(noinline message: () -> String, 
  * @param R the result type of the [executable]
  */
 @API(status = EXPERIMENTAL, since = "5.5")
-fun <R> assertDoesNotThrow(executable: () -> R): R =
-    Assertions.assertDoesNotThrow(ThrowingSupplier(executable))
+inline fun <R> assertDoesNotThrow(executable: () -> R): R =
+    Assertions.assertDoesNotThrow(evaluateAndWrap(executable))
 
 /**
  * Example usage:
@@ -172,7 +172,7 @@ fun <R> assertDoesNotThrow(executable: () -> R): R =
  * @param R the result type of the [executable]
  */
 @API(status = EXPERIMENTAL, since = "5.5")
-fun <R> assertDoesNotThrow(message: String, executable: () -> R): R =
+inline fun <R> assertDoesNotThrow(message: String, executable: () -> R): R =
     assertDoesNotThrow({ message }, executable)
 
 /**
@@ -186,8 +186,19 @@ fun <R> assertDoesNotThrow(message: String, executable: () -> R): R =
  * @param R the result type of the [executable]
  */
 @API(status = EXPERIMENTAL, since = "5.5")
-fun <R> assertDoesNotThrow(message: () -> String, executable: () -> R): R =
-    Assertions.assertDoesNotThrow(ThrowingSupplier(executable), Supplier(message))
+inline fun <R> assertDoesNotThrow(noinline message: () -> String, executable: () -> R): R =
+    Assertions.assertDoesNotThrow(
+        evaluateAndWrap(executable),
+        Supplier(message)
+    )
+
+@PublishedApi
+internal inline fun <R> evaluateAndWrap(executable: () -> R): ThrowingSupplier<R> = try {
+    val result = executable()
+    ThrowingSupplier { result }
+} catch (throwable: Throwable) {
+    ThrowingSupplier { throw throwable }
+}
 
 /**
  * Example usage:
