a8ef251d9a3fd41484af24f914a055b7775a1c95
Sam Brannen <sbrannen@vmware.com>
2021-08-30 16:14:57 +0200

    Document @TestClassOrder support
    Closes: #2699

---
+++ documentation/src/test/java/example/OrderedNestedTestClassesDemo.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2015-2021 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+
+package example;
+
+// tag::user_guide[]
+import org.junit.jupiter.api.ClassOrderer;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestClassOrder;
+
+@TestClassOrder(ClassOrderer.OrderAnnotation.class)
+class OrderedNestedTestClassesDemo {
+
+	@Nested
+	@Order(1)
+	class PrimaryTests {
+
+		@Test
+		void test1() {
+		}
+	}
+
+	@Nested
+	@Order(2)
+	class SecondaryTests {
+
+		@Test
+		void test2() {
+		}
+	}
+}
+//end::user_guide[]
