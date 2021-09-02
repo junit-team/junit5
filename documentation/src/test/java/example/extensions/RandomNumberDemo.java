1f9e1b700ba21dec3843cb9056a69742a60be1ed
Sam Brannen <sbrannen@vmware.com>
2021-09-02 14:50:06 +0200

    Use compiled code for RandomNumberExtension demo

---
+++ documentation/src/test/java/example/extensions/RandomNumberDemo.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2015-2021 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+
+package example.extensions;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+@Disabled("RandomNumberExtension has not been implemented")
+//tag::user_guide[]
+class RandomNumberDemo {
+
+	// use random number field in test methods and @BeforeEach
+	// or @AfterEach lifecycle methods
+	@Random
+	private int randomNumber1;
+
+	RandomNumberDemo(@Random int randomNumber2) {
+		// use random number in constructor
+	}
+
+	@BeforeEach
+	void beforeEach(@Random int randomNumber3) {
+		// use random number in @BeforeEach method
+	}
+
+	@Test
+	void test(@Random int randomNumber4) {
+		// use random number in test method
+	}
+
+}
+//end::user_guide[]
---
+++ documentation/src/test/java/example/extensions/RandomNumberExtension.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2015-2021 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+
+package example.extensions;
+
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolver;
+
+class RandomNumberExtension implements BeforeAllCallback, BeforeEachCallback, ParameterResolver {
+
+	@Override
+	public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) {
+		return false;
+	}
+
+	@Override
+	public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) {
+		return null;
+	}
+
+	@Override
+	public void beforeAll(ExtensionContext context) {
+	}
+
+	@Override
+	public void beforeEach(ExtensionContext context) {
+	}
+
+}
