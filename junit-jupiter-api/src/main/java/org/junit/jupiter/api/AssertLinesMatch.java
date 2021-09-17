32d9a62e7d39408fe5db0dcb123740cfa9675527
Christian Femers <femers@in.tum.de>
2021-09-18 01:31:52 +0200

    Trim the line passed to parseFastForwardLimit before any further usage
    This implementation is analogous to the implementation in
    isFastForwardLine that reassigns the trimmed line to the parameter.

--- junit-jupiter-api/src/main/java/org/junit/jupiter/api/AssertLinesMatch.java
+++ junit-jupiter-api/src/main/java/org/junit/jupiter/api/AssertLinesMatch.java
@@ -202,7 +202,8 @@ class AssertLinesMatch {
 	}
 
 	static int parseFastForwardLimit(String fastForwardLine) {
-		String text = fastForwardLine.trim().substring(2, fastForwardLine.length() - 2).trim();
+		fastForwardLine = fastForwardLine.trim();
+		String text = fastForwardLine.substring(2, fastForwardLine.length() - 2).trim();
 		try {
 			int limit = Integer.parseInt(text);
 			condition(limit > 0, () -> format("fast-forward(%d) limit must be greater than zero", limit));
