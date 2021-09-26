ecd7b2f3dd4aea2d42fdfdec6d5f10568415af81
Marc Philipp <mail@marcphilipp.de>
2021-09-26 17:06:44 +0200

    Extract build-metadata into pre-compiled script plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,39 +1,11 @@
-import java.time.OffsetDateTime
-import java.time.Instant
-import java.time.ZoneOffset
-import java.time.format.DateTimeFormatter
-
 plugins {
-	id("net.nemerosa.versioning")
 	id("com.github.ben-manes.versions") // gradle dependencyUpdates
 	id("com.diffplug.spotless")
 	id("io.spring.nohttp")
 	id("io.github.gradle-nexus.publish-plugin")
+	`build-metadata`
 	`jacoco-conventions`
 }
-
-val buildTimeAndDate: OffsetDateTime by extra {
-
-	// SOURCE_DATE_EPOCH is a UNIX timestamp for pinning build metadata against
-	// in order to achieve reproducible builds
-	//
-	// More details - https://reproducible-builds.org/docs/source-date-epoch/
-
-	if (System.getenv().containsKey("SOURCE_DATE_EPOCH")) {
-
-		val sourceDateEpoch = System.getenv("SOURCE_DATE_EPOCH").toLong()
-
-		Instant.ofEpochSecond(sourceDateEpoch).atOffset(ZoneOffset.UTC)
-
-	} else {
-		OffsetDateTime.now()
-	}
-}
-
-val buildDate: String by extra { DateTimeFormatter.ISO_LOCAL_DATE.format(buildTimeAndDate) }
-val buildTime: String by extra { DateTimeFormatter.ofPattern("HH:mm:ss.SSSZ").format(buildTimeAndDate) }
-val buildRevision: String by extra { versioning.info.commit }
-val builtByValue by extra { project.findProperty("builtBy") ?: project.property("defaultBuiltBy") }
 
 val platformProjects by extra(listOf(
 		projects.junitPlatformCommons,
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,6 @@ pluginManagement {
 		id("com.gradle.enterprise") version "3.7"
 		id("com.gradle.enterprise.test-distribution") version "2.2"
 		id("com.gradle.common-custom-user-data-gradle-plugin") version "1.4.2"
-		id("net.nemerosa.versioning") version "2.14.0"
 		id("com.github.ben-manes.versions") version "0.39.0"
 		id("com.diffplug.spotless") version "5.15.1"
 		id("org.ajoberstar.git-publish") version "3.0.0"
