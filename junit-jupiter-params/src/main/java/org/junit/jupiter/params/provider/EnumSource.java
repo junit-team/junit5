209a59f34d5bd0c70c7a8478c3da9b14372213b2
Narendra Pathai <narendra.pathai@gmail.com>
2021-11-04 00:14:58 +0530

    Add EnumSource.MATCH_NONE mode (#2768)
    The new mode selects only those enum constants whose names match none of
    the supplied patterns.
    
    Resolves #2761.

--- junit-jupiter-params/src/main/java/org/junit/jupiter/params/provider/EnumSource.java
+++ junit-jupiter-params/src/main/java/org/junit/jupiter/params/provider/EnumSource.java
@@ -11,6 +11,7 @@
 package org.junit.jupiter.params.provider;
 
 import static java.util.stream.Collectors.toSet;
+import static org.apiguardian.api.API.Status.EXPERIMENTAL;
 import static org.apiguardian.api.API.Status.STABLE;
 
 import java.lang.annotation.Documented;
@@ -86,6 +87,7 @@ public @interface EnumSource {
 	 * @see Mode#EXCLUDE
 	 * @see Mode#MATCH_ALL
 	 * @see Mode#MATCH_ANY
+	 * @see Mode#MATCH_NONE
 	 * @see #names
 	 */
 	Mode mode() default Mode.INCLUDE;
@@ -121,7 +123,17 @@ public @interface EnumSource {
 		 *
 		 * @see java.util.stream.Stream#anyMatch(java.util.function.Predicate)
 		 */
-		MATCH_ANY(Mode::validatePatterns, (name, patterns) -> patterns.stream().anyMatch(name::matches));
+		MATCH_ANY(Mode::validatePatterns, (name, patterns) -> patterns.stream().anyMatch(name::matches)),
+
+		/**
+		 * Select only those enum constants whose names match none of the patterns supplied
+		 * via the {@link EnumSource#names} attribute.
+		 *
+		 * @since 5.9
+		 * @see java.util.stream.Stream#noneMatch(java.util.function.Predicate)
+		 */
+		@API(status = EXPERIMENTAL, since = "5.9")
+		MATCH_NONE(Mode::validatePatterns, (name, patterns) -> patterns.stream().noneMatch(name::matches));
 
 		private final Validator validator;
 		private final BiPredicate<String, Set<String>> selector;
