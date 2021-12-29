8171da4deba47f58f20e60546ef7b382f36c7f4b
Marc Philipp <mail@marcphilipp.de>
2021-12-29 12:11:22 +0100

    Update Shadow Gradle plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -14,7 +14,7 @@ dependencies {
 	implementation("biz.aQute.bnd:biz.aQute.bnd.gradle:6.1.0")
     implementation("com.diffplug.spotless:spotless-plugin-gradle:6.0.0")
     implementation("com.github.ben-manes:gradle-versions-plugin:0.39.0")
-    implementation("gradle.plugin.com.github.johnrengelman:shadow:7.1.1")
+    implementation("gradle.plugin.com.github.johnrengelman:shadow:7.1.2")
     implementation("gradle.plugin.net.nemerosa:versioning:2.15.1")
 	implementation("org.gradle:test-retry-gradle-plugin:1.3.1")
 	compileOnly("com.gradle.enterprise:test-distribution-gradle-plugin:2.2.1") // keep in sync with root settings.gradle.kts
