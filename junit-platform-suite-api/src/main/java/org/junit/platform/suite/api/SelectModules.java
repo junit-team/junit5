e69243ae0f3c68f07d075ac3901c98c375d040f0
JUnit Team <team@junit.org>
2022-01-06 10:00:09 +0100

    Update copyright

--- junit-platform-suite-api/src/main/java/org/junit/platform/suite/api/SelectModules.java
+++ junit-platform-suite-api/src/main/java/org/junit/platform/suite/api/SelectModules.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2021 the original author or authors.
+ * Copyright 2015-2022 the original author or authors.
  *
  * All rights reserved. This program and the accompanying materials are
  * made available under the terms of the Eclipse Public License v2.0 which
