87a29768a4b1e56388d99b3af87281674cf13061
Jerome Prinet <jprinet@gradle.com>
2022-01-17 11:12:29 +0100

    Bump up Gradle plugin dependencies

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -17,7 +17,7 @@ dependencies {
     implementation("gradle.plugin.com.github.johnrengelman:shadow:7.1.2")
     implementation("gradle.plugin.net.nemerosa:versioning:2.15.1")
 	implementation("org.gradle:test-retry-gradle-plugin:1.3.1")
-	compileOnly("com.gradle.enterprise:test-distribution-gradle-plugin:2.2.1") // keep in sync with root settings.gradle.kts
+	compileOnly("com.gradle.enterprise:test-distribution-gradle-plugin:2.2.2") // keep in sync with root settings.gradle.kts
 }
 
 tasks {
