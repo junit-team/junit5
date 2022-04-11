d8328d5245dab0a675fe9d25ac872da74694c5c2
Christian Stein <sormuras@gmail.com>
2022-04-11 10:15:28 +0200

    Use system's console charset for theme detection
    Prior to this commit the charset returned by
    `Charset.defaultCharset()` was always used to determine
    the default theme used by the JUnit `ConsoleLauncher`
    application.
    
    Now, on Java 17 and later, Java's Console API is queried
    in order to retrieve the user-specified of the charset used
    by the system console -- before falling back to the default
    charset returned by `Charset.defaultCharset()`.
    
    Co-authored-by: Marc Philipp <mail@marcphilipp.de>

---
+++ platform-tests/src/test/java/org/junit/platform/console/options/ConsoleUtilsTests.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2015-2022 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+
+package org.junit.platform.console.options;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.nio.charset.Charset;
+
+import org.junit.jupiter.api.Test;
+
+class ConsoleUtilsTests {
+
+	@Test
+	void consoleCharsetReportedByConsoleUtilsIsEitherNativeCharsetOrDefaultCharset() {
+		var console = System.console();
+		var expected = console != null ? console.charset() : Charset.defaultCharset();
+		assertEquals(expected, ConsoleUtils.charset());
+	}
+
+}
