f2e10c96d2dd51bcf8f22803e0f6c71c3fe296fc
Marc Philipp <mail@marcphilipp.de>
2022-04-22 12:19:50 +0200

    Enable Predictive Test Selection for local and PR builds (#2895)
    This PR enables Gradle Enterprise Predictive Test Selection for local
    and PR builds. CI builds on main and release/* will continue to always
    execute all tests.

--- platform-tooling-support-tests/platform-tooling-support-tests.gradle.kts
+++ platform-tooling-support-tests/platform-tooling-support-tests.gradle.kts
@@ -66,21 +66,14 @@ val unzipMavenDistribution by tasks.registering(Copy::class) {
 }
 
 tasks.test {
-	// Opt-in via system property: '-Dplatform.tooling.support.tests.enabled=true'
-	enabled = System.getProperty("platform.tooling.support.tests.enabled")?.toBoolean() ?: false
 
-	// The following if-block is necessary since Gradle will otherwise
-	// always publish all mavenizedProjects even if this "test" task
-	// is not executed.
-	if (enabled) {
-		// All maven-aware projects must be installed, i.e. published to the local repository
-		val mavenizedProjects: List<Project> by rootProject
-		val tempRepoName: String by rootProject
+	// All maven-aware projects must be installed, i.e. published to the local repository
+	val mavenizedProjects: List<Project> by rootProject
+	val tempRepoName: String by rootProject
 
-		(mavenizedProjects + projects.junitBom.dependencyProject)
-				.map { project -> project.tasks.named("publishAllPublicationsTo${tempRepoName.capitalize()}Repository") }
-				.forEach { dependsOn(it) }
-	}
+	(mavenizedProjects + projects.junitBom.dependencyProject)
+			.map { project -> project.tasks.named("publishAllPublicationsTo${tempRepoName.capitalize()}Repository") }
+			.forEach { dependsOn(it) }
 
 	val tempRepoDir: File by rootProject
 	jvmArgumentProviders += MavenRepo(tempRepoDir)
