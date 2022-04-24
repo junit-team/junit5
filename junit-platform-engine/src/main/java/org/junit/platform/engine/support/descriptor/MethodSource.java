ab727b4602c14a2a47425302c21a4168155e2fe5
“fll02020” <fll987@163.com>
2022-04-25 03:55:14 +0800

    fix the spotlessApply

--- junit-platform-engine/src/main/java/org/junit/platform/engine/support/descriptor/MethodSource.java
+++ junit-platform-engine/src/main/java/org/junit/platform/engine/support/descriptor/MethodSource.java
@@ -209,16 +209,16 @@ public class MethodSource implements TestSource {
 		if (this.javaMethod == null) {
 			if (StringUtils.isNotBlank(this.methodParameterTypes)) {
 				this.javaMethod = ReflectionUtils.findMethod(this.javaClass, this.methodName,
-						this.methodParameterTypes).orElseThrow(
+					this.methodParameterTypes).orElseThrow(
 						() -> new PreconditionViolationException(String.format(
-								"Could not find method with name [%s] and parameter types [%s] in class [%s].",
-								this.methodName, this.methodParameterTypes, this.javaClass.getName())));
+							"Could not find method with name [%s] and parameter types [%s] in class [%s].",
+							this.methodName, this.methodParameterTypes, this.javaClass.getName())));
 			}
 			else {
 				this.javaMethod = ReflectionUtils.findMethod(this.javaClass, this.methodName).orElseThrow(
-						() -> new PreconditionViolationException(
-								String.format("Could not find method with name [%s] in class [%s].", this.methodName,
-										this.javaClass.getName())));
+					() -> new PreconditionViolationException(
+						String.format("Could not find method with name [%s] in class [%s].", this.methodName,
+							this.javaClass.getName())));
 			}
 		}
 	}
