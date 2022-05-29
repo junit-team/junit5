987fec0bad000bb54df219dfc36e9f91dae56dac
“fll02020” <fll987@163.com>
2022-05-29 16:29:56 +0800

    test change

--- junit-jupiter-api/src/main/java/org/junit/jupiter/api/MethodOrderer.java
+++ junit-jupiter-api/src/main/java/org/junit/jupiter/api/MethodOrderer.java
@@ -16,12 +16,12 @@ import static org.apiguardian.api.API.Status.EXPERIMENTAL;
 import static org.apiguardian.api.API.Status.STABLE;
 
 import java.lang.reflect.Method;
-import java.util.Optional;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Set;
-import java.util.HashSet;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 import org.apiguardian.api.API;
 import org.junit.jupiter.api.parallel.ExecutionMode;
