009b8d8898268217f4c2c958556b3d1309663910
Gilberto J Requena <gilberto.requena@syncier.com>
2022-06-20 22:56:15 +0200

    Remove unused import

--- junit-jupiter-engine/src/main/java/org/junit/jupiter/engine/extension/TimeoutInvocationFactory.java
+++ junit-jupiter-engine/src/main/java/org/junit/jupiter/engine/extension/TimeoutInvocationFactory.java
@@ -12,7 +12,6 @@ package org.junit.jupiter.engine.extension;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Supplier;
