9bbb69975a6175ca33c32fe50d6fa3a0e800c6bb
M.P. Korstanje <rien.korstanje@gmail.com>
2022-09-27 11:57:32 +0200

    Fix fallback to ForkJoinPools Java 7 constructor on Java 9 and higher
    When an invalid ParallelExecutionConfiguration is provided on Java 9+ the
    `ForkJoinPoolHierarchicalTestExecutorService` will fall back to using the
    since Java 7 constructor. As a result misconfiguration goes unnoticed.
    
    Separated the exceptions thrown by finding the Java 9+ constructor from those
    thrown by invoking the constructor with an invalid configuration. The former
    can be ignored, the latter should not be.
    
    Fixes: #3045

---
+++ platform-tests/src/test/java/org/junit/platform/engine/support/hierarchical/ForkJoinPoolHierarchicalTestExecutorServiceTests.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2015-2022 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+
+package org.junit.platform.engine.support.hierarchical;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.commons.JUnitException;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ForkJoinPoolHierarchicalTestExecutorServiceTests {
+
+	@Mock
+	ParallelExecutionConfiguration configuration;
+
+	@Test
+	void exceptionsFromInvalidConfigurationAreNotSwallowed() {
+		when(configuration.getParallelism()).thenReturn(2);
+		when(configuration.getMaxPoolSize()).thenReturn(1); // invalid, should be > parallelism
+		when(configuration.getCorePoolSize()).thenReturn(1);
+		when(configuration.getMinimumRunnable()).thenReturn(1);
+		when(configuration.getSaturatePredicate()).thenReturn(__ -> true);
+		when(configuration.getKeepAliveSeconds()).thenReturn(0);
+
+		JUnitException exception = assertThrows(JUnitException.class,
+			() -> new ForkJoinPoolHierarchicalTestExecutorService(configuration));
+		assertThat(exception).hasMessage("Failed to create ForkJoinPool");
+		assertThat(exception).rootCause().isInstanceOf(IllegalArgumentException.class);
+	}
+
+}
