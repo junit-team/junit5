2ad86a26511569632331468b08ed8fd3c1aa97d7
Marc Philipp <mail@marcphilipp.de>
2022-10-01 21:38:17 +0200

    Make `assemble` configuration cache compatible

--- buildSrc/src/main/kotlin/org/junit/gradle/java/PatchModuleArgumentProvider.kt
+++ buildSrc/src/main/kotlin/org/junit/gradle/java/PatchModuleArgumentProvider.kt
@@ -3,36 +3,45 @@ package org.junit.gradle.java
 import javaModuleName
 import org.gradle.api.Named
 import org.gradle.api.Project
+import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.file.FileCollection
+import org.gradle.api.provider.Property
 import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.*
 import org.gradle.kotlin.dsl.get
 import org.gradle.kotlin.dsl.the
 import org.gradle.process.CommandLineArgumentProvider
+import javax.inject.Inject
 
-class PatchModuleArgumentProvider(compiledProject: Project, patchModuleProject: Project) : CommandLineArgumentProvider,
-	Named {
+abstract class PatchModuleArgumentProvider @Inject constructor(compiledProject: Project, patchModuleProject: Project) :
+    CommandLineArgumentProvider, Named {
 
-	@get:Input
-	val module: String = patchModuleProject.javaModuleName
+    @get:Input
+    abstract val module: Property<String>
 
-	@get:InputFiles
-	@get:PathSensitive(PathSensitivity.RELATIVE)
-	val patch: Provider<FileCollection> = compiledProject.provider {
-		if (patchModuleProject == compiledProject)
-			compiledProject.files(compiledProject.the<SourceSetContainer>().matching { it.name.startsWith("main") }.map { it.output })
-		else
-			patchModuleProject.files(patchModuleProject.the<SourceSetContainer>()["main"].java.srcDirs)
-	}
+    @get:InputFiles
+    @get:PathSensitive(PathSensitivity.RELATIVE)
+    abstract val patch: ConfigurableFileCollection
 
-	override fun asArguments(): List<String> {
-		val path = patch.get().filter { it.exists() }.asPath
-		if (path.isEmpty()) {
-			return emptyList()
-		}
-		return listOf("--patch-module", "$module=$path")
-	}
+    init {
+        module.convention(patchModuleProject.javaModuleName)
+        patch.from(compiledProject.provider {
+            if (patchModuleProject == compiledProject)
+                compiledProject.files(compiledProject.the<SourceSetContainer>().matching { it.name.startsWith("main") }
+                    .map { it.output })
+            else
+                patchModuleProject.files(patchModuleProject.the<SourceSetContainer>()["main"].java.srcDirs)
+        })
+    }
 
-	@Internal
-	override fun getName() = "patch-module($module)"
+    override fun asArguments(): List<String> {
+        val path = patch.filter { it.exists() }.asPath
+        if (path.isEmpty()) {
+            return emptyList()
+        }
+        return listOf("--patch-module", "${module.get()}=$path")
+    }
+
+    @Internal
+    override fun getName() = "patch-module(${module.get()})"
 }
