d672deb5a5f73d5a73cfec6f5b9447124d347755
Marc Philipp <mail@marcphilipp.de>
2022-10-01 13:35:32 +0200

    Use local variable instead of project's extra properties

--- buildSrc/src/main/kotlin/build-metadata.gradle.kts
+++ buildSrc/src/main/kotlin/build-metadata.gradle.kts
@@ -4,15 +4,13 @@ import java.time.OffsetDateTime
 import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
 
-val buildTimeAndDate: OffsetDateTime by extra {
-
-	// SOURCE_DATE_EPOCH is a UNIX timestamp for pinning build metadata against
-	// in order to achieve reproducible builds
-	//
-	// More details - https://reproducible-builds.org/docs/source-date-epoch/
-
+val buildTimeAndDate =
 	if (System.getenv().containsKey("SOURCE_DATE_EPOCH")) {
 
+		// SOURCE_DATE_EPOCH is a UNIX timestamp for pinning build metadata against
+		// in order to achieve reproducible builds
+		//
+		// More details - https://reproducible-builds.org/docs/source-date-epoch/
 		val sourceDateEpoch = System.getenv("SOURCE_DATE_EPOCH").toLong()
 
 		Instant.ofEpochSecond(sourceDateEpoch).atOffset(ZoneOffset.UTC)
@@ -20,7 +18,6 @@ val buildTimeAndDate: OffsetDateTime by extra {
 	} else {
 		OffsetDateTime.now()
 	}
-}
 
 val buildDate: String by extra { DateTimeFormatter.ISO_LOCAL_DATE.format(buildTimeAndDate) }
 val buildTime: String by extra { DateTimeFormatter.ofPattern("HH:mm:ss.SSSZ").format(buildTimeAndDate) }
