e018f60963d36b25f845f59278f511332dc7fa72
Marc Philipp <mail@marcphilipp.de>
2022-10-01 13:32:08 +0200

    Configure BND to be compatible with configuration cache

--- buildSrc/src/main/kotlin/osgi-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/osgi-conventions.gradle.kts
@@ -10,60 +10,65 @@ val importAPIGuardian = "org.apiguardian.*;resolution:=\"optional\""
 // This task enhances `jar` and `shadowJar` tasks with the bnd
 // `BundleTaskExtension` extension which allows for generating OSGi
 // metadata into the jar
-tasks.withType<Jar>().matching {
-	task: Jar -> task.name == "jar" || task.name == "shadowJar"
+tasks.withType<Jar>().matching { task: Jar ->
+	task.name == "jar" || task.name == "shadowJar"
 }.configureEach {
-	val bte = extensions.create<BundleTaskExtension>(BundleTaskExtension.NAME, this)
-
 	extra["importAPIGuardian"] = importAPIGuardian
 
-	// These are bnd instructions necessary for generating OSGi metadata.
-	// We've generalized these so that they are widely applicable limiting
-	// module configurations to special cases.
-	bte.setBnd("""
-			# Set the Bundle-SymbolicName to the archiveBaseName.
-			# We don't use the archiveClassifier which Bnd will use
-			# in the default Bundle-SymbolicName value.
-			Bundle-SymbolicName: ${'$'}{task.archiveBaseName}
+	extensions.create<BundleTaskExtension>(BundleTaskExtension.NAME, this).apply {
+		properties.set(provider {
+			mapOf("project.description" to project.description)
+		})
+		// These are bnd instructions necessary for generating OSGi metadata.
+		// We've generalized these so that they are widely applicable limiting
+		// module configurations to special cases.
+		setBnd(
+			"""
+				# Set the Bundle-SymbolicName to the archiveBaseName.
+				# We don't use the archiveClassifier which Bnd will use
+				# in the default Bundle-SymbolicName value.
+				Bundle-SymbolicName: ${'$'}{task.archiveBaseName}
 
-			# Set the Bundle-Name from the project description
-			Bundle-Name: ${'$'}{project.description}
+				# Set the Bundle-Name from the project description
+				Bundle-Name: ${'$'}{project.description}
 
-			# These are the general rules for package imports.
-			Import-Package: \
-				${importAPIGuardian},\
-				org.junit.platform.commons.logging;status=INTERNAL,\
-				kotlin.*;resolution:="optional",\
-				*
+				# These are the general rules for package imports.
+				Import-Package: \
+					${importAPIGuardian},\
+					org.junit.platform.commons.logging;status=INTERNAL,\
+					kotlin.*;resolution:="optional",\
+					*
 
-			# This tells bnd not to complain if a module doesn't actually import
-			# the kotlin and apiguardian packages, but enough modules do to make it a default.
-			-fixupmessages.kotlin.import: "Unused Import-Package instructions: \\[kotlin.*\\]";is:=ignore
-			-fixupmessages.apiguardian.import: "Unused Import-Package instructions: \\[org.apiguardian.*\\]";is:=ignore
+				# This tells bnd not to complain if a module doesn't actually import
+				# the kotlin and apiguardian packages, but enough modules do to make it a default.
+				-fixupmessages.kotlin.import: "Unused Import-Package instructions: \\[kotlin.*\\]";is:=ignore
+				-fixupmessages.apiguardian.import: "Unused Import-Package instructions: \\[org.apiguardian.*\\]";is:=ignore
 
-			# This tells bnd to ignore classes it finds in `META-INF/versions/`
-			# because bnd doesn't yet support multi-release jars.
-			-fixupmessages.wrong.dir: "Classes found in the wrong directory: \\{META-INF/versions/...";is:=ignore
+				# This tells bnd to ignore classes it finds in `META-INF/versions/`
+				# because bnd doesn't yet support multi-release jars.
+				-fixupmessages.wrong.dir: "Classes found in the wrong directory: \\{META-INF/versions/...";is:=ignore
 
-			# Don't scan for Class.forName package imports.
-			# See https://bnd.bndtools.org/instructions/noclassforname.html
-			-noclassforname: true
+				# Don't scan for Class.forName package imports.
+				# See https://bnd.bndtools.org/instructions/noclassforname.html
+				-noclassforname: true
 
-			# Don't add all the extra headers bnd normally adds.
-			# See https://bnd.bndtools.org/instructions/noextraheaders.html
-			-noextraheaders: true
+				# Don't add all the extra headers bnd normally adds.
+				# See https://bnd.bndtools.org/instructions/noextraheaders.html
+				-noextraheaders: true
 
-			# Don't add the Private-Package header.
-			# See https://bnd.bndtools.org/instructions/removeheaders.html
-			-removeheaders: Private-Package
+				# Don't add the Private-Package header.
+				# See https://bnd.bndtools.org/instructions/removeheaders.html
+				-removeheaders: Private-Package
 
-			# Instruct the APIGuardianAnnotations how to operate.
-			# See https://bnd.bndtools.org/instructions/export-apiguardian.html
-			-export-apiguardian: *;version=${'$'}{versionmask;===;${'$'}{version_cleanup;${'$'}{task.archiveVersion}}}
-		""")
+				# Instruct the APIGuardianAnnotations how to operate.
+				# See https://bnd.bndtools.org/instructions/export-apiguardian.html
+				-export-apiguardian: *;version=${'$'}{versionmask;===;${'$'}{version_cleanup;${'$'}{task.archiveVersion}}}
+			"""
+		)
 
-	// Do the actual work putting OSGi stuff in the jar.
-	doLast(bte.buildAction())
+		// Do the actual work putting OSGi stuff in the jar.
+		doLast(buildAction())
+	}
 }
 
 // Bnd's Resolve task uses a properties file for its configuration. This
@@ -90,7 +95,7 @@ val osgiVerification by configurations.creating {
 // that its metadata is valid. If the metadata is invalid this task will
 // fail.
 val verifyOSGi by tasks.registering(Resolve::class) {
-	bndrun.fileProvider(osgiProperties.map{ it.outputFile })
+	bndrun.fileProvider(osgiProperties.map { it.outputFile })
 	outputBndrun.set(layout.buildDirectory.file("resolvedOSGiProperties.bndrun"))
 	isReportOptional = false
 	// By default bnd will use jars found in:
