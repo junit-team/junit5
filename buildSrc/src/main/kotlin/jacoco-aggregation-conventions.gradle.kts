1dc664d53525b552190f8e8db3e5c0d4270ec096
Marc Philipp <mail@marcphilipp.de>
2022-11-13 15:37:11 +0100

    Fix build when JaCoCo is enabled

--- buildSrc/src/main/kotlin/jacoco-aggregation-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/jacoco-aggregation-conventions.gradle.kts
@@ -1,24 +1,11 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-import org.gradle.api.attributes.LibraryElements
-import org.gradle.api.attributes.LibraryElements.LIBRARY_ELEMENTS_ATTRIBUTE
 import org.gradle.api.attributes.TestSuiteType
 import org.gradle.kotlin.dsl.invoke
 import org.gradle.kotlin.dsl.`jacoco-report-aggregation`
-import org.gradle.kotlin.dsl.named
 import org.gradle.testing.jacoco.plugins.JacocoCoverageReport
-import org.junit.gradle.jacoco.JacocoConventions.COVERAGE_CLASSES
 
 plugins {
 	id("jacoco-conventions")
 	`jacoco-report-aggregation`
-}
-
-configurations {
-	allCodeCoverageReportClassDirectories {
-		attributes {
-			attribute(LIBRARY_ELEMENTS_ATTRIBUTE, objects.named(LibraryElements::class, COVERAGE_CLASSES))
-		}
-	}
 }
 
 reporting {
