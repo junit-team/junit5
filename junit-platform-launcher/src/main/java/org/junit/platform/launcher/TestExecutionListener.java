50c28aa1f6a4dde1b85ee47649f774718703c9e3
Juliette de Rancourt <derancourt.juliette@gmail.com>
2022-11-16 10:18:16 +0100

    Document in JavaDoc

--- junit-platform-launcher/src/main/java/org/junit/platform/launcher/TestExecutionListener.java
+++ junit-platform-launcher/src/main/java/org/junit/platform/launcher/TestExecutionListener.java
@@ -25,6 +25,10 @@ import org.junit.platform.engine.reporting.ReportEntry;
  * Concrete implementations may therefore override one or more of these methods
  * to be notified of the selected events.
  *
+ * <p>{@code TestExecutionListener} are called sequentially in registration order.
+ * Test case execution won't start before all {@link #executionStarted(TestIdentifier)}
+ * calls have returned.
+ *
  * <p>JUnit provides two example implementations.
  *
  * <ul>
