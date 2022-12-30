b1bd8c3ea2492de750061969c3e2b7e47b6ccd99
Marc Philipp <mail@marcphilipp.de>
2022-12-30 19:48:57 +0100

    Add JRE.JAVA_21

--- junit-jupiter-engine/src/test/java/org/junit/jupiter/api/condition/EnabledForJreRangeIntegrationTests.java
+++ junit-jupiter-engine/src/test/java/org/junit/jupiter/api/condition/EnabledForJreRangeIntegrationTests.java
@@ -24,6 +24,7 @@ import static org.junit.jupiter.api.condition.EnabledOnJreIntegrationTests.onJav
 import static org.junit.jupiter.api.condition.EnabledOnJreIntegrationTests.onJava18;
 import static org.junit.jupiter.api.condition.EnabledOnJreIntegrationTests.onJava19;
 import static org.junit.jupiter.api.condition.EnabledOnJreIntegrationTests.onJava20;
+import static org.junit.jupiter.api.condition.EnabledOnJreIntegrationTests.onJava21;
 import static org.junit.jupiter.api.condition.EnabledOnJreIntegrationTests.onJava8;
 import static org.junit.jupiter.api.condition.EnabledOnJreIntegrationTests.onJava9;
 import static org.junit.jupiter.api.condition.JRE.JAVA_17;
@@ -77,7 +78,7 @@ class EnabledForJreRangeIntegrationTests {
 	@Test
 	@EnabledForJreRange(min = JAVA_18)
 	void javaMin18() {
-		assertTrue(onJava18() || onJava19() || onJava20());
+		assertTrue(onJava18() || onJava19() || onJava20() || onJava21());
 		assertFalse(onJava17());
 	}
 
@@ -85,7 +86,7 @@ class EnabledForJreRangeIntegrationTests {
 	@EnabledForJreRange(min = OTHER, max = OTHER)
 	void other() {
 		assertFalse(onJava8() || onJava9() || onJava10() || onJava11() || onJava12() || onJava13() || onJava14()
-				|| onJava15() || onJava16() || onJava17() || onJava18() || onJava19() || onJava20());
+				|| onJava15() || onJava16() || onJava17() || onJava18() || onJava19() || onJava20() || onJava21());
 	}
 
 }
