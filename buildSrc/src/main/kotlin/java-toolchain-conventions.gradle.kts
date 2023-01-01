f19cd20fd4b313167061730666d2c26256eb4a82
Marc Philipp <mail@marcphilipp.de>
2023-01-01 17:40:41 +0100

    Use toolchains for Kotlin tasks

--- buildSrc/src/main/kotlin/java-toolchain-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/java-toolchain-conventions.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
+
 project.pluginManager.withPlugin("java") {
 	val javaToolchainVersion: String? by project
 	val defaultLanguageVersion = JavaLanguageVersion.of(17)
@@ -5,10 +7,21 @@ project.pluginManager.withPlugin("java") {
 
 	val extension = the<JavaPluginExtension>()
 	val javaToolchainService = the<JavaToolchainService>()
+
 	extension.toolchain.languageVersion.set(javaLanguageVersion)
+
+	pluginManager.withPlugin("org.jetbrains.kotlin.jvm") {
+		configure<KotlinJvmProjectExtension> {
+			jvmToolchain {
+				languageVersion.set(javaLanguageVersion)
+			}
+		}
+	}
+
 	tasks.withType<JavaExec>().configureEach {
 		javaLauncher.set(javaToolchainService.launcherFor(extension.toolchain))
 	}
+
 	tasks.withType<JavaCompile>().configureEach {
 		outputs.cacheIf { javaLanguageVersion == defaultLanguageVersion }
 		doFirst {
@@ -19,6 +32,7 @@ project.pluginManager.withPlugin("java") {
 			}
 		}
 	}
+
 	tasks.withType<GroovyCompile>().configureEach {
 		javaLauncher.set(javaToolchainService.launcherFor {
 			// Groovy does not yet support JDK 19, see https://issues.apache.org/jira/browse/GROOVY-10569
