17785a6320fe8c2fc218506f6fd5c69e6834cd61
Marc Philipp <mail@marcphilipp.de>
2023-01-10 15:22:50 +0100

    Upgrade to v0.0.11 of the nohttp plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,7 +14,7 @@ pluginManagement {
 		id("org.asciidoctor.jvm.convert") version "4.0.0-alpha.1"
 		id("org.asciidoctor.jvm.pdf") version "4.0.0-alpha.1"
 		id("me.champeau.jmh") version "0.6.8"
-		id("io.spring.nohttp") version "0.0.10"
+		id("io.spring.nohttp") version "0.0.11"
 		id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
 	}
 }
