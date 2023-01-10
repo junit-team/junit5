87648fd975e865ae17df9980318441ac0ea1620e
Marc Philipp <mail@marcphilipp.de>
2023-01-10 15:15:06 +0100

    Update Gradle Enterprise plugin to 3.12.2
    Closes #3126.
    Closes #3127.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,7 @@ pluginManagement {
 		gradlePluginPortal()
 	}
 	plugins {
-		id("com.gradle.enterprise") version "3.12.1" // keep in sync with buildSrc/build.gradle.kts
+		id("com.gradle.enterprise") version "3.12.2" // keep in sync with buildSrc/build.gradle.kts
 		id("com.gradle.common-custom-user-data-gradle-plugin") version "1.8.2"
 		id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
 		id("org.ajoberstar.git-publish") version "4.1.1"
