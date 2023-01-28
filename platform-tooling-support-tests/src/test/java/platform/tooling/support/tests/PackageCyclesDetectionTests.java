076e9c3d1a5a94abb919b7064fca581604318513
Marc Philipp <mail@marcphilipp.de>
2023-01-28 13:56:34 +0100

    Restore current import order

--- platform-tooling-support-tests/src/test/java/platform/tooling/support/tests/PackageCyclesDetectionTests.java
+++ platform-tooling-support-tests/src/test/java/platform/tooling/support/tests/PackageCyclesDetectionTests.java
@@ -18,6 +18,7 @@ import de.sormuras.bartholdy.tool.CyclesDetector;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
+
 import platform.tooling.support.MavenRepo;
 
 /**
