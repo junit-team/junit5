40b715df632e2379b2acb2d6f8f94f16066f76e5
Marc Philipp <mail@marcphilipp.de>
2023-02-17 21:42:29 +0100

    Make it possible to pass args to ConsoleLauncher Gradle task
    The task can now be called as:
    
        :documentation:consoleLauncher --args="--help ..."

--- gradle/plugins/src/main/kotlin/junitbuild/exec/RunConsoleLauncher.kt
+++ gradle/plugins/src/main/kotlin/junitbuild/exec/RunConsoleLauncher.kt
@@ -1,22 +1,20 @@
 package junitbuild.exec
 
+import org.apache.tools.ant.types.Commandline
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.provider.ListProperty
 import org.gradle.api.provider.Property
-import org.gradle.api.tasks.Classpath
-import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.Internal
-import org.gradle.api.tasks.OutputDirectory
-import org.gradle.api.tasks.SourceSetContainer
-import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.*
+import org.gradle.api.tasks.options.Option
 import org.gradle.kotlin.dsl.get
 import org.gradle.kotlin.dsl.the
 import org.gradle.process.CommandLineArgumentProvider
 import org.gradle.process.ExecOperations
 import trackOperationSystemAsInput
 import java.io.ByteArrayOutputStream
+import java.util.*
 import javax.inject.Inject
 
 abstract class RunConsoleLauncher @Inject constructor(private val execOperations: ExecOperations): DefaultTask() {
@@ -26,6 +24,9 @@ abstract class RunConsoleLauncher @Inject constructor(private val execOperations
 
     @get:Input
     abstract val args: ListProperty<String>
+
+    @get:Input
+    abstract val commandLineArgs: ListProperty<String>
 
     @get:OutputDirectory
     abstract val reportsDir: DirectoryProperty
@@ -62,6 +63,7 @@ abstract class RunConsoleLauncher @Inject constructor(private val execOperations
             args("--scan-classpath")
             args("--config=junit.platform.reporting.open.xml.enabled=true")
             args(this@RunConsoleLauncher.args.get())
+            args(this@RunConsoleLauncher.commandLineArgs.get())
             argumentProviders += CommandLineArgumentProvider {
                 listOf(
                     "--reports-dir=${reportsDir.get()}",
@@ -82,5 +84,11 @@ abstract class RunConsoleLauncher @Inject constructor(private val execOperations
             System.out.flush()
         }
         result.rethrowFailure().assertNormalExitValue()
+    }
+
+    @Suppress("unused")
+    @Option(option = "args", description = "Additional command line arguments for the console launcher")
+    fun setCliArgs(args: String) {
+        commandLineArgs.set(Commandline.translateCommandline(args).toList())
     }
 }
