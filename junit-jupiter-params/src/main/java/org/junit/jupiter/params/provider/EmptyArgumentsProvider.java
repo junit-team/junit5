e0e8778e4842433bcefee156524f0d32b7b6e592
Stefano Cordio <stefano_cordio@epam.com>
2023-02-21 09:16:25 +0100

    Add support for concrete Collection/Map types to EmptySource (#3159)
    This adds support for:
    * `Collection` and subtypes with a public no-arg constructor
    * `SortedSet`
    * `NavigableSet`
    * `Map` subtypes with a public no-arg constructor
    * `SortedMap`
    * `NavigableMap`
    
    Resolves #2791.

--- junit-jupiter-params/src/main/java/org/junit/jupiter/params/provider/EmptyArgumentsProvider.java
+++ junit-jupiter-params/src/main/java/org/junit/jupiter/params/provider/EmptyArgumentsProvider.java
@@ -11,13 +11,21 @@
 package org.junit.jupiter.params.provider;
 
 import static org.junit.jupiter.params.provider.Arguments.arguments;
+import static org.junit.platform.commons.util.ReflectionUtils.newInstance;
 
 import java.lang.reflect.Array;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.NavigableMap;
+import java.util.NavigableSet;
+import java.util.Optional;
 import java.util.Set;
+import java.util.SortedMap;
+import java.util.SortedSet;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.extension.ExtensionContext;
@@ -44,14 +52,35 @@ class EmptyArgumentsProvider implements ArgumentsProvider {
 		if (String.class.equals(parameterType)) {
 			return Stream.of(arguments(""));
 		}
+		if (Collection.class.equals(parameterType)) {
+			return Stream.of(arguments(Collections.emptySet()));
+		}
 		if (List.class.equals(parameterType)) {
 			return Stream.of(arguments(Collections.emptyList()));
 		}
 		if (Set.class.equals(parameterType)) {
 			return Stream.of(arguments(Collections.emptySet()));
 		}
+		if (SortedSet.class.equals(parameterType)) {
+			return Stream.of(arguments(Collections.emptySortedSet()));
+		}
+		if (NavigableSet.class.equals(parameterType)) {
+			return Stream.of(arguments(Collections.emptyNavigableSet()));
+		}
 		if (Map.class.equals(parameterType)) {
 			return Stream.of(arguments(Collections.emptyMap()));
+		}
+		if (SortedMap.class.equals(parameterType)) {
+			return Stream.of(arguments(Collections.emptySortedMap()));
+		}
+		if (NavigableMap.class.equals(parameterType)) {
+			return Stream.of(arguments(Collections.emptyNavigableMap()));
+		}
+		if (Collection.class.isAssignableFrom(parameterType) || Map.class.isAssignableFrom(parameterType)) {
+			Optional<Constructor<?>> defaultConstructor = getDefaultConstructor(parameterType);
+			if (defaultConstructor.isPresent()) {
+				return Stream.of(arguments(newInstance(defaultConstructor.get())));
+			}
 		}
 		if (parameterType.isArray()) {
 			Object array = Array.newInstance(parameterType.getComponentType(), 0);
@@ -61,6 +90,15 @@ class EmptyArgumentsProvider implements ArgumentsProvider {
 		throw new PreconditionViolationException(
 			String.format("@EmptySource cannot provide an empty argument to method [%s]: [%s] is not a supported type.",
 				testMethod.toGenericString(), parameterType.getName()));
+	}
+
+	private static Optional<Constructor<?>> getDefaultConstructor(Class<?> clazz) {
+		try {
+			return Optional.of(clazz.getConstructor());
+		}
+		catch (NoSuchMethodException e) {
+			return Optional.empty();
+		}
 	}
 
 }
