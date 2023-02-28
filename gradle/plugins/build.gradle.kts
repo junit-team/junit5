a2817c7d419691f0d4aad9fc53cafe912c877631
JUnit Build Master <team@junit.org>
2023-02-28 20:06:11 +0100

    Update Gradle plugins (#3179)
    * Bump me.champeau.jmh from 0.6.8 to 0.7.0
    * Bump com.github.ben-manes.versions from 0.45.0 to 0.46.0
    * Bump io.github.gradle-nexus.publish-plugin from 1.1.0 to 1.2.0
    * Bump gradle.plugin.com.github.johnrengelman:shadow from 7.1.2 to 8.0.0

--- gradle/plugins/build.gradle.kts
+++ gradle/plugins/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
 	`kotlin-dsl`
-	id("com.github.ben-manes.versions") version "0.45.0"
+	id("com.github.ben-manes.versions") version "0.46.0"
 }
 
 repositories {
@@ -14,7 +14,7 @@ dependencies {
 	implementation("biz.aQute.bnd:biz.aQute.bnd.gradle:6.4.0")
 	implementation("com.diffplug.spotless:spotless-plugin-gradle:6.15.0")
 	implementation("com.github.ben-manes:gradle-versions-plugin:0.45.0")
-	implementation("gradle.plugin.com.github.johnrengelman:shadow:7.1.2")
+	implementation("gradle.plugin.com.github.johnrengelman:shadow:8.0.0")
 	compileOnly("com.gradle:gradle-enterprise-gradle-plugin:3.12.3") // keep in sync with root settings.gradle.kts
 }
 
