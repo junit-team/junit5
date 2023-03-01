f9c5a9caec700169573b3c4608b67648f4aa1bf2
Marc Philipp <mail@marcphilipp.de>
2023-03-01 21:09:35 +0100

    Fix tests

--- junit-platform-console/src/main/java/org/junit/platform/console/ConsoleLauncher.java
+++ junit-platform-console/src/main/java/org/junit/platform/console/ConsoleLauncher.java
@@ -14,11 +14,13 @@ import static org.apiguardian.api.API.Status.INTERNAL;
 import static org.apiguardian.api.API.Status.MAINTAINED;
 
 import java.io.PrintWriter;
+import java.util.function.Function;
 
 import org.apiguardian.api.API;
-import org.junit.platform.console.options.CommandLineOptionsParser;
+import org.junit.platform.console.options.CommandLineOptions;
 import org.junit.platform.console.options.CommandResult;
 import org.junit.platform.console.options.MainCommand;
+import org.junit.platform.console.tasks.ConsoleTestExecutor;
 
 /**
  * The {@code ConsoleLauncher} is a stand-alone application for launching the
@@ -38,21 +40,24 @@ public class ConsoleLauncher {
 
 	@API(status = INTERNAL, since = "1.0")
 	public static CommandResult<?> run(PrintWriter out, PrintWriter err, String... args) {
-		ConsoleLauncher consoleLauncher = new ConsoleLauncher(null, out, err);
+		ConsoleLauncher consoleLauncher = new ConsoleLauncher(ConsoleTestExecutor::new, out, err);
 		return consoleLauncher.run(args);
 	}
 
+	private final Function<CommandLineOptions, ConsoleTestExecutor> consoleTestExecutorFactory;
 	private final PrintWriter out;
 	private final PrintWriter err;
 
-	ConsoleLauncher(CommandLineOptionsParser commandLineOptionsParser, PrintWriter out, PrintWriter err) {
+	ConsoleLauncher(Function<CommandLineOptions, ConsoleTestExecutor> consoleTestExecutorFactory, PrintWriter out,
+			PrintWriter err) {
+		this.consoleTestExecutorFactory = consoleTestExecutorFactory;
 		this.out = out;
 		this.err = err;
 	}
 
 	CommandResult<?> run(String... args) {
 		try {
-			return MainCommand.run(out, err, args);
+			return new MainCommand(consoleTestExecutorFactory).run(out, err, args);
 		}
 		finally {
 			out.flush();
