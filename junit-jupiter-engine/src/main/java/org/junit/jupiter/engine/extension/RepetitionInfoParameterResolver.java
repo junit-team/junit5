e0237d97812b3c99c18f50a4a238bbd2cc6f0bef
Sam Brannen <sam@sambrannen.com>
2023-04-15 17:43:39 +0200

    Prototype for new @RepeatedTest(failureCount) feature
    This commit is a working prototype for a new `failureCount` feature in
    @RepeatedTest which allows a repeated test to be configured to skip
    remaining repetitions after a certain number of failures have been
    encountered.
    
    Co-authored-by: Christian Stein <sormuras@gmail.com>
    See: #2925

--- junit-jupiter-engine/src/main/java/org/junit/jupiter/engine/extension/RepetitionInfoParameterResolver.java
+++ junit-jupiter-engine/src/main/java/org/junit/jupiter/engine/extension/RepetitionInfoParameterResolver.java
@@ -10,10 +10,13 @@
 
 package org.junit.jupiter.engine.extension;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.api.extension.ParameterResolver;
+import org.junit.jupiter.api.extension.TestWatcher;
 import org.junit.platform.commons.util.ToStringBuilder;
 
 /**
@@ -22,14 +25,19 @@ import org.junit.platform.commons.util.ToStringBuilder;
  *
  * @since 5.0
  */
-class RepetitionInfoParameterResolver implements ParameterResolver {
+class RepetitionInfoParameterResolver implements ParameterResolver, TestWatcher {
 
 	private final int currentRepetition;
 	private final int totalRepetitions;
+	private final int failureThreshold;
+	private final AtomicInteger failureCount;
 
-	public RepetitionInfoParameterResolver(int currentRepetition, int totalRepetitions) {
+	public RepetitionInfoParameterResolver(int currentRepetition, int totalRepetitions, int failureThreshold,
+			AtomicInteger failureCount) {
 		this.currentRepetition = currentRepetition;
 		this.totalRepetitions = totalRepetitions;
+		this.failureThreshold = failureThreshold;
+		this.failureCount = failureCount;
 	}
 
 	@Override
@@ -39,17 +47,28 @@ class RepetitionInfoParameterResolver implements ParameterResolver {
 
 	@Override
 	public RepetitionInfo resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) {
-		return new DefaultRepetitionInfo(this.currentRepetition, this.totalRepetitions);
+		return new DefaultRepetitionInfo(this.currentRepetition, this.totalRepetitions, this.failureThreshold,
+			this.failureCount.get());
+	}
+
+	@Override
+	public void testFailed(ExtensionContext context, Throwable cause) {
+		this.failureCount.incrementAndGet();
 	}
 
 	private static class DefaultRepetitionInfo implements RepetitionInfo {
 
 		private final int currentRepetition;
 		private final int totalRepetitions;
+		private final int failureThreshold;
+		private final int failureCount;
 
-		DefaultRepetitionInfo(int currentRepetition, int totalRepetitions) {
+		public DefaultRepetitionInfo(int currentRepetition, int totalRepetitions, int failureThreshold,
+				int failureCount) {
 			this.currentRepetition = currentRepetition;
 			this.totalRepetitions = totalRepetitions;
+			this.failureThreshold = failureThreshold;
+			this.failureCount = failureCount;
 		}
 
 		@Override
@@ -63,14 +82,27 @@ class RepetitionInfoParameterResolver implements ParameterResolver {
 		}
 
 		@Override
+		public int getFailureThreshold() {
+			return this.failureThreshold;
+		}
+
+		@Override
+		public int getFailureCount() {
+			return this.failureCount;
+		}
+
+		@Override
 		public String toString() {
 			// @formatter:off
 			return new ToStringBuilder(this)
 					.append("currentRepetition", this.currentRepetition)
 					.append("totalRepetitions", this.totalRepetitions)
+					.append("failureThreshold", this.failureThreshold)
+					.append("failureCount", this.failureCount)
 					.toString();
 			// @formatter:on
 		}
 
 	}
+
 }
