ae029a38d37463be94d342e28f1ac6961d43d27d
Stefano Cordio <stefano_cordio@epam.com>
2023-07-16 17:43:44 +0200

    Restore `ParameterContext` methods for documentation purposes

--- junit-jupiter-api/src/main/java/org/junit/jupiter/api/extension/ParameterContext.java
+++ junit-jupiter-api/src/main/java/org/junit/jupiter/api/extension/ParameterContext.java
@@ -13,9 +13,11 @@ package org.junit.jupiter.api.extension;
 import static org.apiguardian.api.API.Status.EXPERIMENTAL;
 import static org.apiguardian.api.API.Status.STABLE;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.Executable;
 import java.lang.reflect.Parameter;
+import java.util.List;
 import java.util.Optional;
 
 import org.apiguardian.api.API;
@@ -96,6 +98,36 @@ public interface ParameterContext extends AnnotatedElementContext {
 	@Override
 	default AnnotatedElement getAnnotatedElement() {
 		return getParameter();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 5.1.1
+	 */
+	@API(status = STABLE, since = "5.10")
+	@Override
+	default boolean isAnnotated(Class<? extends Annotation> annotationType) {
+		return AnnotatedElementContext.super.isAnnotated(annotationType);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 5.1.1
+	 */
+	@API(status = STABLE, since = "5.10")
+	@Override
+	default <A extends Annotation> Optional<A> findAnnotation(Class<A> annotationType) {
+		return AnnotatedElementContext.super.findAnnotation(annotationType);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @since 5.1.1
+	 */
+	@API(status = STABLE, since = "5.10")
+	@Override
+	default <A extends Annotation> List<A> findRepeatableAnnotations(Class<A> annotationType) {
+		return AnnotatedElementContext.super.findRepeatableAnnotations(annotationType);
 	}
 
 }
