5797850322bdd2c1a2d6b1d0943b03472a0aa92f
Driptaroop Das <driptaroop.das@gmail.com>
2024-04-16 17:02:29 +0200

    Document test exception handling
    Documents how exceptions in tests are handled. This PR contains documentation that describes,
    
    1. Uncaught exceptions cause test failures by default
    2. How assertions are implemented using exceptions
    3. JUnit 5 does not differentiate between failed assertions and other exceptions but IDEs and other tools may choose to do so by checking for instances of AssertionError
    4. Adds assertDoesNotThrow example for Java

---
+++ documentation/src/test/java/example/exception/AssertDoesNotThrowExceptionDemoTestCase.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2015-2024 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+
+package example.exception;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+// @formatter:off
+// tag::user_guide[]
+
+class AssertDoesNotThrowExceptionDemoTestCase {
+
+	@Test
+	void assertDoesNotThrowExceptionTest() {
+		Assertions.assertThrows(RuntimeException.class, () -> {
+			failsDueToException();
+			Assertions.assertDoesNotThrow(() -> {
+				shouldNotThrowException();
+			});
+		});
+	}
+
+	void failsDueToException(){
+		throw new RuntimeException();
+	}
+
+	void shouldNotThrowException(){}
+}
+
+// end::user_guide[]
+// @formatter:on
---
+++ documentation/src/test/java/example/exception/UncaughtExceptionHandlingDemoTestCase.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2015-2024 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+
+package example.exception;
+
+import org.junit.jupiter.api.Test;
+
+// @formatter:off
+// tag::user_guide[]
+
+class UncaughtExceptionHandlingDemoTestCase {
+
+    @Test
+    void failsDueToUncaughtException() {
+        throw new RuntimeException("This exception will cause a test failure");
+    }
+}
+
+// end::user_guide[]
+// @formatter:on
