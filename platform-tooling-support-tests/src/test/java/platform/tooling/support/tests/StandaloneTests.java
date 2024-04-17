539f6a5cf1a9486c5417e9200092075a7b180132
Marc Philipp <mail@marcphilipp.de>
2024-04-17 15:29:24 +0200

    Revert "Make StandaloneTests lenient against unexpected error output"
    This reverts commit 932ba219626d1511a5dce5f9d8605967f6a9c59c.

--- platform-tooling-support-tests/src/test/java/platform/tooling/support/tests/StandaloneTests.java
+++ platform-tooling-support-tests/src/test/java/platform/tooling/support/tests/StandaloneTests.java
@@ -21,6 +21,7 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.List;
 
 import de.sormuras.bartholdy.Result;
 import de.sormuras.bartholdy.jdk.Jar;
@@ -391,7 +392,7 @@ class StandaloneTests {
 
 		var workspace = Request.WORKSPACE.resolve("standalone");
 		var expectedOutLines = Files.readAllLines(workspace.resolve("expected-out.txt"));
-		var expectedErrLines = Files.readAllLines(workspace.resolve("expected-err.txt"));
+		var expectedErrLines = getExpectedErrLinesOnJava8(workspace);
 		assertLinesMatch(expectedOutLines, result.getOutputLines("out"));
 		assertLinesMatch(expectedErrLines, result.getOutputLines("err"));
 
@@ -428,7 +429,7 @@ class StandaloneTests {
 
 		var workspace = Request.WORKSPACE.resolve("standalone");
 		var expectedOutLines = Files.readAllLines(workspace.resolve("expected-out.txt"));
-		var expectedErrLines = Files.readAllLines(workspace.resolve("expected-err.txt"));
+		var expectedErrLines = getExpectedErrLinesOnJava8(workspace);
 		assertLinesMatch(expectedOutLines, result.getOutputLines("out"));
 		assertLinesMatch(expectedErrLines, result.getOutputLines("err"));
 
@@ -438,6 +439,13 @@ class StandaloneTests {
 				+ " (group ID: org.junit.jupiter, artifact ID: junit-jupiter-engine, version: " + jupiterVersion));
 		assertTrue(result.getOutput("err").contains("junit-vintage"
 				+ " (group ID: org.junit.vintage, artifact ID: junit-vintage-engine, version: " + vintageVersion));
+	}
+
+	private static List<String> getExpectedErrLinesOnJava8(Path workspace) throws IOException {
+		var expectedErrLines = new ArrayList<String>();
+		expectedErrLines.add(">> JAVA VERSION >>");
+		expectedErrLines.addAll(Files.readAllLines(workspace.resolve("expected-err.txt")));
+		return expectedErrLines;
 	}
 
 	@Test
