f18e745b90f6670abbfeca16bdfd58e921e11c07
Marc Philipp <mail@marcphilipp.de>
2024-05-16 17:22:47 +0200

    Fix formatting

--- junit-platform-console/src/main/java/org/junit/platform/console/options/CommandFacade.java
+++ junit-platform-console/src/main/java/org/junit/platform/console/options/CommandFacade.java
@@ -35,7 +35,7 @@ public class CommandFacade {
 	public CommandResult<?> run(PrintWriter out, PrintWriter err, String[] args) {
 		String version = ManifestVersionProvider.getImplementationVersion();
 		System.setProperty("junit.docs.version",
-				version == null ? "current" : (version.endsWith("-SNAPSHOT") ? "snapshot" : version));
+			version == null ? "current" : (version.endsWith("-SNAPSHOT") ? "snapshot" : version));
 		return new MainCommand(consoleTestExecutorFactory).run(out, err, args);
 	}
 }
