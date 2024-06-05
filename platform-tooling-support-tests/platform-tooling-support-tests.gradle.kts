49f90b3c5e3fa8b9128a7bf40a3ebfc2e056a649
Marc Philipp <mail@marcphilipp.de>
2024-06-05 15:49:32 +0200

    Try to fix CC incompatibility

--- platform-tooling-support-tests/platform-tooling-support-tests.gradle.kts
+++ platform-tooling-support-tests/platform-tooling-support-tests.gradle.kts
@@ -92,9 +92,11 @@ dependencies {
 	}
 }
 
+val mavenDistributionDir = layout.buildDirectory.dir("maven-distribution")
+
 val unzipMavenDistribution by tasks.registering(Sync::class) {
 	from(zipTree(mavenDistributionClasspath.flatMap { d -> d.elements.map { e -> e.single() } }))
-	into(layout.buildDirectory.dir("maven-distribution"))
+	into(mavenDistributionDir)
 }
 
 val normalizeMavenRepo by tasks.registering(Sync::class) {
@@ -137,7 +139,7 @@ tasks.test {
 
 	jvmArgumentProviders += JarPath(project, thirdPartyJarsClasspath.get(), "thirdPartyJars")
 	jvmArgumentProviders += JarPath(project, antJarsClasspath.get(), "antJars")
-	jvmArgumentProviders += MavenDistribution(project, unzipMavenDistribution)
+	jvmArgumentProviders += MavenDistribution(project, unzipMavenDistribution, mavenDistributionDir)
 
 	(options as JUnitPlatformOptions).apply {
 		includeEngines("archunit")
@@ -218,11 +220,11 @@ class JarPath(project: Project, configuration: Configuration, @Input val key: St
 	override fun asArguments() = listOf("-D${key}=${files.asPath}")
 }
 
-class MavenDistribution(project: Project, sourceTask: TaskProvider<*>) : CommandLineArgumentProvider {
+class MavenDistribution(project: Project, sourceTask: TaskProvider<*>, distributionDir: Provider<Directory>) : CommandLineArgumentProvider {
 	@InputDirectory
 	@PathSensitive(RELATIVE)
 	val mavenDistribution: DirectoryProperty = project.objects.directoryProperty()
-		.value(project.layout.dir(sourceTask.map { it.outputs.files.singleFile.listFiles()!!.single() }))
+		.fileProvider(project.files(distributionDir).builtBy(sourceTask).elements.map { it.single().asFile })
 
 	override fun asArguments() = listOf("-DmavenDistribution=${mavenDistribution.get().asFile.absolutePath}")
 }
