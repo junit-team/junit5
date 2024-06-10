b725bb9dd64eba52a43bbfd9b479c0256f5de308
Marc Philipp <mail@marcphilipp.de>
2024-06-10 19:46:45 +0200

    Add JTE dependency

--- gradle/plugins/code-generator/build.gradle.kts
+++ gradle/plugins/code-generator/build.gradle.kts
@@ -9,4 +9,5 @@ repositories {
 dependencies {
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.17.1")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.1")
+    implementation("gg.jte:jte:3.1.12")
 }
