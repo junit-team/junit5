3dd2335d94ad2f1bc47d6ebe811d1c710b0d8ab3
Marc Philipp <mail@marcphilipp.de>
2024-06-11 17:45:00 +0200

    Remove unused version catalog

--- gradle/base/settings.gradle.kts
+++ gradle/base/settings.gradle.kts
@@ -1,11 +1,3 @@
-dependencyResolutionManagement {
-	versionCatalogs {
-		create("libs") {
-			from(files("../libs.versions.toml"))
-		}
-	}
-}
-
 rootProject.name = "base"
 
 include("code-generator-model")
