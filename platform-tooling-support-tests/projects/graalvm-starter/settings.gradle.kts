cfc9e65a1b16aa639db3dc1c506b27f70ad83026
Sam Brannen <104798+sbrannen@users.noreply.github.com>
2024-09-05 13:20:14 +0200

    Indent with tabs

--- platform-tooling-support-tests/projects/graalvm-starter/settings.gradle.kts
+++ platform-tooling-support-tests/projects/graalvm-starter/settings.gradle.kts
@@ -1,11 +1,11 @@
 pluginManagement {
-    plugins {
-        id("org.graalvm.buildtools.native") version "0.10.1"
-    }
-    repositories {
-        mavenCentral()
-        gradlePluginPortal()
-    }
+	plugins {
+		id("org.graalvm.buildtools.native") version "0.10.1"
+	}
+	repositories {
+		mavenCentral()
+		gradlePluginPortal()
+	}
 }
 
 rootProject.name = "graalvm-starter"
