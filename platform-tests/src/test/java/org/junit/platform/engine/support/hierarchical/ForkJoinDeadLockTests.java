e951f595b27b01e9f4e7875ed307fde1e04c4c2b
Leonard Brünings <leonard.bruenings@gradle.com>
2024-09-11 15:29:51 +0200

    Deadlock Reproducer
    Co-authored-by: Marc Philipp <mail@marcphilipp.de>

---
+++ platform-tests/src/test/java/org/junit/platform/engine/support/hierarchical/ForkJoinDeadLockTests.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2015-2024 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+
+package org.junit.platform.engine.support.hierarchical;
+
+import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass;
+
+import java.util.concurrent.CountDownLatch;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.api.parallel.Isolated;
+import org.junit.jupiter.engine.Constants;
+import org.junit.platform.testkit.engine.EngineTestKit;
+
+public class ForkJoinDeadLockTests {
+
+	// https://github.com/junit-team/junit5/issues/3945
+	@Test
+	@Timeout(10)
+	void forkJoinExecutionDoesNotLeadToDeadLock() {
+		EngineTestKit.engine("junit-jupiter").selectors(selectClass(FirstTestCase.class),
+			selectClass(IsolatedTestCase.class), selectClass(Isolated2TestCase.class)) //
+				.configurationParameter(Constants.PARALLEL_EXECUTION_ENABLED_PROPERTY_NAME, "true") //
+				.configurationParameter(Constants.DEFAULT_PARALLEL_EXECUTION_MODE, "concurrent") //
+				.configurationParameter(Constants.DEFAULT_CLASSES_EXECUTION_MODE_PROPERTY_NAME, "concurrent") //
+				.configurationParameter(Constants.PARALLEL_CONFIG_STRATEGY_PROPERTY_NAME, "fixed") //
+				.configurationParameter(Constants.PARALLEL_CONFIG_FIXED_MAX_POOL_SIZE_PROPERTY_NAME, "3") //
+				.configurationParameter(Constants.PARALLEL_CONFIG_FIXED_PARALLELISM_PROPERTY_NAME, "3") //
+				.execute();
+	}
+
+	@SuppressWarnings("JUnitMalformedDeclaration")
+	public static class FirstTestCase {
+		public static CountDownLatch countDownLatch = new CountDownLatch(2);
+
+		@BeforeAll
+		static void beforeAll() {
+			// System.out.println("forkJoinWorkerThread = " + Thread.currentThread());
+		}
+
+		@Test
+		void test1() throws InterruptedException {
+			// System.out.println("FirstTestCase.test1 Thread.currentThread() = " + Thread.currentThread());
+			await();
+		}
+
+		@Test
+		void test2() throws InterruptedException {
+			// System.out.println("FirstTestCase.test2 Thread.currentThread() = " + Thread.currentThread());
+			await();
+		}
+
+		private void await() throws InterruptedException {
+			countDownLatch.countDown();
+			countDownLatch.await();
+		}
+	}
+
+	@SuppressWarnings("JUnitMalformedDeclaration")
+	@Isolated
+	public static class IsolatedTestCase {
+		@Test
+		void test1() {
+			// System.out.println("Isolated Thread.currentThread() = " + Thread.currentThread());
+		}
+	}
+	@SuppressWarnings("JUnitMalformedDeclaration")
+	@Isolated
+	public static class Isolated2TestCase {
+		@Test
+		void test1() {
+			// System.out.println("Isolated2 Thread.currentThread() = " + Thread.currentThread());
+		}
+	}
+}
