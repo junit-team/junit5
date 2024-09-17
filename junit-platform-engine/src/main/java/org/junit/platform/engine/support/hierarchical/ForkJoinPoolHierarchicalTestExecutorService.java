932fe5cde670b0441635f65a7fb059c0d40db6dc
Leonard Brünings <leonard.bruenings@gradle.com>
2024-09-17 10:58:05 +0200

    Fix deadlock in `ForkJoinPoolHierarchicalTestExecutorService` (#3981)
    The service now checks if the `ExclusiveTask` that should run
    is executed on a thread that is already executing another task.
    If this is scenario is detected, it checks if the lock is compatible to
    the enclosing locks.
    1. If compatible, it is executed and marked done
    2. If incompatible, it is added to a list of deferred tasks and left
       unfinished. The deferred tasks will be re-forked afterwards.
    
    Fixes #3945.
    
    ---------
    
    Co-authored-by: Marc Philipp <mail@marcphilipp.de>
    (cherry picked from commit 88f88591abc9900a3e30f0c3a9fca524b5c0f8b5)

--- junit-platform-engine/src/main/java/org/junit/platform/engine/support/hierarchical/ForkJoinPoolHierarchicalTestExecutorService.java
+++ junit-platform-engine/src/main/java/org/junit/platform/engine/support/hierarchical/ForkJoinPoolHierarchicalTestExecutorService.java
@@ -16,6 +16,8 @@ import static org.junit.platform.engine.support.hierarchical.Node.ExecutionMode.
 
 import java.lang.Thread.UncaughtExceptionHandler;
 import java.lang.reflect.Constructor;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
@@ -26,7 +28,6 @@ import java.util.concurrent.ForkJoinPool.ForkJoinWorkerThreadFactory;
 import java.util.concurrent.ForkJoinTask;
 import java.util.concurrent.ForkJoinWorkerThread;
 import java.util.concurrent.Future;
-import java.util.concurrent.RecursiveAction;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -51,7 +52,9 @@ import org.junit.platform.engine.ConfigurationParameters;
 public class ForkJoinPoolHierarchicalTestExecutorService implements HierarchicalTestExecutorService {
 
 	private final ForkJoinPool forkJoinPool;
+	private final TaskEventListener taskEventListener;
 	private final int parallelism;
+	private final ThreadLocal<ThreadLock> threadLocks = ThreadLocal.withInitial(ThreadLock::new);
 
 	/**
 	 * Create a new {@code ForkJoinPoolHierarchicalTestExecutorService} based on
@@ -71,7 +74,13 @@ public class ForkJoinPoolHierarchicalTestExecutorService implements Hierarchical
 	 */
 	@API(status = STABLE, since = "1.10")
 	public ForkJoinPoolHierarchicalTestExecutorService(ParallelExecutionConfiguration configuration) {
+		this(configuration, TaskEventListener.NOOP);
+	}
+
+	ForkJoinPoolHierarchicalTestExecutorService(ParallelExecutionConfiguration configuration,
+			TaskEventListener taskEventListener) {
 		forkJoinPool = createForkJoinPool(configuration);
+		this.taskEventListener = taskEventListener;
 		parallelism = forkJoinPool.getParallelism();
 		LoggerFactory.getLogger(getClass()).config(() -> "Using ForkJoinPool with parallelism of " + parallelism);
 	}
@@ -132,7 +141,7 @@ public class ForkJoinPoolHierarchicalTestExecutorService implements Hierarchical
 		if (testTask.getExecutionMode() == CONCURRENT && ForkJoinTask.getSurplusQueuedTaskCount() < parallelism) {
 			return exclusiveTask.fork();
 		}
-		exclusiveTask.compute();
+		exclusiveTask.execSync();
 		return completedFuture(null);
 	}
 
@@ -143,7 +152,7 @@ public class ForkJoinPoolHierarchicalTestExecutorService implements Hierarchical
 	@Override
 	public void invokeAll(List<? extends TestTask> tasks) {
 		if (tasks.size() == 1) {
-			new ExclusiveTask(tasks.get(0)).compute();
+			new ExclusiveTask(tasks.get(0)).execSync();
 			return;
 		}
 		Deque<ExclusiveTask> nonConcurrentTasks = new LinkedList<>();
@@ -169,7 +178,7 @@ public class ForkJoinPoolHierarchicalTestExecutorService implements Hierarchical
 
 	private void executeNonConcurrentTasks(Deque<ExclusiveTask> nonConcurrentTasks) {
 		for (ExclusiveTask task : nonConcurrentTasks) {
-			task.compute();
+			task.execSync();
 		}
 	}
 
@@ -177,7 +186,18 @@ public class ForkJoinPoolHierarchicalTestExecutorService implements Hierarchical
 			Deque<ExclusiveTask> concurrentTasksInReverseOrder) {
 		for (ExclusiveTask forkedTask : concurrentTasksInReverseOrder) {
 			forkedTask.join();
+			resubmitDeferredTasks();
 		}
+	}
+
+	private void resubmitDeferredTasks() {
+		List<ExclusiveTask> deferredTasks = threadLocks.get().deferredTasks;
+		for (ExclusiveTask deferredTask : deferredTasks) {
+			if (!deferredTask.isDone()) {
+				deferredTask.fork();
+			}
+		}
+		deferredTasks.clear();
 	}
 
 	@Override
@@ -186,8 +206,8 @@ public class ForkJoinPoolHierarchicalTestExecutorService implements Hierarchical
 	}
 
 	// this class cannot not be serialized because TestTask is not Serializable
-	@SuppressWarnings("serial")
-	static class ExclusiveTask extends RecursiveAction {
+	@SuppressWarnings({ "serial", "RedundantSuppression" })
+	class ExclusiveTask extends ForkJoinTask<Void> {
 
 		private final TestTask testTask;
 
@@ -195,17 +215,61 @@ public class ForkJoinPoolHierarchicalTestExecutorService implements Hierarchical
 			this.testTask = testTask;
 		}
 
+		/**
+		 * Always returns {@code null}.
+		 *
+		 * @return {@code null} always
+		 */
+		public final Void getRawResult() {
+			return null;
+		}
+
+		/**
+		 * Requires null completion value.
+		 */
+		protected final void setRawResult(Void mustBeNull) {
+		}
+
+		void execSync() {
+			boolean completed = exec();
+			if (!completed) {
+				throw new IllegalStateException(
+					"Task was deferred but should have been executed synchronously: " + testTask);
+			}
+		}
+
 		@SuppressWarnings("try")
 		@Override
-		public void compute() {
-			try (ResourceLock lock = testTask.getResourceLock().acquire()) {
+		public boolean exec() {
+			// Check if this task is compatible with the current resource lock, if there is any.
+			// If not, we put this task in the thread local as a deferred task
+			// and let the worker thread fork it once it is done with the current task.
+			ResourceLock resourceLock = testTask.getResourceLock();
+			ThreadLock threadLock = threadLocks.get();
+			if (!threadLock.areAllHeldLocksCompatibleWith(resourceLock)) {
+				threadLock.addDeferredTask(this);
+				taskEventListener.deferred(testTask);
+				// Return false to indicate that this task is not done yet
+				// this means that .join() will wait.
+				return false;
+			}
+			try (ResourceLock lock = resourceLock.acquire()) {
+				threadLock.incrementNesting(lock);
 				testTask.execute();
+				return true;
 			}
 			catch (InterruptedException e) {
 				ExceptionUtils.throwAsUncheckedException(e);
 			}
+			finally {
+				threadLock.decrementNesting();
+			}
 		}
 
+		@Override
+		public String toString() {
+			return "ExclusiveTask [" + testTask + "]";
+		}
 	}
 
 	static class WorkerThreadFactory implements ForkJoinPool.ForkJoinWorkerThreadFactory {
@@ -226,6 +290,36 @@ public class ForkJoinPoolHierarchicalTestExecutorService implements Hierarchical
 			setContextClassLoader(contextClassLoader);
 		}
 
+	}
+
+	static class ThreadLock {
+		private final Deque<ResourceLock> locks = new ArrayDeque<>(2);
+		private final List<ExclusiveTask> deferredTasks = new ArrayList<>();
+
+		void addDeferredTask(ExclusiveTask task) {
+			deferredTasks.add(task);
+		}
+
+		void incrementNesting(ResourceLock lock) {
+			locks.push(lock);
+		}
+
+		@SuppressWarnings("resource")
+		void decrementNesting() {
+			locks.pop();
+		}
+
+		boolean areAllHeldLocksCompatibleWith(ResourceLock lock) {
+			return locks.stream().allMatch(l -> l.isCompatible(lock));
+		}
+	}
+
+	interface TaskEventListener {
+
+		TaskEventListener NOOP = __ -> {
+		};
+
+		void deferred(TestTask testTask);
 	}
 
 }
