ad235a6ff52e5affc2c345e7a930ff461e16035f
Marc Philipp <mail@marcphilipp.de>
2024-11-27 20:01:08 +0100

    Adapt ReflectionCompatibilityTests

--- platform-tooling-support-tests/src/test/java/platform/tooling/support/tests/ReflectionCompatibilityTests.java
+++ platform-tooling-support-tests/src/test/java/platform/tooling/support/tests/ReflectionCompatibilityTests.java
@@ -12,21 +12,17 @@ package platform.tooling.support.tests;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static platform.tooling.support.Helper.TOOL_TIMEOUT;
+import static platform.tooling.support.tests.Projects.copyToWorkspace;
 
-import java.nio.file.Paths;
-
-import de.sormuras.bartholdy.tool.GradleWrapper;
+import java.nio.file.Path;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.parallel.ResourceLock;
+import org.junit.jupiter.api.io.TempDir;
 import org.opentest4j.TestAbortedException;
 
 import platform.tooling.support.Helper;
 import platform.tooling.support.MavenRepo;
-import platform.tooling.support.Request;
 import platform.tooling.support.process.ProcessStarters;
 
 /**
@@ -34,25 +30,17 @@ import platform.tooling.support.process.ProcessStarters;
  */
 class ReflectionCompatibilityTests {
 
-	@ResourceLock(Projects.REFLECTION_TESTS)
 	@Test
-	void gradle_wrapper() {
-		var request = Request.builder() //
-				.setTool(new GradleWrapper(Paths.get(".."))) //
-				.setProject(Projects.REFLECTION_TESTS) //
+	void gradle_wrapper(@TempDir Path workspace) throws Exception {
+		var result = ProcessStarters.gradlew() //
+				.workingDir(copyToWorkspace(Projects.REFLECTION_TESTS, workspace)) //
 				.addArguments("-Dmaven.repo=" + MavenRepo.dir()) //
 				.addArguments("build", "--no-daemon", "--stacktrace", "--no-build-cache", "--warning-mode=fail") //
-				.setTimeout(TOOL_TIMEOUT) //
 				.putEnvironment("JDK8", Helper.getJavaHome("8").orElseThrow(TestAbortedException::new).toString()) //
-				.setJavaHome(ProcessStarters.getGradleJavaHome().orElseThrow(TestAbortedException::new)) //
-				.build();
+				.startAndWait();
 
-		var result = request.run();
-
-		assertFalse(result.isTimedOut(), () -> "tool timed out: " + result);
-
-		assertEquals(0, result.getExitCode());
-		assertTrue(result.getOutputLines("out").stream().anyMatch(line -> line.contains("BUILD SUCCESSFUL")));
-		assertThat(result.getOutput("out")).contains("Using Java version: 1.8");
+		assertEquals(0, result.exitCode());
+		assertTrue(result.stdOut().lines().anyMatch(line -> line.contains("BUILD SUCCESSFUL")));
+		assertThat(result.stdOut()).contains("Using Java version: 1.8");
 	}
 }
