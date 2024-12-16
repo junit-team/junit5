f92b14dc5604256ef5a4ab75ad1f5ebd9d5e8ddf
Marc Philipp <mail@marcphilipp.de>
2024-12-16 15:40:42 +0100

    Return "constant" predicates for default case

--- junit-platform-commons/src/main/java/org/junit/platform/commons/util/ClassNamePatternFilterUtils.java
+++ junit-platform-commons/src/main/java/org/junit/platform/commons/util/ClassNamePatternFilterUtils.java
@@ -103,16 +103,16 @@ public class ClassNamePatternFilterUtils {
 	}
 
 	private static <T> Predicate<T> createPredicateFromPatterns(String patterns, Function<T, String> classNameProvider,
-			FilterType mode) {
+			FilterType type) {
 		if (ALL_PATTERN.equals(patterns)) {
-			return __ -> mode == FilterType.INCLUDE;
+			return type == FilterType.INCLUDE ? __ -> true : __ -> false;
 		}
 
 		List<Pattern> patternList = convertToRegularExpressions(patterns);
 		return object -> {
 			boolean isMatchingAnyPattern = patternList.stream().anyMatch(
 				pattern -> pattern.matcher(classNameProvider.apply(object)).matches());
-			return (mode == FilterType.INCLUDE) == isMatchingAnyPattern;
+			return (type == FilterType.INCLUDE) == isMatchingAnyPattern;
 		};
 	}
 
