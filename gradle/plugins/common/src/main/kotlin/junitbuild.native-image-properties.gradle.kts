7e4fd2570ec8eaed0679d12b2fae41fe64fa0a0b
Marc Philipp <mail@marcphilipp.de>
2024-12-18 14:22:22 +0100

    Validate listed classes exist

--- gradle/plugins/common/src/main/kotlin/junitbuild.native-image-properties.gradle.kts
+++ gradle/plugins/common/src/main/kotlin/junitbuild.native-image-properties.gradle.kts
@@ -1,33 +1,55 @@
 import junitbuild.graalvm.NativeImagePropertiesExtension
+import java.util.zip.ZipFile
 
 plugins {
-    `java-library`
+	`java-library`
 }
 
 val extension = extensions.create<NativeImagePropertiesExtension>("nativeImageProperties").apply {
-    val resourceFile: RegularFile = layout.projectDirectory.file("src/nativeImage/initialize-at-build-time")
-    if (resourceFile.asFile.exists()) {
-        initializeAtBuildTime.convention(providers.fileContents(resourceFile).asText.map { it.trim().lines() })
-    } else {
-        initializeAtBuildTime.empty()
-    }
+	val resourceFile: RegularFile = layout.projectDirectory.file("src/nativeImage/initialize-at-build-time")
+	if (resourceFile.asFile.exists()) {
+		initializeAtBuildTime.convention(providers.fileContents(resourceFile).asText.map { it.trim().lines() })
+	} else {
+		initializeAtBuildTime.empty()
+	}
+	initializeAtBuildTime.finalizeValueOnRead()
 }
 
 val outputDir = layout.buildDirectory.dir("resources/nativeImage")
 
-val task = tasks.register<WriteProperties>("nativeImageProperties") {
-    destinationFile = outputDir.map { it.file("META-INF/native-image/${project.group}/${project.name}/native-image.properties") }
-    property("Args", extension.initializeAtBuildTime.map {
-        if (it.isEmpty()) {
-            ""
-        } else {
-            "--initialize-at-build-time=${it.joinToString(",")}"
-        }
-    })
+val propertyFileTask = tasks.register<WriteProperties>("nativeImageProperties") {
+	destinationFile = outputDir.map { it.file("META-INF/native-image/${project.group}/${project.name}/native-image.properties") }
+	property("Args", extension.initializeAtBuildTime.map {
+		if (it.isEmpty()) {
+			""
+		} else {
+			"--initialize-at-build-time=${it.joinToString(",")}"
+		}
+	})
+}
+
+val validationTask = tasks.register("validateNativeImageProperties") {
+	dependsOn(tasks.jar)
+	doLast {
+		val missingClasses = ZipFile(tasks.jar.get().archiveFile.get().asFile).use { zipFile ->
+			extension.initializeAtBuildTime.get().filter { className ->
+				zipFile.entries().asSequence().none {
+					it.name == className.replace('.', '/') + ".class"
+				}
+			}
+		}
+		if (missingClasses.isNotEmpty()) {
+			throw GradleException("The following classes were specified as initialized-at-build-time but do not exist (you should probably remove them from nativeImageProperties.initializeAtBuildTime): $missingClasses")
+		}
+	}
+}
+
+tasks.check {
+	dependsOn(validationTask)
 }
 
 sourceSets {
-    main {
-        output.dir(task.map { outputDir })
-    }
+	main {
+		output.dir(propertyFileTask.map { outputDir })
+	}
 }
