b508b574739c2859dc6b94338b33e208c1840da8
Marc Philipp <mail@marcphilipp.de>
2024-12-18 14:37:31 +0100

    Add Vintage and Suite engines to project used by GraalVmStarterTests

--- platform-tooling-support-tests/projects/graalvm-starter/build.gradle.kts
+++ platform-tooling-support-tests/projects/graalvm-starter/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 
 val jupiterVersion: String by project
 val platformVersion: String by project
+val vintageVersion: String by project
 
 repositories {
 	maven { url = uri(file(System.getProperty("maven.repo"))) }
@@ -13,11 +14,16 @@ repositories {
 
 dependencies {
 	testImplementation("org.junit.jupiter:junit-jupiter:$jupiterVersion")
+	testImplementation("junit:junit:4.13.2")
+	testImplementation("org.junit.platform:junit-platform-suite:$platformVersion")
+	testRuntimeOnly("org.junit.vintage:junit-vintage-engine:$vintageVersion")
 	testRuntimeOnly("org.junit.platform:junit-platform-reporting:$platformVersion")
 }
 
 tasks.test {
-	useJUnitPlatform()
+	useJUnitPlatform {
+		includeEngines("junit-platform-suite")
+	}
 
 	val outputDir = reports.junitXml.outputLocation
 	jvmArgumentProviders += CommandLineArgumentProvider {
