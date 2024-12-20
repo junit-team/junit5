7593ded3d68ffdf11439eb5fe18328d2a06c2d92
Marc Philipp <mail@marcphilipp.de>
2024-12-20 09:00:26 +0100

    Add native-image.properties files to jars (#4208)
    This allows adding or removing entries from the set of classes that are 
    initialized at native image build time without requiring changes to 
    graalvm/native-build-tools. The new precompiled script plugin introduced
    in this commit defines two Gradle tasks: `nativeImageProperties` and 
    `validateNativeImageProperties`. The former is used for writing the 
    `native-image.properties` file to the correct resource folder based (by
    convention) on the `src/nativeImage/initialize-at-build-time` file in 
    each affected subproject. The latter validates that all listed classes
    exist and acts as a safeguard when classes are renamed or deleted.
    
    Resolves #4207.

---
+++ platform-tooling-support-tests/projects/graalvm-starter/src/test/java/com/example/project/VintageTests.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2015-2024 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+
+package com.example.project;
+
+import org.junit.Test;
+
+public class VintageTests {
+	@Test
+	public void test() {
+	}
+}
