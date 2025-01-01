5430f118b778a70ed3e5625f9a535885a86bff62
Marc Philipp <mail@marcphilipp.de>
2025-01-01 17:04:27 +0100

    Add workflow to verify reproducibility of binary release artifacts
    This workflow is supposed to be triggered manually _after_ release
    artifacts have been uploaded to a staging repository and the
    corresponding Git tag has been pushed to GitHub.
    
    To ensure their integrity and reproducibility, the workflow rebuilds
    all binary artifacts and verifies that their checksums are identical to
    those in the staging repository.

---
+++ gradle/plugins/common/src/main/kotlin/junitbuild/release/VerifyBinaryArtifactsAreIdentical.kt
@@ -0,0 +1,80 @@
+package junitbuild.release
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputDirectory
+import org.gradle.api.tasks.PathSensitive
+import org.gradle.api.tasks.PathSensitivity
+import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.options.Option
+import java.io.File
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse.BodyHandlers
+
+abstract class VerifyBinaryArtifactsAreIdentical : DefaultTask() {
+
+    @get:InputDirectory
+    @get:PathSensitive(PathSensitivity.RELATIVE)
+    abstract val localRepoDir: DirectoryProperty
+
+    @get:Input
+    abstract val remoteRepoUrl: Property<String>
+
+    init {
+        // Depends on contents of remote repository
+        outputs.upToDateWhen { false }
+    }
+
+    @Suppress("unused")
+    @Option(
+        option = "remote-repo-url",
+        description = "The URL of the remote repository to compare the local repository against"
+    )
+    fun remoteRepo(url: String) {
+        remoteRepoUrl.set(url)
+    }
+
+    @TaskAction
+    fun execute() {
+        val localRootDir = localRepoDir.get().asFile
+        val baseUrl = remoteRepoUrl.get()
+        val mismatches = mutableListOf<Mismatch>()
+        var numChecks = 0
+        HttpClient.newHttpClient().use { httpClient ->
+            localRootDir.walk().forEach { file ->
+                if (file.isFile && file.name.endsWith(".jar.sha512") && !file.name.endsWith("-javadoc.jar.sha512")) {
+                    val localSha512 = file.readText()
+                    val relativeFile = file.relativeTo(localRootDir)
+                    val url = URI.create("${baseUrl}/${relativeFile.path}")
+                    logger.info("Checking {}...", url)
+                    val request = HttpRequest.newBuilder().GET().uri(url).build()
+                    val response = httpClient.send(request, BodyHandlers.ofString())
+                    val remoteSha512 = if (response.statusCode() == 200) response.body() else "status=${response.statusCode()}"
+                    if (localSha512 != remoteSha512) {
+                        mismatches.add(Mismatch(relativeFile, localSha512, remoteSha512))
+                    }
+                    numChecks++
+                }
+            }
+        }
+        require(numChecks > 0) {
+            "No files found to compare"
+        }
+        require(mismatches.isEmpty()) {
+            "The following files have different SHA-512 checksums in the local and remote repositories:\n\n" +
+                    mismatches.joinToString("\n\n") {
+                        """
+                            ${it.file}
+                            local:  ${it.localSha512}
+                            remote: ${it.remoteSha512}
+                        """.trimIndent()
+                    }
+        }
+    }
+
+    private data class Mismatch(val file: File, val localSha512: String, val remoteSha512: String)
+}
