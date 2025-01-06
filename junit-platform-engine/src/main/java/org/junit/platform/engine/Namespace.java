faebb0c7ca4c05bb29687d8747b42831b4c3b53b
yongjunhong <dev.yongjunh@gmail.com>
2025-01-06 21:17:31 +0900

    Add Namespace by reusing Jupiter's Namespace class
    Issue: #2816

--- junit-platform-engine/src/main/java/org/junit/platform/engine/Namespace.java
+++ junit-platform-engine/src/main/java/org/junit/platform/engine/Namespace.java
@@ -1,5 +1,9 @@
 /*
+<<<<<<< HEAD
  * Copyright 2015-2025 the original author or authors.
+=======
+ * Copyright 2015-2024 the original author or authors.
+>>>>>>> f3adab18f (Add Namespace by reusing Jupiter's Namespace class)
  *
  * All rights reserved. This program and the accompanying materials are
  * made available under the terms of the Eclipse Public License v2.0 which
