1b23aeb9e1f40956764f33d7343f14ae04edfe35
yongjunhong <dev.yongjunh@gmail.com>
2025-03-03 14:06:55 +0900

    Add NamespaceTests
    Issue: #2816

---
+++ platform-tests/src/test/java/org/junit/platform/engine/support/store/NamespaceTests.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2015-2025 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+
+package org.junit.platform.engine.support.store;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class NamespaceTests {
+
+	@Test
+	void namespacesEqualForSamePartsSequence() {
+		Namespace ns1 = Namespace.create("part1", "part2");
+		Namespace ns2 = Namespace.create("part1", "part2");
+
+		assertEquals(ns1, ns2);
+	}
+
+	@Test
+	void orderOfNamespacePartsDoesMatter() {
+		Namespace ns1 = Namespace.create("part1", "part2");
+		Namespace ns2 = Namespace.create("part2", "part1");
+
+		assertNotEquals(ns1, ns2);
+	}
+}
