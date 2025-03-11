dfaa88a4047012349196f03d1346470460a951da
Marc Philipp <mail@marcphilipp.de>
2025-03-11 14:29:44 +0100

    No longer abort on selector processing issues
    Since those are now reported as discovery issues

--- junit-platform-launcher/src/main/java/org/junit/platform/launcher/listeners/discovery/AbortOnFailureLauncherDiscoveryListener.java
+++ junit-platform-launcher/src/main/java/org/junit/platform/launcher/listeners/discovery/AbortOnFailureLauncherDiscoveryListener.java
@@ -10,56 +10,20 @@
 
 package org.junit.platform.launcher.listeners.discovery;
 
-import static org.junit.platform.engine.SelectorResolutionResult.Status.FAILED;
-import static org.junit.platform.engine.SelectorResolutionResult.Status.UNRESOLVED;
-
-import org.junit.platform.commons.JUnitException;
 import org.junit.platform.commons.util.ExceptionUtils;
-import org.junit.platform.engine.DiscoverySelector;
-import org.junit.platform.engine.SelectorResolutionResult;
 import org.junit.platform.engine.UniqueId;
-import org.junit.platform.engine.discovery.UniqueIdSelector;
 import org.junit.platform.launcher.EngineDiscoveryResult;
-import org.junit.platform.launcher.LauncherDiscoveryListener;
 
 /**
  * @since 1.6
  * @see LauncherDiscoveryListeners#abortOnFailure()
  */
-class AbortOnFailureLauncherDiscoveryListener implements LauncherDiscoveryListener {
+class AbortOnFailureLauncherDiscoveryListener extends LoggingLauncherDiscoveryListener {
 
 	@Override
 	public void engineDiscoveryFinished(UniqueId engineId, EngineDiscoveryResult result) {
 		result.getThrowable().ifPresent(ExceptionUtils::throwAsUncheckedException);
-	}
-
-	@Override
-	public void selectorProcessed(UniqueId engineId, DiscoverySelector selector, SelectorResolutionResult result) {
-		if (result.getStatus() == FAILED) {
-			throw new JUnitException(selector + " resolution failed", result.getThrowable().orElse(null));
-		}
-		if (result.getStatus() == UNRESOLVED && selector instanceof UniqueIdSelector) {
-			UniqueId uniqueId = ((UniqueIdSelector) selector).getUniqueId();
-			if (uniqueId.hasPrefix(engineId)) {
-				throw new JUnitException(selector + " could not be resolved");
-			}
-		}
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		return getClass() == obj.getClass();
-	}
-
-	@Override
-	public int hashCode() {
-		return 0;
+		super.engineDiscoveryFinished(engineId, result);
 	}
 
 }
