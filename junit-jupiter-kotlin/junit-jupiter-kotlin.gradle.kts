34b9700e8f53e57fe32e733ee5ee7b9d9d743764
Marc Philipp <mail@marcphilipp.de>
2025-05-06 08:14:14 +0200

    Mark kotlinx.coroutines as optional for OSGi resolution

--- junit-jupiter-kotlin/junit-jupiter-kotlin.gradle.kts
+++ junit-jupiter-kotlin/junit-jupiter-kotlin.gradle.kts
@@ -9,20 +9,18 @@ dependencies {
 	implementation(kotlin("stdlib"))
 	implementation(kotlin("reflect"))
 	implementation(libs.kotlinx.coroutines)
+	osgiVerification(kotlin("osgi-bundle"))
 }
 
-tasks.verifyOSGi {
-	// TODO Fix the following error:
-	// Resolution failed. Summary:
-	//      ⇒ Bundle: junit-jupiter-kotlin cannot be resolved
-	//              ⇒ because Import-Package requirement for: kotlinx.coroutines could not be provided by any available bundle or dependency
-	//
-	//Note: The summary above may be incomplete. Please check the full output below for more hints.
-	//Resolution failed. Capabilities satisfying the following requirements could not be found:
-	//
-	//    [<<INITIAL>>]
-	//      ⇒ osgi.identity: (osgi.identity=junit-jupiter-kotlin)
-	//          ⇒ [junit-jupiter-kotlin version=6.0.0.SNAPSHOT]
-	//              ⇒ osgi.wiring.package: (osgi.wiring.package=kotlinx.coroutines)
-	enabled = false
+tasks.jar {
+	bundle {
+		val importAPIGuardian: String by extra
+		// Mark kotlinx.coroutines as optional since there's no OSGi bundle for it
+		bnd("""
+				Import-Package: \
+					$importAPIGuardian,\
+					kotlinx.coroutines.*;resolution:="optional",\
+					*
+			""")
+	}
 }
