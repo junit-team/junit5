ce5fadd81ea1fa7141e0c5bd410e62f3d02da93e
Marc Philipp <mail@marcphilipp.de>
2025-05-08 14:41:04 +0200

    Delete deprecated `TestPlan` methods

--- platform-tests/src/test/java/org/junit/platform/launcher/core/DefaultLauncherTests.java
+++ platform-tests/src/test/java/org/junit/platform/launcher/core/DefaultLauncherTests.java
@@ -595,25 +595,6 @@ class DefaultLauncherTests {
 	}
 
 	@Test
-	@SuppressWarnings("deprecation")
-	void testPlanThrowsExceptionWhenModified() {
-		TestEngine engine = new TestEngineSpy();
-		var launcher = createLauncher(engine);
-		var testPlan = launcher.discover(request().build());
-		var engineIdentifier = getOnlyElement(testPlan.getRoots());
-		var engineUniqueId = engineIdentifier.getUniqueIdObject();
-		assertThat(testPlan.getChildren(engineIdentifier)).hasSize(1);
-
-		var addedIdentifier = TestIdentifier.from(
-			new TestDescriptorStub(engineUniqueId.append("test", "test2"), "test2"));
-
-		var exception = assertThrows(JUnitException.class, () -> testPlan.add(addedIdentifier));
-		assertThat(exception).hasMessage("Unsupported attempt to modify the TestPlan was detected. "
-				+ "Please contact your IDE/tool vendor and request a fix or downgrade to JUnit 5.7.x (see https://github.com/junit-team/junit5/issues/1732 for details).");
-		assertThat(testPlan.getChildren(engineIdentifier)).hasSize(1).doesNotContain(addedIdentifier);
-	}
-
-	@Test
 	void thirdPartyEngineUsingReservedEngineIdPrefixEmitsWarning(@TrackLogRecords LogRecordListener listener) {
 		var id = "junit-using-reserved-prefix";
 		var launcher = createLauncher(new TestEngineStub(id));
