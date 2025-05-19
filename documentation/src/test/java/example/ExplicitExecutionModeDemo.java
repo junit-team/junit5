b4ba1bfebfca160f44cbe6ed47f172bd754517c6
Kim Gi Uk <2897robo@gmail.com>
2025-05-19 19:44:54 +0900

    Add example for using `@Execution` annotation to User Guide (#4525)
    Resolves #2669.
    
    ---------
    
    Co-authored-by: Marc Philipp <mail@marcphilipp.de>

---
+++ documentation/src/test/java/example/ExplicitExecutionModeDemo.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2015-2025 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+
+package example;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+
+@Execution(ExecutionMode.CONCURRENT)
+class ExplicitExecutionModeDemo {
+
+	@Test
+	void testA() {
+		// concurrent
+	}
+
+	@Test
+	@Execution(ExecutionMode.SAME_THREAD)
+	void testB() {
+		// overrides to same_thread
+	}
+}
