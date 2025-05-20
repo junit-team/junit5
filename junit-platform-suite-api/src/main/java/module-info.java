cb77a6d1e829e925b7d26fc579488965417dea0a
Marc Philipp <mail@marcphilipp.de>
2025-05-20 12:46:32 +0200

    Enable NullAway in junit-platform-suite-api

--- junit-platform-suite-api/src/main/java/module-info.java
+++ junit-platform-suite-api/src/main/java/module-info.java
@@ -8,11 +8,14 @@
  * https://www.eclipse.org/legal/epl-v20.html
  */
 
+import org.jspecify.annotations.NullMarked;
+
 /**
  * Annotations for configuring a test suite on the JUnit Platform.
  *
  * @since 1.0
  */
+@NullMarked
 module org.junit.platform.suite.api {
 	requires static transitive org.apiguardian.api;
 	requires transitive org.junit.platform.commons;
