a1f1dd3622904bef123f9172476ced01d6232b2d
Marc Philipp <mail@marcphilipp.de>
2025-06-06 15:57:04 +0200

    Disable NullAway on OpenJ9

--- gradle/plugins/common/src/main/kotlin/junitbuild.java-nullability-conventions.gradle.kts
+++ gradle/plugins/common/src/main/kotlin/junitbuild.java-nullability-conventions.gradle.kts
@@ -21,7 +21,11 @@ tasks.withType<JavaCompile>().configureEach {
 	options.errorprone {
 		disableAllChecks = true
 		nullaway {
-			enable()
+			if (java.toolchain.implementation.orNull == JvmImplementation.J9) {
+				disable()
+			} else {
+				enable()
+			}
 			isJSpecifyMode = true
 		}
 	}
