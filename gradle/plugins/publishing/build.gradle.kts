e9d7fbb5af4050ed52fb2992594ea0d14ea35dc6
Marc Philipp <mail@marcphilipp.de>
2025-06-18 12:43:04 +0200

    Resolve Dependabot alerts caused by JReleaser
    (cherry picked from commit c8d86c2c3f729e5b3145071518860621c7c0a314)

--- gradle/plugins/publishing/build.gradle.kts
+++ gradle/plugins/publishing/build.gradle.kts
@@ -7,19 +7,12 @@ plugins {
 dependencies {
 	implementation("junitbuild.base:dsl-extensions")
 	implementation(libs.plugins.jreleaser.markerCoordinates)
-}
-
-configurations.configureEach {
-	resolutionStrategy {
-		eachDependency {
-			// Workaround for CVE-2025-4949
-			if (requested.name == "org.eclipse.jgit") {
-				useVersion("6.10.1.202505221210-r")
-			}
-			// Workaround for CVE-2020-36843
-			if (requested.name == "sshj") {
-				useVersion("0.40.0")
-			}
+	constraints {
+		implementation("com.hierynomus:sshj:0.40.0") {
+			because("Workaround for CVE-2020-36843")
+		}
+		implementation("org.eclipse.jgit:org.eclipse.jgit:6.10.1.202505221210-r") {
+			because("Workaround for CVE-2025-4949")
 		}
 	}
 }
