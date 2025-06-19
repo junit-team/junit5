645e13b3cb6672f4133c84bb3509821df3ee26d2
Marc Philipp <mail@marcphilipp.de>
2025-06-19 08:34:11 +0200

    Change minimum required version declaration to avoid Renovate updates

--- gradle/plugins/publishing/build.gradle.kts
+++ gradle/plugins/publishing/build.gradle.kts
@@ -9,10 +9,16 @@ dependencies {
 	implementation("junitbuild.base:dsl-extensions")
 	implementation(libs.plugins.jreleaser.markerCoordinates)
 	constraints {
-		implementation("com.hierynomus:sshj:0.40.0") {
+		implementation("com.hierynomus:sshj") {
+			version {
+				require("0.40.0")
+			}
 			because("Workaround for CVE-2020-36843")
 		}
-		implementation("org.eclipse.jgit:org.eclipse.jgit:6.10.1.202505221210-r") {
+		implementation("org.eclipse.jgit:org.eclipse.jgit") {
+			version {
+				require("6.10.1.202505221210-r")
+			}
 			because("Workaround for CVE-2025-4949")
 		}
 	}
