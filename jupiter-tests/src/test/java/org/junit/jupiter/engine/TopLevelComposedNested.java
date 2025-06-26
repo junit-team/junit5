caf05bfd8dfa7f9367d681f348f8071fec05db02
Marc Philipp <mail@marcphilipp.de>
2025-06-26 09:38:16 +0000

    Avoid reporting discovery issues for composed `@Nested` annotations (#4684)
    Fixes #4681.

---
+++ jupiter-tests/src/test/java/org/junit/jupiter/engine/TopLevelComposedNested.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2015-2025 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+
+package org.junit.jupiter.engine;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.junit.jupiter.api.Nested;
+
+@Nested
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface TopLevelComposedNested {
+}
