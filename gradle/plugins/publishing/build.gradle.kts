cc9b91986f919dbdd34d9f62957a8f42a466c69d
Marc Philipp <mail@marcphilipp.de>
2025-07-01 08:35:58 +0200

    Remove obsolete sshj constraint

--- gradle/plugins/publishing/build.gradle.kts
+++ gradle/plugins/publishing/build.gradle.kts
@@ -9,12 +9,6 @@ dependencies {
 	implementation("junitbuild.base:dsl-extensions")
 	implementation(libs.plugins.jreleaser.markerCoordinates)
 	constraints {
-		implementation("com.hierynomus:sshj") {
-			version {
-				require("0.40.0")
-			}
-			because("Workaround for CVE-2020-36843")
-		}
 		implementation("org.eclipse.jgit:org.eclipse.jgit") {
 			version {
 				require("6.10.1.202505221210-r")
