e5c9a56ab90f0324cf44e427778bf02e40a787ff
Sam Brannen <104798+sbrannen@users.noreply.github.com>
2025-07-02 13:45:07 +0200

    Improve display names in tests
    See #4698
    See #4699
    
    (cherry picked from commit 6b7f17dc8c5ce8486ad33b6c91d591c62714df13)

--- jupiter-tests/src/test/java/org/junit/jupiter/api/condition/ConditionEvaluationResultTests.java
+++ jupiter-tests/src/test/java/org/junit/jupiter/api/condition/ConditionEvaluationResultTests.java
@@ -155,7 +155,7 @@ class ConditionEvaluationResultTests {
 	}
 
 	@Retention(RetentionPolicy.RUNTIME)
-	@ParameterizedTest
+	@ParameterizedTest(name = "[{index}] reason=\"{0}\"")
 	@NullSource
 	@ValueSource(strings = { "", " ", "   ", "\t", "\n" })
 	@interface EmptyReasonsTest {
