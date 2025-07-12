79e1137952318e4465c98412e6ee1abfd46cd026
Vincent Potucek <8830888+Pankraz76@users.noreply.github.com>
2025-07-12 12:11:45 +0200

    use `rewrite` fallback, drop oboslete `spotless` (#3)
    * rewirte
    
    * remove spotless
    
    * remove spotless
    
    * remove spotless
    
    * remove spotless
    
    * remove spotless
    
    * remove spotless
    
    * remove spotless
    
    ---------
    
    Co-authored-by: Vincent Potucek <vpotucek@me.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ group = "org.junit"
 val license by extra(License(
 	name = "Eclipse Public License v2.0",
 	url = uri("https://www.eclipse.org/legal/epl-v20.html"),
-	headerFile = layout.projectDirectory.file("gradle/config/spotless/eclipse-public-license-2.0.java")
+	headerFile = layout.projectDirectory.file("gradle/config/license/eclipse-public-license-2.0.java")
 ))
 
 val platformProjects by extra(listOf(
